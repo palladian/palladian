e77a3265423d087bd8b1c1f523b33f2c5f39c47f
Klemens Muthmann <klemens_muthmann@gmx.de>
2010-07-22 21:02:59 +0000

    Added support for pooling threads in feed reader
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@901 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: 129f5b5a0e9e6f6d53d50e12161adac146f6a640

--- src/main/java/tud/iir/news/DatasetCreator.java
+++ src/main/java/tud/iir/news/DatasetCreator.java
@@ -3,35 +3,31 @@
  */
 package tud.iir.news;
 
-import tud.iir.helper.FileHelper;
-
 /**
  * <p>
- * 
+ * Creates a dataset of feeds.
  * </p>
- *
+ * 
  * @author klemens.muthmann@googlemail.com
  * @version 1.0
  * @since 1.0
- *
+ * 
  */
 public class DatasetCreator {
 
-    
-    
     /**
      * <p>
-     * 
+     * Run creation of the feed dataset from all feeds in the database if possible.
      * </p>
-     *
+     * 
      * @param args
      */
     public static void main(String[] args) {
-        FileHelper.readFileToArray(DatasetCreator.class.getResource("/feeds.txt"));
-        
         FeedChecker feedChecker = FeedChecker.getInstance();
-        
+
+        feedChecker.setCheckApproach(CheckApproach.CHECK_FIXED, true);
         feedChecker.startContinuousReading();
+
     }
 
 }
