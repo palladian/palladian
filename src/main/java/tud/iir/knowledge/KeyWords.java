c19ba6a8b6a5ab2c9d254b0ccceb598e1207e65d
Martin Gregor <mail@m-gregor.de>
2010-07-22 14:41:32 +0000

    Keywords contains different synonyms for date. Depending on webpage part (HTTP, Head, Body, structure).
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@896 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: c9d6c2a5f7c72907a9a2bb0401222c8bc53e7763

---
+++ src/main/java/tud/iir/knowledge/KeyWords.java
@@ -0,0 +1,8 @@
+package tud.iir.knowledge;
+
+public final class KeyWords {
+
+    public static final String[] DATE_DOC_HEAD = { "date", "last-modified", "date-last-modified",
+            "date_first_released", "date_last_published", "DISPLAYDATE", "pdate" };
+    public static final String[] DATE_BODY_STRUC = { "date", "update", "dc:date" + "xsd:date" };
+}
