34f3ce8bf1a7048b4b72dd3f29e04ef01ceb17a7
Klemens Muthmann <klemens_muthmann@gmx.de>
2010-07-23 10:56:39 +0000

    Added test for feed checker
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/branches/www11paper@910 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: db0ffc264a9eaeac993ca7ca208b3e278f9f89e3

---
+++ src/test/java/tud/iir/news/FeedCheckerTest.java
@@ -0,0 +1,55 @@
+/**
+ * Created on: 23.07.2010 09:46:46
+ */
+package tud.iir.news;
+
+import static org.junit.Assert.fail;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import tud.iir.helper.DateHelper;
+
+/**
+ * <p>
+ * Tests whether the feed checker can correctly access feeds.
+ * </p>
+ *
+ * @author klemens.muthmann@googlemail.com
+ * @version 1.0
+ * @since 1.0
+ *
+ */
+public class FeedCheckerTest {
+    
+    private Collection<Feed> fixture;
+    
+    private FeedChecker objectOfClassUnderTest;
+
+    /**
+     * <p>
+     * 
+     * </p>
+     *
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        fixture = new HashSet<Feed>();
+        fixture.add(new Feed("http://www.tagesschau.de/xml/rss2"));
+        objectOfClassUnderTest = new FeedChecker(new CollectionFeedSource(fixture));
+    }
+
+    /**
+     * Test method for {@link tud.iir.news.FeedChecker#startContinuousReading()}.
+     */
+    @Test
+    public void testContinuousReading() {
+        objectOfClassUnderTest.startContinuousReading(10*DateHelper.MINUTE_MS);
+        objectOfClassUnderTest.stopContinuousReading();
+    }
+
+}
