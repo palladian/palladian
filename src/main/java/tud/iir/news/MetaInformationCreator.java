63a5a6c3ac7b6187e0a39f5897e08f449d00e711
Klemens Muthmann <klemens_muthmann@gmx.de>
2010-10-21 10:10:29 +0000

    changed MetaInformationCreator to write ETag to database
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/branches/www11paper@1115 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/main/java/tud/iir/news/MetaInformationCreator.java
+++ src/main/java/tud/iir/news/MetaInformationCreator.java
@@ -21,8 +21,6 @@ import org.apache.log4j.Logger;
 import tud.iir.helper.FileHelper;
 import tud.iir.persistence.DatabaseManager;
 
-import com.mysql.jdbc.Connection;
-
 /**
  * <p>
  * 
@@ -90,12 +88,13 @@ public class MetaInformationCreator {
                 Feed feed = feedStore.getFeedByID(Integer.valueOf(feedId));
                 feed.updateEntries(false);
 
-                Double averageFeedSize = calculateAverageSize(datasetFile, feed.getEntries().size());
-                String feedMetaInformation = getFeedMetaInformation(feed, averageFeedSize);
+//                Double averageFeedSize = calculateAverageSize(datasetFile, feed.getEntries().size());
+//                String feedMetaInformation = getFeedMetaInformation(feed, averageFeedSize);
+                Boolean supportsETag = getSupportsETag(feed);
                 Boolean supports304 = getFeedSupports304(feed);
 
                 FileHelper.prependFile(datasetFile.getAbsolutePath(), feedMetaInformation);
-                writeMetaInformationToDatabase(feed, supports304);
+                writeMetaInformationToDatabase(feed, supports304, supportsETag);
             } catch (IOException e) {
                 LOGGER.error("Could not add meta information to file: " + fileName, e);
             } catch (SQLException e) {
@@ -104,106 +103,128 @@ public class MetaInformationCreator {
         }
     }
 
+//    /**
+//     * <p>
+//     * 
+//     * </p>
+//     * 
+//     * @param datasetFile
+//     * @return
+//     * @throws IOException
+//     */
+//    @SuppressWarnings("unchecked")
+//    protected Double calculateAverageSize(File datasetFile, Integer entriesPerFeedAccess) throws IOException {
+//        Double ret = 0.0;
+//        List<String> datasetEntries = FileUtils.readLines(datasetFile);
+//        if(datasetEntries.size()==0) return Double.NaN;
+//        for (int i = 0; i < (datasetEntries.size() - entriesPerFeedAccess); i++) {
+//            Double windowSum = 0.0;
+//            for (int j = 0; j < entriesPerFeedAccess; j++) {
+//                Double entrySize = getEntrySize(datasetEntries.get(i + j));
+//                if (entrySize != null) {
+//                    windowSum += getEntrySize(datasetEntries.get(i + j));
+//                }
+//            }
+//            ret += windowSum;
+//        }
+//
+//        Double numberOfWindows = (new Integer(datasetEntries.size())).doubleValue() - entriesPerFeedAccess;
+//        ret = (ret + getHeaderSize(datasetEntries.get(0)) * numberOfWindows) / (numberOfWindows);
+//        return ret;
+//    }
+
+//    /**
+//     * <p>
+//     * 
+//     * </p>
+//     * 
+//     * @param string
+//     * @return
+//     */
+//    private Double getHeaderSize(String string) {
+//        String[] entryInformation = string.split(";");
+//        try {
+//            return Double.valueOf(entryInformation[4]);
+//        } catch (NumberFormatException e) {
+//            return null;
+//        }
+//    }
+
+//    /**
+//     * <p>
+//     * 
+//     * </p>
+//     * 
+//     * @param string
+//     * @return
+//     */
+//    private Double getEntrySize(String string) {
+//        String[] entryInformation = string.split(";");
+//        try {
+//            return Double.valueOf(entryInformation[3]);
+//        } catch (NumberFormatException e) {
+//            return null;
+//        }
+//    }
+
+//    /**
+//     * <p>
+//     * 
+//     * </p>
+//     * 
+//     */
+//    private String getFeedMetaInformation(Feed feed, Double averageSize) {
+//        // FEED_ID;FEED_URL;NUMBER_OF_ENTRIES(Window
+//        // Size);AVERAGE_SIZE;FEED_UPDATE_CLASS;SUPPORTS_LAST_MODIFIED_SINCE;SUPPORTS_ETAG;SUPPORTS_COMPRESSION;NUMBER_OF_CHECKS
+//        StringBuilder headerLine = new StringBuilder();
+//        headerLine.append(feed.getId() + ";");
+//        headerLine.append(feed.getFeedUrl() + ";");
+//        headerLine.append(feed.getEntries().size() + ";");
+//        headerLine.append(averageSize + ";");
+//        headerLine.append(feed.getUpdateClass() + ";");
+//        boolean lastModified = false;
+//        boolean etag = false;
+//        boolean compression = false;
+//        try {
+//            URL feedURL = new URL(feed.getFeedUrl());
+//            URLConnection connection = feedURL.openConnection();
+//            lastModified = connection.getHeaderField("Last-Modified") == null;
+//            etag = connection.getHeaderField("Etag") == null;
+//            compression = connection.getHeaderField("Content-Encoding") == null;
+//        } catch (MalformedURLException e) {
+//            LOGGER.error("URL of feed: " + feed.getFeedUrl() + " is malformed!");
+//        } catch (IOException e) {
+//            LOGGER.error("Could not get HTTP header information for feed at address: " + feed.getFeedUrl() + ".");
+//        }
+//        headerLine.append(lastModified + ";");
+//        headerLine.append(etag + ";");
+//        headerLine.append(compression + ";");
+//        headerLine.append(feed.getChecks());
+//        headerLine.append("\n");
+//
+//        return headerLine.toString();
+//    }
+
     /**
      * <p>
      * 
      * </p>
-     * 
-     * @param datasetFile
+     *
+     * @param feed
      * @return
-     * @throws IOException
      */
-    @SuppressWarnings("unchecked")
-    protected Double calculateAverageSize(File datasetFile, Integer entriesPerFeedAccess) throws IOException {
-        Double ret = 0.0;
-        List<String> datasetEntries = FileUtils.readLines(datasetFile);
-        if(datasetEntries.size()==0) return Double.NaN;
-        for (int i = 0; i < (datasetEntries.size() - entriesPerFeedAccess); i++) {
-            Double windowSum = 0.0;
-            for (int j = 0; j < entriesPerFeedAccess; j++) {
-                Double entrySize = getEntrySize(datasetEntries.get(i + j));
-                if (entrySize != null) {
-                    windowSum += getEntrySize(datasetEntries.get(i + j));
-                }
-            }
-            ret += windowSum;
-        }
-
-        Double numberOfWindows = (new Integer(datasetEntries.size())).doubleValue() - entriesPerFeedAccess;
-        ret = (ret + getHeaderSize(datasetEntries.get(0)) * numberOfWindows) / (numberOfWindows);
-        return ret;
-    }
-
-    /**
-     * <p>
-     * 
-     * </p>
-     * 
-     * @param string
-     * @return
-     */
-    private Double getHeaderSize(String string) {
-        String[] entryInformation = string.split(";");
-        try {
-            return Double.valueOf(entryInformation[4]);
-        } catch (NumberFormatException e) {
-            return null;
-        }
-    }
-
-    /**
-     * <p>
-     * 
-     * </p>
-     * 
-     * @param string
-     * @return
-     */
-    private Double getEntrySize(String string) {
-        String[] entryInformation = string.split(";");
-        try {
-            return Double.valueOf(entryInformation[3]);
-        } catch (NumberFormatException e) {
-            return null;
-        }
-    }
-
-    /**
-     * <p>
-     * 
-     * </p>
-     * 
-     */
-    private String getFeedMetaInformation(Feed feed, Double averageSize) {
-        // FEED_ID;FEED_URL;NUMBER_OF_ENTRIES(Window
-        // Size);AVERAGE_SIZE;FEED_UPDATE_CLASS;SUPPORTS_LAST_MODIFIED_SINCE;SUPPORTS_ETAG;SUPPORTS_COMPRESSION;NUMBER_OF_CHECKS
-        StringBuilder headerLine = new StringBuilder();
-        headerLine.append(feed.getId() + ";");
-        headerLine.append(feed.getFeedUrl() + ";");
-        headerLine.append(feed.getEntries().size() + ";");
-        headerLine.append(averageSize + ";");
-        headerLine.append(feed.getUpdateClass() + ";");
-        boolean lastModified = false;
-        boolean etag = false;
-        boolean compression = false;
-        try {
-            URL feedURL = new URL(feed.getFeedUrl());
-            URLConnection connection = feedURL.openConnection();
-            lastModified = connection.getHeaderField("Last-Modified") == null;
-            etag = connection.getHeaderField("Etag") == null;
-            compression = connection.getHeaderField("Content-Encoding") == null;
-        } catch (MalformedURLException e) {
-            LOGGER.error("URL of feed: " + feed.getFeedUrl() + " is malformed!");
-        } catch (IOException e) {
-            LOGGER.error("Could not get HTTP header information for feed at address: " + feed.getFeedUrl() + ".");
-        }
-        headerLine.append(lastModified + ";");
-        headerLine.append(etag + ";");
-        headerLine.append(compression + ";");
-        headerLine.append(feed.getChecks());
-        headerLine.append("\n");
-
-        return headerLine.toString();
+    private Boolean getSupportsETag(Feed feed) {
+        boolean ret = false;
+      try {
+          URL feedURL = new URL(feed.getFeedUrl());
+          URLConnection connection = feedURL.openConnection();
+          ret = connection.getHeaderField("Etag") == null;
+      } catch (MalformedURLException e) {
+          LOGGER.error("URL of feed: " + feed.getFeedUrl() + " is malformed!");
+      } catch (IOException e) {
+          LOGGER.error("Could not get HTTP header information for feed at address: " + feed.getFeedUrl() + ".");
+      }
+      return ret;
     }
 
     /**
@@ -232,14 +253,18 @@ public class MetaInformationCreator {
         return ret;
     }
     
-    private void writeMetaInformationToDatabase(final Feed feed, final Boolean supportsConditionalGet) throws SQLException {
+    private void writeMetaInformationToDatabase(final Feed feed, final Boolean supportsConditionalGet, final Boolean supportsETag) throws SQLException {
         DatabaseManager dbManager = DatabaseManager.getInstance();
         java.sql.Connection conn = dbManager.getConnection();
-        Statement stat = conn.createStatement();
+        Statement supports304Stat = conn.createStatement();
+        Statement supportsETagStat = conn.createStatement();
         Integer id = feed.getId();
         String query = "UPDATE feeds SET supportsConditionalGet="+supportsConditionalGet+" WHERE id="+id;
-        stat.execute(query);
-        stat.close();
+        String supportsETagQuery = "UPDATE feeds SET supportsETag="+supportsETag+" WHERE id="+id;
+        supports304Stat.execute(query);
+        supports304Stat.close();
+        supportsETagStat.execute(supportsETagQuery);
+        supportsETagStat.close();
         conn.close();
     }
 
