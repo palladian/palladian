4791f5007c6968c56ee81d9f2a93bf86f891bd4c
Philipp Katz <philipp@philippkatz.de>
2010-11-04 12:22:57 +0000

    added method to CountMap, added test case
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1183 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: 8e1ed68f01c816bf2663bd85da3c2baef890ae99

---
+++ src/test/java/tud/iir/helper/CountMapTest.java
@@ -0,0 +1,33 @@
+package tud.iir.helper;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class CountMapTest {
+
+    @Test
+    public void testCountMap() {
+
+        CountMap countMap = new CountMap();
+        countMap.increment("one");
+        countMap.increment("one");
+        countMap.increment("one");
+        countMap.increment("two");
+        countMap.increment("two");
+        countMap.increment("two");
+        countMap.increment("two");
+        countMap.increment("two");
+        countMap.increment("three");
+        countMap.increment("three");
+
+        Assert.assertEquals(3, (int) countMap.get("one"));
+        Assert.assertEquals(5, (int) countMap.get("two"));
+        Assert.assertEquals(2, (int) countMap.get("three"));
+
+        Assert.assertEquals(3, (int) countMap.size());
+        Assert.assertEquals(10, (int) countMap.totalSize());
+
+    }
+
+}
