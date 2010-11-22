543fdfc4c417dd529094687be284668391b5654e
Philipp Katz <philipp@philippkatz.de>
2010-11-22 18:24:44 +0000

    modified RegEx in Tokenizer, added test
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1249 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: b25df89e3df7b25ef653b171fe7395ecd9fb6dcb

--- src/test/java/tud/iir/helper/TokenizerTest.java
+++ src/test/java/tud/iir/helper/TokenizerTest.java
@@ -54,6 +54,9 @@ public class TokenizerTest extends TestCase {
         tokens = Tokenizer.tokenize("asp.net is very web 2.0. isn't it? web2.0, .net");
         // CollectionHelper.print(tokens);
         assertEquals(14, tokens.size());
+        
+        tokens = Tokenizer.tokenize("40,000 residents");
+        assertEquals(2, tokens.size());
     }
 
     @Test
