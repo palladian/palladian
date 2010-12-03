1a6a867eefd0fda46f191360af8ca5758ca52779
David Urbansky <davidurbansky@gmail.com>
2010-12-03 13:49:33 +0000

    imprint extraction test
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1271 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: 1f9c5ee90376df0baa1168cd54af2a2d7ef78bb7

--- src/main/java/tud/iir/helper/Temp.java
+++ src/main/java/tud/iir/helper/Temp.java
@@ -5,12 +5,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
 
+import tud.iir.extraction.PageAnalyzer;
 import tud.iir.news.Feed;
 import tud.iir.news.FeedDatabase;
 import tud.iir.news.evaluation.FeedReaderEvaluator;
+import tud.iir.web.Crawler;
 
 /**
  * Dump class to test various algorithms.
@@ -96,11 +101,106 @@ public class Temp {
 
     }
 
+    public static void imprintExtractor(String imprintURL) {
+
+        String companyName = ""; // 8/10 (regex)
+        String ceo = "";
+        String street = ""; // 7/10 (str, regex)
+        String zipAndCity = ""; // 10/10 (regex)
+        String telephone = ""; // 10/10 (regex)
+        String fax = ""; // 10/10 (regex)
+        String email = ""; //
+
+        Crawler crawler = new Crawler();
+        PageAnalyzer pa = new PageAnalyzer();
+
+        Document webPage = crawler.getWebDocument(imprintURL);
+        String rawMarkup = PageAnalyzer.getRawMarkup(webPage);
+        // String plainContent = Crawler.extractBodyContent(rawMarkup, true);
+        String plainContent = Crawler.extractBodyContent(webPage);
+        //plainContent = PageAnalyzer.getReadableTextDump(webPage);
+
+        // search for company name
+        String regex = "^.*\\s(gmbh|e.v.|ltd.|limited|co. kg|gbr)";
+        Matcher m = Pattern.compile(regex, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE).matcher(plainContent);
+
+        if (m.find()) {
+            companyName = m.group().replaceAll("Impressum", "").replaceAll("\\s{2,}", " ").trim();
+            // System.out.println(m.group());
+        }
+
+        // search for zip and City
+        regex = "[0-9]{5}\\s.*?$";
+        m = Pattern.compile(regex, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE).matcher(plainContent);
+
+        if (m.find()) {
+            zipAndCity = m.group().replaceAll("\\s{2,}", " ").trim();
+            // System.out.println(m.group());
+        }
+
+        // search for street
+        regex = "^.{2,20}(straße|str.|strasse)\\s[0-9]{1,4}";
+        m = Pattern.compile(regex, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE).matcher(plainContent);
+
+        if (m.find()) {
+            street = m.group().replaceAll("\\s{2,}", " ").trim();
+            // System.out.println(m.group());
+        }
+
+        // search for telephone
+        // regex = "(?<=(tel.:|telefon:|phone:|fon)\\s?)\\+?[0-9-/() ]{6,20}";
+        regex = "(?<=(tel.:|phone:|fon:|telefon:)\\s?).{0,2}[0-9-/()\\xa0 ]{7,20}";
+        m = Pattern.compile(regex, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE).matcher(plainContent);
+
+        if (m.find()) {
+            telephone = m.group().replaceAll("\\s{2,}", " ").trim();
+            // System.out.println(m.group());
+        }
+
+        // search for fax
+        regex = "(?<=(fax:|telefax:)\\s?)\\+?[0-9-/()\\xa0 ]{7,20}";
+        m = Pattern.compile(regex, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE).matcher(plainContent);
+
+        if (m.find()) {
+            fax = m.group().replaceAll("\\s{2,}", " ").trim();
+            // System.out.println(m.group());
+        }
+
+        // search for e-mail
+        regex = "(?<=(email:|e-mail:)\\s?)[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}";
+        m = Pattern.compile(regex, Pattern.CASE_INSENSITIVE | Pattern.MULTILINE).matcher(plainContent);
+
+        if (m.find()) {
+            email = m.group().replaceAll("\\s{2,}", " ").trim();
+            // System.out.println(m.group());
+        }
+
+        System.out.println("Company : " + companyName);
+        System.out.println("CEO     : " + ceo);
+        System.out.println("Street  : " + street);
+        System.out.println("Zip/City: " + zipAndCity);
+        System.out.println("Phone   : " + telephone);
+        System.out.println("Fax     : " + fax);
+        System.out.println("E-Mail  : " + email);
+
+    }
+
     /**
      * @param args
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
+
+        Temp.imprintExtractor("http://www.autozubehoer.de/Impressum:_:4.html?XTCsid=6i9fm908jrt6j62pefmqi4p200");
+        Temp.imprintExtractor("http://www.granzow.de/web/index.php/impressum.html");
+        Temp.imprintExtractor("http://domcam.de/impressum.html");
+        Temp.imprintExtractor("http://www.sos-kinderdorf.de/sos_kinderdorf/de/impressum,np=76590.html");
+        Temp.imprintExtractor("http://www.clickandprint.de/ServiceImpressum.php?UID=1290466142037850A85D51A5F2E12F836FDDB6688E4CEAF35EA4606");
+        Temp.imprintExtractor("http://www.ti-sa.com/shop/shop_content.php?coID=4&XTCsid=ae0fehu5vlrsit0c04rq38onq1");
+        Temp.imprintExtractor("http://www.schloesser-gbr.de/impressum.php");
+        Temp.imprintExtractor("http://www.goldhahnundsampson.de/shop/About-us-Imprint-Customer-Service:_:4.html?XTCsid=41d7e6492b00e13e54f5a205ba0070dc");
+        Temp.imprintExtractor("http://www.reifen-server.de/cgi-bin/kfzshop/reifenversand.pl?t=impressum&userid=78376");
+        Temp.imprintExtractor("http://www.ledick.de/oxid/Impressum/");
 
         // Temp.createTrainingData();
 
