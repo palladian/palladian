c593661ca3c53ced65d39d020e097b49dce6631a
Michéle Sprejz <michele.sprejz@googlemail.com>
2010-12-03 16:28:08 +0000

    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1276 5654db93-19e5-4567-beb8-64e8c25c7c45

---
+++ src/main/java/emotionanalyzing/WordEntryList.java
@@ -0,0 +1,14 @@
+package emotionanalyzing;
+
+import java.util.ArrayList;
+
+public class WordEntryList extends ArrayList{
+	
+	
+	//public ArrayList<WordEntry> wordEntryArray; 
+	
+	public WordEntryList(){
+		new ArrayList<WordEntry>();
+	}
+
+}
