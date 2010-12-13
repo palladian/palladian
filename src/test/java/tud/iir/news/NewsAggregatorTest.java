a6d9f0a737a51ea59eb1cb8a3187a72270b60dca
Philipp Katz <philipp@philippkatz.de>
2010-12-13 10:18:50 +0000

    fixing the build
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1298 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: 82fe003d2502d972522598004a7e236db8833f72

--- src/test/java/tud/iir/news/NewsAggregatorTest.java
+++ src/test/java/tud/iir/news/NewsAggregatorTest.java
@@ -4,6 +4,8 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import tud.iir.control.AllTests;
+
 public class NewsAggregatorTest {
 
     private static NewsAggregator aggregator;
@@ -11,20 +13,22 @@ public class NewsAggregatorTest {
     @BeforeClass
     public static void before() {
         aggregator = new NewsAggregator(new FeedStoreDummy());
+        aggregator.setDownloadPages(false);
     }
 
-    
     @Test
-    @Ignore
+    // @Ignore
     public void readFeedFromFile() throws NewsAggregatorException {
         aggregator.downloadFeed("data/test/feeds/feed1.xml");
         aggregator.downloadFeed("data/test/feeds/feed2.xml");
     }
-    
+
     @Test
     public void downloadFeed() throws NewsAggregatorException {
-        Feed feed = aggregator.downloadFeed("http://www.gizmodo.de/feed/atom");
-        System.out.println(feed);
+        if (AllTests.ALL_TESTS) {
+            Feed feed = aggregator.downloadFeed("http://www.gizmodo.de/feed/atom");
+            // System.out.println(feed);
+        }
     }
 
 }
