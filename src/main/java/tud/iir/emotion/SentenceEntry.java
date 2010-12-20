ce0676b67fbe1e7f49b8d8edb5bb82fa513640ba
Michéle Sprejz <michele.sprejz@googlemail.com>
2010-12-20 10:37:12 +0000

    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1342 5654db93-19e5-4567-beb8-64e8c25c7c45

---
+++ src/main/java/tud/iir/emotion/SentenceEntry.java
@@ -0,0 +1,33 @@
+package tud.iir.emotion;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class SentenceEntry {
+	
+	public String sentence;
+	
+	public String url;
+	
+	public ArrayList<WordEntry> wordsFound;
+	
+	public SentenceEntry(String sentence, String url){
+		this.sentence = sentence;
+		this.url = url;
+		wordsFound = new ArrayList<WordEntry>();
+	}
+	public String getSentence(){
+		return sentence;
+	}
+	public List<WordEntry> getWordsFound(){
+		return wordsFound;
+	}
+	public String getUrl(){
+		return url;
+	}
+	public void addWordEntry(WordEntry vergleich){
+		wordsFound.add(vergleich);
+	}
+	
+}
