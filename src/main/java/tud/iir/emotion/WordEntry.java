ec4bd6925d5cc14dd63562fa5a667d3400073ef7
Michéle Sprejz <michele.sprejz@googlemail.com>
2010-12-20 10:37:34 +0000

    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1343 5654db93-19e5-4567-beb8-64e8c25c7c45
    Former-commit-id: 26ba0ffa96f026209e9c439eac22408eb689a775

---
+++ src/main/java/tud/iir/emotion/WordEntry.java
@@ -0,0 +1,52 @@
+package tud.iir.emotion;
+
+import java.util.HashMap;
+
+public class WordEntry {
+	
+	public String word;
+	
+	public Integer counter;
+	
+	public String emotionCategory;
+		
+	public HashMap<String, String> sentenceUrlList;
+	
+	public WordEntry(String word, int counter, String emotionCategory) {
+		this.word = word;
+		this.counter = counter;
+		this.emotionCategory = emotionCategory;
+		sentenceUrlList = new HashMap<String, String>();
+	}
+	public String getWord(){
+		return word;
+	}
+	public Integer getCounter(){
+		return counter;
+	}
+	public String getEmotionCategory(){
+		
+		return emotionCategory;
+	}
+	
+	@Override
+	public String toString() {
+		return word + " #" + counter;
+	}
+	
+	public void increment() {
+		counter++;
+	}
+	
+	public void decrement() {
+		counter--;
+	}
+	public void saveSentenceUrl(String satz, String url){
+		sentenceUrlList.put(satz, url);
+	}
+	
+	public HashMap<String, String> getSentenceUrlList(){
+		return sentenceUrlList;
+	}
+		
+}
