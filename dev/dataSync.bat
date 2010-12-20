27789c36ef06e3567a2ed12bbfe0dd9977fb5f85
Philipp Katz <philipp@philippkatz.de>
2010-12-20 12:25:14 +0000

    rSync scripts
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1348 5654db93-19e5-4567-beb8-64e8c25c7c45

--- dev/dataSync.bat
+++ dev/dataSync.bat
@@ -1,13 +1,15 @@
 @ECHO OFF
 
-::: the local directory with the data, no training slash
-SET DESTINATION='/cygdrive/c/Programming/Palladian/data'
+::: the local directory with the data, no trailing slash
+SET DESTINATION=/cygdrive/c/Programming/Palladian/data
 
 ::: password for read access to the server
 ::: do not hard code the admin password here as this file is public
-SET READ_PASSWORD='rittersport'
+SET READ_PASSWORD=rittersport
+SET RSYNC_PASSWORD=
 
-IF EXIST "%DESTINATION%" GOTO begin
+::: TODO check for directory cygwin problem
+::: IF EXIST "%DESTINATION%" GOTO begin
 
 ECHO %DESTINATION% not found, set correct path in script
 GOTO end
@@ -17,17 +19,19 @@ GOTO end
 GOTO case_%1
 
 :case_up
-	rsync -avz "$DESTINATION/datasets" rsync://syncadmin@141.76.40.242/all_data_admin/datasets
-	rsync -avz "$DESTINATION/models" rsync://syncadmin@141.76.40.242/all_data_admin/models
+	rsync -avz --chmod=ugo=rwX %DESTINATION%/datasets/* rsync://syncadmin@141.76.40.242/all_data_admin/datasets
+	rsync -avz --chmod=ugo=rwX %DESTINATION%/models/* rsync://syncadmin@141.76.40.242/all_data_admin/models
 	GOTO end
 
 :case_down
-	SET RSYNC_PASSWORD="$READ_PASSWORD"
-	rsync -avz rsync://syncuser@141.76.40.242/all_data_user/datasets $DESTINATION
-	rsync -avz rsync://syncuser@141.76.40.242/all_data_user/models $DESTINATION
+	SET RSYNC_PASSWORD=%READ_PASSWORD%
+  rsync -rtvz rsync://syncuser@141.76.40.242/all_data_user/datasets %DESTINATION%
+	rsync -rtvz rsync://syncuser@141.76.40.242/all_data_user/models %DESTINATION%
 	GOTO end
 
 :case_
 	ECHO usage: datasync [up/down]
-
+ 
 :end
+
+SET RSYNC_PASSWORD=
