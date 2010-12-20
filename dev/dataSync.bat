224dbbb6b0347b6c69aafcc1924ada8b2b132ece
Philipp Katz <philipp@philippkatz.de>
2010-12-20 16:24:38 +0000

    modified dataSync.bat
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1352 5654db93-19e5-4567-beb8-64e8c25c7c45

--- dev/dataSync.bat
+++ dev/dataSync.bat
@@ -1,32 +1,34 @@
 @ECHO OFF
 
 ::: the local directory with the data, no trailing slash
-SET DESTINATION=/cygdrive/c/Programming/Palladian/data
+SET DESTINATION=C:\Programming\Palladian\data
 
 ::: password for read access to the server
 ::: do not hard code the admin password here as this file is public
 SET READ_PASSWORD=rittersport
 SET RSYNC_PASSWORD=
 
-::: TODO check for directory cygwin problem
-::: IF EXIST "%DESTINATION%" GOTO begin
-
+IF EXIST %DESTINATION%\nul GOTO begin
 ECHO %DESTINATION% not found, set correct path in script
 GOTO end
 
 :begin
 
+:: create Cygwin specific path; e.g. "C:\Path" is converted to "/cygdrive/c/Path"
+FOR /f "delims=" %%a IN ('cygpath.exe -u %DESTINATION%') DO SET CYGWIN_DESTINATION=%%a
+
 GOTO case_%1
 
 :case_up
-	rsync -avz --chmod=ugo=rwX %DESTINATION%/datasets/* rsync://syncadmin@141.76.40.242/all_data_admin/datasets
-	rsync -avz --chmod=ugo=rwX %DESTINATION%/models/* rsync://syncadmin@141.76.40.242/all_data_admin/models
+	::: SET /P RSYNC_PASSWORD=Enter admin password: 
+	rsync -avz --chmod=ugo=rwX %CYGWIN_DESTINATION%/datasets/* rsync://syncadmin@141.76.40.242/all_data_admin/datasets
+	rsync -avz --chmod=ugo=rwX %CYGWIN_DESTINATION%/models/* rsync://syncadmin@141.76.40.242/all_data_admin/models
 	GOTO end
 
 :case_down
 	SET RSYNC_PASSWORD=%READ_PASSWORD%
-  rsync -rtvz rsync://syncuser@141.76.40.242/all_data_user/datasets %DESTINATION%
-	rsync -rtvz rsync://syncuser@141.76.40.242/all_data_user/models %DESTINATION%
+	rsync -rtvz rsync://syncuser@141.76.40.242/all_data_user/datasets %CYGWIN_DESTINATION%
+	rsync -rtvz rsync://syncuser@141.76.40.242/all_data_user/models %CYGWIN_DESTINATION%
 	GOTO end
 
 :case_
