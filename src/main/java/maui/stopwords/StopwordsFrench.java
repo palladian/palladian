5e6b62c33907da708f2a5a5c69500f344c47623c
Philipp Katz <philipp@philippkatz.de>
2010-12-22 16:41:59 +0000

    fixed stopword handling for Maui
    git-svn-id: https://141.76.40.86/svn-students/iircommon/palladianExternalCode@1376 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/main/java/maui/stopwords/StopwordsFrench.java
+++ src/main/java/maui/stopwords/StopwordsFrench.java
@@ -1,10 +1,5 @@
 package maui.stopwords;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.HashSet;
 
 /**
  * Class that can test whether a given string is a stop word.
@@ -17,39 +12,39 @@ public class StopwordsFrench extends Stopwords {
 
 	private static final long serialVersionUID = 1L;
 	
-	/** The location of the stopwords file **/
-	private static String filePath;
-	
-	/** The hashtable containing the list of stopwords */
-	private static HashSet<String> m_Stopwords = null;
+//	/** The location of the stopwords file **/
+//	private static String filePath;
+//	
+//	/** The hashtable containing the list of stopwords */
+//	private static HashSet<String> m_Stopwords = null;
 	
 	public StopwordsFrench(String filePath) {
-		super(filePath);
-		if (m_Stopwords == null) {
-			m_Stopwords = new HashSet<String>();
-			
-			File txt = new File(filePath);	
-			InputStreamReader is;
-			String sw = null;
-			try {
-				is = new InputStreamReader(new FileInputStream(txt), "UTF-8");
-				BufferedReader br = new BufferedReader(is);				
-				while ((sw=br.readLine()) != null)  {
-					m_Stopwords.add(sw);   
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+		super("stopwords_fr.txt");
+//		if (m_Stopwords == null) {
+//			m_Stopwords = new HashSet<String>();
+//			
+//			File txt = new File(filePath);	
+//			InputStreamReader is;
+//			String sw = null;
+//			try {
+//				is = new InputStreamReader(new FileInputStream(txt), "UTF-8");
+//				BufferedReader br = new BufferedReader(is);				
+//				while ((sw=br.readLine()) != null)  {
+//					m_Stopwords.add(sw);   
+//				}
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
 	}
 
 	
-	/** 
-	 * Returns true if the given string is a stop word.
-	 */
-	public boolean isStopword(String str) {
-		return m_Stopwords.contains(str.toLowerCase());
-	}
+//	/** 
+//	 * Returns true if the given string is a stop word.
+//	 */
+//	public boolean isStopword(String str) {
+//		return m_Stopwords.contains(str.toLowerCase());
+//	}
 }
 		
 		
