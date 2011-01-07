c36debdd4b4fd0f09ae8dd499bd731dbc809d7db
Martin Wunderwald <martin.wunderwald@gmail.com>
2011-01-07 16:23:15 +0000

    bugfix for empty what slot
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1464 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/main/java/tud/iir/extraction/event/EventFeatureExtractor.java
+++ src/main/java/tud/iir/extraction/event/EventFeatureExtractor.java
@@ -41,7 +41,7 @@ public class EventFeatureExtractor {
 
     /** the logger for this class */
     private static final Logger LOGGER = Logger
-    .getLogger(EventFeatureExtractor.class);
+            .getLogger(EventFeatureExtractor.class);
 
     /** Instance of this EventFeatureExtractor. **/
     private static EventFeatureExtractor instance = null;
@@ -176,7 +176,7 @@ public class EventFeatureExtractor {
             phrase = annotation.getEntity().getName();
 
             mention = mfactory
-            .create(phrase, annotation.getMostLikelyTagName());
+                    .create(phrase, annotation.getMostLikelyTagName());
             mentionId = coref.resolveMention(mention, 1);
 
             tmpAnnotations = new Annotations();
@@ -251,7 +251,7 @@ public class EventFeatureExtractor {
         }
 
         final double distribution = avgOffset / annotations.size()
-        / event.getText().length();
+                / event.getText().length();
 
         final DecimalFormat twoDForm = new DecimalFormat("#.###");
 
@@ -296,7 +296,7 @@ public class EventFeatureExtractor {
     public Annotations getDateAnnotations(String text) {
         OpenNLPNER timeNer = new OpenNLPNER();
         timeNer
-        .loadModel("data/models/opennlp/namefind/en-ner-time.bin,data/models/opennlp/namefind/en-ner-date.bin");
+                .loadModel("data/models/opennlp/namefind/en-ner-time.bin,data/models/opennlp/namefind/en-ner-date.bin");
 
         return timeNer.getAnnotations(text);
 
@@ -481,7 +481,7 @@ public class EventFeatureExtractor {
                 if (event != null && event.getText() != null) {
 
                     Map<Annotations, FeatureObject> featureMap = event
-                    .getAnnotationFeatures();
+                            .getAnnotationFeatures();
                     // hm.put(url, e);
 
                     if (event.getAnnotationFeatures() == null) {
@@ -511,9 +511,9 @@ public class EventFeatureExtractor {
                                     if (annotation.getEntity().getName()
                                             .toLowerCase().contains(
                                                     positive.toLowerCase())
-                                                    || positive.contains(annotation
-                                                            .getEntity().getName()
-                                                            .toLowerCase())) {
+                                            || positive.contains(annotation
+                                                    .getEntity().getName()
+                                                    .toLowerCase())) {
                                         contains = true;
                                     }
 
@@ -593,11 +593,33 @@ public class EventFeatureExtractor {
          * featureExtractor.getSentences(event1.getText())) { ((OpenNLPParser)
          * parser).link(((OpenNLPParser) parser) .getFullParse(str)); }
          */
-        // ner = new OpenNLPNER();
-        // ner.loadModel("data/models/opennlp/namefind/en-ner-person.bin,data/models/opennlp/namefind/en-ner-location.bin,data/models/opennlp/namefind/en-ner-organization.bin");
+        // tud.iir.extraction.entity.ner.tagger.StanfordNER oner = new
+        // tud.iir.extraction.entity.ner.tagger.StanfordNER();
+        // oner
+        // .loadModel("data/models/stanfordner/data/ner-eng-ie.crf-3-all2008.ser.gz");
+
+        // tud.iir.extraction.entity.ner.tagger.OpenNLPNER oner = new
+        // tud.iir.extraction.entity.ner.tagger.OpenNLPNER();
+        // oner.loadModel("data/models/opennlp/namefind/en-ner-person.bin,data/models/opennlp/namefind/en-ner-location.bin,data/models/opennlp/namefind/en-ner-organization.bin");
+
+        // tud.iir.extraction.entity.ner.tagger.LingPipeNER oner = new
+        // tud.iir.extraction.entity.ner.tagger.LingPipeNER();
+        // oner.loadModel("data/models/lingpipe/ne-en-news-muc6.AbstractCharLmRescoringChunker");
+
+        // tud.iir.extraction.entity.ner.tagger.TUDNER oner = new
+        // tud.iir.extraction.entity.ner.tagger.TUDNER();
+        // oner.loadModel("data/models/tudner/tudner.model");
+
+        tud.iir.extraction.entity.ner.tagger.OpenCalaisNER oner = new tud.iir.extraction.entity.ner.tagger.OpenCalaisNER();
+
+        String text = "5 January 2011 Last updated at 09:59 GMT Pakistan is on high alert as the funeral of assassinated Punjab governor Salman Taseer takes place in Lahore. Mr Taseer was shot dead by one of his own bodyguards who was angered by his opposition to blasphemy laws. Thousands gathered in Lahore for the burial of Mr Taseer, one of Pakistan's most outspoken liberal politicians. US Secretary of State Hillary Clinton described his death as \"a great loss\", saying he had promoted tolerance. The governor - a senior member of the governing Pakistan People's Party (PPP) - had recently angered Islamists by appealing for a Christian woman, sentenced to death for blasphemy, to be pardoned. Acting alone? Prime Minister Yousuf Raza Gilani declared three days of national mourning and appealed for calm. However, some religious leaders have praised the governor's killer and called for a boycott of the ceremonies in Lahore, says the BBC's Orla Guerin in Islamabad. Continue reading the main story Analysis M Ilyas Khan BBC News, Islamabad The assassination of Governor Salman Taseer appears to have raised the level of threat against liberal voices in Pakistan. While many religious leaders have publicly justified the murder, the liberal sections of the society have been more cautious in condemning it. This is due to the rising tendency in the society to silence voices of religious dissent by force, a tendency promoted by militant groups and condoned by religious forces active in the political sphere. Even within the clerical community, many liberal voices have been silenced. Some have been blown up in suicide attacks, others have migrated. In a country where religious politicians have never won an election, this policy of intimidation has expanded their influence. They often distance themselves from acts of militancy but never fail to justify them. For example, they often condemn suicide attacks by militants on civilian targets, but in the same breath qualify the act as caused by 'anger over excesses being committed against Muslims by Western powers'. Following Mr Taseer's assassination, they mostly did the same: condemning the act but justifying the killer who 'acted in defence of the dignity of the Prophet'. As is evident from Mr Taseer's assassination, any counter-argument can invoke a decree of death. Five hundred scholars from the moderate Barelvi sect of Sunni Muslims have warned that anyone who expresses grief over the assassination could suffer the same fate. \"No Muslim should attend the funeral or even try to pray for Salman Taseer or even express any kind of regret or sympathy over the incident,\" said the Jamaat-e-Ahl-e-Sunnat Pakistan in a statement. It said anyone who expressed sympathy over the death of a blasphemer was also committing blasphemy. One of Mr Taseer's bodyguards, Malik Mumtaz Hussein Qadri, was detained immediately after the shooting at Kohsar Market in Islamabad. He confessed to the murder, said Pakistan's interior minister, Rehman Malik. Police are now questioning the rest of Mr Taseer's security detail and are also carrying out an inquiry into the governor's security arrangements. \"We will investigate whether it was an individual act or there is some organisation behind it,\" Mr Malik told a news conference. The BBC's Ilyas Khan says the most obvious questions being asked at the moment are whether the killer acted alone, and why did other members of Mr Taseer's security team not try to prevent the assassination. There are few credible explanations so far as to why the guard was able to empty two magazines of his sub-machine gun on the governor without being shot by his colleagues, our correspondent says. 'A great loss' The assassination was condemned by world leaders. \"I had the opportunity to meet Governor Taseer in Pakistan and I admired his work to promote tolerance and the education of Pakistan's future generations,\" said Mrs Clinton in a statement. \"His death is a great loss.\" UN Secretary General Ban Ki-moon and UK Foreign Secretary William Hague also spoke out against the killing. Pakistan's high commissioner to London, Wajid Shamshul Hassan, told the BBC's Newshour programme that the assassination exposed the divisions in his country. \"It has shown that you can be held hostage by a minority of [radical] religious people and they can do whatever they want. That is not the way we are going to allow in the country,\" he said. \"We will be tough on them. Unless we get rid of such people in our society, unless we purge them from the various security agencies, you can't feel that justice will be done.\" Mr Taseer - a close associate of President Asif Ali Zardari - made headlines by appealing for the pardon of Christian woman Asia Bibi who had been sentenced to death for allegedly insulting the Prophet Muhammad. His death is the most high-profile assassination in Pakistan since former prime minister Benazir Bhutto was killed in December 2007. The BBC's Aleem Maqbool in Islamabad says Mr Taseer was one of Pakistan's most important political figures and his death will add further instability to the country. The government led by Mr Taseer's Pakistan People's Party is under threat after one of its coalition partners walked out at the weekend. The International Monetary Fund (IMF) is withholding the latest tranche of its $11.3bn loan to Islamabad, while petrol prices have increased sharply and chronic fuel shortages are causing unrest. Pakistan is also under pressure from the US to move against militants in the tribal areas bordering Afghanistan.";
+
+        LOGGER.info(text.length());
 
         final StopWatch sw = new StopWatch();
         sw.start();
+
+        Annotations annos = oner.getAnnotations(text);
 
         // PhraseChunker pc = new PhraseChunker(PhraseChunker.CHUNKER_OPENNLP);
         // pc.chunk(event.getTitle());
