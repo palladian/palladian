98785e4accf7fc3d7ea98185146b8a863f895142
Philipp Katz <philipp@philippkatz.de>
2011-01-24 18:19:14 +0000

    added unreachable counter, finally removed NewsAggregator
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1553 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/main/java/tud/iir/news/Feed.java
+++ src/main/java/tud/iir/news/Feed.java
@@ -299,6 +299,10 @@ public class Feed {
     public void setUnreachableCount(int unreachableCount) {
         this.unreachableCount = unreachableCount;
     }
+    
+    public void incrementUnreachableCount() {
+        unreachableCount++;
+    }
 
     public int getUnreachableCount() {
         return unreachableCount;
