dc2c94907fcd70838e02b997c8df47e7a7b50461
Philipp Katz <philipp@philippkatz.de>
2011-02-19 22:25:43 +0000

    stub for database testing
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1769 5654db93-19e5-4567-beb8-64e8c25c7c45

---
+++ src/test/java/ws/palladian/persistence/DatabaseManagerTest.java
@@ -0,0 +1,26 @@
+package ws.palladian.persistence;
+
+import junit.framework.Assert;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DatabaseManagerTest {
+
+    private static DatabaseManager databaseManager;
+
+    @BeforeClass
+    public static void beforeClass() {
+        databaseManager = new DatabaseManager();
+        databaseManager
+                .runUpdate("CREATE TABLE test (id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY (id));");
+    }
+
+    @Test
+    public void testInsert() {
+        String sql = "INSERT INTO test (name) VALUES (?)";
+        Assert.assertEquals(1, databaseManager.runInsertReturnId(sql, "bob"));
+        Assert.assertEquals(2, databaseManager.runInsertReturnId(sql, "mary"));
+    }
+
+}
