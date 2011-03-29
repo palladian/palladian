d73032763174df57337fa513dbefb5b06854f5a1
David Urbansky <davidurbansky@gmail.com>
2011-03-29 20:14:55 +0000

    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1896 5654db93-19e5-4567-beb8-64e8c25c7c45
    Former-commit-id: ad9d47c1ef6eff71e278309d2c3ef13a27419d5d

---
+++ src/test/java/ws/palladian/retrieval/CrawlerTest.java
@@ -0,0 +1,170 @@
+package ws.palladian.retrieval;
+
+import junit.framework.TestCase;
+import ws.palladian.control.AllTests;
+import ws.palladian.extraction.PageAnalyzer;
+import ws.palladian.helper.UrlHelper;
+import ws.palladian.helper.html.XPathHelper;
+import ws.palladian.retrieval.DocumentRetriever;
+import ws.palladian.retrieval.TBODYFix;
+
+/**
+ * Test cases for the crawler.
+ * 
+ * @author David Urbansky
+ * @author Philipp Katz
+ * @author Klemens Muthmann
+ */
+public class CrawlerTest extends TestCase {
+
+    public CrawlerTest(String name) {
+        super(name);
+    }
+
+    public void testGetCleanURL() {
+        assertEquals("amazon.com/", UrlHelper.getCleanURL("http://www.amazon.com/"));
+        assertEquals("amazon.com/", UrlHelper.getCleanURL("http://amazon.com/"));
+        assertEquals("amazon.com/", UrlHelper.getCleanURL("https://www.amazon.com/"));
+        assertEquals("amazon.com", UrlHelper.getCleanURL("https://amazon.com"));
+        assertEquals("amazon.com/", UrlHelper.getCleanURL("www.amazon.com/"));
+        assertEquals("amazon.com/", UrlHelper.getCleanURL("amazon.com/"));
+    }
+
+    public void testGetDomain() {
+        // System.out.println(crawler.getDomain("http://www.flashdevices.net/2008/02/updated-flash-enabled-devices.html",
+        // false));
+        assertEquals("http://www.flashdevices.net",
+                UrlHelper.getDomain("http://www.flashdevices.net/2008/02/updated-flash-enabled-devices.html", true));
+        assertEquals("www.flashdevices.net",
+                UrlHelper.getDomain("http://www.flashdevices.net/2008/02/updated-flash-enabled-devices.html", false));
+
+        assertEquals("http://blog.wired.com",
+                UrlHelper.getDomain("http://blog.wired.com/underwire/2008/10/theres-yet-anot.html", true));
+        assertEquals("blog.wired.com",
+                UrlHelper.getDomain("http://blog.wired.com/underwire/2008/10/theres-yet-anot.html", false));
+
+        // added by Philipp
+        assertEquals("https://example.com", UrlHelper.getDomain("https://example.com/index.html"));
+        assertEquals("", UrlHelper.getDomain(""));
+        assertEquals("", UrlHelper.getDomain(null));
+        assertEquals("", UrlHelper.getDomain("file:///test.html")); // TODO return localhost here?
+        assertEquals("localhost", UrlHelper.getDomain("file://localhost/test.html", false));
+    }
+
+    public void testGetSiblingPage() {
+        if (AllTests.ALL_TESTS) {
+            DocumentRetriever crawler = new DocumentRetriever();
+            assertEquals("http://www.cineplex.com/Movies/AllMovies.aspx?sort=2",
+                    crawler.getSiblingPage("http://www.cineplex.com/Movies/AllMovies.aspx"));
+            assertEquals("http://www.flashdevices.net/2008/02/",
+                    crawler.getSiblingPage("http://www.flashdevices.net/2008/02/updated-flash-enabled-devices.html"));
+            assertEquals("http://blog.wired.com/underwire/2008/10/star-trek-trail.html",
+                    crawler.getSiblingPage("http://blog.wired.com/underwire/2008/10/theres-yet-anot.html"));
+            assertEquals("http://asia.cnet.com/reviews/notebooks/0,39050495,39315110-2,00.htm",
+                    crawler.getSiblingPage("http://asia.cnet.com/reviews/notebooks/0,39050495,39315110,00.htm"));
+            assertEquals("http://cars.about.com/od/helpforcarbuyers/tp/ag_top_fuelsave.htm",
+                    crawler.getSiblingPage("http://cars.about.com/od/helpforcarbuyers/tp/top10_fuel.htm"));
+            assertEquals("http://www.blu-ray.com/movies/movies.php?genre=action&page=1",
+                    crawler.getSiblingPage("http://www.blu-ray.com/movies/movies.php?genre=action"));
+            assertEquals("http://forums.whirlpool.net.au/forum-replies.cfm?t=1037458",
+                    crawler.getSiblingPage("http://forums.whirlpool.net.au/forum-replies-archive.cfm/1037458.html"));
+        }
+    }
+
+    public void testLinkHandling() {
+        DocumentRetriever documentRetriever = new DocumentRetriever();
+        documentRetriever.setDocument(CrawlerTest.class.getResource("/pageContentExtractor/test9.html").getFile());
+        assertEquals("http://www.example.com/test.html", PageAnalyzer.getLinks(documentRetriever.getDocument(), true, true)
+                .iterator().next());
+
+        documentRetriever.setDocument(CrawlerTest.class.getResource("/pageContentExtractor/test10.html").getFile());
+        assertEquals("http://www.example.com/test.html", PageAnalyzer.getLinks(documentRetriever.getDocument(), true, true)
+                .iterator().next());
+    }
+
+    public void testMakeFullURL() {
+
+        assertEquals("http://www.xyz.de/page.html", UrlHelper.makeFullURL("http://www.xyz.de", "", "page.html"));
+        assertEquals("http://www.xyz.de/page.html", UrlHelper.makeFullURL("http://www.xyz.de", null, "page.html"));
+        assertEquals("http://www.xyz.de/page.html",
+                UrlHelper.makeFullURL("http://www.xyz.de/index.html", "", "page.html"));
+        assertEquals("http://www.xyz.de/page.html",
+                UrlHelper.makeFullURL("http://www.xyz.de/index.html", "/directory", "/page.html"));
+        assertEquals("http://www.xyz.de/directory/page.html",
+                UrlHelper.makeFullURL("http://www.xyz.de/index.html", "/directory", "./page.html"));
+        assertEquals("http://www.xyz.de/directory/page.html",
+                UrlHelper.makeFullURL("http://www.xyz.de/index.html", "/directory/directory", "../page.html"));
+
+        assertEquals("http://www.abc.de/page.html",
+                UrlHelper.makeFullURL("http://www.xyz.de", "", "http://www.abc.de/page.html"));
+        assertEquals("http://www.abc.de/page.html",
+                UrlHelper.makeFullURL("http://www.xyz.de", "http://www.abc.de/", "/page.html"));
+
+        assertEquals("http://www.example.com/page.html",
+                UrlHelper.makeFullURL("/some/file/path.html", "http://www.example.com/page.html"));
+        assertEquals("", UrlHelper.makeFullURL("http://www.xyz.de", "mailto:example@example.com"));
+
+        assertEquals("http://www.example.com/page.html",
+                UrlHelper.makeFullURL(null, null, "http://www.example.com/page.html"));
+
+        // when no linkUrl is supplied, we cannot determine the full URL, so just return an empty String.
+        assertEquals("", UrlHelper.makeFullURL(null, "http://www.example.com", null));
+        assertEquals("", UrlHelper.makeFullURL("http://www.example.com", null, null));
+        assertEquals("", UrlHelper.makeFullURL(null, null, "/page.html"));
+
+    }
+
+    public void testNekoBugs() {
+
+        // produces a StackOverflowError -- see
+        // http://sourceforge.net/tracker/?func=detail&aid=3109537&group_id=195122&atid=952178
+        // Crawler crawler = new Crawler();
+        // Document doc =
+        // crawler.getWebDocument(CrawlerTest.class.getResource("/webPages/NekoTestcase3109537.html").getFile());
+        // assertNotNull(doc);
+
+    }
+
+    /**
+     * Test undesired behavior from NekoHTML for which we introduced workarounds/fixes.
+     * See {@link TBODYFix}.
+     */
+    public void testNekoWorkarounds() {
+
+        DocumentRetriever crawler = new DocumentRetriever();
+        assertEquals(
+                3,
+                XPathHelper.getXhtmlNodes(
+                        crawler.getWebDocument(CrawlerTest.class.getResource("/webPages/NekoTableTestcase1.html")
+                                .getFile()), "//TABLE/TR[1]/TD").size());
+        assertEquals(
+                3,
+                XPathHelper.getXhtmlNodes(
+                        crawler.getWebDocument(CrawlerTest.class.getResource("/webPages/NekoTableTestcase2.html")
+                                .getFile()), "//TABLE/TBODY/TR[1]/TD").size());
+        assertEquals(
+                3,
+                XPathHelper.getXhtmlNodes(
+                        crawler.getWebDocument(CrawlerTest.class.getResource("/webPages/NekoTableTestcase3.html")
+                                .getFile()), "//TABLE/TBODY/TR[1]/TD").size());
+        assertEquals(
+                3,
+                XPathHelper.getXhtmlNodes(
+                        crawler.getWebDocument(CrawlerTest.class.getResource("/webPages/NekoTableTestcase4.html")
+                                .getFile()), "//TABLE/TR[1]/TD").size());
+
+    }
+
+    public void testParseXml() {
+
+        DocumentRetriever crawler = new DocumentRetriever();
+
+        // parse errors will yield in a null return
+        assertNotNull(crawler
+                .getXMLDocument(CrawlerTest.class.getResource("/xmlDocuments/invalid-chars.xml").getFile()));
+        assertNotNull(crawler.getXMLDocument(CrawlerTest.class.getResource("/feeds/sourceforge02.xml").getFile()));
+        assertNotNull(crawler.getXMLDocument(CrawlerTest.class.getResource("/feeds/feed061.xml").getFile()));
+
+    }
+
+}
---
+++ src/test/java/ws/palladian/retrieval/feeds/FeedContentClassifierTest.java
@@ -0,0 +1,65 @@
+package ws.palladian.retrieval.feeds;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ws.palladian.control.AllTests;
+import ws.palladian.retrieval.feeds.FeedContentClassifier;
+import ws.palladian.retrieval.feeds.FeedContentClassifier.FeedContentType;
+
+public class FeedContentClassifierTest {
+
+    FeedContentClassifier classifier = new FeedContentClassifier();
+
+    @Test
+    public void testFeedContentClassifier() {
+
+        if (AllTests.ALL_TESTS) {
+
+            // Daring Fireball
+            // TODO links to different sites
+            assertEquals(FeedContentType.PARTIAL, classifier.determineContentType(FeedContentClassifierTest.class.getResource("/feeds/feed9.xml").getFile()));
+
+            // Ars Technica / Infinite Loop
+            // TODO mixed, some entries contain partial, some full content
+            assertEquals(FeedContentType.PARTIAL, classifier.determineContentType(FeedContentClassifierTest.class.getResource("/feeds/feed10.xml").getFile()));
+
+            // esse est percipi
+            assertEquals(FeedContentType.PARTIAL, classifier.determineContentType(FeedContentClassifierTest.class.getResource("/feeds/feed11.xml").getFile()));
+
+            // Panic Blog
+            assertEquals(FeedContentType.FULL, classifier.determineContentType(FeedContentClassifierTest.class.getResource("/feeds/feed12.xml").getFile()));
+
+            // Engadget
+            // TODO they have additional press releases on some pages, which are not in the feed
+            // assertEquals(FeedContentType.FULL, classifier.determineFeedTextType("data/test/feeds/feed13.xml"));
+
+            // Wired / Gadget Lab
+            // assertEquals(FeedContentType.FULL, classifier.determineFeedTextType("data/test/feeds/feed14.xml"));
+
+            // Ars Technica / Gears & Gadgets
+            // TODO mixed, some entries contain partial, sume full content
+            assertEquals(FeedContentType.PARTIAL, classifier.determineContentType(FeedContentClassifierTest.class.getResource("/feeds/feed15.xml").getFile()));
+
+            // SlashGear
+            // TODO they acutally have MORE content in the feed entries, as they contain a "read more" list
+            // assertEquals(FeedContentType.FULL, classifier.determineFeedTextType("data/test/feeds/feed16.xml"));
+
+            // heise online
+            assertEquals(FeedContentType.NONE, classifier.determineContentType(FeedContentClassifierTest.class.getResource("/feeds/feed17.xml").getFile()));
+
+            // Wired Top Stories
+            assertEquals(FeedContentType.PARTIAL, classifier.determineContentType(FeedContentClassifierTest.class.getResource("/feeds/feed18.xml").getFile()));
+
+            // FAZ.NET
+            assertEquals(FeedContentType.PARTIAL, classifier.determineContentType(FeedContentClassifierTest.class.getResource("/feeds/feed19.xml").getFile()));
+
+            // Spreeblick
+            assertEquals(FeedContentType.FULL, classifier.determineContentType(FeedContentClassifierTest.class.getResource("/feeds/feed20.xml").getFile()));
+
+        }
+
+    }
+
+}
