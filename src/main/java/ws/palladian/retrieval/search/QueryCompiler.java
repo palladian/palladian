db46dcaf6a603aa52632a2a2187dbf7d443dc5b0
Philipp Katz <philipp@philippkatz.de>
2011-04-01 21:54:52 +0000

    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1938 5654db93-19e5-4567-beb8-64e8c25c7c45
    Former-commit-id: b3a50ea2adc6f8dc6546a0146ad2db4876e66faf

--- src/main/java/ws/palladian/retrieval/search/QueryCompiler.java
+++ src/main/java/ws/palladian/retrieval/search/QueryCompiler.java
@@ -1,28 +1,20 @@
 package ws.palladian.retrieval.search;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.commons.collections15.Bag;
 import org.apache.commons.collections15.bag.HashBag;
 
 import ws.palladian.helper.FileHelper;
 import ws.palladian.helper.LineAction;
-import edu.umass.cs.mallet.base.util.Random;
 
 public class QueryCompiler {
 
     public static void main(String[] args) {
-
-        String dmozCatergoriesFile = "/home/pk/Desktop/categories.txt";
-
+        String dmozCatergoriesFile = "/Users/pk/Desktop/categories.txt";
         List<String> queries = readQueriesFromDmoz(dmozCatergoriesFile);
-        List<String> combinedQueries = combineQueries(queries, 100000);
-        FileHelper.writeToFile("/home/pk/Desktop/finalQueries.txt", combinedQueries);
-
+        FileHelper.writeToFile("/Users/pk/Desktop/finalQueries.txt", queries);
     }
 
     /**
@@ -80,41 +72,5 @@ public class QueryCompiler {
 
     }
 
-    public static List<String> combineQueries(List<String> queries, int targetCount) {
-
-        int availableQueries = queries.size();
-
-        Set<String> combinedQueries = new HashSet<String>();
-        Random random = new Random();
-
-        if (availableQueries > targetCount) {
-
-            // we have more queries than we want, create a random subset of specified size
-            Collections.shuffle(queries);
-            combinedQueries.addAll(queries.subList(0, targetCount));
-
-        } else {
-
-            // add all supplied queries to result
-            combinedQueries.addAll(queries);
-
-            // create combined queries
-            // for (int i = 0; i < targetCount - availableQueries; i++) {
-            while (combinedQueries.size() < targetCount) {
-
-                // get a query term randomly by combining two single queries
-                String queryTerm1 = queries.get(random.nextInt(queries.size()));
-                String queryTerm2 = queries.get(random.nextInt(queries.size()));
-                combinedQueries.add(queryTerm1 + " " + queryTerm2);
-            }
-            
-        }
-
-        // return random shuffled result
-        ArrayList<String> result = new ArrayList<String>(combinedQueries);
-        Collections.shuffle(result);
-        return result;
-
-    }
 
 }
