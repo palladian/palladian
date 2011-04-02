5b771c2fe00d06ed970d78d37292fe50f6f7dfd4
Philipp Katz <philipp@philippkatz.de>
2011-04-02 11:41:29 +0000

    fixed Alchemy and OpenCalais keyphrase extraction
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1945 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: 40a6a941daa598dbabbbb11b0e2e05327c83c9bb

--- src/main/java/ws/palladian/extraction/keyphrase/OpenCalaisSocialTagger.java
+++ src/main/java/ws/palladian/extraction/keyphrase/OpenCalaisSocialTagger.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
@@ -12,6 +11,8 @@ import org.apache.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import ws.palladian.helper.ConfigHolder;
+import ws.palladian.helper.collection.CollectionHelper;
 import ws.palladian.retrieval.HTTPPoster;
 
 /**
@@ -36,19 +37,13 @@ public class OpenCalaisSocialTagger extends KeyphraseExtractor {
     private String apiKey = "";
 
     public OpenCalaisSocialTagger() {
-
-        try {
-            PropertiesConfiguration config = new PropertiesConfiguration("config/apikeys.conf");
-            apiKey = config.getString("opencalais.api.key");
-        } catch (ConfigurationException e) {
-            LOGGER.error("could not get api key from config/apikeys.conf, " + e.getMessage());
-        }
-
+        PropertiesConfiguration config = ConfigHolder.getInstance().getConfig();
+        apiKey = config.getString("api.opencalais.key");
     }
 
     @Override
     public List<Keyphrase> extract(String inputText) {
-        
+
         List<Keyphrase> keyphrases = new ArrayList<Keyphrase>();
 
         PostMethod postMethod = new PostMethod("http://api.opencalais.com/tag/rs/enrich");
@@ -64,10 +59,7 @@ public class OpenCalaisSocialTagger extends KeyphraseExtractor {
 
         // create the content of the request
         String paramsXML = "<c:params xmlns:c=\"http://s.opencalais.com/1/pred/\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"><c:processingDirectives c:contentType=\"text/raw\" c:outputFormat=\"application/json\" c:enableMetadataType=\"SocialTags\"></c:processingDirectives><c:userDirectives c:allowDistribution=\"true\" c:allowSearch=\"true\"></c:userDirectives></c:params>";
-        NameValuePair[] data = { 
-                new NameValuePair("content", inputText), 
-                new NameValuePair("paramsXML", paramsXML) 
-        };
+        NameValuePair[] data = { new NameValuePair("content", inputText), new NameValuePair("paramsXML", paramsXML) };
         postMethod.setRequestBody(data);
 
         HTTPPoster poster = new HTTPPoster();
@@ -89,8 +81,8 @@ public class OpenCalaisSocialTagger extends KeyphraseExtractor {
 
                     String name = jsonObj.getString("name");
                     int importance = jsonObj.getInt("importance");
-                    LOGGER.info(name + " " + importance);
-                    
+                    LOGGER.debug(name + " " + importance);
+
                     keyphrases.add(new Keyphrase(name, importance));
 
                 }
@@ -100,16 +92,16 @@ public class OpenCalaisSocialTagger extends KeyphraseExtractor {
         } catch (JSONException e) {
             LOGGER.error(e);
         }
-        
+
         return keyphrases;
 
     }
-    
+
     @Override
     public boolean needsTraining() {
         return false;
     }
-    
+
     @Override
     public String getExtractorName() {
         return "OpenCalais SocialTags";
@@ -118,8 +110,9 @@ public class OpenCalaisSocialTagger extends KeyphraseExtractor {
     public static void main(String[] args) {
 
         OpenCalaisSocialTagger extractor = new OpenCalaisSocialTagger();
-        extractor
+        List<Keyphrase> keywords = extractor
                 .extract("The world's largest maker of solar inverters announced Monday that it will locate its first North American manufacturing plant in Denver. \"We see a huge market coming in the U.S.,\" said Pierre-Pascal Urbon, the company's chief financial officer. Solar inverters convert the direct current created by solar panels into an alternating current accessible to the larger electrical grid. The company, based in Kassel, north of Frankfurt, Germany, boasts growing sales of about $1.2 billion a year. \"We are creating economic opportunity,\" said Gov. Bill Ritter at a press conference. He added that creating core manufacturing jobs will help Colorado escape the recession sooner.");
+        CollectionHelper.print(keywords);
 
     }
 
