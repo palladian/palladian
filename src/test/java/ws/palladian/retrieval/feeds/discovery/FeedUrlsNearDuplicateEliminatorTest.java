a93891f01fc1aa3fbd24af4c9dbfe5e426dae28d
Philipp Katz <philipp@philippkatz.de>
2011-04-06 10:16:01 +0000

    fixed dedup code
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@1983 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/test/java/ws/palladian/retrieval/feeds/discovery/FeedUrlsNearDuplicateEliminatorTest.java
+++ src/test/java/ws/palladian/retrieval/feeds/discovery/FeedUrlsNearDuplicateEliminatorTest.java
@@ -27,15 +27,15 @@ public class FeedUrlsNearDuplicateEliminatorTest {
         deDup = FeedUrlsNearDuplicateEliminator.deDuplicate(urls);
         Assert.assertEquals(1, deDup.size());
         Assert.assertEquals("http://en.citizendium.org/wiki?title=Special:RecentChanges&feed=atom", deDup.get(0));
-        
+
         urls = new ArrayList<String>();
         urls.add("http://www.gaucin.com/en.feed?type=rss");
         urls.add("http://www.gaucin.com/en.feed?type=atom");
         deDup = FeedUrlsNearDuplicateEliminator.deDuplicate(urls);
         Assert.assertEquals(1, deDup.size());
         Assert.assertEquals("http://www.gaucin.com/en.feed?type=atom", deDup.get(0));
-        
-        urls = new ArrayList<String>();        
+
+        urls = new ArrayList<String>();
         urls.add("http://fukui-oozora.jugem.jp/?mode=rss");
         urls.add("http://fukui-oozora.jugem.jp/?mode=atom");
         deDup = FeedUrlsNearDuplicateEliminator.deDuplicate(urls);
@@ -47,7 +47,9 @@ public class FeedUrlsNearDuplicateEliminatorTest {
         urls.add("http://hob3photography.smugmug.com/hack/feed.mg?Type=nickname&Data=hob3photography&format=rss200");
         deDup = FeedUrlsNearDuplicateEliminator.deDuplicate(urls);
         Assert.assertEquals(1, deDup.size());
-        Assert.assertEquals("http://hob3photography.smugmug.com/hack/feed.mg?Type=nickname&Data=hob3photography&format=atom10", deDup.get(0));
+        Assert.assertEquals(
+                "http://hob3photography.smugmug.com/hack/feed.mg?Type=nickname&Data=hob3photography&format=atom10",
+                deDup.get(0));
 
         urls = new ArrayList<String>();
         urls.add("http://riadzany.blogspot.com/feeds/posts/default");
@@ -62,41 +64,41 @@ public class FeedUrlsNearDuplicateEliminatorTest {
         deDup = FeedUrlsNearDuplicateEliminator.deDuplicate(urls);
         Assert.assertEquals(1, deDup.size());
         Assert.assertEquals("http://www.organicfacts.net/feed/atom.html", deDup.get(0));
-        
+
         urls = new ArrayList<String>();
         urls.add("http://prevent-swine-flu.com/feed/");
         urls.add("http://prevent-swine-flu.com/feed/atom/");
         deDup = FeedUrlsNearDuplicateEliminator.deDuplicate(urls);
         Assert.assertEquals(1, deDup.size());
         Assert.assertEquals("http://prevent-swine-flu.com/feed/atom/", deDup.get(0));
-        
+
         urls = new ArrayList<String>();
         urls.add("http://pigai.livejournal.com/data/rss");
         urls.add("http://pigai.livejournal.com/data/atom");
         deDup = FeedUrlsNearDuplicateEliminator.deDuplicate(urls);
         Assert.assertEquals(1, deDup.size());
         Assert.assertEquals("http://pigai.livejournal.com/data/atom", deDup.get(0));
-        
+
         urls = new ArrayList<String>();
         urls.add("http://www.sdli.org/index.php/sdli/rss_2.0/");
         urls.add("http://www.sdli.org/index.php/sdli/atom/");
         deDup = FeedUrlsNearDuplicateEliminator.deDuplicate(urls);
         Assert.assertEquals(1, deDup.size());
         Assert.assertEquals("http://www.sdli.org/index.php/sdli/atom/", deDup.get(0));
-        
+
         urls = new ArrayList<String>();
         urls.add("http://williamsport-ohio.apartmenthomeliving.com/feed.rss");
         urls.add("http://williamsport-ohio.apartmenthomeliving.com/feed.atom");
         deDup = FeedUrlsNearDuplicateEliminator.deDuplicate(urls);
         Assert.assertEquals(1, deDup.size());
         Assert.assertEquals("http://williamsport-ohio.apartmenthomeliving.com/feed.atom", deDup.get(0));
-        
+
         urls = new ArrayList<String>();
         urls.add("http://bluewaterjrhawks.com/feed.xml");
         deDup = FeedUrlsNearDuplicateEliminator.deDuplicate(urls);
         Assert.assertEquals(1, deDup.size());
         Assert.assertEquals("http://bluewaterjrhawks.com/feed.xml", deDup.get(0));
-        
+
         urls = new ArrayList<String>();
         urls.add("http://www.bluewaterfl.com/feed/");
         urls.add("http://www.bluewaterfl.com/home/feed/");
@@ -104,7 +106,7 @@ public class FeedUrlsNearDuplicateEliminatorTest {
         Assert.assertEquals(2, deDup.size());
         Assert.assertEquals("http://www.bluewaterfl.com/feed/", deDup.get(0));
         Assert.assertEquals("http://www.bluewaterfl.com/home/feed/", deDup.get(1));
-        
+
         urls = new ArrayList<String>();
         urls.add("http://www.thefreedictionary.com/_/WoD/rss.aspx?type=article");
         urls.add("http://www.thefreedictionary.com/_/WoD/rss.aspx?type=history");
@@ -112,7 +114,17 @@ public class FeedUrlsNearDuplicateEliminatorTest {
         urls.add("http://www.thefreedictionary.com/_/WoD/rss.aspx?type=quote");
         deDup = FeedUrlsNearDuplicateEliminator.deDuplicate(urls);
         Assert.assertEquals(4, deDup.size());
-        
+
+        urls = new ArrayList<String>();
+        urls.add("http://easterndiocese.org/news");
+        urls.add("http://easterndiocese.org/news/index.rss?cid=1");
+        urls.add("http://easterndiocese.org/news/index.rss?cid=2");
+        deDup = FeedUrlsNearDuplicateEliminator.deDuplicate(urls);
+        Assert.assertEquals(2, deDup.size());
+        System.out.println(deDup);
+        Assert.assertEquals(true, deDup.contains("http://easterndiocese.org/news/index.rss?cid=1"));
+        Assert.assertEquals(true, deDup.contains("http://easterndiocese.org/news/index.rss?cid=2"));
+
     }
 
 }
