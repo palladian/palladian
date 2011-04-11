907e52c6edd78ab2621431900a60e0299511d5fa
Klemens Muthmann <klemens_muthmann@gmx.de>
2011-04-11 14:48:33 +0000

    scheduler task slightly refactored to provide additional output.
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2033 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: fc6c133abd3a4ab26da5e802cd2de226090be2c4

--- src/main/java/ws/palladian/retrieval/feeds/SchedulerTask.java
+++ src/main/java/ws/palladian/retrieval/feeds/SchedulerTask.java
@@ -1,8 +1,8 @@
 package ws.palladian.retrieval.feeds;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TimerTask;
+import java.util.TreeMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -12,105 +12,123 @@ import org.apache.log4j.Logger;
 import ws.palladian.helper.date.DateHelper;
 
 /**
- * A scheduler task handles the distribution of feeds to worker threads that read these feeds.
+ * A scheduler task handles the distribution of feeds to worker threads that
+ * read these feeds.
  * 
  * @author Klemens Muthmann
  * 
  */
 class SchedulerTask extends TimerTask {
 
-    private static final Logger LOGGER = Logger.getLogger(SchedulerTask.class);
+	/**
+	 * The logger for objects of this class. Configure it using
+	 * <tt>src/main/resources/log4j.xml</tt>.
+	 */
+	private static final Logger LOGGER = Logger.getLogger(SchedulerTask.class);
 
-    /**
-     * The collection of all the feeds this scheduler should create update threads for.
-     */
-    private final FeedReader feedReader;
+	/**
+	 * The collection of all the feeds this scheduler should create update
+	 * threads for.
+	 */
+	private transient final FeedReader feedReader;
 
-    /**
-     * The thread pool managing threads that read feeds from the feed sources provided by {@link #collectionOfFeeds}.
-     */
-    private final ExecutorService threadPool;
+	/**
+	 * The thread pool managing threads that read feeds from the feed sources
+	 * provided by {@link #collectionOfFeeds}.
+	 */
+	private transient final ExecutorService threadPool;
 
-    private final Map<Integer, Future<?>> scheduledTasks;
+	/**
+	 * Tasks currently scheduled but not yet checked.
+	 */
+	private transient final Map<Integer, Future<?>> scheduledTasks;
 
-    /**
-     * Creates a new scheduler task with a maximum allowed number of threads and a list of feeds to read updates from.
-     * 
-     * @param collectionOfFeeds The collection of all the feeds this scheduler should create update threads for.
-     * @param threadPoolSize The maximum number of threads to distribute reading feeds to.
-     */
-    public SchedulerTask(FeedReader feedReader) {
-        super();
-        threadPool = Executors.newFixedThreadPool(feedReader.getThreadPoolSize());
-        this.feedReader = feedReader;
-        scheduledTasks = new HashMap<Integer, Future<?>>();
-    }
+	/**
+	 * Creates a new {@code SchedulerTask} for a feed reader.
+	 * 
+	 * @param feedReader
+	 *            The feed reader containing settings and providing the
+	 *            collection of feeds to check.
+	 */
+	public SchedulerTask(final FeedReader feedReader) {
+		super();
+		threadPool = Executors.newFixedThreadPool(feedReader
+				.getThreadPoolSize());
+		this.feedReader = feedReader;
+		scheduledTasks = new TreeMap<Integer, Future<?>>();
+	}
 
-    /*
-     * (non-Javadoc)
-     * @see java.util.TimerTask#run()
-     */
-    @Override
-    public void run() {
-        LOGGER.debug("wake up to check feeds");
-        int feedCount = 0;
-        for (Feed feed : feedReader.getFeeds()) {
-            LOGGER.debug("checking feed at address: " + feed.getFeedUrl());
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.TimerTask#run()
+	 */
+	@Override
+	public void run() {
+		LOGGER.debug("wake up to check feeds");
+		int feedCount = 0;
+		for (Feed feed : feedReader.getFeeds()) {
+			if (needsLookup(feed)) {
+				if (LOGGER.isDebugEnabled()) {
+					LOGGER.debug("Scheduling feed at address: "
+							+ feed.getFeedUrl());
+				}
+				// check whether feed is in the queue already
+				if (isScheduled(feed.getId())) {
+					if (LOGGER.isTraceEnabled()) {
+						LOGGER.trace("It seems the machine cannot keep up with update intervall since feed "
+								+ feed.getId() + " is already scheduled.");
+					}
+					continue;
+				}
+				// incrementThreadPoolSize();
+				scheduledTasks.put(feed.getId(),
+						threadPool.submit(new FeedTask(feed, feedReader)));
+				feedCount++;
+			}
 
-            // check whether feed is in the queue already
-            if (isScheduled(feed.getId())) {
-                LOGGER.error("It seems the machine cannot keep up with update intervall since feed " + feed.getId()
-                        + " is already scheduled.");
-                continue;
-            }
+		}
+		LOGGER.info("Scheduled " + feedCount + " feeds for reading");
+		LOGGER.info("Queue now contains: " + scheduledTasks.size());
+	}
 
-            if (needsLookup(feed)) {
-                // incrementThreadPoolSize();
-                scheduledTasks.put(feed.getId(), threadPool.submit(new FeedTask(feed, feedReader)));
-                feedCount++;
-            }
+	/**
+	 * Returns whether the last time the provided feed was checked for updates
+	 * is further in the past than its update interval.
+	 * 
+	 * @param feed
+	 *            The feed to check.
+	 * @return {@code true} if this feeds check interval is over and
+	 *         {@code false} otherwise.
+	 */
+	private Boolean needsLookup(final Feed feed) {
+		final long now = System.currentTimeMillis();
+		return feed.getChecks() == 0
+				|| feed.getLastPollTime() == null
+				|| now - feed.getLastPollTime().getTime() > feed
+						.getUpdateInterval() * DateHelper.MINUTE_MS;
+	}
 
-        }
-
-        LOGGER.info("scheduled " + feedCount + " feeds for reading");
-    }
-
-    /**
-     * <p>
-     * Returns whether the last time the provided feed was checked for updates is further in the past than its update
-     * interval.
-     * </p>
-     * 
-     * @param feed The feed to check.
-     * @return {@code true} if this feeds check interval is over and {@code false} otherwise.
-     */
-    private Boolean needsLookup(Feed feed) {
-        long now = System.currentTimeMillis();
-        return feed.getChecks() == 0 || feed.getLastPollTime() == null
-                || now - feed.getLastPollTime().getTime() > feed.getUpdateInterval() * DateHelper.MINUTE_MS;
-    }
-
-    /**
-     * <p>
-     * Checks if this feed is already queued for updates. If this is the case one should not queue it a second time to
-     * reduce traffic.
-     * </p>
-     * 
-     * @param feedId
-     *            The id of the feed to check.
-     * @return {@code true} if this feed is already queued and {@code false} otherwise.
-     */
-    private Boolean isScheduled(final Integer feedId) {
-        Future<?> future = scheduledTasks.get(feedId);
-        if (future == null) {
-            return false;
-        } else {
-            if (future.isDone()) {
-                scheduledTasks.remove(feedId);
-                return false;
-            } else {
-                return true;
-            }
-        }
-    }
+	/**
+	 * Checks if this feed is already queued for updates. If this is the case
+	 * one should not queue it a second time to reduce traffic.
+	 * 
+	 * @param feedId
+	 *            The id of the feed to check.
+	 * @return {@code true} if this feed is already queued and {@code false}
+	 *         otherwise.
+	 */
+	private Boolean isScheduled(final Integer feedId) {
+		final Future<?> future = scheduledTasks.get(feedId);
+		Boolean ret = true;
+		if (future == null) {
+			ret = false;
+		} else {
+			if (future.isDone()) {
+				scheduledTasks.remove(feedId);
+				ret = false;
+			}
+		}
+		return ret;
+	}
 }
