1e47147b2f8f1befed8e68e9a1d48ff631dc26ae
Klemens Muthmann <klemens_muthmann@gmx.de>
2011-04-14 07:35:00 +0000

    Added Functionality to check feeds to MetaInformationCreator Debugged Scheduler Task.
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2055 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/main/java/ws/palladian/retrieval/feeds/meta/MetaInformationCreationTask.java
+++ src/main/java/ws/palladian/retrieval/feeds/meta/MetaInformationCreationTask.java
@@ -13,19 +13,23 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 
+import com.jolbox.bonecp.ConnectionTesterThread;
+
+import ws.palladian.helper.date.DateHelper;
 import ws.palladian.helper.math.MathHelper;
 import ws.palladian.persistence.DatabaseManager;
+import ws.palladian.retrieval.ConnectionTimeoutPool;
 import ws.palladian.retrieval.feeds.Feed;
 
 /**
- * <p>
- * The MetaInformationCreator gets information about last modified since and ETag support as well as information about
- * the header size.
- * </p>
+ * The MetaInformationCreator gets information about last modified since and
+ * ETag support as well as information about the header size.
  * 
  * @author Klemens Muthmann
  * @author David Urbansky
@@ -35,174 +39,247 @@ import ws.palladian.retrieval.feeds.Feed;
  */
 public final class MetaInformationCreationTask implements Runnable {
 
-    private final static Logger LOGGER = Logger.getLogger(MetaInformationCreator.class);
-    
+	private final static Logger LOGGER = Logger
+			.getLogger(MetaInformationCreator.class);
 
-    private Feed feed;
+	private Feed feed;
 
-    private DatabaseManager dbManager = new DatabaseManager();
+	private DatabaseManager dbManager = new DatabaseManager();
 
-    private static final String psSupportsLMS = "UPDATE feeds SET supportsLMS=? WHERE id=?";
+	private static final String psSupportsLMS = "UPDATE feeds SET supportsLMS=? WHERE id=?";
 
-    private static final String psSupportsEtag = "UPDATE feeds SET supportsETag=? WHERE id=?";
+	private static final String psSupportsEtag = "UPDATE feeds SET supportsETag=? WHERE id=?";
 
-    private static final String psResponseSize = "UPDATE feeds SET conditionGetResponseSize=? WHERE id=?";
+	private static final String psResponseSize = "UPDATE feeds SET conditionalGetResponseSize=? WHERE id=?";
 
-    private static final String psSupportsPubSubHubBub = "UPDATE feeds SET supportsPubSubHubBub=? WHERE id=?";
+	private static final String psSupportsPubSubHubBub = "UPDATE feeds SET supportsPubSubHubBub=? WHERE id=?";
 
-    public MetaInformationCreationTask(Feed feed) {
-        this.feed = feed;
-    }
+	private static final String psIsAccessibleFeed = "UPDATE feeds SET isAccessibleFeed=? WHERE id=?";
 
-    /**
-     * <p>
-     * Create meta information, that is, find:
-     * <ul>
-     * <li>Etag support</li>
-     * <li>last modified since support</li>
-     * <li>If conditional get is supported also the size of the reply.</li>
-     * </ul>
-     * </p>
-     * 
-     */
-    @Override
-    public void run() {
+	private static final Pattern[] validFeedPatterns = new Pattern[] {
+			Pattern.compile("<rss"), Pattern.compile("<feed"),
+			Pattern.compile("<rdf:RDF") };
 
-        URLConnection connection = null;
+	private String currentFeedContent;
 
-        try {
-            URL feedURL = new URL(feed.getFeedUrl());
-            connection = feedURL.openConnection();
-            connection.setIfModifiedSince(System.currentTimeMillis() + 60000);
-            connection.connect();
+	public MetaInformationCreationTask(Feed feed) {
+		this.feed = feed;
+	}
 
-        } catch (MalformedURLException e) {
-            LOGGER.error("URL of feed with id: " + feed.getId() + " is malformed!", e);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        Boolean supports304 = false;
+	/**
+	 * Create meta information, that is, find:
+	 * <ul>
+	 * <li>Etag support</li>
+	 * <li>last modified since support</li>
+	 * <li>If conditional get is supported also the size of the reply.</li>
+	 * </ul>
+	 */
+	@Override
+	public void run() {
 
-        try {
+		HttpURLConnection connection = null;
 
-            supports304 = getFeedSupports304((HttpURLConnection) connection);
+		try {
+			URL feedURL = new URL(feed.getFeedUrl());
+			connection = (HttpURLConnection) feedURL.openConnection();
+			ConnectionTimeoutPool timeoutPool = ConnectionTimeoutPool
+					.getInstance();
+			timeoutPool.add(connection, 2 * DateHelper.MINUTE_MS);
+			connection.setIfModifiedSince(System.currentTimeMillis() + 60000);
+			connection
+					.setRequestProperty(
+							"User-Agent",
+							"Mozilla/5.0 (Windows; U; Windows NT 6.0; en-GB; rv:1.9.0.4) Gecko/2008102920 Firefox/3.0.4");
+			connection.connect();
 
-        } catch (IOException e) {
-            LOGGER.error("Could not get HTTP header information for feed with id: " + feed.getId() + ".");
-        }
+			currentFeedContent = getContent(feedURL);
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("URL of feed with id: " + feed.getId()
+					+ " is malformed!", e);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not reed from feed with id: "
+					+ feed.getId(), e);
+		}
 
-        Boolean supportsETag = getSupportsETag(connection);
-        Integer responseSize = -1;
-        if (supports304) {
-            responseSize = getFeedResponseSize((HttpURLConnection) connection);
-        }
-        Boolean supportsPubSubHubBub = Boolean.valueOf(false);
+		Boolean isAccessibleFeed = false;
+		try {
+			isAccessibleFeed = isAccessibleFeed(connection);
+		} catch (IOException e) {
+			LOGGER.error("Unable to check if feed at URL " + feed.getFeedUrl()
+					+ " is accessible.");
+		}
 
-        try {
-            supportsPubSubHubBub = getFeedSupportsPubSubHubBub(connection);
-        } catch (IOException e1) {
-            LOGGER.error("Could not get Content with information about PubSubHubBub information for feed with id: "+feed.getId()+".");
-        }
+		Boolean supports304 = false;
 
-        try {
-            writeMetaInformationToDatabase(feed, supports304, supportsETag, responseSize, supportsPubSubHubBub);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Unable to store results to Database.", e);
-        }
+		try {
 
-        feed.freeMemory();
-        feed.setLastHeadlines("");
-        feed.setItems(null);
+			supports304 = getFeedSupports304(connection);
 
-        MetaInformationCreator.counter++;
-        LOGGER.info("percent done: "
-                + MathHelper.round(100 * MetaInformationCreator.counter
-                        / (double) MetaInformationCreator.collectionSize, 2) + "(" + MetaInformationCreator.counter
-                        + ")");
-    }
+		} catch (IOException e) {
+			LOGGER.error("Could not get HTTP header information for feed with id: "
+					+ feed.getId() + ".");
+		}
 
-    private Boolean getFeedSupportsPubSubHubBub(final URLConnection connection) throws IOException {
-        final InputStream plainXMLInput = connection.getInputStream();
-        final String plainXML = IOUtils.toString(plainXMLInput);
-        IOUtils.closeQuietly(plainXMLInput);
-        if (plainXML.contains("rel=\"hub\"")) {
-            return Boolean.valueOf(true);
-        } else {
-            return Boolean.valueOf(false);
-        }
-    }
+		Boolean supportsETag = getSupportsETag(connection);
+		Integer responseSize = -1;
+		if (supports304) {
+			responseSize = getFeedResponseSize(connection);
+		}
+		Boolean supportsPubSubHubBub = Boolean.valueOf(false);
 
-    private Integer getFeedResponseSize(final HttpURLConnection connection) {
-        int ret = 0;
-        try {
-            if (HttpURLConnection.HTTP_NOT_MODIFIED == connection.getResponseCode()) {
-                ret = new Integer((connection.getContentLength() == -1 ? 0 : connection.getContentLength())
-                        + sumHeaderFieldSize(connection.getHeaderFields()));
-            } else {
-                ret = new Integer(-1);
-            }
-        } catch (IOException e) {
-            LOGGER.error("Could not read header fields");
-        }
-        return ret;
-    }
+		try {
+			supportsPubSubHubBub = getFeedSupportsPubSubHubBub(connection);
+		} catch (IOException e1) {
+			LOGGER.error("Could not get Content with information about PubSubHubBub information for feed with id: "
+					+ feed.getId() + ".");
+		}
 
-    private Boolean getSupportsETag(final URLConnection connection) {
-        boolean ret = false;
-        ret = connection.getHeaderField("Etag") == null;
-        return ret;
-    }
+		try {
+			writeMetaInformationToDatabase(feed, supports304, supportsETag,
+					responseSize, supportsPubSubHubBub, isAccessibleFeed);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to store results to Database.",
+					e);
+		}
 
-    /**
-     * <p>
-     * Checks if a web feeds server does support condition gets.
-     * </p>
-     * 
-     * @param feed The feed to check.
-     * @param responseSize Contains the size of the returned message with HTTP Status code 304 or -1 if conditional get
-     *            is not supported.
-     * @return {@code true} if the feed supports conditional gets, {@code false} otherwise.
-     * @throws IOException
-     */
-    private Boolean getFeedSupports304(final HttpURLConnection connection) throws IOException {
-        Boolean ret = false;
+		feed.freeMemory();
+		feed.setLastHeadlines("");
+		// feed.setItems(null);
 
-        if (HttpURLConnection.HTTP_NOT_MODIFIED == connection.getResponseCode()) {
-            ret = true;
-        }
-        return ret;
-    }
+		MetaInformationCreator.counter++;
+		LOGGER.info("percent done: "
+				+ MathHelper.round(100 * MetaInformationCreator.counter
+						/ (double) MetaInformationCreator.collectionSize, 2)
+				+ "(" + MetaInformationCreator.counter + ")");
+	}
 
-    private void writeMetaInformationToDatabase(Feed feed, Boolean supportsLMS, Boolean supportsETag,
-            Integer responseSizeValue, Boolean supportsPubSubHubBub) throws SQLException {
+	private String getContent(URL feedURL) throws IOException {
+		InputStream feedInput = null;
+		String ret = "";
+		try {
+			HttpURLConnection connection = (HttpURLConnection) feedURL
+					.openConnection();
+			ConnectionTimeoutPool timeoutPool = ConnectionTimeoutPool
+					.getInstance();
+			timeoutPool.add(connection, 2 * DateHelper.MINUTE_MS);
+			connection
+					.setRequestProperty(
+							"User-Agent",
+							"Mozilla/5.0 (Windows; U; Windows NT 6.0; en-GB; rv:1.9.0.4) Gecko/2008102920 Firefox/3.0.4");
+			feedInput = connection.getInputStream();
+			ret = IOUtils.toString(feedInput);
+		} finally {
+			IOUtils.closeQuietly(feedInput);
+		}
+		return ret;
+	}
 
-        Integer id = feed.getId();
-        
-        dbManager.runUpdate(psSupportsLMS, supportsLMS, id);
-        dbManager.runUpdate(psSupportsEtag, supportsETag, id);
-        dbManager.runUpdate(psResponseSize, responseSizeValue, id);
-        dbManager.runUpdate(psSupportsPubSubHubBub, supportsPubSubHubBub, id);
-    }
+	private Boolean isAccessibleFeed(HttpURLConnection connection)
+			throws IOException {
+		if (HttpURLConnection.HTTP_NOT_FOUND == connection.getResponseCode()
+				|| HttpURLConnection.HTTP_FORBIDDEN == connection
+						.getResponseCode()) {
+			return false;
+		}
+		if (currentFeedContent != null) {
+			for (Pattern pattern : validFeedPatterns) {
+				Matcher matcher = pattern.matcher(currentFeedContent);
+				if (matcher.find()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 
-    /**
-     * @param headerFields The header fields from an http connection.
-     * @return The summed up size of all header fields in bytes
-     */
-    private int sumHeaderFieldSize(Map<String, List<String>> headerFields) {
-        Integer ret = 0;
-        for (Entry<String, List<String>> entry : headerFields.entrySet()) {
-            String key = entry.getKey();
-            if (key != null) {
-                byte[] bytes = key.getBytes();
-                ret += bytes.length;
-            }
-            for (String value : entry.getValue()) {
-                ret += value.getBytes().length;
-            }
-        }
-        return ret;
-    }
+	private Boolean getFeedSupportsPubSubHubBub(final URLConnection connection)
+			throws IOException {
+		if (currentFeedContent != null
+				&& currentFeedContent.contains("rel=\"hub\"")) {
+			return Boolean.valueOf(true);
+		} else {
+			return Boolean.valueOf(false);
+		}
+	}
+
+	private Integer getFeedResponseSize(final HttpURLConnection connection) {
+		int ret = 0;
+		try {
+			if (HttpURLConnection.HTTP_NOT_MODIFIED == connection
+					.getResponseCode()) {
+				ret = new Integer((connection.getContentLength() == -1 ? 0
+						: connection.getContentLength())
+						+ sumHeaderFieldSize(connection.getHeaderFields()));
+			} else {
+				ret = new Integer(-1);
+			}
+		} catch (IOException e) {
+			LOGGER.error("Could not read header fields");
+		}
+		return ret;
+	}
+
+	private Boolean getSupportsETag(final URLConnection connection) {
+		boolean ret = false;
+		ret = connection.getHeaderField("Etag") == null;
+		return ret;
+	}
+
+	/**
+	 * Checks if a web feeds server does support condition gets.
+	 * 
+	 * @param feed
+	 *            The feed to check.
+	 * @param responseSize
+	 *            Contains the size of the returned message with HTTP Status
+	 *            code 304 or -1 if conditional get is not supported.
+	 * @return {@code true} if the feed supports conditional gets, {@code false}
+	 *         otherwise.
+	 * @throws IOException
+	 */
+	private Boolean getFeedSupports304(final HttpURLConnection connection)
+			throws IOException {
+		Boolean ret = false;
+
+		if (HttpURLConnection.HTTP_NOT_MODIFIED == connection.getResponseCode()) {
+			ret = true;
+		}
+		return ret;
+	}
+
+	private void writeMetaInformationToDatabase(Feed feed, Boolean supportsLMS,
+			Boolean supportsETag, Integer responseSizeValue,
+			Boolean supportsPubSubHubBub, Boolean isAccessibleFeed)
+			throws SQLException {
+
+		Integer id = feed.getId();
+
+		dbManager.runUpdate(psSupportsLMS, supportsLMS, id);
+		dbManager.runUpdate(psSupportsEtag, supportsETag, id);
+		dbManager.runUpdate(psResponseSize, responseSizeValue, id);
+		dbManager.runUpdate(psSupportsPubSubHubBub, supportsPubSubHubBub, id);
+		dbManager.runUpdate(psIsAccessibleFeed, isAccessibleFeed, id);
+	}
+
+	/**
+	 * @param headerFields
+	 *            The header fields from an http connection.
+	 * @return The summed up size of all header fields in bytes
+	 */
+	private int sumHeaderFieldSize(Map<String, List<String>> headerFields) {
+		Integer ret = 0;
+		for (Entry<String, List<String>> entry : headerFields.entrySet()) {
+			String key = entry.getKey();
+			if (key != null) {
+				byte[] bytes = key.getBytes();
+				ret += bytes.length;
+			}
+			for (String value : entry.getValue()) {
+				ret += value.getBytes().length;
+			}
+		}
+		return ret;
+	}
 
 }
