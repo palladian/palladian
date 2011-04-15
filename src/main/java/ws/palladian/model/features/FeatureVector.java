826bf67b844f4b770afb4100205e96b2dc0ec830
David Urbansky <davidurbansky@gmail.com>
2011-04-15 16:58:33 +0000

    added two nodes in nodes.fla fixed minor bug in FileFormatParser
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2085 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/main/java/ws/palladian/model/features/FeatureVector.java
+++ src/main/java/ws/palladian/model/features/FeatureVector.java
@@ -1,54 +1,24 @@
 package ws.palladian.model.features;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
-/**
- * A class to describe collections of {@code Feature}s extracted from some
- * document. Based on its {@code FeatureVector} the document can be processed by
- * Information Retrieval components like classifiers or clusterers.
- * 
- * @author Klemens Muthmann
- * @author David Urbansky
- */
-public final class FeatureVector {
-    /**
-     * A map of all {@code Feature}s in this vector. It maps from the {@code Feature}s {@code FeatureVector} wide unique
-     * identifier to an
-     * actual {@code Feature} instance containing the value. The value might be
-     * of any java object type.
-     */
-    private final transient Map<String, Feature<?>> features;
+public class FeatureVector<F extends Feature<V>, V> implements FeatureValue, Iterable<F> {
 
-    /**
-     * Creates a new empty {@code FeatureVector}. Too fill it with {@link Feature}s call {@link #add(String, Feature)}.
-     */
+    private Map<String, F> features;
+
     public FeatureVector() {
-        features = new HashMap<String, Feature<?>>();
+        features = new HashMap<String, F>();
     }
 
-    /**
-     * Adds a new {@code Feature} to this {@code FeatureVector}.
-     * 
-     * @param identifier
-     *            The {@code Feature}s {@code FeatureVector} wide unique
-     *            identifier.
-     * @param newFeature
-     *            The actual {@code Feature} instance containing the value.
-     */
-    public void add(String identifier, Feature<?> newFeature) {
-        features.put(identifier, newFeature);
+    public void add(F feature) {
+        features.put(feature.getName(), feature);
     }
 
-    /**
-     * Provides a {@code Feature} from this {@code FeatureVector}.
-     * 
-     * @param identifier
-     *            The {@code FeatureVector} wide unique identifier of the
-     *            requested {@code Feature}.
-     * @return The {@code Feature} with identifier {@code identifier} or {@code null} if no such {@code Feature} exists.
-     */
-    public Feature<?> get(String identifier) {
-        return features.get(identifier);
+    @Override
+    public Iterator<F> iterator() {
+        return features.values().iterator();
     }
+
 }
--- src/main/java/ws/palladian/model/features/NominalFeature.java
+++ src/main/java/ws/palladian/model/features/NominalFeature.java
@@ -1,21 +1,6 @@
 package ws.palladian.model.features;
 
-/**
- * A convenience class for nominal {@code Feature}s binding {@code T} to a {@code String} value.
- * 
- * @author Klemens Muthmann
- * @author David Urbansky
- */
-public final class NominalFeature extends Feature<String> {
+public class NominalFeature extends Feature {
 
-    /**
-     * Creates a new {@code NominalFeature} instance with all attributes initialized.
-     * 
-     * @param name The {@code FeatureVector} wide unique identifier of this {@code Feature}.
-     * @param value The {@code String} value of this {@code Feature}.
-     */
-    public NominalFeature(String name, String value) {
-        super(name, value);
-    }
 
 }
