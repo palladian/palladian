37941be8971b794a4ab8aa2ca3eca4fe227e31df
Philipp Katz <philipp@philippkatz.de>
2011-04-21 11:06:40 +0000

    JavaDoc for ROME date parser
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2135 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: bb2f3f9c91da6f20598fcdc6ccc068311d8c7cf6

--- src/main/java/ws/palladian/retrieval/feeds/rome/RawDateModule.java
+++ src/main/java/ws/palladian/retrieval/feeds/rome/RawDateModule.java
@@ -2,11 +2,27 @@ package ws.palladian.retrieval.feeds.rome;
 
 import com.sun.syndication.feed.module.Module;
 
+/**
+ * This module keeps raw date information from feed items as string. This allows us to process the date strings with our
+ * own date parsing techniques, as ROME itself is very with date parsing and only parses date formats which are defined
+ * by the respective Atom or RSS specifications.
+ * 
+ * The module itself is configured in the rome.properties file. For more information see the attached links.
+ * 
+ * This is a hack, we are basically abusing ROME's module mechanism, but it works quite well :) .
+ * 
+ * @see http://sujitpal.blogspot.com/2007/10/custom-modules-with-rome.html
+ * @see http://java.net/projects/rome/lists/dev/archive/2005-02/message/73
+ * 
+ * @author Philipp Katz
+ * 
+ */
 public interface RawDateModule extends Module {
-    
+
     static final String URI = "ws.palladian.retrieval.feeds.rome.RawDateModule";
-    
+
     String getRawDate();
+
     void setRawDate(String rawDate);
 
 }
