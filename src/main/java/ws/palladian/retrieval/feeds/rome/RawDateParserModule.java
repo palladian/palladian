fc24ad938ba5776f468fd728618cfd06986ba026
Philipp Katz <philipp@philippkatz.de>
2011-04-21 09:54:25 +0000

    finished and cleaned up DateParser for ROME
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2133 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: 44a63700a69022c6fd7aa7eec004142a6c3c82ea

--- src/main/java/ws/palladian/retrieval/feeds/rome/RawDateParserModule.java
+++ src/main/java/ws/palladian/retrieval/feeds/rome/RawDateParserModule.java
@@ -2,20 +2,53 @@ package ws.palladian.retrieval.feeds.rome;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.jdom.Element;
 
 import com.sun.syndication.feed.module.Module;
 import com.sun.syndication.io.ModuleParser;
 
-public class RawDateParserModule implements ModuleParser{
+public abstract class RawDateParserModule implements ModuleParser {
 
     /** The logger for this class. */
-    private static final Logger LOGGER = Logger.getLogger(RawDateParserModule.class);
+    // private static final Logger LOGGER = Logger.getLogger(RawDateParserModule.class);
 
-    @Override
-    public String getNamespaceUri() {
-        return null;
+    private static final String ATOM_NS = "http://www.w3.org/2005/Atom";
+    private static final String RSS_20_NS = "http://backend.userland.com/rss2";
+    private static final String RSS_10_NS = "http://purl.org/rss/1.0/";
+
+    public static class RawDateParserModuleRss extends RawDateParserModule {
+
+        @Override
+        public String getNamespaceUri() {
+            return null;
+        }
+
+    }
+    
+    public static class RawDateParserModuleRss20NS extends RawDateParserModule {
+
+        @Override
+        public String getNamespaceUri() {
+            return RSS_20_NS;
+        }
+        
+    }
+    
+    public static class RawDataParserModuleRss10 extends RawDateParserModule {
+
+        @Override
+        public String getNamespaceUri() {
+            return RSS_10_NS;
+        }
+        
+    }
+
+    public static class RawDateParserModuleAtom extends RawDateParserModule {
+
+        @Override
+        public String getNamespaceUri() {
+            return ATOM_NS;
+        }
     }
 
     @Override
@@ -26,15 +59,11 @@ public class RawDateParserModule implements ModuleParser{
         for (Element childElement : children) {
             // search for a node containing "date" in its name
             if (childElement.getName().toLowerCase().contains("date")) {
-                LOGGER.debug("found " + childElement.getName() + " in element " + element.getTextNormalize());
+                // LOGGER.debug("found " + childElement.getName() + " in element " + element.getTextNormalize());
                 rawDateModule.setRawDate(childElement.getText());
                 break;
             }
         }
-//        Element child = element.getChild("pubDate");
-//        if (child != null) {
-//            rawDateModule.setRawDate(child.getText());
-//        }
         return rawDateModule;
     }
 
