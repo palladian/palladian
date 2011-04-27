45da884e30ded1d8275396a9b57c803eb8f5ccec
Martin Gregor <mail@m-gregor.de>
2011-04-27 20:09:59 +0000

    refactor DateWekaInstanceFactory
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2162 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: 849b81d04de98ac1c185b6bb6170f787fca52b9f

--- src/main/java/ws/palladian/helper/date/DateWekaInstanceFactory.java
+++ src/main/java/ws/palladian/helper/date/DateWekaInstanceFactory.java
@@ -1,14 +1,11 @@
 package ws.palladian.helper.date;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringReader;
 
-import weka.core.Attribute;
-import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
 import ws.palladian.daterecognition.KeyWords;
@@ -26,79 +23,43 @@ public class DateWekaInstanceFactory {
 
 	private PageDateType pageDateType;
 
-	private Attribute classIndex;
-
-	private Attribute hour;
-	private Attribute minute;
-	private Attribute second;
-
-	private Attribute relDocPos;
-	private Attribute ordDocPos;
-	private Attribute ordAgePos;
-
-	private Attribute keyClass;
-	private Attribute keyLoc;
-	private Attribute keyDiff;
-
-	private Attribute simpleTag;
-	private Attribute hTag;
-	private Attribute tagName;
-
-	private Attribute hasStructureDate;
-	private Attribute inMetaDates;
-	private Attribute inUrl;
-
-	private Attribute relCntSame;
-	private Attribute relSize;
-
-	private Attribute distPosBefore;
-	private Attribute distPosAfter;
-	private Attribute distAgeBefore;
-	// private Attribute distAgeAfter = new private Attribute("distAgeAfter");
-
-	private Attribute format;
-	private Attribute keyword;
-	private Attribute excatness;
-
-	private Attribute keyLoc201;
-	private Attribute keyLoc202;
-
-	private Attribute isKeyClass1;
-	private Attribute isKeyClass2;
-	private Attribute isKeyClass3;
+	private StringBuffer template = new StringBuffer();
 
 	public DateWekaInstanceFactory(PageDateType pageDateType) {
 		this.pageDateType = pageDateType;
-		setAttributes();
+		readTemplate();
 	}
 
-	public Instances getDateInstanceByArffTemplate(ContentDate date) {
-		Instances instances = null;
+	private void readTemplate() {
+		String arffTemplate = DateWekaInstanceFactory.class.getResource(
+				"/wekaClassifier/template.arff").getFile();
+		BufferedReader reader;
 		try {
-			String arffTemplate = DateWekaInstanceFactory.class.getResource(
-					"/wekaclassifier/template.arff").getFile();
-			String arffTemp = DateWekaInstanceFactory.class.getResource("/wekaclassifier/temp.arff").getFile();
-			BufferedReader reader = new BufferedReader(new FileReader(
-					arffTemplate));
-
-//			File file = new File("data/wekaclassifier/temp.arff");
-			FileWriter fileWriter = new FileWriter(arffTemp, false);
-			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+			reader = new BufferedReader(new FileReader(arffTemplate));
 			String line;
 			while ((line = reader.readLine()) != null) {
-				bufferedWriter.write(line + "\n");
+				template.append(line + "\n");
 			}
-			bufferedWriter.write("0," + datefeaturesToString(date));
-			bufferedWriter.close();
-			fileWriter.close();
 			reader.close();
-			reader = new BufferedReader(new FileReader(arffTemp));
-			instances = new Instances(reader);
-			instances.setClassIndex(0);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return instances;
+	}
+
+	public Instance getDateInstanceByArffTemplate(ContentDate date) {
+		Instance instance = null;
+		StringBuffer dateInstanceBuffer = this.template;
+		dateInstanceBuffer.append("0," + datefeaturesToString(date) + "\n");
+		BufferedReader reader = new BufferedReader(new StringReader(
+				dateInstanceBuffer.toString()));
+		try {
+			instance = new Instances(reader).firstInstance();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return instance;
 	}
 
 	private String datefeaturesToString(ContentDate date) {
@@ -180,252 +141,6 @@ public class DateWekaInstanceFactory {
 				+ keyword + "," + excatness + "," + keyLoc201 + "," + keyLoc202
 				+ "," + isKeyClass1 + "," + isKeyClass2 + "," + isKeyClass3;
 		return dateString;
-	}
-
-	public Instance instantiate(ContentDate date) {
-		Instance instance = null;
-
-		if (isInstance(date)) {
-
-			String formatString = date.getFormat();
-			if (!isNormalFormat(formatString)) {
-				formatString = changeFormat(formatString);
-			}
-
-			String tagNameString = date.getTag();
-			if (!isNormalTag(tagNameString)) {
-				tagNameString = "A";
-			}
-
-			String keywordString = date.getKeyword();
-			if (keywordString != null) {
-				keywordString = keywordString.toLowerCase();
-			}
-			if (!isNormalKeyword(keywordString)) {
-				keywordString = "null";
-			}
-
-			instance = new Instance(28);
-			instance.setDataset(getFeatureSet());
-
-			String hourString = (date.get(ExtractedDate.HOUR) == -1) ? "0"
-					: "1";
-			String minuteString = date.get(ExtractedDate.MINUTE) == -1 ? "0"
-					: "1";
-			String secondString = date.get(ExtractedDate.SECOND) == -1 ? "0"
-					: "1";
-
-			instance.setValue(hour, String.valueOf(hourString));
-			instance.setValue(minute, minuteString);
-			instance.setValue(second, secondString);
-
-			instance.setValue(relDocPos, date.getRelDocPos());
-			instance.setValue(ordDocPos, date.getOrdDocPos());
-			instance.setValue(ordAgePos, date.getOrdAgePos());
-
-			int keyPrio = KeyWords.getKeywordPriority(date.getKeyword());
-			keyPrio = keyPrio == -1 ? 0 : keyPrio;
-			String keyClassString = String.valueOf(keyPrio);
-			instance.setValue(keyClass, keyClassString);
-			instance.setValue(keyLoc, date.getKeyLoc());
-			instance.setValue(keyDiff, date.getKeyDiff());
-
-			instance.setValue(simpleTag, date.getSimpleTag());
-			instance.setValue(hTag, date.gethTag());
-			instance.setValue(tagName, tagNameString);
-
-			instance.setValue(hasStructureDate, date.hasStrucutreDate() ? "1"
-					: "0");
-			instance.setValue(inMetaDates, date.isInMetaDates() ? "1" : "0");
-			instance.setValue(inUrl, date.isInUrl() ? "1" : "0");
-
-			instance.setValue(relCntSame, date.getRelCntSame());
-			instance.setValue(relSize, date.getRelSize());
-
-			instance.setValue(distPosBefore, date.getDistPosBefore());
-			instance.setValue(distPosAfter, date.getDistPosAfter());
-			instance.setValue(distAgeBefore, date.getDistAgeAfter());
-
-			// instance.setValue(distAgeBefore, date.getDistAgeBefore());
-			// instance.setValue(distAgeAfter, date.getDistAgeAfter());
-
-			instance.setValue(format, formatString);
-			instance.setValue(keyword, keywordString);
-			instance.setValue(excatness, String.valueOf(date.getExactness()));
-
-			instance.setValue(keyLoc201, date.getKeyLoc201());
-			instance.setValue(keyLoc202, date.getKeyLoc202());
-
-			instance.setValue(isKeyClass1, date.getIsKeyClass1());
-			instance.setValue(isKeyClass2, date.getIsKeyClass2());
-			instance.setValue(isKeyClass3, date.getIsKeyClass3());
-		}
-		return instance;
-	}
-
-	private Instances getFeatureSet() {
-		FastVector attributeVec = new FastVector(29);
-		attributeVec.addElement(hour);
-		attributeVec.addElement(minute);
-		attributeVec.addElement(second);
-
-		attributeVec.addElement(relDocPos);
-		attributeVec.addElement(ordDocPos);
-		attributeVec.addElement(ordAgePos);
-
-		attributeVec.addElement(keyClass);
-		attributeVec.addElement(keyLoc);
-		attributeVec.addElement(keyDiff);
-
-		attributeVec.addElement(simpleTag);
-		attributeVec.addElement(hTag);
-		attributeVec.addElement(tagName);
-
-		attributeVec.addElement(hasStructureDate);
-		attributeVec.addElement(inMetaDates);
-		attributeVec.addElement(inUrl);
-
-		attributeVec.addElement(relCntSame);
-		attributeVec.addElement(relSize);
-
-		attributeVec.addElement(distPosBefore);
-		attributeVec.addElement(distPosAfter);
-		attributeVec.addElement(distAgeBefore);
-		// attributeVec.addElement(distAgeAfter);
-
-		attributeVec.addElement(format);
-		attributeVec.addElement(keyword);
-		attributeVec.addElement(excatness);
-
-		attributeVec.addElement(keyLoc201);
-		attributeVec.addElement(keyLoc202);
-
-		attributeVec.addElement(isKeyClass1);
-		attributeVec.addElement(isKeyClass2);
-		attributeVec.addElement(isKeyClass3);
-
-		attributeVec.addElement(classIndex);
-
-		Instances featureSet = new Instances("featureSet", attributeVec, 0);
-		featureSet.setClass(classIndex);
-
-		return featureSet;
-	}
-
-	private static FastVector getVector(String numString) {
-		String[] nums = numString.split(";");
-		FastVector vector = new FastVector(nums.length);
-		for (int i = 0; i < nums.length; i++) {
-			vector.addElement(nums[i]);
-		}
-		return vector;
-	}
-
-	/** TODO numeric values in richtiger reihenfolge hinzufügen */
-	private static FastVector getTagVector() {
-		// FastVector vector = new FastVector();
-		//		
-		// vector.addElement("SPAN");
-		// vector.addElement("P");
-		// vector.addElement("LI");
-		// vector.addElement("FONT");
-		// vector.addElement("DIV");
-		// vector.addElement("H2");
-		// vector.addElement("A");
-		// vector.addElement("B");
-		// vector.addElement("OPTION");
-		// vector.addElement("SMALL");
-		// vector.addElement("EM");
-		// vector.addElement("H3");
-		// vector.addElement("CENTER");
-		// vector.addElement("ABBR");
-		// vector.addElement("NOBR");
-		// vector.addElement("STRONG");
-		// vector.addElement("TD");
-		// vector.addElement("CITE");
-		// vector.addElement("BODY");
-		// vector.addElement("I");
-		// vector.addElement("PRE");
-		// vector.addElement("DD");
-		// vector.addElement("DT");
-		// vector.addElement("BLOCKQUOTE");
-		// vector.addElement("HTML:ABBR");
-		// vector.addElement("H4");
-		// vector.addElement("H5");
-		// vector.addElement("TITLE");
-		// vector.addElement("PAUSING");
-		// vector.addElement("DL");
-		// vector.addElement("U");
-		// vector.addElement("ADDRESS");
-		// vector.addElement("EDITDATE");
-		// vector.addElement("H6");
-		// vector.addElement("CODE");
-		// vector.addElement("H1");
-		// vector.addElement("TIME");
-		// return vector;
-		return getVector("SPAN;STRONG;P;EM;LI;H5;FONT;DIV;H2;A;B;I;CENTER;OPTION;"
-				+ "SMALL;TD;TITLE;H3;ADDRESS;BODY;ABBR;NOBR;EDITDATE;CITE;"
-				+ "H6;PRE;H4;CODE;H1;DD;BLOCKQUOTE;DT;TIME;HTML:ABBR;PAUSING;DL;U");
-	}
-
-	private static FastVector getFormatVector() {
-		// FastVector vector = new FastVector();
-		// vector.addElement("MMMM DD, YYYY");
-		// vector.addElement("MMMM DD, YYYY YYYY HH:MM:SS +UTC");
-		// vector.addElement("MM/DD/YYYY") ;
-		// vector.addElement("DD. MMMM YYYY") ;
-		// vector.addElement("DD.MM.YYYY") ;
-		// vector.addElement("YYYY-MM-DD") ;
-		// vector.addElement("YYYY_MM_DD") ;
-		// vector.addElement("MM/DD/YYYY HH:MM:SS +UTC") ;
-		// vector.addElement("DD. MMMM YYYY HH:MM:SS +UTC");
-		// vector.addElement("YYYYMMDD") ;
-		// vector.addElement("YYYY-DDD");
-		// vector.addElement("WD, DD MMM YYYY HH:MM:SS +UTC") ;
-		// vector.addElement("MMMM-DD-YYYY") ;
-		// vector.addElement("YYYY-MMM-D") ;
-		// vector.addElement("DD.MM.YYYY HH:MM:SS +UTC") ;
-		// vector.addElement("YYYY-MM-DDTHH:MM:SS+HH:MM");
-		// vector.addElement("YYYYDDD") ;
-		// vector.addElement("WD MMM DD_1 HH:MM:SS YYYY") ;
-		// return vector;
-		return getVector("MMMM DD, YYYY;" + "MMMM DD, YYYY YYYY HH:MM:SS +UTC;"
-				+ "MM/DD/YYYY;" + "DD. MMMM YYYY;" + "DD.MM.YYYY;"
-				+ "YYYY-MM-DD;" + "YYYY_MM_DD;" + "YYYYMMDD;"
-				+ "MM/DD/YYYY HH:MM:SS +UTC;" + "DD. MMMM YYYY HH:MM:SS +UTC;"
-				+ "DD.MM.YYYY HH:MM:SS +UTC;" + "MMMM-DD-YYYY;"
-				+ "YYYY-MM-DDTHH:MM:SS+HH:MM;" + "YYYY-DDD;"
-				+ "WD, DD MMM YYYY HH:MM:SS +UTC;" + "YYYY-MMM-D;"
-				+ "WD MMM DD_1 HH:MM:SS YYYY;" + "YYYYDDD");
-	}
-
-	private static FastVector getKeywordVector() {
-		// FastVector vector = new FastVector();
-		// vector.addElement("date");
-		// vector.addElement("null");
-		// vector.addElement("posted");
-		// vector.addElement("update");
-		// vector.addElement("release");
-		// vector.addElement("added");
-		// vector.addElement("updated");
-		// vector.addElement("create");
-		// vector.addElement("publish");
-		// vector.addElement("released");
-		// vector.addElement("published");
-		// vector.addElement("revised");
-		// vector.addElement("created");
-		// vector.addElement("pdate");
-		// vector.addElement("revise");
-		// vector.addElement("last modified");
-		// vector.addElement("date-header");
-		// vector.addElement("pubdate");
-		// vector.addElement("datetime");
-		// vector.addElement("geschrieben");
-		// return vector;
-		return getVector("null;updated;date;created;posted;published;"
-				+ "release;update;added;released;last modified;"
-				+ "create;publish;pubdate;pdate;revised;"
-				+ "date-header;revise;datetime;geschrieben");
 	}
 
 	private static boolean isInstance(ContentDate date) {
@@ -516,55 +231,4 @@ public class DateWekaInstanceFactory {
 		return result;
 	}
 
-	private void setAttributes() {
-		FastVector vector01 = getVector("0;1");
-
-		String typeString;
-		if (pageDateType.equals(PageDateType.publish)) {
-			typeString = "pub";
-		} else {
-			typeString = "mod";
-		}
-
-		classIndex = new Attribute(typeString, getVector("1;0"));
-
-		hour = new Attribute("hour", vector01);
-		minute = new Attribute("minute", vector01);
-		second = new Attribute("second", vector01);
-
-		relDocPos = new Attribute("relDocPos");
-		ordDocPos = new Attribute("ordDocPos");
-		ordAgePos = new Attribute("ordAgePos");
-
-		keyClass = new Attribute("keyClass", getVector("0;2;3;1"));
-		keyLoc = new Attribute("keyLoc", getVector("0;2;1"));
-		keyDiff = new Attribute("keyDiff");
-
-		simpleTag = new Attribute("simpleTag", vector01);
-		hTag = new Attribute("hTag", vector01);
-		tagName = new Attribute("tagName", getTagVector());
-
-		hasStructureDate = new Attribute("hasStructureDate", vector01);
-		inMetaDates = new Attribute("inMetaDates", vector01);
-		inUrl = new Attribute("inUrl", vector01);
-
-		relCntSame = new Attribute("relCntSame");
-		relSize = new Attribute("relSize");
-
-		distPosBefore = new Attribute("distPosBefore");
-		distPosAfter = new Attribute("distPosAfter");
-		distAgeBefore = new Attribute("distAgeBefore");
-		// distAgeAfter = new Attribute("distAgeAfter");
-
-		format = new Attribute("format", getFormatVector());
-		keyword = new Attribute("keyword", getKeywordVector());
-		excatness = new Attribute("excatness", getVector("3;5;6;4"));
-
-		keyLoc201 = new Attribute("keyLoc201", vector01);
-		keyLoc202 = new Attribute("keyLoc202", vector01);
-
-		isKeyClass1 = new Attribute("isKeyClass1", vector01);
-		isKeyClass2 = new Attribute("isKeyClass2", vector01);
-		isKeyClass3 = new Attribute("isKeyClass3", vector01);
-	}
 }
--- src/test/java/ws/palladian/daterecognition/weka/DateWekaInstanceFactoryTest.java
+++ src/test/java/ws/palladian/daterecognition/weka/DateWekaInstanceFactoryTest.java
@@ -3,16 +3,12 @@ package ws.palladian.daterecognition.weka;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashSet;
 
-import static org.hamcrest.core.IsNot.*;
 import org.junit.Test;
 
 import weka.core.Attribute;
-import weka.core.FastVector;
 import weka.core.Instance;
 import ws.palladian.daterecognition.dates.ContentDate;
 import ws.palladian.daterecognition.technique.PageDateType;
@@ -20,48 +16,13 @@ import ws.palladian.helper.date.DateWekaInstanceFactory;
 
 public class DateWekaInstanceFactoryTest {
 
-	private String[] numericAttributes = { "relDocPos", "ordDocPos",
-			"ordAgePos", "keyDiff", "relCntSame", "relSize", "distPosBefore",
-			"distPosAfter", "distAgeBefore", };
-	private String[] nominalAttributes = { "hour", "minute", "second",
-			"keyClass", "keyLoc", "simpleTag", "hTag", "tagName",
-			"hasStructureDate", "inMetaDates", "inUrl", "format", "keyword",
-			"excatness", "keyLoc201", "keyLoc202", "isKeyClass1",
-			"isKeyClass2", "isKeyClass3" };
-
 	@Test
-	public void testCreateAttribute() {
-		DateWekaInstanceFactory dwif = new DateWekaInstanceFactory(
-				PageDateType.publish);
-		Class<? extends DateWekaInstanceFactory> dwifClass = dwif.getClass();
-		Class[] param = new Class[1];
-		param[0] = String.class;
-		Attribute attribute = null;
-		Method method;
-		try {
-			method = dwifClass.getDeclaredMethod("getVector", param);
-			method.setAccessible(true);
-			FastVector vectorResult;
-			vectorResult = (FastVector) method.invoke(dwif, "0;1");
-			attribute = new Attribute("hour", vectorResult);
-		} catch (SecurityException e1) {
-			e1.printStackTrace();
-		} catch (NoSuchMethodException e1) {
-			e1.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		assertThat(attribute.name(), is("hour"));
-
+	public void testGetDateInstanceByArff(){
+		DateWekaInstanceFactory dwif = new DateWekaInstanceFactory(PageDateType.publish);
+		Instance instance = dwif.getDateInstanceByArffTemplate(getTestDate());
+		assertThat(isNull(instance), is(false));
 	}
-
+	
 	@Test
 	public void testDatefeaturesToString() {
 		ContentDate date = getTestDate();
@@ -97,151 +58,11 @@ public class DateWekaInstanceFactoryTest {
 				is("0,0,0,0.375,0.75,0.5,1,0,0.8,0,0,P,0,0,0,0.25,0.01,4840,6042,14113,'MMMM DD, YYYY',create,3,0,1,1,0,0"));
 	}
 
-	@Test
-	public void testGetDateInstanceByArffTemplate(){
-		DateWekaInstanceFactory dwif = new DateWekaInstanceFactory(PageDateType.publish);
-		Instance instance = dwif.getDateInstanceByArffTemplate(getTestDate()).firstInstance();
-		assertThat(isNull(instance), is(false));
-	}
-	
 	private boolean isNull(Object obj){
 		return obj == null;
 	}
 	
-	@Test
-	public void testGetVector() {
-		String[] vector01 = { "0", "1" };
-		testGetVector(vector01);
-		String[] vector0231 = { "0", "2", "3", "1" };
-		testGetVector(vector0231);
-		String[] vector021 = { "0", "2", "1" };
-		testGetVector(vector021);
-		String[] vector3564 = { "3", "5", "6", "4" };
-		testGetVector(vector3564);
-	}
-
-	private void testGetVector(String[] getVectorParameter) {
-		DateWekaInstanceFactory dwif = new DateWekaInstanceFactory(
-				PageDateType.publish);
-		Class<? extends DateWekaInstanceFactory> dwifClass = dwif.getClass();
-		Class[] param = new Class[1];
-		param[0] = String.class;
-		Method method;
-		FastVector vectorResult = new FastVector();
-		try {
-			method = dwifClass.getDeclaredMethod("getVector", param);
-			method.setAccessible(true);
-			String getVectorParameterString = "";
-			for (int i = 0; i < getVectorParameter.length; i++) {
-				if (i == 0) {
-					getVectorParameterString = getVectorParameter[i];
-				} else {
-					getVectorParameterString += ";" + getVectorParameter[i];
-				}
-			}
-			vectorResult = (FastVector) method.invoke(dwif,
-					getVectorParameterString);
-		} catch (SecurityException e1) {
-			e1.printStackTrace();
-		} catch (NoSuchMethodException e1) {
-			e1.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		for (int i = 0; i < vectorResult.capacity(); i++) {
-			String value = (String) vectorResult.elementAt(i);
-			assertThat(value, is(getVectorParameter[i]));
-		}
-	}
-
-	@Test
-	public void testAttributes() {
-		DateWekaInstanceFactory dwif = new DateWekaInstanceFactory(
-				PageDateType.publish);
-		Class<? extends DateWekaInstanceFactory> dwifClass = dwif.getClass();
-
-		HashSet<String> attributes = getAttributeNames();
-
-		for (String name : attributes) {
-			Field field = null;
-			try {
-				field = dwifClass.getDeclaredField(name);
-				field.setAccessible(true);
-				Attribute attribute = getAttribute(field, dwif);
-				testAttributeName(attribute, name);
-				testAttributeType(attribute, name);
-			} catch (SecurityException e) {
-				e.printStackTrace();
-			} catch (NoSuchFieldException e) {
-				e.printStackTrace();
-			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
-			}
-			testFieldType(field);
-		}
-	}
-
-	private void testAttributeType(Attribute attribute, String name) {
-		if (getNominalNames().contains(name)) {
-			assertThat(attribute.isNominal(), is(true));
-		}
-		if (getNumericNames().contains(name)) {
-			assertThat(attribute.isNumeric(), is(true));
-		}
-	}
-
-	private void testAttributeName(Attribute attribute, String name) {
-		assertThat(attribute.name(), is(name));
-	}
-
-	private void testFieldType(Field field) {
-		assertThat(field.getGenericType(), is(Attribute.class.getClass()));
-	}
-
-	private HashSet<String> getAttributeNames() {
-		HashSet<String> result = new HashSet<String>();
-		result.addAll(getNumericNames());
-		result.addAll(getNominalNames());
-		return result;
-	}
-
-	private HashSet<String> getNumericNames() {
-		HashSet<String> result = new HashSet<String>();
-		for (int i = 0; i < this.numericAttributes.length; i++) {
-			result.add(numericAttributes[i]);
-		}
-		return result;
-	}
-
-	private HashSet<String> getNominalNames() {
-		HashSet<String> result = new HashSet<String>();
-		for (int i = 0; i < this.nominalAttributes.length; i++) {
-			result.add(nominalAttributes[i]);
-		}
-		return result;
-	}
-
-	private Attribute getAttribute(Field field, Object obj) {
-		try {
-			return ((Attribute) field.get(obj));
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	@Test
-	public void testInstantiate() {
-
-	}
+	
 
 	private ContentDate getTestDate() {
 		ContentDate cDate = new ContentDate("April 01, 2011", "MMMM DD, YYYY");
