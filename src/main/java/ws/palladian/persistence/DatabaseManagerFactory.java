a96a51d1ff4f050c14e2c6e29c7de4cee699eef2
Klemens Muthmann <klemens_muthmann@gmx.de>
2011-05-27 17:46:29 +0000

    made db Manager completely static fixed ordering and calling of db Manager
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2323 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/main/java/ws/palladian/persistence/DatabaseManagerFactory.java
+++ src/main/java/ws/palladian/persistence/DatabaseManagerFactory.java
@@ -18,8 +18,7 @@ import ws.palladian.helper.ConfigHolder;
  * singleton with care to not populate all layers of your application if
  * database access code.
  * 
- * The Factory is able to load new subclasses of {@code DatabaseManager}
- * dynamically if they are on the class path.
+ * The Factory is able to load new subclasses of {@code DatabaseManager} dynamically if they are on the class path.
  * 
  * FIXME: fill out these fucking empty javadocs
  * 
@@ -28,120 +27,91 @@ import ws.palladian.helper.ConfigHolder;
  * @author David Urbansky
  */
 public final class DatabaseManagerFactory {
-	/**
+
+    private final static Map<String, ConnectionManager> connectionManagerRegistry = new ConcurrentHashMap<String, ConnectionManager>();
+
+    private DatabaseManagerFactory() {
+        super();
+    }
+
+    @Deprecated
+    public static <D extends DatabaseManager> D create(Class<D> managerClass) {
+        // The configuration file can be found under
+        // config/palladian.properties.
+        PropertiesConfiguration config = ConfigHolder.getInstance().getConfig();
+
+        String driver = config.getString("db.driver");
+        String jdbcUrl = config.getString("db.jdbcUrl");
+        String username = config.getString("db.username");
+        String password = config.getString("db.password");
+
+        if (driver == null || jdbcUrl == null || username == null) {
+            throw new IllegalStateException("Database properties are missing in Palladian properties.");
+        }
+        if (password == null) {
+            password = "";
+        }
+
+        return create(managerClass, driver, jdbcUrl, username, password);
+    }
+
+    /**
+     * Create a DatabaseManager with configurations from a config file. The
+     * configuration file must at least provide the following fields:
+     * <ul>
+     * <li>db.driver</li>
+     * <li>db.jdbcUrl</li>
+     * <li>db.username</li>
+     * <li>db.password</li>
+     * </ul>
      * 
+     * @param managerClassName The fully qualified name of the DatabaseManager class.
+     * @param config The config file containing the four required fields.
+     * @return A configured DatabaseManager with access to a connection pool
      */
-	private final Map<String, ConnectionManager> connectionManagerRegistry;
+    public static <D extends DatabaseManager> D create(Class<D> managerClass, PropertiesConfiguration config) {
 
-	/**
-	 * The only instance of this manager for the current class loader. This is
-	 * necessary to keep a central registry for {@link ConnectionManager}s so
-	 * that each database connection is represented by only one
-	 * {@code ConnectionManager}. The singleton is initialized directly to make
-	 * it thread safe.
-	 */
-	private static final DatabaseManagerFactory INSTANCE = new DatabaseManagerFactory();
+        String driver = config.getString("db.driver");
+        String jdbcUrl = config.getString("db.jdbcUrl");
+        String username = config.getString("db.username");
+        String password = config.getString("db.password");
 
-	public static DatabaseManagerFactory getInstance() {
-		return INSTANCE;
-	}
+        if (driver == null || jdbcUrl == null || username == null) {
+            throw new IllegalStateException("Database properties are missing in Palladian properties.");
+        }
+        if (password == null) {
+            password = "";
+        }
 
-	/**
-     * 
-     */
-	private DatabaseManagerFactory() {
-		super();
-		connectionManagerRegistry = new ConcurrentHashMap<String, ConnectionManager>();
-	}
+        return create(managerClass, driver, jdbcUrl, username, password);
+    }
 
-	public DatabaseManager create(String managerClassName) {
-		// The configuration file can be found under
-		// config/palladian.properties.
-		PropertiesConfiguration config = ConfigHolder.getInstance().getConfig();
+    public <D extends DatabaseManager> D create(Class<D> managerClass, String jdbcDriverClassName,
+            String jdbcConnectionURL, String username) {
+        return create(managerClass, jdbcDriverClassName, jdbcConnectionURL, username, "");
+    }
 
-		String driver = config.getString("db.driver");
-		String jdbcUrl = config.getString("db.jdbcUrl");
-		String username = config.getString("db.username");
-		String password = config.getString("db.password");
+    public static <D extends DatabaseManager> D create(Class<D> managerClass, String jdbcDriverClassName,
+            String jdbcConnectionUrl, String username, String password) {
+        try {
+            Constructor<D> dbManagerConstructor = managerClass.getDeclaredConstructor(ConnectionManager.class);
+            dbManagerConstructor.setAccessible(true);
+            D ret = dbManagerConstructor.newInstance(getConnectionManager(jdbcDriverClassName, jdbcConnectionUrl,
+                    username, password));
+            return ret;
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Unable to instantiate DatabaseManager", e);
+        }
 
-		if (driver == null || jdbcUrl == null || username == null) {
-			throw new IllegalStateException(
-					"Database properties are missing in Palladian properties.");
-		}
-		if (password == null) {
-			password = "";
-		}
+    }
 
-		return create(managerClassName, driver, jdbcUrl, username, password);
-	}
-
-	/**
-	 * Create a DatabaseManager with configurations from a config file. The
-	 * configuration file must at least provide the following fields:
-	 * <ul>
-	 * <li>db.driver</li>
-	 * <li>db.jdbcUrl</li>
-	 * <li>db.username</li>
-	 * <li>db.password</li>
-	 * </ul>
-	 * 
-	 * @param managerClassName The fully qualified name of the DatabaseManager class.
-	 * @param config The config file containing the four required fields.
-	 * @return A configured DatabaseManager with access to a connection pool 
-	 */
-	public DatabaseManager create(String managerClassName,
-			PropertiesConfiguration config) {
-
-		String driver = config.getString("db.driver");
-		String jdbcUrl = config.getString("db.jdbcUrl");
-		String username = config.getString("db.username");
-		String password = config.getString("db.password");
-
-		if (driver == null || jdbcUrl == null || username == null) {
-			throw new IllegalStateException(
-					"Database properties are missing in Palladian properties.");
-		}
-		if (password == null) {
-			password = "";
-		}
-
-		return create(managerClassName, driver, jdbcUrl, username, password);
-	}
-
-	public DatabaseManager create(String managerClassName,String jdbcDriverClassName, String jdbcConnectionURL,String username) {
-		return create(managerClassName, jdbcDriverClassName, jdbcConnectionURL,username, "");
-	}
-
-	@SuppressWarnings("unchecked")
-	public DatabaseManager create(String managerClassName,String jdbcDriverClassName, String jdbcConnectionUrl,
-			String username, String password) {
-		try {
-			Class<DatabaseManager> databaseManagerClass = (Class<DatabaseManager>) Class
-					.forName(managerClassName);
-			Constructor<DatabaseManager> dbManagerConstructor = databaseManagerClass
-					.getDeclaredConstructor(ConnectionManager.class);
-			dbManagerConstructor.setAccessible(true);
-			DatabaseManager ret = dbManagerConstructor
-					.newInstance(getConnectionManager(jdbcDriverClassName,
-							jdbcConnectionUrl, username, password));
-			return ret;
-		} catch (Exception e) {
-			throw new IllegalArgumentException(
-					"Unable to instantiate DatabaseManager", e);
-		}
-
-	}
-
-	private synchronized Object getConnectionManager(
-			String jdbcDriverClassName, String jdbcConnectionUrl,
-			String username, String password) {
-		ConnectionManager connectionManager = connectionManagerRegistry
-				.get(jdbcConnectionUrl);
-		if (connectionManager == null) {
-			connectionManager = new ConnectionManager(jdbcDriverClassName,
-					jdbcConnectionUrl, username, password);
-			connectionManagerRegistry.put(jdbcConnectionUrl, connectionManager);
-		}
-		return connectionManager;
-	}
+    private static synchronized Object getConnectionManager(String jdbcDriverClassName, String jdbcConnectionUrl,
+            String username, String password) {
+        ConnectionManager connectionManager = connectionManagerRegistry.get(jdbcConnectionUrl);
+        if (connectionManager == null) {
+            connectionManager = new ConnectionManager(jdbcDriverClassName, jdbcConnectionUrl, username, password);
+            connectionManagerRegistry.put(jdbcConnectionUrl, connectionManager);
+        }
+        return connectionManager;
+    }
 }
