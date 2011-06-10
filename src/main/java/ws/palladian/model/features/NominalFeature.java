4e361c7252d28c22a5134e930396632f1ed6c0e4
Klemens Muthmann <klemens_muthmann@gmx.de>
2011-06-10 21:17:48 +0000

    added documentation to pipeline processing components.
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2461 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/main/java/ws/palladian/model/features/NominalFeature.java
+++ src/main/java/ws/palladian/model/features/NominalFeature.java
@@ -1,7 +1,9 @@
 package ws.palladian.model.features;
 
 /**
+ * <p>
  * A convenience class for nominal {@code Feature}s binding {@code T} to a {@code String} value.
+ * </p>
  * 
  * @author Klemens Muthmann
  * @author David Urbansky
@@ -9,7 +11,9 @@ package ws.palladian.model.features;
 public final class NominalFeature extends Feature<String> {
 
     /**
+     * <p>
      * Creates a new {@code NominalFeature} instance with all attributes initialized.
+     * </p>
      * 
      * @param name The {@code FeatureVector} wide unique identifier of this {@code Feature}.
      * @param value The {@code String} value of this {@code Feature}.
--- src/main/java/ws/palladian/preprocessing/PipelineDocument.java
+++ src/main/java/ws/palladian/preprocessing/PipelineDocument.java
@@ -2,53 +2,126 @@ package ws.palladian.preprocessing;
 
 import ws.palladian.model.features.FeatureVector;
 
+/**
+ * <p>
+ * Represents a document processed by a {@link PipelineProcessor}. These documents are the input for pipelines. They
+ * contain the content that is processed by the pipeline and the features extracted from that content as well as some
+ * modified content.
+ * </p>
+ * <p>
+ * This class represents documents as {@code String}s and thus can be used for text documents at the moment only.
+ * </p>
+ * 
+ * @author David Urbansky
+ * @author Klemens Muthmann
+ * @version 1.0
+ * @since 1.0
+ */
 public class PipelineDocument {
 
+    /**
+     * <p>
+     * A vector of all features extracted for this document by some pipeline.
+     * </p>
+     */
     private FeatureVector featureVector;
 
+    /**
+     * <p>
+     * The unmodified original content representing the document.
+     * </p>
+     */
     private String originalContent;
 
+    /**
+     * <p>
+     * The content of this document modified by some {@link PipelineProcessor}.
+     * </p>
+     */
     private String modifiedContent;
 
+    /**
+     * <p>
+     * Creates a new {@code PipelineDocument} with initialized content. This instance is ready to be processed by a
+     * {@link ProcessingPipeline}.
+     * </p>
+     * 
+     * @param originalContent The content of this {@code PipelineDocument}.
+     */
     public PipelineDocument(String originalContent) {
         this.originalContent = originalContent;
         this.modifiedContent = originalContent;
         this.featureVector = new FeatureVector();
     }
 
+    /**
+     * <p>
+     * Provides a special structured representation of a document as used by classifiers or clusterers.
+     * </p>
+     * 
+     * @return A vector of all features extracted for this document by some pipeline.
+     */
     public FeatureVector getFeatureVector() {
         return featureVector;
     }
 
+    /**
+     * <p>
+     * Resets this documents {@code FeatureVector} overwriting all features previously extracted.
+     * </p>
+     * 
+     * @param featureVector The new {@code FeatureVector} of this document.
+     */
     public void setFeatureVector(FeatureVector featureVector) {
         this.featureVector = featureVector;
     }
 
     /**
-     * @return The original content of the document.
+     * <p>
+     * Provides the original content of the processed document as retrieved from the web, the local file system or any
+     * other source providing documents.
+     * </p>
+     * 
+     * @return The unmodified original content representing the document.
      */
     public String getOriginalContent() {
         return originalContent;
     }
 
+    /**
+     * <p>
+     * Resets this documents content completely overwriting any previous original content.
+     * </p>
+     * 
+     * @param originalContent The new unmodified original content representing the document.
+     */
     public void setOriginalContent(String originalContent) {
         this.originalContent = originalContent;
     }
 
     /**
-     * @return The modified content of the document.
+     * <p>
+     * Provides the modified content of this document. Modified content is usually inserted by {@link PipelineProcessor}
+     * s.
+     * </p>
+     * 
+     * @return The modified content of the document or {@code null} if no modified content is available yet.
      */
     public String getModifiedContent() {
         return modifiedContent;
     }
 
+    /**
+     * <p>
+     * Resets the modified content completely overwriting any old modified content.
+     * </p>
+     * 
+     * @param modifiedContent The content of this document modified by some {@link PipelineProcessor}.
+     */
     public void setModifiedContent(String modifiedContent) {
         this.modifiedContent = modifiedContent;
     }
 
-    /* (non-Javadoc)
-     * @see java.lang.Object#toString()
-     */
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
@@ -61,7 +134,5 @@ public class PipelineDocument {
         builder.append("]");
         return builder.toString();
     }
-    
-    
 
 }
