ec51007849ca35e44ed878552a07fc9b8ac901e7
Sandro Seichert <sandro.reichert@gmail.com>
2011-07-03 02:38:33 +0000

    Helper to get date from http header
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2515 5654db93-19e5-4567-beb8-64e8c25c7c45

---
+++ src/main/java/ws/palladian/helper/HTTPHelper.java
@@ -0,0 +1,43 @@
+package ws.palladian.helper;
+
+import java.util.Date;
+
+import org.apache.http.impl.cookie.DateParseException;
+import org.apache.http.impl.cookie.DateUtils;
+import org.apache.log4j.Logger;
+
+import ws.palladian.retrieval.HttpResult;
+
+/**
+ * Some HTTP specific helper methods
+ * 
+ * @author Sandro Reichert
+ * 
+ */
+public class HTTPHelper {
+
+    /** The logger for this class. */
+    public static final Logger LOGGER = Logger.getLogger(HTTPHelper.class);
+
+    /**
+     * Get a date from http header.
+     * 
+     * @param httpResult The {@link HttpResult} to get the date from.
+     * @param headerName The name of the header field to get.
+     * @return The extracted date or <code>null</code> if the given header name is not present or the date is invalid.
+     */
+    public static final Date getDateFromHeader(HttpResult httpResult, String headerName) {
+        String dateString = httpResult.getHeaderString(headerName);
+        Date date = null;
+        if (dateString != null && !dateString.isEmpty()) {
+            try {
+                date = DateUtils.parseDate(dateString);
+            } catch (DateParseException e) {
+                LOGGER.error("Could nor parse http header value for " + headerName + ": \"" + dateString + "\". "
+                        + e.getMessage());
+            }
+        }
+        return date;
+    }
+
+}
