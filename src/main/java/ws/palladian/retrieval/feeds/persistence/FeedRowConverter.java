1a9f3332b255536beca69004899ee3b79e5aa152
Sandro Seichert <sandro.reichert@gmail.com>
2011-07-07 22:23:32 +0000

    * fix: reading Integer and Boolean from database more securely * fix: remove control characters
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2569 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/main/java/ws/palladian/retrieval/feeds/persistence/FeedRowConverter.java
+++ src/main/java/ws/palladian/retrieval/feeds/persistence/FeedRowConverter.java
@@ -3,6 +3,7 @@ package ws.palladian.retrieval.feeds.persistence;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import ws.palladian.helper.SQLHelper;
 import ws.palladian.persistence.RowConverter;
 import ws.palladian.retrieval.feeds.Feed;
 
@@ -11,11 +12,6 @@ public class FeedRowConverter implements RowConverter<Feed> {
     @Override
     public Feed convert(ResultSet resultSet) throws SQLException {
 
-        /*
-         * Caution! Using (Integer) resultSet.getObject() instead of getInt() since getInt() returns 0 if database
-         * contains NULL, but we need to differentiate between 0 and NULL, e.g. empty window or not a feed.
-         * This is a bit dangerous since the db field MUST NOT be UNSIGNED! --Sandro 01.07.2011
-         */
 
         Feed feed = new Feed();
         feed.setId(resultSet.getInt("id"));
@@ -25,37 +21,37 @@ public class FeedRowConverter implements RowConverter<Feed> {
         feed.setUnparsableCount(resultSet.getInt("unparsableCount"));
         feed.setMisses(resultSet.getInt("misses"));
         feed.setNumberOfItemsReceived(resultSet.getInt("totalItems"));
-        feed.setWindowSize((Integer) resultSet.getObject("windowSize"));
-        feed.setVariableWindowSize((Boolean) resultSet.getObject("hasVariableWindowSize"));
-        feed.setUpdateInterval((Integer) resultSet.getObject("checkInterval"));
+        feed.setWindowSize(SQLHelper.getInteger(resultSet, "windowSize"));
+        feed.setVariableWindowSize(SQLHelper.getBoolean(resultSet, "hasVariableWindowSize"));
+        feed.setUpdateInterval(SQLHelper.getInteger(resultSet, "checkInterval"));
         feed.setLastPollTime(resultSet.getTimestamp("lastPollTime"));
         feed.setLastSuccessfulCheckTime(resultSet.getTimestamp("lastSuccessfulCheck"));
         feed.setLastMissTime(resultSet.getTimestamp("lastMissTimestamp"));
         feed.setLastFeedEntry(resultSet.getTimestamp("lastFeedEntry"));
-        feed.getMetaInformation().setAccessible((Boolean) resultSet.getObject("isAccessibleFeed"));
+        feed.getMetaInformation().setAccessible(SQLHelper.getBoolean(resultSet, "isAccessibleFeed"));
         feed.setBlocked(resultSet.getBoolean("blocked"));
         feed.setTotalProcessingTime(resultSet.getLong("totalProcessingTime"));
         feed.setNewestItemHash(resultSet.getString("newestItemHash"));
         feed.setLastETag(resultSet.getString("lastEtag"));
         feed.setHttpLastModified(resultSet.getTimestamp("lastModified"));
         feed.setLastFeedTaskResult(resultSet.getString("lastResult"));
-        feed.setActivityPattern((Integer) resultSet.getObject("activityPattern"));
+        feed.setActivityPattern(SQLHelper.getInteger(resultSet, "activityPattern"));
         feed.getMetaInformation().setFeedFormat(resultSet.getString("feedFormat"));
         feed.getMetaInformation().setByteSize(resultSet.getLong("feedSize"));
         feed.getMetaInformation().setSiteUrl(resultSet.getString("siteUrl"));
         feed.getMetaInformation().setTitle(resultSet.getString("title"));
         feed.getMetaInformation().setAdded(resultSet.getTimestamp("added"));
         feed.getMetaInformation().setLanguage(resultSet.getString("language"));
-        feed.getMetaInformation().setHasItemIds((Boolean) resultSet.getObject("hasItemIds"));
-        feed.getMetaInformation().setHasPubDate((Boolean) resultSet.getObject("hasPubDate"));
-        feed.getMetaInformation().setHasCloud((Boolean) resultSet.getObject("hasCloud"));
-        feed.getMetaInformation().setTtl((Integer) resultSet.getObject("ttl"));
-        feed.getMetaInformation().setHasSkipHours((Boolean) resultSet.getObject("hasSkipHours"));
-        feed.getMetaInformation().setHasSkipDays((Boolean) resultSet.getObject("hasSkipDays"));
-        feed.getMetaInformation().setHasUpdated((Boolean) resultSet.getObject("hasUpdated"));
-        feed.getMetaInformation().setHasPublished((Boolean) resultSet.getObject("hasPublished"));
-        feed.getMetaInformation().setSupportsPubSubHubBub((Boolean) resultSet.getObject("supportsPubSubHubBub"));
-        feed.getMetaInformation().setCgHeaderSize((Integer) resultSet.getObject("httpHeaderSize"));
+        feed.getMetaInformation().setHasItemIds(SQLHelper.getBoolean(resultSet, "hasItemIds"));
+        feed.getMetaInformation().setHasPubDate(SQLHelper.getBoolean(resultSet, "hasPubDate"));
+        feed.getMetaInformation().setHasCloud(SQLHelper.getBoolean(resultSet, "hasCloud"));
+        feed.getMetaInformation().setTtl(SQLHelper.getInteger(resultSet, "ttl"));
+        feed.getMetaInformation().setHasSkipHours(SQLHelper.getBoolean(resultSet, "hasSkipHours"));
+        feed.getMetaInformation().setHasSkipDays(SQLHelper.getBoolean(resultSet, "hasSkipDays"));
+        feed.getMetaInformation().setHasUpdated(SQLHelper.getBoolean(resultSet, "hasUpdated"));
+        feed.getMetaInformation().setHasPublished(SQLHelper.getBoolean(resultSet, "hasPublished"));
+        feed.getMetaInformation().setSupportsPubSubHubBub(SQLHelper.getBoolean(resultSet, "supportsPubSubHubBub"));
+        feed.getMetaInformation().setCgHeaderSize(SQLHelper.getInteger(resultSet, "httpHeaderSize"));
         
         return feed;
 
