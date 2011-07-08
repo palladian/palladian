530be6c2e8921df0afab86a12f8a471d4bb7d98f
Philipp Katz <philipp@philippkatz.de>
2011-07-08 09:32:58 +0000

    minor modifications and one TODO in parser package
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2579 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/main/java/ws/palladian/retrieval/parser/DocumentParser.java
+++ src/main/java/ws/palladian/retrieval/parser/DocumentParser.java
@@ -5,12 +5,51 @@ import java.io.InputStream;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 
+import ws.palladian.retrieval.DocumentRetriever;
 import ws.palladian.retrieval.HttpResult;
 
+/**
+ * <p>
+ * Interface for document parsers. A {@link DocumentParser} takes various types of inputs and produces a
+ * {@link Document}.
+ * </p>
+ * 
+ * @author Philipp Katz
+ */
 public interface DocumentParser {
-    
+
+    /**
+     * <p>
+     * Parse a {@link Document} from the provided {@link InputSource}.
+     * </p>
+     * 
+     * @param inputSource
+     * @return
+     * @throws ParserException In case, parsing fails.
+     */
     Document parse(InputSource inputSource) throws ParserException;
+
+    /**
+     * <p>
+     * Parse a {@link Document} from the provided {@link InputStream}.
+     * </p>
+     * 
+     * @param inputStream
+     * @return
+     * @throws ParserException In case, parsing fails.
+     */
     Document parse(InputStream inputStream) throws ParserException;
+
+    /**
+     * <p>
+     * Parse a {@link Document} from the provided {@link HttpResult}, which can be obtained via
+     * {@link DocumentRetriever}.
+     * </p>
+     * 
+     * @param httpResult
+     * @return
+     * @throws ParserException In case, parsing fails.
+     */
     Document parse(HttpResult httpResult) throws ParserException;
 
 }
