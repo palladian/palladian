fe2f96581bb37bd7ab36b8570d7497edac3fab7c
Philipp Katz <philipp@philippkatz.de>
2011-07-08 09:32:58 +0000

    minor modifications and one TODO in parser package
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2579 5654db93-19e5-4567-beb8-64e8c25c7c45
    
    Former-commit-id: 530be6c2e8921df0afab86a12f8a471d4bb7d98f

--- src/main/java/ws/palladian/retrieval/parser/BaseDocumentParser.java
+++ src/main/java/ws/palladian/retrieval/parser/BaseDocumentParser.java
@@ -1,12 +1,26 @@
 package ws.palladian.retrieval.parser;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 import ws.palladian.retrieval.HttpResult;
 
+/**
+ * <p>
+ * Base implementation for document parsers.
+ * </p>
+ * 
+ * @author Philipp Katz
+ */
 public abstract class BaseDocumentParser implements DocumentParser {
+
+    @Override
+    public Document parse(InputStream inputStream) throws ParserException {
+        return parse(new InputSource(inputStream));
+    }
 
     @Override
     public Document parse(HttpResult httpResult) throws ParserException {
--- src/main/java/ws/palladian/retrieval/parser/NekoHtmlParser.java
+++ src/main/java/ws/palladian/retrieval/parser/NekoHtmlParser.java
@@ -1,7 +1,6 @@
 package ws.palladian.retrieval.parser;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 import org.apache.xerces.xni.parser.XMLDocumentFilter;
 import org.cyberneko.html.parsers.DOMParser;
@@ -11,16 +10,23 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXNotRecognizedException;
 import org.xml.sax.SAXNotSupportedException;
 
-
+/**
+ * <p>
+ * Wrapper for CyberNeko HTML Parser.
+ * </p>
+ * 
+ * @see http://nekohtml.sourceforge.net/
+ * @author Philipp Katz
+ */
 public class NekoHtmlParser extends BaseDocumentParser implements DocumentParser {
 
     @Override
     public Document parse(InputSource inputSource) throws ParserException {
-        
+
         DOMParser parser = new DOMParser();
-        
+
         try {
-            
+
             // experimental fix for http://redmine.effingo.de/issues/5
             // also see: tud.iir.web.CrawlerTest.testNekoWorkarounds()
             // Philipp, 2010-11-10
@@ -30,11 +36,16 @@ public class NekoHtmlParser extends BaseDocumentParser implements DocumentParser
             // catching Throwable in #setDocument above; guess we have to wait for a new Neko release,
             // supposedly breaking other stuff :(
             parser.setFeature("http://cyberneko.org/html/features/insert-namespaces", true);
-            parser.setProperty("http://cyberneko.org/html/properties/filters", new XMLDocumentFilter[] { new TBODYFix() });
+            parser.setProperty("http://cyberneko.org/html/properties/filters",
+                    new XMLDocumentFilter[] { new TBODYFix() });
             // end fix.
-            
+
+            // TODO
+            // http://nekohtml.sourceforge.net/faq.html#uppercase
+            // parser.setProperty("http://cyberneko.org/html/properties/names/elems", "lower")
+
             parser.parse(inputSource);
-            
+
         } catch (SAXNotRecognizedException e) {
             throw new ParserException(e);
         } catch (SAXNotSupportedException e) {
@@ -47,14 +58,9 @@ public class NekoHtmlParser extends BaseDocumentParser implements DocumentParser
             // dirty workaround, as NekoHTML produces many kind of unexpected exceptions
             throw new ParserException(t);
         }
-        
-        return parser.getDocument();
-        
-    }
 
-    @Override
-    public Document parse(InputStream inputStream) throws ParserException {
-        return parse(new InputSource(inputStream));
+        return parser.getDocument();
+
     }
 
 }
