9d37bbcfc4114027e93e2959fc6fe2972d73b322
Philipp Katz <philipp@philippkatz.de>
2011-09-01 18:58:37 +0000

    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2759 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/main/java/ws/palladian/retrieval/feeds/parser/FeedParser.java
+++ src/main/java/ws/palladian/retrieval/feeds/parser/FeedParser.java
@@ -8,10 +8,21 @@ import org.w3c.dom.Document;
 import ws.palladian.retrieval.HttpResult;
 import ws.palladian.retrieval.feeds.Feed;
 
+/**
+ * <p>
+ * A {@link FeedParser} takes various input data, like {@link File}s, {@link HttpResult}s, or {@link InputStream}s and
+ * parses them as RSS or Atom feeds, generating an instance of {@link Feed}.
+ * </p>
+ * 
+ * @author Philipp Katz
+ */
 public interface FeedParser {
 
     /**
-     * <p>Retrieve a feed from a gzipped {@link HttpResult}. These gzips are available for example in the <a href="http://areca.co/8/Feed-Item-Dataset-TUDCS5">TUD-CS5</a> feed dataset.</p>
+     * <p>
+     * Retrieve a feed from a gzipped {@link HttpResult}. These gzips are available for example in the <a
+     * href="http://areca.co/8/Feed-Item-Dataset-TUDCS5">TUD-CS5</a> feed dataset.
+     * </p>
      * 
      * @param file The file with the RSS or Atom feed.
      * @param serializedGzip If true, the feed will be read from a serialized and gzipped {@link HttpResult}.
@@ -21,7 +32,9 @@ public interface FeedParser {
     Feed getFeed(File file, boolean serializedGzip) throws FeedParserException;
 
     /**
-     * <p>Retrieve a feed from local file system.</p>
+     * <p>
+     * Retrieve a feed from local file system.
+     * </p>
      * 
      * @param file The file with the RSS or Atom feed.
      * @return The parsed feed.
@@ -30,7 +43,9 @@ public interface FeedParser {
     Feed getFeed(File file) throws FeedParserException;
 
     /**
+     * <p>
      * Downloads a feed from the specified URL.
+     * </p>
      * 
      * @param feedUrl the URL to the RSS or Atom feed.
      * @return
@@ -39,7 +54,9 @@ public interface FeedParser {
     Feed getFeed(String feedUrl) throws FeedParserException;
 
     /**
-     * <p>Parse a feed from the specified {@link HttpResult}.</p>
+     * <p>
+     * Parse a feed from the specified {@link HttpResult}.
+     * </p>
      * 
      * @param httpResult The httpResult from the request.
      * @return The parsed feed.
@@ -48,14 +65,25 @@ public interface FeedParser {
     Feed getFeed(HttpResult httpResult) throws FeedParserException;
 
     /**
-     * Returns a feed from the specified Document.
+     * <p>
+     * Returns a feed from the specified {@link Document}.
+     * </p>
      * 
      * @param document the Document containing the RSS or Atom feed.
      * @return
      * @throws FeedParserException
      */
     Feed getFeed(Document document) throws FeedParserException;
-    
+
+    /**
+     * <p>
+     * Parse a feed from the specified {@link InputStream}.
+     * </p>
+     * 
+     * @param inputStream
+     * @return
+     * @throws FeedParserException
+     */
     Feed getFeed(InputStream inputStream) throws FeedParserException;
 
 }
