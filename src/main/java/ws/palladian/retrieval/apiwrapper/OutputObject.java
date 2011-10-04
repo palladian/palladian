f0351c5ef0f504f29576d28c62087a385b71e108
David Urbansky <davidurbansky@gmail.com>
2011-10-04 14:34:35 +0000

    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2802 5654db93-19e5-4567-beb8-64e8c25c7c45
    Former-commit-id: 103394b84a721fab2e838ff993498411154e5f94

--- src/main/java/ws/palladian/retrieval/apiwrapper/OutputObject.java
+++
@@ -1,7 +0,0 @@
-package ws.palladian.retrieval.apiwrapper;
-
-import java.util.HashSet;
-
-class OutputObject {
-    public HashSet<ParameterBinding> parameterBindings = new HashSet<ParameterBinding>();
-}
