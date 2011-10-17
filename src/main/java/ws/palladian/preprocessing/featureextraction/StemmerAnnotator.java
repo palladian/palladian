50af7ef99fbda29f30a506a1792c5c480d1be806
David Urbansky <davidurbansky@gmail.com>
2011-10-17 07:08:52 +0000

    added knime open source day slides
    git-svn-id: https://141.76.40.86/svn-students/iircommon/toolkit/trunk@2885 5654db93-19e5-4567-beb8-64e8c25c7c45

--- src/main/java/ws/palladian/preprocessing/featureextraction/StemmerAnnotator.java
+++ src/main/java/ws/palladian/preprocessing/featureextraction/StemmerAnnotator.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.tartarus.snowball.SnowballStemmer;
 import org.tartarus.snowball.ext.englishStemmer;
+import org.tartarus.snowball.ext.germanStemmer;
 
 import ws.palladian.model.features.FeatureVector;
 import ws.palladian.model.features.NominalFeature;
