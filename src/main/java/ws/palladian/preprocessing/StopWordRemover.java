6128eababf262a4d0ad4036f537f60a60510c9f9
David Urbansky <davidurbansky@gmail.com>
2011-11-15 01:04:35 +0100

    simplified parts of the StringHelper

--- src/main/java/ws/palladian/preprocessing/StopWordRemover.java
+++ src/main/java/ws/palladian/preprocessing/StopWordRemover.java
@@ -2,14 +2,19 @@ package ws.palladian.preprocessing;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import ws.palladian.helper.FileHelper;
 
 public class StopWordRemover implements PipelineProcessor {
 
+    /** The logger for this class. */
+    private static final Logger LOGGER = Logger.getLogger(StopWordRemover.class);
+    
     private static final long serialVersionUID = 5014188120999997379L;
 
     public String removeStopWords(String text) {
-    	List<String> stopWords = FileHelper.readFileToArray(StopWordRemover.class.getResource("/stopwords_en.txt"));
+        List<String> stopWords = FileHelper.readFileToArray(StopWordRemover.class.getResource("/stopwords_en.txt"));
 
     	for (String stopWord : stopWords) {
 
@@ -25,10 +30,14 @@ public class StopWordRemover implements PipelineProcessor {
     }
     
     @Override
-    public void process(PipelineDocument document) {       
+    public void process(PipelineDocument document) {
         String content = document.getModifiedContent();
 
-        content = removeStopWords(content);
+        // try {
+            content = removeStopWords(content);
+        // } catch (FileNotFoundException e) {
+        // LOGGER.fatal("could not remove stop words, " + e.getMessage());
+        // }
 
         document.setModifiedContent(content);
     }
