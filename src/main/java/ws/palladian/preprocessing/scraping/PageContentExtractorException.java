59157d1126ede77c35aa15d15a2c4889b5f7130d
Philipp Katz <philipp@philippkatz.de>
2011-12-08 19:48:56 +0100

    some fixes when parsing (X)HTML documents for NekoHtmlParser, general clean up for scraping classes, fixing errors in ReadabilityContentExtractor, which prevented processing of pages with incorrect namespace prefixes.

--- src/main/java/ws/palladian/preprocessing/scraping/PageContentExtractorException.java
+++ src/main/java/ws/palladian/preprocessing/scraping/PageContentExtractorException.java
@@ -2,24 +2,22 @@ package ws.palladian.preprocessing.scraping;
 
 public class PageContentExtractorException extends Exception {
 
-    private static final long serialVersionUID = -5723919183701989944L;
+    private static final long serialVersionUID = 1L;
 
     public PageContentExtractorException() {
-        // TODO Auto-generated constructor stub
+        super();
     }
 
-    public PageContentExtractorException(Throwable t) {
-        super(t);
+    public PageContentExtractorException(Throwable throwable) {
+        super(throwable);
     }
 
-    public PageContentExtractorException(String message, Throwable cause) {
-        super(message, cause);
-        // TODO Auto-generated constructor stub
+    public PageContentExtractorException(String message, Throwable throwable) {
+        super(message, throwable);
     }
 
     public PageContentExtractorException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
     }
 
 }
