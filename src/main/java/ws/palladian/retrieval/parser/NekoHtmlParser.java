e77eeae4486d4335a363e9f3db9507472a2d8ea3
Philipp Katz <philipp@philippkatz.de>
2012-03-09 17:12:01 +0100

    Fix for missing namespaces for elements inserted by NekoHTML (ref #29), breaks tbody fix and others.
    Former-commit-id: 737c8d77126fbaebffac51da7f75fd1ad208daf0

--- src/main/java/ws/palladian/retrieval/parser/NekoHtmlParser.java
+++ src/main/java/ws/palladian/retrieval/parser/NekoHtmlParser.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.xerces.parsers.DOMParser;
 import org.apache.xerces.xni.parser.XMLDocumentFilter;
 import org.cyberneko.html.HTMLConfiguration;
+import org.cyberneko.html.HTMLTagBalancerFixed;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -41,13 +42,21 @@ public class NekoHtmlParser extends BaseDocumentParser implements DocumentParser
             // catching Throwable in #setDocument above; guess we have to wait for a new Neko release,
             // supposedly breaking other stuff :(
             parser.setFeature("http://cyberneko.org/html/features/insert-namespaces", true);
-            
-            NekoTbodyFix tbodyFix = new NekoTbodyFix();
+
+            // NekoTbodyFix tbodyFix = new NekoTbodyFix();
             // end fix.
 
-            XMLDocumentFilter[] filters = new XMLDocumentFilter[] { tbodyFix, new PreflightFilter() };
+            // XMLDocumentFilter[] filters = new XMLDocumentFilter[] { tbodyFix, new PreflightFilter() };
+            // parser.setProperty("http://cyberneko.org/html/properties/filters", filters);
+
+            // fix for issue where inserted elements did not get the correct namespace
+            // implemented in HTMLTagBalancerFixed for now. For more information see
+            // https://bitbucket.org/palladian/palladian/issue/29/tr-fix-for-neko-html
+            // https://sourceforge.net/tracker/?func=detail&aid=3151253&group_id=195122&atid=952178
+            parser.setFeature("http://cyberneko.org/html/features/balance-tags", false);
+            XMLDocumentFilter[] filters = { new HTMLTagBalancerFixed(), new PreflightFilter() };
             parser.setProperty("http://cyberneko.org/html/properties/filters", filters);
-            
+
             // per dafault, assume UTF-8 encoding (makes more sense than Windows-1252 default)
             parser.setProperty("http://cyberneko.org/html/properties/default-encoding", "UTF-8");
 
