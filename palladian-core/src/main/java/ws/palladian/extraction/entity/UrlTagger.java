c7d6ae85198c80a657dff01c5606b49a9ea12d89
Philipp Katz <philipp@philippkatz.de>
2012-04-03 19:54:22 +0200

    q'n'd clean up of UrlTagger
    Former-commit-id: 4ac5613cf61a0edef6ca1a70c66cfaa7750552d6

--- palladian-core/src/main/java/ws/palladian/extraction/entity/UrlTagger.java
+++ palladian-core/src/main/java/ws/palladian/extraction/entity/UrlTagger.java
@@ -10,17 +10,28 @@ import java.util.regex.Pattern;
  * TODO also recognize "cinefreaks.com" (without www and http)
  * 
  * @author David Urbansky
+ * @author Philipp Katz
  */
 public class UrlTagger {
 
 	/** The tag name for URLs. */
 	public static final String URI_TAG_NAME = "URI";
 	
+	// http://www.faqs.org/rfcs/rfc1738.html
+	
+	private static final String DOMAIN_ALLOWED_CHARACTERS = "[^;/?:@=&\\s]";
+	private static final String PATH_ALLOWED_CHARACTERS = "[^\\s]";
+	private static final String NEGLECTED_ENDINGS = "[.,?!;\\[\\](){}\"\\s]";
+	
+	// TODO to extend, see http://en.wikipedia.org/wiki/List_of_Internet_top-level_domains
+	private static final String TOP_LEVEL_DOMAINS = "(de|com|cc|tv|us|net|org|gov|mil|edu|fr|it|com.au|co.uk|ws)";
+	
 	/** The URL regular expression. */
 //	private static final String URL_REGEXP = "((http://|https://|www.).*?(?=[.,;?!]?(\\s|\\]|\\))|[.,;?!]?$))|([A-Za-z.0-9-]*?\\.(de|com|cc|tv|us|net|org|gov|mil|edu|fr|it|com.au|co.uk)[/A-Za-z0-9-]*(\\.[A-Za-z]{2,5})?)";
 //    private static final String URL_REGEXP = "((http://|https://|www.).{0,63}?(?=[.,;?!]?(\\s|\\]|\\))|[.,;?!]?$))|([A-Za-z.0-9-]{0,63}?\\.(de|com|cc|tv|us|net|org|gov|mil|edu|fr|it|com.au|co.uk)[/A-Za-z0-9-\\?=&\\+]{0,1024}(\\.[A-Za-z]{2,5})?)";
 //	private static final String URL_REGEXP = "(http(s)?://)?([A-Za-z0-9-.]*?\\.(de|com|cc|tv|us|net|org|gov|mil|edu|fr|it|com.au|co.uk)((/[/A-Za-z0-9-?=&+]*(\\.[A-Za-z?]{2,5})?)|(?=(\\s|\\.|\\)|\\]|\\?|\\!|\\,))))";
-	private static final String URL_REGEXP = "(http(s)?://)?([A-Za-z0-9-.]{0,63}?\\.(de|com|cc|tv|us|net|org|gov|mil|edu|fr|it|com.au|co.uk)((/[/A-Za-z0-9-?=&+]{0,255}(\\.[A-Za-z?]{2,5})?)|(?=(\\s|\\.|\\)|\\]|\\?|\\!|\\,))))";
+//	private static final String URL_REGEXP = "(http(s)?://)?([A-Za-z0-9-.]{0,63}?\\.(de|com|cc|tv|us|net|org|gov|mil|edu|fr|it|com.au|co.uk)((/[/A-Za-z0-9-?=&+]{0,255}(\\.[A-Za-z?]{2,5})?)|(?=(\\s|\\.|\\)|\\]|\\?|\\!|\\,))))";
+	private static final String URL_REGEXP = "(http(s)?://)?("+DOMAIN_ALLOWED_CHARACTERS+"{0,63}?\\."+TOP_LEVEL_DOMAINS+"((/"+PATH_ALLOWED_CHARACTERS+"{0,255}(\\.[A-Za-z?]{2,5})?)|(?="+NEGLECTED_ENDINGS+")))";
 
 	private final Pattern urlPattern;
 
