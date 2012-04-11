85b85a0b83c26effe6259de84baec91453c704dd
Philipp Katz <philipp@philippkatz.de>
2012-04-11 14:32:08 +0200

    Move UrlHelperTest to correct project.

--- palladian-commons/src/test/java/ws/palladian/helper/UrlHelperTest.java
+++ palladian-commons/src/test/java/ws/palladian/helper/UrlHelperTest.java
@@ -1,4 +1,4 @@
-package ws.palladian.retrieval.helper;
+package ws.palladian.helper;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -6,8 +6,6 @@ import static org.junit.Assert.fail;
 import java.util.List;
 
 import org.junit.Test;
-
-import ws.palladian.helper.UrlHelper;
 
 public class UrlHelperTest {
 
