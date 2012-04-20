031bc5fbda7c19d4f3ef89cac11d8a6743bebd80
Philipp Katz <philipp@philippkatz.de>
2012-04-20 16:03:50 +0200

    Fix, comment, and improve PhrasenessAnnotator.
    Former-commit-id: 1a9fa826a9aba9adebd6c672e37aa2fbc70c09c5

--- palladian-core/src/main/java/ws/palladian/extraction/keyphrase/features/PhrasenessAnnotator.java
+++ palladian-core/src/main/java/ws/palladian/extraction/keyphrase/features/PhrasenessAnnotator.java
@@ -9,16 +9,29 @@ import ws.palladian.extraction.token.TokenizerInterface;
 import ws.palladian.model.features.Annotation;
 import ws.palladian.model.features.AnnotationFeature;
 import ws.palladian.model.features.AnnotationGroup;
+import ws.palladian.model.features.FeatureDescriptor;
+import ws.palladian.model.features.FeatureDescriptorBuilder;
 import ws.palladian.model.features.FeatureVector;
 import ws.palladian.model.features.NumericFeature;
 
 /**
  * <p>
- * Calculates the "Generalized Dice Coefficient" for {@link AnnotationGroup}s consisting of multiple Tokens based on
- * their occurence counts. Groups of tokens with high co-occurence frequency are weighted highly. See
+ * Calculates a <i>Generalized Dice Coefficient</i> for {@link Annotation}s consisting of multiple Tokens based on their
+ * occurrence counts. Groups of tokens with high co-occurrence frequencies are weighted highly. See
  * "Automatic Glossary Extraction: Beyond Terminology Identification"; Youngja Park, Roy J Byrd, Branimir K Boguraev,
  * 2002; and "HUMB: Automatic Key Term Extraction from Scientific Articles in GROBID"; Patrice Lopez, Laurent Romary,
- * 2010.
+ * 2010. The Generalized Dice Coefficient is defined as:
+ * 
+ * <pre>
+ *  |T| * log10(f(T)) * f(T)
+ * ——————————————————————————
+ *   sum_(w_i in T) f(w_i)
+ * </pre>
+ * 
+ * Where <code>|T|</code> is the number of words in Term <code>T</code>, <code>f(T)</code> is the count of Term
+ * <code>T</code>, <code>f(w_i)</code> is the frequency of word <code>w_i</code>. As the weight of single word terms
+ * only depends on their own frequencies and therefore results in much bigger results than for multi-word terms, these
+ * terms are scaled down to a certain fraction, usually 0.1.
  * </p>
  * 
  * @author Philipp Katz
@@ -26,53 +39,75 @@ import ws.palladian.model.features.NumericFeature;
 public class PhrasenessAnnotator implements PipelineProcessor {
 
     private static final long serialVersionUID = 1L;
-    public static final String PROVIDED_FEATURE = "phraseness";
 
-    private static final double SINGLE_WORD_TERM_FACTOR = 0.1;
+    /** The {@link FeatureDescriptor} describing the Generalized Dice Coefficient annotated by this component. */
+    public static final FeatureDescriptor<NumericFeature> GENERALIZED_DICE = FeatureDescriptorBuilder.build(
+            "ws.palladian.features.generalizedDice", NumericFeature.class);
+
+    /** Default factor for weighting single word terms, as suggested by Park, Byrd, Boguraev. */
+    private static final double DEFAULT_SINGLE_WORD_TERM_FACTOR = 0.1;
+
+    /** The defined factor used for scaling single word terms. */
+    private final double singleWordTermFactor;
+
+    /**
+     * <p>
+     * Create a new {@link PhrasenessAnnotator} with the specified factor for weighting single word terms.
+     * </p>
+     * 
+     * @param singleWordTermFactor The factor with which to weight single word phrases.
+     */
+    public PhrasenessAnnotator(double singleWordTermFactor) {
+        this.singleWordTermFactor = singleWordTermFactor;
+    }
+
+    /**
+     * <p>
+     * Create a new {@link PhrasenessAnnotator} with the default factor for weighting single word terms (0.1).
+     * </p>
+     */
+    public PhrasenessAnnotator() {
+        this(DEFAULT_SINGLE_WORD_TERM_FACTOR);
+    }
 
     @Override
     public void process(PipelineDocument document) {
         FeatureVector featureVector = document.getFeatureVector();
-        AnnotationFeature annotationFeature = (AnnotationFeature) featureVector.get(TokenizerInterface.PROVIDED_FEATURE_DESCRIPTOR);
+        AnnotationFeature annotationFeature = featureVector.get(TokenizerInterface.PROVIDED_FEATURE_DESCRIPTOR);
         List<Annotation> annotations = annotationFeature.getValue();
 
         for (Annotation annotation : annotations) {
-            NumericFeature phrasenessFeature;
+            NumericFeature generalizedDiceFeature;
+            double phraseCount = getCount(annotation);
+
             if (annotation instanceof AnnotationGroup) {
-                AnnotationGroup group = (AnnotationGroup) annotation;
-
+                AnnotationGroup group = (AnnotationGroup)annotation;
                 int numberOfTerms = group.getAnnotations().size();
-                double phraseCount = getCount(group);
                 double wordCount = getCount(group.getAnnotations());
-                double diceCoefficient = (numberOfTerms * Math.log10(phraseCount) * phraseCount) / wordCount;
-
-                phrasenessFeature = new NumericFeature(PROVIDED_FEATURE, diceCoefficient);
+                double generalizedDice = (numberOfTerms * Math.log10(phraseCount) * phraseCount) / wordCount;
+                generalizedDiceFeature = new NumericFeature(GENERALIZED_DICE, generalizedDice);
             } else {
-
-                double phraseCount = getCount(annotation);
-                double diceCoefficient = Math.log10(phraseCount) * phraseCount;
-
-                phrasenessFeature = new NumericFeature(PROVIDED_FEATURE, SINGLE_WORD_TERM_FACTOR * diceCoefficient);
+                double generalizedDice = Math.log10(phraseCount);
+                generalizedDiceFeature = new NumericFeature(GENERALIZED_DICE, singleWordTermFactor * generalizedDice);
             }
-            annotation.getFeatureVector().add(phrasenessFeature);
+            annotation.getFeatureVector().add(generalizedDiceFeature);
         }
     }
 
     private double getCount(List<Annotation> annotations) {
-        double frequencySum = 0;
+        double count = 0;
         for (Annotation annotation : annotations) {
-            frequencySum += getCount(annotation);
+            count += getCount(annotation);
         }
-        return frequencySum;
+        return count;
     }
 
     private double getCount(Annotation annotation) {
-        if (annotation.getFeatureVector().get(TokenMetricsCalculator.FREQUENCY) == null) {
-            // System.err.println("something is wrong for " + annotation);
-            return 1;
+        NumericFeature countFeature = annotation.getFeatureVector().get(TokenMetricsCalculator.COUNT);
+        if (countFeature == null) {
+            throw new IllegalStateException("Expected feature \"" + TokenMetricsCalculator.COUNT + "\" is missing.");
         }
-        double frequency = (Double) annotation.getFeatureVector().get(TokenMetricsCalculator.COUNT).getValue();
-        return frequency;
+        return countFeature.getValue();
     }
 
 }
