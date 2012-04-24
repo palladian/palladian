22d14610c5589556072fb22b1fdc5de0b9a38c8e
Klemens Muthmann <klemens_muthmann@gmx.de>
2012-04-24 17:44:47 +0200

    Merged HtmlCleaner feature branch which also contains a small refactoring on how PipelineProcessors communicate.

---
+++ palladian-core/src/main/java/ws/palladian/extraction/feature/LowerCaser.java
@@ -0,0 +1,55 @@
+/**
+ * Created on: 17.04.2012 23:52:40
+ */
+package ws.palladian.extraction.feature;
+
+import java.util.Collection;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import ws.palladian.extraction.AbstractPipelineProcessor;
+import ws.palladian.extraction.PipelineDocument;
+
+/**
+ * <p>
+ * Lowercases the content of the document.
+ * </p>
+ * 
+ * @author Klemens Muthmann
+ * @version 1.0
+ * @since 0.1.7
+ */
+public class LowerCaser extends AbstractPipelineProcessor {
+
+    /**
+     * <p>
+     * Used to serialize objects of this class. Should only change of the set of attributes of this class changes.
+     * </p>
+     */
+    private static final long serialVersionUID = -5655408816402154527L;
+
+    /**
+     * {@see AbstractPipelineProcessor#AbstractPipelineProcessor()}
+     */
+    public LowerCaser() {
+        super();
+    }
+
+    /**
+     * {@see AbstractPipelineProcessor#AbstractPipelineProcessor(Collection)}
+     * 
+     * @param documentToInputMapping {@see AbstractPipelineProcessor#AbstractPipelineProcessor(Collection)}
+     */
+    public LowerCaser(Collection<Pair<String, String>> documentToInputMapping) {
+        super(documentToInputMapping);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void processDocument(PipelineDocument document) {
+        String text = document.getOriginalContent();
+        String modifiedText = text.toLowerCase();
+        document.setModifiedContent(modifiedText);
+    }
+
+}
