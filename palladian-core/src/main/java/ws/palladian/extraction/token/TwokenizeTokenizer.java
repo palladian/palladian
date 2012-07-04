d426614a43f571f6e2b755578fe12b626e8bc7df
Philipp Katz <philipp@philippkatz.de>
2012-07-04 22:01:14 +0200

    Fix TwokenizeTokenizer (the only tokenizer we have, which keeps URLs!).
    Former-commit-id: 68bd0df05be83c558d9a31ecf1edc6b4d6c3dc4e

--- palladian-core/src/main/java/ws/palladian/extraction/token/TwokenizeTokenizer.java
+++ palladian-core/src/main/java/ws/palladian/extraction/token/TwokenizeTokenizer.java
@@ -25,12 +25,19 @@ public final class TwokenizeTokenizer extends BaseTokenizer {
     public void processDocument(PipelineDocument<String> document) throws DocumentUnprocessableException {
         String text = document.getContent();
         List<String> tokens = Twokenize.tokenizeForTagger_J(text);
-        System.err.println(tokens);
         AnnotationFeature annotationFeature = new AnnotationFeature(PROVIDED_FEATURE_DESCRIPTOR);
         int endPosition = 0;
         int index = 0;
         for (String token : tokens) {
             int startPosition = text.indexOf(token, endPosition);
+
+            // XXX bugfix, as the tokenizer seems to transform &gt; to > automatically,
+            // so we cannot determine the index for the annotation correctly. In this
+            // case, set it by former endPosition which should be okay. I guess.
+            if (startPosition == -1) {
+                startPosition = endPosition + 1;
+            }
+            
             endPosition = startPosition + token.length();
             annotationFeature.add(new PositionAnnotation(document, startPosition, endPosition, index++));
         }
