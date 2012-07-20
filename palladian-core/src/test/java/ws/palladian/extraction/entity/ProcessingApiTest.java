e8520fee880a86d7a3b16d69e07ad3e9de1b0bbd
Klemens Muthmann <klemens_muthmann@gmx.de>
2012-07-20 09:38:46 +0200

    Fixed failing ProcessingApiTest (hopefully).

--- palladian-core/src/test/java/ws/palladian/extraction/entity/ProcessingApiTest.java
+++ palladian-core/src/test/java/ws/palladian/extraction/entity/ProcessingApiTest.java
@@ -5,6 +5,8 @@ package ws.palladian.extraction.entity;
 
 import static org.junit.Assert.assertThat;
 
+import java.io.File;
+
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
@@ -16,30 +18,53 @@ import ws.palladian.processing.features.Annotation;
 import ws.palladian.processing.features.TextAnnotationFeature;
 
 /**
- * @author Klemens Muthmann
+ * <p>
+ * Tests whether the processing API works correct for the Named Entity Recognition.
+ * </p>
  * 
+ * @author Klemens Muthmann
+ * @version 2.0
+ * @since 0.1.7
  */
 public final class ProcessingApiTest {
 
-	private String FIXTURE = "John J. Smith and the Nexus One location iphone 4 mention Seattle in the text John J. Smith lives in Seattle.";
+    /**
+     * <p>
+     * An example text to check Named Entity Recognition on.
+     * </p>
+     */
+    private String FIXTURE = "John J. Smith and the Nexus One location iphone 4 mention Seattle in the text John J. Smith lives in Seattle.";
 
-	@Test
-	public void testProcessing() throws Exception {
-		String tudnerEnModel = ResourceHelper.getResourcePath("/ner/tudnerEn.model.gz");
-		PalladianNer nerProcessor = new PalladianNer(
-				LanguageMode.English);
-        
-        nerProcessor.loadModel(tudnerEnModel);
-		
-		PipelineDocument<String> document = new PipelineDocument<String>(
-				FIXTURE);
-		nerProcessor.processDocument(document);
-		
-		TextAnnotationFeature extractedEntities = document.getFeature(NamedEntityRecognizer.PROVIDED_FEATURE_DESCRIPTOR);
-		
-		assertThat(extractedEntities.getValue().size(),Matchers.is(5));
-		for(Annotation<String> nerAnnotation:extractedEntities.getValue()) {
-			assertThat(nerAnnotation,Matchers.notNullValue());
-		}
-	}
+    /**
+     * <p>
+     * Tests the Palladian NER with the processing API on {@link #FIXTURE}
+     * </p>
+     * 
+     * @throws Exception If something goes wrong.
+     */
+    @Test
+    public void testProcessing() throws Exception {
+        PalladianNer nerProcessor = new PalladianNer(LanguageMode.English);
+
+        // Training NER model
+        File trainingFile = ResourceHelper.getResourceFile("/ner/training.txt");
+        File modelFile = File.createTempFile("tudnerEn", ".model.gz");
+        nerProcessor.train(trainingFile, modelFile);
+
+        // Loading trained NER model
+        nerProcessor.loadModel(modelFile.getCanonicalPath());
+
+        // Processing example Document
+        PipelineDocument<String> document = new PipelineDocument<String>(FIXTURE);
+        nerProcessor.processDocument(document);
+
+        // Checking results
+        TextAnnotationFeature extractedEntities = document
+                .getFeature(NamedEntityRecognizer.PROVIDED_FEATURE_DESCRIPTOR);
+
+        assertThat(extractedEntities.getValue().size(), Matchers.is(5));
+        for (Annotation<String> nerAnnotation : extractedEntities.getValue()) {
+            assertThat(nerAnnotation, Matchers.notNullValue());
+        }
+    }
 }
