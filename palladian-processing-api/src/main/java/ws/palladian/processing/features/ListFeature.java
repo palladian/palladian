578cc7867c252ad6e1af90fe3df6d1fd97f90c25
Klemens Muthmann <klemens_muthmann@gmx.de>
2012-08-17 07:39:00 +0200

    updated sparse arff writer with an update feature and handling of ListFeatures.

---
+++ palladian-processing-api/src/main/java/ws/palladian/processing/features/ListFeature.java
@@ -0,0 +1,54 @@
+/**
+ * Created on: 14.08.2012 14:41:48
+ */
+package ws.palladian.processing.features;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * <p>
+ * A feature collecting multiple values as a list.
+ * </p>
+ * 
+ * @author Klemens Muthmann
+ * @version 1.0
+ * @since 3.0.0
+ */
+public final class ListFeature<T> extends Feature<List<? extends T>> {
+
+    /**
+     * {@inheritDoc #ListFeature(FeatureDescriptor, List)}
+     */
+    public ListFeature(FeatureDescriptor<? extends Feature<List<? extends T>>> descriptor, List<? extends T> value) {
+        super(descriptor, value);
+    }
+
+    /**
+     * <p>
+     * Creates a new {@code ListFeature} using a descriptor and all the values from the provided array.
+     * </p>
+     * 
+     * @param descriptor The descriptor identifying the new {@code Feature} within a {@link FeatureVector}.
+     * @param value The array containing all the values for the new {@code Feature}.
+     */
+    public ListFeature(FeatureDescriptor<? extends Feature<List<? extends T>>> descriptor, T[] value) {
+        super(descriptor, Arrays.asList(value));
+    }
+
+    /**
+     * {@inheritDoc #ListFeature(String, List)}
+     */
+    public ListFeature(String name, List<? extends T> value) {
+        super(name, value);
+    }
+
+    /**
+     * @return the list backing this {@code Feature}.
+     */
+    public List<? extends T> getList() {
+        return new ArrayList<T>(getValue());
+    }
+
+}
