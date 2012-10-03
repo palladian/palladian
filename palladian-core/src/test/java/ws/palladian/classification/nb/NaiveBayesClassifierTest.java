c9d886544276f27161ea55cdcb86e77819d56469
Philipp Katz <philipp@philippkatz.de>
2012-10-03 13:59:06 +0200

    Merged NaiveBayesClassifier tests.
    Former-commit-id: 17f791b3b7923feb1f55d57a1d483d2861ed9c04

--- palladian-core/src/test/java/ws/palladian/classification/nb/NaiveBayesClassifierTest.java
+++ palladian-core/src/test/java/ws/palladian/classification/nb/NaiveBayesClassifierTest.java
@@ -1,15 +1,19 @@
 package ws.palladian.classification.nb;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 import org.junit.Test;
 
 import ws.palladian.classification.CategoryEntries;
+import ws.palladian.classification.ClassificationUtils;
 import ws.palladian.classification.InstanceBuilder;
 import ws.palladian.classification.NominalInstance;
 import ws.palladian.helper.collection.CollectionHelper;
+import ws.palladian.helper.io.ResourceHelper;
 import ws.palladian.helper.math.MathHelper;
 import ws.palladian.processing.features.FeatureVector;
 
@@ -20,13 +24,13 @@ import ws.palladian.processing.features.FeatureVector;
  * 
  * @author David Urbansky
  * @author Philipp Katz
- * @formatter:off
  */
 public class NaiveBayesClassifierTest {
-    
+
     @Test
-    public void testNaiveBayesNominal() {
+    public void testNaiveBayesWithPlayData() {
         List<NominalInstance> instances = CollectionHelper.newArrayList();
+        // @formatter:off
         instances.add(new InstanceBuilder().set("outlook", "sunny").set("temp", "hot").set("humidity", "high").set("windy", "false").create("no"));
         instances.add(new InstanceBuilder().set("outlook", "sunny").set("temp", "hot").set("humidity", "high").set("windy", "true").create("no"));
         instances.add(new InstanceBuilder().set("outlook", "overcast").set("temp", "hot").set("humidity", "high").set("windy", "false").create("yes"));
@@ -59,8 +63,9 @@ public class NaiveBayesClassifierTest {
         categoryEntries = bayesClassifier.predict(featureVector, model);
         assertEquals(0.426, categoryEntries.getCategoryEntry("yes").getAbsoluteRelevance(), 0.001);
         assertEquals(0.574, categoryEntries.getCategoryEntry("no").getAbsoluteRelevance(), 0.001);
+        // @formatter:on
     }
-    
+
     @Test
     public void testNaiveBayesNumeric() {
         List<NominalInstance> instances = CollectionHelper.newArrayList();
@@ -70,16 +75,50 @@ public class NaiveBayesClassifierTest {
         instances.add(new InstanceBuilder().set("f", 18.0).create("Phone"));
         instances.add(new InstanceBuilder().set("f", 66.0).create("Phone"));
         instances.add(new InstanceBuilder().set("f", 290.0).create("Phone"));
-        
+
         NaiveBayesClassifier bayesClassifier = new NaiveBayesClassifier();
         NaiveBayesModel model = bayesClassifier.learn(instances);
-        
+
         // create an instance to classify
         FeatureVector featureVector = new InstanceBuilder().set("f", 16.0).create();
         CategoryEntries categoryEntries = bayesClassifier.predict(featureVector, model);
-        
+
         assertEquals(0.944, MathHelper.round(categoryEntries.getMostLikelyCategoryEntry().getRelevance(), 3), 0.01);
         assertEquals("Case", categoryEntries.getMostLikelyCategoryEntry().getCategory().getName());
+    }
+
+    @Test
+    public void testNaiveBayesWithAdultIncomeData() throws FileNotFoundException {
+        List<NominalInstance> instances = ClassificationUtils.createInstances(
+                ResourceHelper.getResourcePath("/classifier/adultData.txt"), false);
+        double accuracy = testWithSplit(instances);
+        assertTrue(accuracy > 0.77);
+    }
+
+    @Test
+    public void testNaiveBayesWithDiabetesData() throws FileNotFoundException {
+        List<NominalInstance> instances = ClassificationUtils.createInstances(
+                ResourceHelper.getResourcePath("/classifier/diabetesData.txt"), false);
+        double accuracy = testWithSplit(instances);
+        assertTrue(accuracy > 0.77);
+    }
+
+    private double testWithSplit(List<NominalInstance> instances) {
+        List<NominalInstance> train = instances.subList(0, instances.size() / 2);
+        List<NominalInstance> test = instances.subList(instances.size() / 2, instances.size() - 1);
+
+        NaiveBayesClassifier bayesClassifier = new NaiveBayesClassifier();
+        NaiveBayesModel bayesModel = bayesClassifier.learn(train);
+
+        int correctlyClassified = 0;
+        for (NominalInstance testInstance : test) {
+            CategoryEntries prediction = bayesClassifier.predict(testInstance.featureVector, bayesModel);
+            String categoryName = prediction.getMostLikelyCategoryEntry().getCategory().getName();
+            if (categoryName.equals(testInstance.targetClass)) {
+                correctlyClassified++;
+            }
+        }
+        return (double)correctlyClassified / test.size();
     }
 
 }
