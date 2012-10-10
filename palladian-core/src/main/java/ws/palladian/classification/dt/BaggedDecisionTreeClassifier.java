47404397762d44c16632dbc9817cc5501a08e144
Philipp Katz <philipp@philippkatz.de>
2012-10-10 22:19:24 +0200

    Cleanup.
    Former-commit-id: 53f73b890b895c9bc82d8677db1a514eb8ec2cbd

--- palladian-core/src/main/java/ws/palladian/classification/dt/BaggedDecisionTreeClassifier.java
+++ palladian-core/src/main/java/ws/palladian/classification/dt/BaggedDecisionTreeClassifier.java
@@ -11,6 +11,7 @@ import ws.palladian.classification.Classifier;
 import ws.palladian.classification.Instance;
 import ws.palladian.classification.Predictor;
 import ws.palladian.classification.text.evaluation.Dataset;
+import ws.palladian.classification.utils.ClassificationUtils;
 import ws.palladian.helper.collection.CollectionHelper;
 import ws.palladian.helper.collection.CountMap;
 import ws.palladian.processing.features.FeatureVector;
@@ -52,7 +53,8 @@ public final class BaggedDecisionTreeClassifier implements Classifier<BaggedDeci
         CountMap<String> categories = CountMap.create();
         for (DecisionTreeModel decisionTreeModel : model.getModels()) {
             CategoryEntries entriesResult = classifier.classify(vector, decisionTreeModel);
-            CategoryEntry categoryResult = entriesResult.get(0);
+            // CategoryEntry categoryResult = entriesResult.get(0);
+            CategoryEntry categoryResult = ClassificationUtils.getSingleBestCategoryEntry(entriesResult);
             String category = categoryResult.getCategory();
             categories.add(category);
         }
