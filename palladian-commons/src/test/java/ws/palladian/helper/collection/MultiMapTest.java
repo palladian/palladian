78643737472f98f0393efe23b1aa4b8ed96da83a
Simon Schabel <me@simonschabel.com>
2012-10-12 11:21:05 +0200

    imrovements
    Former-commit-id: 8d0c99064ab49fcae9b9774767a2280c15cbd72b

--- palladian-commons/src/test/java/ws/palladian/helper/collection/MultiMapTest.java
+++ palladian-commons/src/test/java/ws/palladian/helper/collection/MultiMapTest.java
@@ -1,23 +1,23 @@
-package ws.palladian.helper.collection;
-
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
-public class MultiMapTest {
-    
-    @Test
-    public void testMultiMap() {
-        MultiMap<String, String> multiMap = new MultiMap<String, String>();
-        multiMap.put("key1", "value1");
-        multiMap.put("key1", "value2");
-        multiMap.put("key1", "value3");
-        multiMap.put("key2", "value1");
-        multiMap.put("key3", "value5");
-        multiMap.put("key3", "value6");
-        
-        assertEquals(3, multiMap.size());
-        assertEquals(6, multiMap.allValues().size());
-    }
-
-}
+//package ws.palladian.helper.collection;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//import org.junit.Test;
+//
+//public class MultiMapTest {
+//    
+//    @Test
+//    public void testMultiMap() {
+//        MultiMap<String, String> multiMap = new MultiMap<String, String>();
+//        multiMap.put("key1", "value1");
+//        multiMap.put("key1", "value2");
+//        multiMap.put("key1", "value3");
+//        multiMap.put("key2", "value1");
+//        multiMap.put("key3", "value5");
+//        multiMap.put("key3", "value6");
+//        
+//        assertEquals(3, multiMap.size());
+//        assertEquals(6, multiMap.allValues().size());
+//    }
+//
+//}
