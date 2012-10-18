5ae1892219eae909297503ba783cad4eeef17cce
Philipp Katz <philipp@philippkatz.de>
2012-10-18 13:18:56 +0200

    remove ClassificationUtils#getSingleBestCategoryEntry (exists already in CategoryEntries).
    Former-commit-id: 962de702512379e257a3f05e839acce90ceeba35

--- palladian-core/src/main/java/ws/palladian/classification/utils/ClassificationUtils.java
+++ palladian-core/src/main/java/ws/palladian/classification/utils/ClassificationUtils.java
@@ -8,8 +8,6 @@ import java.util.Random;
 
 import org.apache.commons.lang3.Validate;
 
-import ws.palladian.classification.CategoryEntries;
-import ws.palladian.classification.CategoryEntry;
 import ws.palladian.classification.Instance;
 import ws.palladian.helper.collection.CollectionHelper;
 import ws.palladian.helper.io.FileHelper;
@@ -38,39 +36,6 @@ public final class ClassificationUtils {
     private ClassificationUtils() {
         throw new UnsupportedOperationException(
                 "Unable to instantiate ClassificationUtils. This class is a utility class. It makes no sense to instantiate it.");
-    }
-
-    public static CategoryEntry getSingleBestCategoryEntry(CategoryEntries entries) {
-        CategoryEntries limitedCategories = limitCategories(entries, 1, 0.0);
-        if (limitedCategories.iterator().hasNext()) {
-            return limitedCategories.iterator().next();
-        }
-        return null;
-    }
-
-    /**
-     * <p>
-     * Creates a new CategoryEntries object by limiting an existing one to a number of different categories, which need
-     * to have a relevance score above a provided threshold.
-     * </p>
-     * 
-     * @param number
-     *            Number of categories to keep.
-     * @param relevanceThreshold
-     *            Categories must have at least this much relevance to be kept.
-     */
-    public static CategoryEntries limitCategories(CategoryEntries categories, int number, double relevanceThreshold) {
-        CategoryEntries limitedCategories = new CategoryEntries();
-        int n = 0;
-        for (CategoryEntry c : categories) {
-            if (n < number && c.getProbability() >= relevanceThreshold) {
-                // XXX added by Philipp, lower memory consumption.
-                // c.setCategoryEntries(limitedCategories);
-                limitedCategories.add(c);
-            }
-            n++;
-        }
-        return limitedCategories;
     }
 
     /**
