7c6a11b8f93cb6f90476c7e97601325d7dba7fbf
David Urbansky <davidurbansky@gmail.com>
2012-10-20 00:24:10 +0200

    ~3% speed up for html tag stripping + cleaner interface, refs#79 removed duplicate code in PageAnalyzer minor improvements in StringHelper added first version of TimeIntervalParser

---
+++ palladian-commons/src/main/java/ws/palladian/helper/date/TimeIntervalParser.java
@@ -0,0 +1,76 @@
+package ws.palladian.helper.date;
+
+import ws.palladian.helper.nlp.StringHelper;
+
+/**
+ * <p>
+ * Extract mentions of time intervals in texts and transform them, e.g. "the movie lasted 2 hours and 5 minutes" => 125
+ * minutes.
+ * </p>
+ * 
+ * @author David Urbansky
+ * 
+ */
+public class TimeIntervalParser {
+
+    /**
+     * <p>
+     * Return the number of seconds to which the interval in the string was normalized.
+     * </p>
+     * 
+     * @param string The input string.
+     * @return The number of seconds of the mentioned time interval. Returns <tt>Null</tt> if nothing was found.
+     */
+    public static Long parse(String string) {
+        int days = 0;
+        int hours = 0;
+        int minutes = 0;
+        long seconds = 0;
+
+        boolean parsed = false;
+
+        try {
+            days = Integer.valueOf(StringHelper.getRegexpMatch("[0-9]+(?=\\s?(days?))", string));
+            parsed = true;
+        } catch (Exception e) {
+        }
+
+        try {
+            hours = Integer.valueOf(StringHelper.getRegexpMatch("[0-9]+(?=\\s?(hours?|hrs?))", string));
+            parsed = true;
+        } catch (Exception e) {
+        }
+
+        try {
+            minutes = Integer.valueOf(StringHelper.getRegexpMatch("[0-9]+(?=\\s?(minutes?|mins?))", string));
+            parsed = true;
+        } catch (Exception e) {
+        }
+        try {
+            seconds = Long.valueOf(StringHelper.getRegexpMatch("[0-9]+(?=\\s?(seconds?|secs?))", string));
+            parsed = true;
+        } catch (Exception e) {
+        }
+
+        if (parsed) {
+            seconds += 86400 * days + 3600 * hours + 60 * minutes;
+        } else {
+            return null;
+        }
+
+        return seconds;
+    }
+
+    /**
+     * <p>
+     * </p>
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(TimeIntervalParser.parse("the movie lasted 2 hours and 5 minutes"));
+        System.out.println(TimeIntervalParser.parse("4 hrs 20 mins"));
+        System.out.println(TimeIntervalParser.parse("1 day 23 mins"));
+    }
+
+}
