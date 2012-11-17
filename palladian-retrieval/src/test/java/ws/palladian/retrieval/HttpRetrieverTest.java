e015f338392948a1346c54e252322a00817529d3
Philipp Katz <philipp@philippkatz.de>
2012-11-17 10:42:49 +0100

    Fix HttpRetriever#getRedirectUrls (closes #133).
    Former-commit-id: e7f1e95b696b8c68d0346acd1746a1ea37110e51

--- palladian-retrieval/src/test/java/ws/palladian/retrieval/HttpRetrieverTest.java
+++ palladian-retrieval/src/test/java/ws/palladian/retrieval/HttpRetrieverTest.java
@@ -2,6 +2,7 @@ package ws.palladian.retrieval;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.List;
 import java.util.concurrent.Executors;
 
 import org.apache.log4j.Logger;
@@ -11,11 +12,12 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import ws.palladian.helper.collection.CollectionHelper;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-@Ignore // for now.
 public class HttpRetrieverTest {
     
     /** The logger for this class. */
@@ -48,9 +50,20 @@ public class HttpRetrieverTest {
     }
     
     @Test(timeout = 5000, expected = HttpException.class)
+    @Ignore // for now.
     public void testGetTimeout() throws HttpException {
         HttpRetriever httpRetriever = new HttpRetriever(1000, 1000, 0, 1);
         httpRetriever.httpGet("http://localhost:" + PORT + "/");
+    }
+
+    /** See : https://bitbucket.org/palladian/palladian/issue/133/redirecting-throws-unexpected-error */
+    @Test
+    @Ignore
+    public void testRedirects_issue133() throws Exception {
+        HttpRetriever httpRetriever = HttpRetrieverFactory.getHttpRetriever();
+        String url = "http://feedproxy.google.com/~r/pwcooks/~3/iqsvvvykjpo/";
+        List<String> redirectUrls = httpRetriever.getRedirectUrls(url);
+        CollectionHelper.print(redirectUrls);
     }
     
     // TODO add further tests.
