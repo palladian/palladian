19991a251d8cfdcafebf5b8fd315b20f5b0eb98f
David Urbansky <davidurbansky@gmail.com>
2012-12-03 14:19:32 +0100

    added AlchemyApiContentExtractor, fixes #146 added language selection to StopWordRemover, removed deprecation

--- palladian-core/src/main/java/ws/palladian/extraction/feature/StopWordRemover.java
+++ palladian-core/src/main/java/ws/palladian/extraction/feature/StopWordRemover.java
@@ -3,22 +3,44 @@ package ws.palladian.extraction.feature;
 import java.io.InputStream;
 import java.util.List;
 
+import ws.palladian.helper.constants.Language;
 import ws.palladian.helper.io.FileHelper;
 import ws.palladian.helper.nlp.StringHelper;
 import ws.palladian.processing.TextDocument;
 
 /**
- * @deprecated Use {@link StopTokenRemover} instead.
+ * <p>
+ * Remove stop words from a text.
+ * </p>
  */
-@Deprecated
 public class StopWordRemover extends TextDocumentPipelineProcessor {
 
     private List<String> stopWords;
 
+    /**
+     * <p>
+     * Default constructor for English texts.
+     * </p>
+     */
     public StopWordRemover() {
+        this(Language.ENGLISH);
+    }
+
+    /**
+     * <p>
+     * Choose a language (English or German).
+     * </p>
+     * 
+     * @param language The language for which the stop words should be removed.
+     */
+    public StopWordRemover(Language language) {
         InputStream inputStream = null;
         try {
-            inputStream = this.getClass().getResourceAsStream("/stopwords_en.txt");
+            if (language.equals(Language.ENGLISH)) {
+                inputStream = this.getClass().getResourceAsStream("/stopwords_en.txt");
+            } else if (language.equals(Language.GERMAN)) {
+                inputStream = this.getClass().getResourceAsStream("/stopwords_de.txt");
+            }
             stopWords = FileHelper.readFileToArray(inputStream);
         } finally {
             FileHelper.close(inputStream);
