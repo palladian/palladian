e865e077477f99fd9ef6115a81ccca41376ce4e8
Klemens Muthmann <klemens_muthmann@gmx.de>
2012-12-21 17:02:49 +0100

    Added LibSvmPredictor

---
+++ palladian-core/src/main/java/ws/palladian/extraction/pos/filter/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Created on: 04.12.2012 08:19:13
+ */
+/**
+ * <p>
+ * 
+ * </p>
+ * 
+ * @author Klemens Muthmann
+ * @version 1.0
+ * @since
+ */
+package ws.palladian.extraction.pos.filter;
