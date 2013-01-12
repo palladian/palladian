8727169b8504a33fdc70b3701048aa13302ad371
Klemens Muthmann <klemens_muthmann@gmx.de>
2013-01-12 18:32:48 +0100

    Branch for fixing the current problems with the sentence splitter regular expression.

--- palladian-core/src/test/java/ws/palladian/extraction/token/TokenizerTest.java
+++ palladian-core/src/test/java/ws/palladian/extraction/token/TokenizerTest.java
@@ -9,6 +9,8 @@ import java.util.List;
 import org.junit.Test;
 
 import ws.palladian.helper.constants.Language;
+import ws.palladian.processing.TextDocument;
+import ws.palladian.processing.features.PositionAnnotation;
 
 /**
  * <p>
@@ -261,6 +263,14 @@ public class TokenizerTest {
         sentences = Tokenizer.getSentences(inputText);
         assertEquals(2, sentences.size());
 
+        inputText = "It happened again. ;-( Soo mean!";
+        sentences = Tokenizer.getSentences(inputText);
+        assertEquals(2, sentences.size());
+
+        inputText = "Not the \"what happenend?\" :) But this problem is one of the worst mistakes we made (I did!) in a very long time.";
+        sentences = Tokenizer.getSentences(inputText);
+        assertEquals(2, sentences.size());
+
         inputText = "IT IS three years since Senator Barack Obama pronounced that America “is no longer a Christian nation—at least, not just.” The words sounded harsher than he intended: bla.";
         sentences = Tokenizer.getSentences(inputText);
         assertEquals(2, sentences.size());
@@ -272,5 +282,16 @@ public class TokenizerTest {
         inputText = "Das ist z.B. sooo groß.";
         sentences = Tokenizer.getSentences(inputText, Language.GERMAN);
         assertEquals(1, sentences.size());
+    }
+
+    @Test
+    public void testSentenceOnTextWithMaskBetweenSentences() throws Exception {
+        String inputText = "Not the \"what happenend?\" :) But this problem is one of the worst mistakes we made (I did!) in a very long time.";
+        List<PositionAnnotation> sentences = Tokenizer.getSentences(new TextDocument(inputText), "testFeature");
+        assertEquals(2, sentences.size());
+
+        // String inputText = FileHelper.readFileToString(ResourceHelper.getResourceFile("/texts/contribution04.txt"));
+        // List<PositionAnnotation> sentences = Tokenizer.getSentences(new TextDocument(inputText), "testFeature");
+        // System.out.println(sentences);
     }
 }
