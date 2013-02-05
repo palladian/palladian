a2a59131a6f5042b40f9d6a481f99061e38882f6
Philipp Katz <philipp@philippkatz.de>
2013-02-05 16:11:04 +0100

    Move some code to util class, JavaDoc.
    Former-commit-id: 93d358f15a4f58d6c0bc14fd7c04240cf628a0a6

--- palladian-core/src/main/java/ws/palladian/extraction/location/GeonamesLocationSource.java
+++ palladian-core/src/main/java/ws/palladian/extraction/location/GeonamesLocationSource.java
@@ -1,8 +1,6 @@
 package ws.palladian.extraction.location;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.lang3.Validate;
 import org.w3c.dom.Document;
@@ -18,47 +16,21 @@ import ws.palladian.retrieval.HttpRetrieverFactory;
 import ws.palladian.retrieval.parser.DocumentParser;
 import ws.palladian.retrieval.parser.ParserFactory;
 
+/**
+ * <p>
+ * A {@link LocationSource} witch uses the <a href="http://www.geonames.org">Geonames</a> API. The services provides
+ * 2000 queries/hour for free accounts.
+ * </p>
+ * 
+ * @see <a href="http://www.geonames.org/login">Account registration</a>
+ * @see <a href="http://www.geonames.org/manageaccount">Account activation</a> (enable access to web services after
+ *      registration)
+ * @author Philipp Katz
+ */
 public class GeonamesLocationSource implements LocationSource {
 
     /** The logger for this class. */
     // private static final Logger LOGGER = LoggerFactory.getLogger(GeonamesLocationSource.class);
-
-    /** Mapping from feature codes from the dataset to LocationType. */
-    private static final Map<String, LocationType> FEATURE_MAPPING;
-
-    static {
-        // TODO check, whether those mappings make sense
-        // http://download.geonames.org/export/dump/featureCodes_en.txt
-        // http://www.geonames.org/export/codes.html
-        Map<String, LocationType> temp = CollectionHelper.newHashMap();
-        temp.put("A", LocationType.UNIT);
-        temp.put("A.PCL", LocationType.COUNTRY);
-        temp.put("A.PCLD", LocationType.COUNTRY);
-        temp.put("A.PCLF", LocationType.COUNTRY);
-        temp.put("A.PCLH", LocationType.COUNTRY);
-        temp.put("A.PCLI", LocationType.COUNTRY);
-        temp.put("A.PCLIX", LocationType.COUNTRY);
-        temp.put("A.PCLS", LocationType.COUNTRY);
-        temp.put("H", LocationType.LANDMARK);
-        temp.put("L", LocationType.POI);
-        temp.put("L.AREA", LocationType.REGION);
-        temp.put("L.COLF", LocationType.REGION);
-        temp.put("L.CONT", LocationType.CONTINENT);
-        temp.put("L.RGN", LocationType.REGION);
-        temp.put("L.RGNE", LocationType.REGION);
-        temp.put("L.RGNH", LocationType.REGION);
-        temp.put("L.RGNL", LocationType.REGION);
-        temp.put("P", LocationType.CITY);
-        temp.put("R", LocationType.POI);
-        temp.put("S", LocationType.POI);
-        temp.put("T", LocationType.LANDMARK);
-        temp.put("U", LocationType.LANDMARK);
-        temp.put("U.BDLU", LocationType.REGION);
-        temp.put("U.PLNU", LocationType.REGION);
-        temp.put("U.PRVU", LocationType.REGION);
-        temp.put("V", LocationType.POI);
-        FEATURE_MAPPING = Collections.unmodifiableMap(temp);
-    }
 
     private final String username;
 
@@ -69,6 +41,13 @@ public class GeonamesLocationSource implements LocationSource {
     /** Count the number of executed request since start. */
     public static int requestCount = 0;
 
+    /**
+     * <p>
+     * Create a new {@link GeonamesLocationSource}.
+     * </p>
+     * 
+     * @param username The signed up user name, not <code>null</code> or empty.
+     */
     public GeonamesLocationSource(String username) {
         Validate.notEmpty(username, "username must not be empty");
         this.username = username;
@@ -139,22 +118,8 @@ public class GeonamesLocationSource implements LocationSource {
         location.setLongitude(longitude);
         location.setPopulation(population);
         location.setPrimaryName(primaryName);
-        location.setType(mapType(featureClass, featureCode));
+        location.setType(GeonamesUtil.mapType(featureClass, featureCode));
         return location;
-    }
-
-    static LocationType mapType(String featureClass, String featureCode) {
-        // first, try lookup by full feature code (e.g. 'L.CONT')
-        LocationType locationType = FEATURE_MAPPING.get(String.format("%s.%s", featureClass, featureCode));
-        if (locationType != null) {
-            return locationType;
-        }
-        // second, try lookup only be feature class (e.g. 'A')
-        locationType = FEATURE_MAPPING.get(featureClass);
-        if (locationType != null) {
-            return locationType;
-        }
-        return LocationType.UNDETERMINED;
     }
 
     @Override
