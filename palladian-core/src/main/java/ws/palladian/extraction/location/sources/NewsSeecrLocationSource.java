38836db2717c342e816e84f00a8a961aea4cd68a
Philipp Katz <philipp@philippkatz.de>
2013-02-11 18:40:04 +0100

    Code cleanup in NewsSeecrLocationSource

--- palladian-core/src/main/java/ws/palladian/extraction/location/sources/NewsSeecrLocationSource.java
+++ palladian-core/src/main/java/ws/palladian/extraction/location/sources/NewsSeecrLocationSource.java
@@ -1,22 +1,10 @@
 package ws.palladian.extraction.location.sources;
 
-import java.math.BigInteger;
-import java.security.GeneralSecurityException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
-import javax.crypto.Mac;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.Validate;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,41 +27,25 @@ import ws.palladian.retrieval.helper.HttpHelper;
  * 52feznh45ezmjxgfzorrk6ooagyadg
  * 
  * @author Sky
- * 
+ * @author Philipp Katz
  */
 public class NewsSeecrLocationSource implements LocationSource {
 
     /** The logger for this class. */
     private static final Logger LOGGER = LoggerFactory.getLogger(NewsSeecrLocationSource.class);
 
-    private static final String SEARCHER_NAME = "NewsSeecr";
-
-    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+    /** Configuration key for the Mashape public key. */
+    public static final String CONFIG_MASHAPE_PUBLIC_KEY = "api.newsseecr.mashapePublicKey";
+    /** Configuration key for the Mashape private key. */
+    public static final String CONFIG_MASHAPE_PRIVATE_KEY = "api.newsseecr.mashapePrivateKey";
 
     private static final String BASE_URL = "https://qqilihq-newsseecr.p.mashape.com/locations";
-
-    private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
 
     private final String mashapePublicKey;
 
     private final String mashapePrivateKey;
 
-    /** Configuraiton key for the Mashape public key. */
-    public static final String CONFIG_MASHAPE_PUBLIC_KEY = "api.newsseecr.mashapePublicKey";
-    /** Configuration key for the Mashape private key. */
-    public static final String CONFIG_MASHAPE_PRIVATE_KEY = "api.newsseecr.mashapePrivateKey";
-
-    private static final Map<String, LocationType> LOCATION_MAPPING;
-
-    static {
-        Map<String, LocationType> temp = CollectionHelper.newHashMap();
-        temp.put("Country", LocationType.COUNTRY);
-        temp.put("Nation", LocationType.COUNTRY);
-        temp.put("County", LocationType.UNIT);
-        temp.put("City", LocationType.CITY);
-        temp.put("Metropole", LocationType.CITY);
-        LOCATION_MAPPING = Collections.unmodifiableMap(temp);
-    }
+    private final HttpRetriever retriever = HttpRetrieverFactory.getHttpRetriever();
 
     /**
      * <p>
@@ -92,165 +64,86 @@ public class NewsSeecrLocationSource implements LocationSource {
 
     @Override
     public List<Location> retrieveLocations(String locationName) {
-        List<Location> locations = CollectionHelper.newArrayList();
-        HttpRetriever retriever = HttpRetrieverFactory.getHttpRetriever();
-
-        HttpRequest request = new HttpRequest(HttpMethod.GET, BASE_URL + "/search");
-        request.addParameter("locationName", locationName);
-
-        // FIXME this is duplicate code from NewsSeecrSearcher
-        String mashapeHeader;
-        try {
-            mashapeHeader = generateMashapeHeader(mashapePublicKey, mashapePrivateKey);
-        } catch (GeneralSecurityException e) {
-            throw new RuntimeException("Error while creating Authorization header: " + e.getMessage(), e);
-        }
-        LOGGER.debug("Authorization header = " + mashapeHeader);
-        request.addHeader("X-Mashape-Authorization", mashapeHeader);
-
-        LOGGER.debug("Performing request: " + request);
-        HttpResult result;
-        try {
-            result = retriever.execute(request);
-        } catch (HttpException e) {
-            throw new RuntimeException("Encountered HTTP error when executing the request: " + request + ": "
-                    + e.getMessage(), e);
-        }
-        if (result.getStatusCode() != 200) {
-            // TODO get message
-            throw new RuntimeException("Encountered HTTP status " + result.getStatusCode()
-                    + " when executing the request: " + request + ", result: " + HttpHelper.getStringContent(result));
-        }
-
-        String jsonString = HttpHelper.getStringContent(result);
-        LOGGER.debug("JSON result: " + jsonString);
-
-        try {
-            JSONArray resultArray = JPathHelper.get(jsonString, "results", JSONArray.class);
-            for (int i = 0; i < resultArray.length(); i++) {
-                JSONObject resultObject = resultArray.getJSONObject(i);
-                Integer id = JPathHelper.get(resultObject, "id", Integer.class);
-                Double latitude = JPathHelper.get(resultObject, "latitude", Double.class);
-                Double longitude = JPathHelper.get(resultObject, "longitude", Double.class);
-                String primaryName = JPathHelper.get(resultObject, "primaryName", String.class);
-                String type = JPathHelper.get(resultObject, "locationType", String.class);
-                Long population = JPathHelper.get(resultObject, "population", Long.class);
-                List<String> alternativeNames = new ArrayList(Arrays.asList(JPathHelper.get(resultObject,
-                        "alternateNames", JSONArray.class)));
-
-                Location location = new Location();
-                location.setId(id);
-                location.setLatitude(latitude);
-                location.setLongitude(longitude);
-                location.setPopulation(population);
-                location.setPrimaryName(primaryName);
-                location.setType(LocationType.valueOf(type));
-                location.setAlternativeNames(alternativeNames);
-
-                locations.add(location);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(
-                    "Error while parsing the JSON response (" + jsonString + "): " + e.getMessage(), e);
-        }
-
-        return locations;
-    }
-
-    // https://www.mashape.com/docs/consume/rest
-    public static String generateMashapeHeader(String publicKey, String privateKey) throws InvalidKeyException,
-    NoSuchAlgorithmException {
-        return new String(Base64.encodeBase64(String.format("%s:%s", publicKey, sha1hmac(publicKey, privateKey))
-                .getBytes()));
-    }
-
-    // Code taken from:
-    // https://github.com/Mashape/mashape-java-client-library/blob/master/src/main/java/com/mashape/client/http/utils/CryptUtils.java
-    static String sha1hmac(String publicKey, String privateKey) throws NoSuchAlgorithmException, InvalidKeyException {
-        SecretKey key = new SecretKeySpec(privateKey.getBytes(), HMAC_SHA1_ALGORITHM);
-        Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
-        mac.init(key);
-        byte[] rawHmac = mac.doFinal(publicKey.getBytes());
-        BigInteger hash = new BigInteger(1, rawHmac);
-        String hmac = hash.toString(16);
-        if (hmac.length() % 2 != 0) {
-            hmac = "0" + hmac;
-        }
-        return hmac;
+        HttpRequest request = new HttpRequest(HttpMethod.GET, BASE_URL);
+        request.addParameter("name", locationName);
+        String jsonString = retrieveResult(request);
+        return parseResultArray(jsonString);
     }
 
     @Override
     public Location retrieveLocation(int locationId) {
-        throw new UnsupportedOperationException();
+        HttpRequest request = new HttpRequest(HttpMethod.GET, BASE_URL + "/" + locationId);
+        String jsonString = retrieveResult(request);
+        try {
+            return parseSingleResult(new JSONObject(jsonString));
+        } catch (JSONException e) {
+            throw new IllegalStateException("Error while parsing the JSON response (" + jsonString + "): "
+                    + e.getMessage(), e);
+        }
     }
 
     @Override
     public List<Location> getHierarchy(Location location) {
+        HttpRequest request = new HttpRequest(HttpMethod.GET, BASE_URL + "/" + location.getId() + "/hierarchy");
+        String jsonString = retrieveResult(request);
+        return parseResultArray(jsonString);
+    }
 
-        List<Location> locations = CollectionHelper.newArrayList();
-
-        HttpRetriever retriever = HttpRetrieverFactory.getHttpRetriever();
-
-        HttpRequest request = new HttpRequest(HttpMethod.GET, BASE_URL + "/getHierarchy");
-        request.addParameter("locationId", location.getId());
-
-        // FIXME this is duplicate code from NewsSeecrSearcher
-        String mashapeHeader;
-        try {
-            mashapeHeader = generateMashapeHeader(mashapePublicKey, mashapePrivateKey);
-        } catch (GeneralSecurityException e) {
-            throw new RuntimeException("Error while creating Authorization header: " + e.getMessage(), e);
-        }
-        LOGGER.debug("Authorization header = " + mashapeHeader);
-        request.addHeader("X-Mashape-Authorization", mashapeHeader);
-
+    private String retrieveResult(HttpRequest request) {
+        MashapeUtil.signRequest(request, mashapePublicKey, mashapePrivateKey);
         LOGGER.debug("Performing request: " + request);
         HttpResult result;
         try {
             result = retriever.execute(request);
         } catch (HttpException e) {
-            throw new RuntimeException("Encountered HTTP error when executing the request: " + request + ": "
+            throw new IllegalStateException("Encountered HTTP error when executing the request: " + request + ": "
                     + e.getMessage(), e);
         }
         if (result.getStatusCode() != 200) {
             // TODO get message
-            throw new RuntimeException("Encountered HTTP status " + result.getStatusCode()
+            throw new IllegalStateException("Encountered HTTP status " + result.getStatusCode()
                     + " when executing the request: " + request + ", result: " + HttpHelper.getStringContent(result));
         }
+        return HttpHelper.getStringContent(result);
+    }
 
-        String jsonString = HttpHelper.getStringContent(result);
-        System.out.println(jsonString);
-
+    private List<Location> parseResultArray(String jsonString) {
+        List<Location> locations = CollectionHelper.newArrayList();
         try {
             JSONArray resultArray = JPathHelper.get(jsonString, "results", JSONArray.class);
             for (int i = 0; i < resultArray.length(); i++) {
                 JSONObject resultObject = resultArray.getJSONObject(i);
-                Integer id = JPathHelper.get(resultObject, "id", Integer.class);
-                Double latitude = JPathHelper.get(resultObject, "latitude", Double.class);
-                Double longitude = JPathHelper.get(resultObject, "longitude", Double.class);
-                String primaryName = JPathHelper.get(resultObject, "primaryName", String.class);
-                String type = JPathHelper.get(resultObject, "locationType", String.class);
-                Long population = JPathHelper.get(resultObject, "population", Long.class);
-                List<String> alternativeNames = new ArrayList(Arrays.asList(JPathHelper.get(resultObject,
-                        "alternateNames", JSONArray.class)));
-
-                Location location1 = new Location();
-                location1.setId(id);
-                location1.setLatitude(latitude);
-                location1.setLongitude(longitude);
-                location1.setPopulation(population);
-                location1.setPrimaryName(primaryName);
-                location1.setType(LocationType.valueOf(type));
-                location1.setAlternativeNames(alternativeNames);
-
-                locations.add(location1);
+                Location location = parseSingleResult(resultObject);
+                locations.add(location);
             }
         } catch (Exception e) {
-            throw new RuntimeException("Error while parsing the JSON response (" + jsonString + "): " + e.getMessage(),
-                    e);
+            throw new IllegalStateException("Error while parsing the JSON response (" + jsonString + "): "
+                    + e.getMessage(), e);
         }
-
         return locations;
+    }
+
+    public Location parseSingleResult(JSONObject resultObject) throws JSONException {
+        Integer id = JPathHelper.get(resultObject, "id", Integer.class);
+        Double latitude = JPathHelper.get(resultObject, "latitude", Double.class);
+        Double longitude = JPathHelper.get(resultObject, "longitude", Double.class);
+        String primaryName = JPathHelper.get(resultObject, "primaryName", String.class);
+        String type = JPathHelper.get(resultObject, "locationType", String.class);
+        Long population = JPathHelper.get(resultObject, "population", Long.class);
+        List<String> alternativeNames = CollectionHelper.newArrayList();
+        JSONArray jsonArray = JPathHelper.get(resultObject, "alternateNames", JSONArray.class);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            alternativeNames.add(jsonArray.getString(i));
+        }
+        Location location = new Location();
+        location.setId(id);
+        location.setLatitude(latitude);
+        location.setLongitude(longitude);
+        location.setPopulation(population);
+        location.setPrimaryName(primaryName);
+        location.setType(LocationType.valueOf(type));
+        location.setAlternativeNames(alternativeNames);
+        return location;
     }
 
     public static void main(String[] args) {
@@ -264,5 +157,9 @@ public class NewsSeecrLocationSource implements LocationSource {
 
         List<Location> hierarchyLocations = newsSeecrLocationSource.getHierarchy(loc);
         CollectionHelper.print(hierarchyLocations);
+
+        Location loc2 = newsSeecrLocationSource.retrieveLocation(2921044);
+        System.out.println(loc2);
+        System.out.println(loc2.getAlternativeNames());
     }
 }
