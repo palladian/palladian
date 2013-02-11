ca34fe31c983041c5a62ab36b8e0c30a65ef9539
Philipp Katz <philipp@philippkatz.de>
2013-02-11 18:51:30 +0100

    Move MashapeUtil to palladian-retrieval, document, test, remove duplicated code.

--- palladian-retrieval/src/main/java/ws/palladian/retrieval/helper/MashapeUtil.java
+++ palladian-retrieval/src/main/java/ws/palladian/retrieval/helper/MashapeUtil.java
@@ -1,4 +1,4 @@
-package ws.palladian.extraction.location.sources;
+package ws.palladian.retrieval.helper;
 
 import java.math.BigInteger;
 import java.security.InvalidKeyException;
@@ -9,24 +9,45 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.Validate;
 
 import ws.palladian.retrieval.HttpRequest;
 
-// XXX also use this in ws.palladian.retrieval.search.news.NewsSeecrSearcher
-public class MashapeUtil {
+/**
+ * <p>
+ * Utility class to sign requests for <a href="https://www.mashape.com">Mashape</a>.
+ * </p>
+ * 
+ * @see <a href="https://www.mashape.com/docs/consume/rest">Consume an API in Plain REST</a>
+ * @author Philipp Katz
+ */
+public final class MashapeUtil {
 
     /** Key of the Mashape Authorization header. */
     private static final String MASHAPE_AUTHORIZATION_HEADER = "X-Mashape-Authorization";
 
     private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
 
-    public static void signRequest(HttpRequest request, String mashapePublicKey, String mashapePrivateKey) {
-        String mashapeHeader = MashapeUtil.generateMashapeHeader(mashapePublicKey, mashapePrivateKey);
+    /**
+     * <p>
+     * Sign the given {@link HttpRequest} with the specified public/private key combination and put the authorization in
+     * the HTTP header {@value #MASHAPE_AUTHORIZATION_HEADER}.
+     * </p>
+     * 
+     * @param request The request to sign, not <code>null</code>.
+     * @param publicKey The public key for signing, not empty or <code>null</code>.
+     * @param privateKey The private key for signing, not empty or <code>null</code>.
+     */
+    public static void signRequest(HttpRequest request, String publicKey, String privateKey) {
+        Validate.notNull(request, "request must not be null");
+        Validate.notEmpty(publicKey, "publicKey must not be empty");
+        Validate.notEmpty(privateKey, "privateKey must not be empty");
+        String mashapeHeader = MashapeUtil.generateMashapeHeader(publicKey, privateKey);
         request.addHeader(MASHAPE_AUTHORIZATION_HEADER, mashapeHeader);
     }
 
     // https://www.mashape.com/docs/consume/rest
-    public static String generateMashapeHeader(String publicKey, String privateKey) {
+    static String generateMashapeHeader(String publicKey, String privateKey) {
         try {
             return new String(Base64.encodeBase64(String.format("%s:%s", publicKey, sha1hmac(publicKey, privateKey))
                     .getBytes()));
