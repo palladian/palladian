78f11d6ef6b0a617f326fd3eecdb1c35361e11d5
Philipp Katz <philipp@philippkatz.de>
2013-02-20 19:58:54 +0100

    Fix web test (-_-)

--- palladian-retrieval/src/test/java/ws/palladian/retrieval/PageAnalyzerTest.java
+++ palladian-retrieval/src/test/java/ws/palladian/retrieval/PageAnalyzerTest.java
@@ -9,27 +9,30 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
 import ws.palladian.helper.html.XPathHelper;
+import ws.palladian.helper.io.ResourceHelper;
+import ws.palladian.retrieval.parser.DocumentParser;
+import ws.palladian.retrieval.parser.ParserFactory;
 
 public class PageAnalyzerTest {
 
     @Test
     public void testXPathConstruction_issue159() throws Exception {
-        DocumentRetriever ret = new DocumentRetriever();
-        Document wd = ret.getWebDocument("http://www.evi.com/q/who_starred_in_the_invisible_army");
+        DocumentParser htmlParser = ParserFactory.createHtmlParser();
+        Document document = htmlParser.parse(ResourceHelper.getResourceFile("/webPages/test_issue159.html"));
 
-        Set<String> xPaths = PageAnalyzer.constructAllXPaths(wd, "Who starred in The Invisible Army", false, true);
-        XPathSet xps = new XPathSet();
-        xps.add(xPaths);
-        String longestXPath = xps.getLongestXPath();
+        Set<String> xPaths = PageAnalyzer
+                .constructAllXPaths(document, "Who starred in The Invisible Army", false, true);
+        XPathSet xPathSet = new XPathSet();
+        xPathSet.add(xPaths);
+        String longestXPath = xPathSet.getLongestXPath();
 
         // System.out.println("constructed xPath: " + longestXPath);
-        Node node = XPathHelper.getXhtmlNode(wd, longestXPath);
+        Node node = XPathHelper.getXhtmlNode(document, longestXPath);
         // System.out.println("node that was found when constructing, must not be null and it is: " + node);
         // System.out.println("node that was found when constructing, must not be null and it is: "
         // + node.getTextContent());
 
         assertNotNull(node);
     }
-
 
 }
