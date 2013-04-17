31816658146c51de27ae71df8801a76033285c62
Philipp Katz <philipp@philippkatz.de>
2013-04-17 14:55:22 +0200

    Start quick and dirty CoordinateTagger (ref #184).
    Former-commit-id: 19f91e40a207c2cf95e79c9d4ebfe19008cc233f

---
+++ palladian-core/src/test/java/ws/palladian/extraction/location/CoordinateTaggerTest.java
@@ -0,0 +1,48 @@
+package ws.palladian.extraction.location;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class CoordinateTaggerTest {
+
+    @Test
+    public void testLocationTagger() {
+        CoordinateTagger tagger = new CoordinateTagger();
+
+        List<LocationAnnotation> annotations = tagger.getAnnotations("40.446195,-79.948862");
+        assertEquals(1, annotations.size());
+        assertEquals(40.446195, annotations.get(0).getLocation().getLatitude(), 0);
+        assertEquals(-79.948862, annotations.get(0).getLocation().getLongitude(), 0);
+
+        annotations = tagger.getAnnotations("40.446195, -79.948862");
+        assertEquals(1, annotations.size());
+        assertEquals(40.446195, annotations.get(0).getLocation().getLatitude(), 0);
+        assertEquals(-79.948862, annotations.get(0).getLocation().getLongitude(), 0);
+
+        annotations = tagger.getAnnotations("40.446195 -79.948862");
+        assertEquals(1, annotations.size());
+        assertEquals(40.446195, annotations.get(0).getLocation().getLatitude(), 0);
+        assertEquals(-79.948862, annotations.get(0).getLocation().getLongitude(), 0);
+
+        annotations = tagger.getAnnotations("40.446195N 79.948862W");
+        assertEquals(1, annotations.size());
+        assertEquals(40.446195, annotations.get(0).getLocation().getLatitude(), 0);
+        assertEquals(-79.948862, annotations.get(0).getLocation().getLongitude(), 0);
+
+        annotations = tagger.getAnnotations("40°26′47″N 079°58′36″W");
+        assertEquals(1, annotations.size());
+        assertEquals(40.446195, annotations.get(0).getLocation().getLatitude(), 0.05);
+        assertEquals(-79.948862, annotations.get(0).getLocation().getLongitude(), 0.05);
+
+        // 40:26:46.302N 079:56:55.903W
+        // 40°26′47″N 079°58′36″W
+        // 40d 26′ 47″ N 079d 58′ 36″ W
+        // 40.446195N 79.948862W
+        // 40.446195, -79.948862
+        // 40° 26.7717, -79° 56.93172
+    }
+
+}
