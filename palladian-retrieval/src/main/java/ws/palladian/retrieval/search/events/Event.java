250f60823a980c269acdd7b4a735c38b5502a53b
David Urbansky <davidurbansky@gmail.com>
2013-05-05 19:10:52 +0200

    duration for events filtering of events

--- palladian-retrieval/src/main/java/ws/palladian/retrieval/search/events/Event.java
+++ palladian-retrieval/src/main/java/ws/palladian/retrieval/search/events/Event.java
@@ -59,6 +59,20 @@ public class Event {
         this.endDate = endDate;
     }
 
+    /**
+     * <p>
+     * Get the duration of the event in milliseconds.
+     * </p>
+     * 
+     * @return The duration of the event in milliseconds or null if unknown.
+     */
+    public Long getDuration() {
+        if (getEndDate() == null) {
+            return null;
+        }
+        return getEndDate().getTime() - getStartDate().getTime();
+    }
+
     public String getRecurringString() {
         return recurringString;
     }
