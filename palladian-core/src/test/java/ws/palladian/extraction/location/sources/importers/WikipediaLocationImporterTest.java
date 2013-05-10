b0506c431ba4b5c931daa78ee8b7d8fa51e5fa9b
Philipp Katz <philipp@philippkatz.de>
2013-05-10 18:25:55 +0200

    Fix FIXME, add some asserts.
    Former-commit-id: a37774dc401f1b32ba9091203b55ec552b0198f8

--- palladian-core/src/test/java/ws/palladian/extraction/location/sources/importers/WikipediaLocationImporterTest.java
+++ palladian-core/src/test/java/ws/palladian/extraction/location/sources/importers/WikipediaLocationImporterTest.java
@@ -47,23 +47,29 @@ public class WikipediaLocationImporterTest {
 
         locations = WikipediaLocationImporter.extractCoordinateTag("{{Coord|57|18|22|N|4|27|32|W|display=title}}");
         assertEquals(1, locations.size());
+        assertEquals("title", locations.get(0).display);
 
         locations = WikipediaLocationImporter.extractCoordinateTag("{{Coord|44.112|N|87.913|W|display=title}}");
         assertEquals(1, locations.size());
+        assertEquals("title", locations.get(0).display);
 
         locations = WikipediaLocationImporter.extractCoordinateTag("{{Coord|44.112|-87.913|display=title}}");
         assertEquals(1, locations.size());
+        assertEquals("title", locations.get(0).display);
 
         locations = WikipediaLocationImporter
                 .extractCoordinateTag("{{Coord|44.117|-87.913|dim:30_region:US-WI_type:event|display=inline,title|name=accident site}}");
         assertEquals(1, locations.size());
+        assertEquals("inline,title", locations.get(0).display);
 
         locations = WikipediaLocationImporter
                 .extractCoordinateTag("{{coord|61.1631|-149.9721|type:landmark_globe:earth_region:US-AK_scale:150000_source:gnis|name=Kulis Air National Guard Base}}");
         assertEquals(1, locations.size());
+        assertEquals("landmark", locations.get(0).type);
 
         locations = WikipediaLocationImporter.extractCoordinateTag("{{coord|46|43|N|7|58|E|type:waterbody}}");
         assertEquals(1, locations.size());
+        assertEquals("waterbody", locations.get(0).type);
 
         locations = WikipediaLocationImporter.extractCoordinateTag("{{coord|51.501|-0.142|dim:120m}}");
         assertEquals(1, locations.size());
@@ -77,38 +83,49 @@ public class WikipediaLocationImporterTest {
         locations = WikipediaLocationImporter
                 .extractCoordinateTag("{{Coord|51|28|N|9|25|W|region:IE_type:isle|display=title}}");
         assertEquals(1, locations.size());
+        assertEquals("title", locations.get(0).display);
 
         locations = WikipediaLocationImporter
                 .extractCoordinateTag("{{Coord|40|43|N|74|0|W|region:US-NY|display=inline}}");
         assertEquals(1, locations.size());
+        assertEquals("inline", locations.get(0).display);
 
         locations = WikipediaLocationImporter
                 .extractCoordinateTag("{{Coord|51|1|41|N|13|43|36|E|type:edu_region:DE-SN|display=title}}");
         assertEquals(1, locations.size());
+        assertEquals("title", locations.get(0).display);
 
         locations = WikipediaLocationImporter
                 .extractCoordinateTag("{{coord|51|3|7|N|13|44|30|E|display=it|region:DE_type:landmark}}");
         assertEquals(1, locations.size());
+        assertEquals("landmark", locations.get(0).type);
+        assertEquals("it", locations.get(0).display);
 
         locations = WikipediaLocationImporter
                 .extractCoordinateTag("{{Coord|38.89767|-77.03655|region:US-DC_type:landmark|display=title}}");
         assertEquals(1, locations.size());
+        assertEquals("landmark", locations.get(0).type);
+        assertEquals("title", locations.get(0).display);
 
         locations = WikipediaLocationImporter
                 .extractCoordinateTag("{{Coord|40|43|N|74|0|W|region:US-NY|display=inline}}");
         assertEquals(1, locations.size());
+        assertEquals("inline", locations.get(0).display);
 
         locations = WikipediaLocationImporter
                 .extractCoordinateTag("{{Coord|40|44|54.36|N|73|59|08.36|W|region:US-NY_type:landmark|name=Empire State Building|display=inline,title}}");
         assertEquals(1, locations.size());
+        assertEquals("landmark", locations.get(0).type);
+        assertEquals("inline,title", locations.get(0).display);
 
         locations = WikipediaLocationImporter.extractCoordinateTag("{{coord|22|S|43|W}}");
         assertEquals(1, locations.size());
 
-        // FIXME 'display' not extracted correctly
         locations = WikipediaLocationImporter
                 .extractCoordinateTag("{{coord|52|28|N|1|55|W|region:GB_type:city|notes=<ref>{{cite web|url=http://www.fallingrain.com/world/UK/0/Birmingham.html|title=Birmingham}}</ref>|display=inline,title}}");
         assertEquals(1, locations.size());
+        assertEquals("city", locations.get(0).type);
+        assertEquals("inline,title", locations.get(0).display);
     }
 
     @Test
