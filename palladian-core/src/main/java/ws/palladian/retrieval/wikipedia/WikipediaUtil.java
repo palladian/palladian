ba853d6cfe6e8ca52aea3e690dfaab740aa60644
Philipp Katz <philipp@philippkatz.de>
2013-05-26 22:58:09 +0200

    Improve wiki markup stripping.

--- palladian-core/src/main/java/ws/palladian/retrieval/wikipedia/WikipediaUtil.java
+++ palladian-core/src/main/java/ws/palladian/retrieval/wikipedia/WikipediaUtil.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import ws.palladian.helper.html.HtmlHelper;
 import ws.palladian.helper.io.FileHelper;
 import ws.palladian.helper.nlp.StringHelper;
 
@@ -14,49 +15,42 @@ import ws.palladian.helper.nlp.StringHelper;
  * Utility functionality for working with <a href="http://www.mediawiki.org/">MediaWiki</a> markup.
  * </p>
  * 
+ * @see <a href="http://en.wikipedia.org/wiki/Help:Wiki_markup">Wiki markup</a>
  * @author Philipp Katz
  */
 public final class WikipediaUtil {
 
+    private static final Pattern REF_PATTERN = Pattern.compile("<ref(?:\\s[^>]*)?>[^<]*</ref>|<ref[^/>]*/>",
+            Pattern.MULTILINE);
+    private static final Pattern HEADING_PATTERN = Pattern.compile("^={1,6}([^=]*)={1,6}$", Pattern.MULTILINE);
+    private static final Pattern CONVERT_PATTERN = Pattern
+            .compile("\\{\\{convert\\|([\\d.]+)\\|([\\w°]+)(\\|[^}]*)?\\}\\}");
+    private static final Pattern INTERNAL_LINK_PATTERN = Pattern.compile("\\[\\[([^|\\]]*)(?:\\|([^|\\]]*))?\\]\\]");
+    private static final Pattern EXTERNAL_LINK_PATTERN = Pattern.compile("\\[http([^\\s]+)(?:\\s([^\\]]+))\\]");
+
     public static String stripMediaWikiMarkup(String markup) {
 
         // strip everything in <ref> tags
-        Pattern refPattern = Pattern.compile("<ref(?:\\s[^>]*)?>[^<]*</ref>|<ref[^/>]*/>", Pattern.MULTILINE);
-        String result = refPattern.matcher(markup).replaceAll("");
-
-        // strip HTML comments
-        result = result.replaceAll("<!--[^>]*-->", "");
+        String result = REF_PATTERN.matcher(markup).replaceAll("");
 
         // resolve HTML entities
         result = StringEscapeUtils.unescapeHtml4(result);
 
+        // remove HTML markup
+        result = HtmlHelper.stripHtmlTags(result);
+
         // replace headlines
-        Pattern titlePattern = Pattern.compile("^={1,4}([^=]*)={1,4}$", Pattern.MULTILINE);
-        result = titlePattern.matcher(result).replaceAll("$1\n");
+        result = HEADING_PATTERN.matcher(result).replaceAll("$1\n");
 
         // replace formatting
-        result = result.replaceAll("'''|''", "");
+        result = result.replaceAll("'''''|'''|''", "");
 
         // replace {{convert|...}} tags
-        Pattern convertPattern = Pattern.compile("\\{\\{convert\\|([\\d.]+)\\|([\\w°]+)(\\|[^}]*)?\\}\\}");
-        result = convertPattern.matcher(result).replaceAll("$1 $2");
+        result = CONVERT_PATTERN.matcher(result).replaceAll("$1 $2");
 
-        // replace links
-        // FIXME not working for internal links
-        Pattern linkPattern = Pattern.compile("\\[\\[?([^|\\]]*)(?:\\|([^|\\]]*))?\\]\\]?");
-        Matcher linkMatcher = linkPattern.matcher(result);
-        StringBuffer buffer = new StringBuffer();
-        while (linkMatcher.find()) {
-            String target = linkMatcher.group(1);
-            String text = linkMatcher.group(2);
-            String replacement = StringUtils.EMPTY;
-            if (!target.toLowerCase().startsWith("category:")) {
-                replacement = text != null ? text : target;
-            }
-            linkMatcher.appendReplacement(buffer, Matcher.quoteReplacement(replacement));
-        }
-        linkMatcher.appendTail(buffer);
-        result = buffer.toString();
+        // replace internal links
+        result = processLinks(result, INTERNAL_LINK_PATTERN);
+        result = processLinks(result, EXTERNAL_LINK_PATTERN);
 
         // remove everything left in between { ... } and [ ... ]
         result = removeArea(result, '{', '}');
@@ -65,10 +59,29 @@ public final class WikipediaUtil {
         // remove single line breaks; but keep lists (lines starting with *)
         result = result.replaceAll("(?<!\n)\n(?![*\n])", " ");
 
+        // remove double whitespace and line breaks, trim
         result = StringHelper.removeDoubleWhitespaces(result);
         result = result.replaceAll("\n{2,}", "\n\n");
         result = result.trim();
+
         return result;
+    }
+
+    private static String processLinks(String string, Pattern linkPattern) {
+        Matcher linkMatcher = linkPattern.matcher(string);
+        StringBuffer buffer = new StringBuffer();
+        while (linkMatcher.find()) {
+            String target = linkMatcher.group(1);
+            String text = linkMatcher.group(2);
+            String replacement = StringUtils.EMPTY;
+            // special treatment: ignore category: links completely for now
+            if (!target.toLowerCase().startsWith("category:")) {
+                replacement = text != null ? text : target;
+            }
+            linkMatcher.appendReplacement(buffer, Matcher.quoteReplacement(replacement));
+        }
+        linkMatcher.appendTail(buffer);
+        return buffer.toString();
     }
 
     private static String removeArea(String string, char begin, char end) {
@@ -93,7 +106,7 @@ public final class WikipediaUtil {
 
     public static void main(String[] args) {
         // String wikipediaPage = FileHelper.readFileToString("/Users/pk/Desktop/newYork.wikipedia");
-        String wikipediaPage = FileHelper.readFileToString("/Users/pk/Desktop/sample.wikipedia");
+        String wikipediaPage = FileHelper.readFileToString("/Users/pk/Desktop/sample2.wikipedia");
         String text = stripMediaWikiMarkup(wikipediaPage);
         System.out.println(text);
     }
