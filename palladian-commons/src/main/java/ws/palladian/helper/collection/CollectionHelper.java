9f8018cdfcde0e6cdeddc2fee7724cc6a82cfc73
Philipp Katz <philipp@philippkatz.de>
2013-06-20 15:52:28 +0200

    CollectionHelper#newTreeMap

--- palladian-commons/src/main/java/ws/palladian/helper/collection/CollectionHelper.java
+++ palladian-commons/src/main/java/ws/palladian/helper/collection/CollectionHelper.java
@@ -13,6 +13,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import org.apache.commons.lang3.Validate;
 import org.apache.commons.lang3.tuple.Pair;
@@ -229,10 +230,22 @@ public final class CollectionHelper {
      * <code>Map&lt;String, Integer&gt; map = CollectionHelper.newHashMap();</code>.
      * </p>
      * 
-     * @return
+     * @return A new {@link HashMap}.
      */
     public static <K, V> HashMap<K, V> newHashMap() {
         return new HashMap<K, V>();
+    }
+
+    /**
+     * <p>
+     * Create a new {@link TreeMap}. This method allows omitting the type parameter when creating the TreeMap:
+     * <code>Map&lt;String, Integer&gt; map = CollectionHelper.newTreeMap();</code>.
+     * </p>
+     * 
+     * @return A new {@link TreeMap}.
+     */
+    public static <K, V> TreeMap<K, V> newTreeMap() {
+        return new TreeMap<K, V>();
     }
 
     /**
@@ -241,7 +254,7 @@ public final class CollectionHelper {
      * <code>List&lt;String&gt; list = CollectionHelper.newArrayList();</code>.
      * </p>
      * 
-     * @return
+     * @return A new {@link ArrayList}.
      */
     public static <E> ArrayList<E> newArrayList() {
         return new ArrayList<E>();
@@ -270,7 +283,7 @@ public final class CollectionHelper {
      * <code>Set&lt;String&gt; set = CollectionHelper.newHashSet();</code>.
      * </p>
      * 
-     * @return
+     * @return A new {@link HashSet}.
      */
     public static <E> HashSet<E> newHashSet() {
         return new HashSet<E>();
