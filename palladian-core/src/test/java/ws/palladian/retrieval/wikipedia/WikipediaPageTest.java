15935561ef0408d7e59ada9421117b6b3b702bfa
Philipp Katz <philipp@philippkatz.de>
2013-07-24 13:40:45 +0200

    Fix plenty of errors encountered on Wikipedia import while extended run.

--- palladian-core/src/test/java/ws/palladian/retrieval/wikipedia/WikipediaPageTest.java
+++ palladian-core/src/test/java/ws/palladian/retrieval/wikipedia/WikipediaPageTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import ws.palladian.helper.collection.CollectionHelper;
 import ws.palladian.helper.io.FileHelper;
 import ws.palladian.helper.io.ResourceHelper;
 import ws.palladian.retrieval.wikipedia.WikipediaPage.WikipediaInfobox;
@@ -17,7 +18,7 @@ public class WikipediaPageTest {
     public void testWikipediaPage() throws FileNotFoundException {
         String markup = FileHelper.readFileToString(ResourceHelper.getResourceFile("/Dresden.wikipedia"));
         WikipediaPage page = new WikipediaPage(0, 0, "Dresden", markup);
-        assertEquals("german location", page.getInfoboxType());
+        assertEquals("german location", page.getInfoboxes().get(0).getName());
         assertEquals(4, page.getCategories().size());
         assertEquals(484, page.getLinks().size());
     }
@@ -30,6 +31,48 @@ public class WikipediaPageTest {
         List<WikipediaInfobox> infoboxes = page.getInfoboxes();
         assertEquals(1, infoboxes.size());
         assertEquals("river", infoboxes.get(0).getName());
+    }
+
+    @Test
+    public void testInfoboxExtraction() throws FileNotFoundException {
+        String markup = FileHelper.readFileToString(ResourceHelper.getResourceFile("/Dresden.wikipedia"));
+        WikipediaPage page = new WikipediaPage(0, 0, "Dresden", markup);
+        List<WikipediaInfobox> infoboxes = page.getInfoboxes();
+        assertEquals(1, infoboxes.size());
+        WikipediaInfobox infobox = CollectionHelper.getFirst(infoboxes);
+        assertEquals(34, infobox.size());
+        assertEquals("Dresden", infobox.getEntry("Name"));
+        assertEquals("City", infobox.getEntry("Art"));
+        assertEquals("Dresden-Altstadt von der Marienbruecke-II.jpg", infobox.getEntry("image_photo"));
+        assertEquals("300px", infobox.getEntry("imagesize"));
+        assertEquals("", infobox.getEntry("image_caption"));
+        // ...
+        assertEquals("1206", infobox.getEntry("year"));
+
+        markup = FileHelper.readFileToString(ResourceHelper.getResourceFile("/Stack_Overflow.wikipedia"));
+        page = new WikipediaPage(0, 0, "Stack Overflow", markup);
+        infoboxes = page.getInfoboxes();
+        assertEquals(1, infoboxes.size());
+        infobox = CollectionHelper.getFirst(infoboxes);
+        assertEquals(17, infobox.size());
+        assertEquals(
+                "84 ({{as of|2013|02|15|alt=February 2013}})<ref name=\"alexa\">{{cite web|url= http://www.alexa.com/siteinfo/stackoverflow.com |title= Stackoverflow.com Site Info | publisher= [[Alexa Internet]] |accessdate= 2013-02-15 }}</ref>",
+                infobox.getEntry("alexa"));
+
+        markup = FileHelper.readFileToString(ResourceHelper
+                .getResourceFile("/wikipedia/Dry_Fork_(Cheat_River).wikipedia"));
+        page = new WikipediaPage(0, 0, "Dry Fork (Cheat River)", markup);
+        infoboxes = page.getInfoboxes();
+        assertEquals(1, infoboxes.size());
+        infobox = CollectionHelper.getFirst(infoboxes);
+        assertEquals("river", infobox.getName());
+        assertEquals(70, infobox.size());
+
+        markup = FileHelper.readFileToString(ResourceHelper
+                .getResourceFile("/wikipedia/Muskingum_University.wikipedia"));
+        page = new WikipediaPage(0, 0, "Muskingum University", markup);
+        infoboxes = page.getInfoboxes();
+        assertEquals(2, infoboxes.size());
     }
 
 }
