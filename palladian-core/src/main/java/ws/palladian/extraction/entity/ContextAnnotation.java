e637e67e1f66b0fd8fa1021511d1cf15ad07cd60
Philipp Katz <philipp@philippkatz.de>
2013-08-01 17:05:37 +0200

    Merge NerAnnotation into ContextAnnotation.
    Former-commit-id: 14cba8b504a13ed20e5d4156bc4b281a1a2b51e8

--- palladian-core/src/main/java/ws/palladian/extraction/entity/ContextAnnotation.java
+++ palladian-core/src/main/java/ws/palladian/extraction/entity/ContextAnnotation.java
@@ -2,10 +2,22 @@ package ws.palladian.extraction.entity;
 
 import org.apache.commons.lang3.StringUtils;
 
+import ws.palladian.classification.CategoryEntries;
+import ws.palladian.classification.CategoryEntriesMap;
+import ws.palladian.extraction.entity.tagger.NerHelper;
 import ws.palladian.helper.nlp.StringHelper;
 import ws.palladian.processing.features.Annotated;
 
-public class ContextAnnotation extends NerAnnotation {
+public class ContextAnnotation implements Annotated {
+
+    /** The category of the instance, null if not classified. */
+    private CategoryEntriesMap tags = new CategoryEntriesMap();
+
+    /** The start index of the annotation in the annotated text. */
+    private int offset;
+
+    /** The annotated entity. */
+    private String entity;
 
     /** The left context of the annotation */
     private final String leftContext;
@@ -14,7 +26,9 @@ public class ContextAnnotation extends NerAnnotation {
     private final String rightContext;
 
     public ContextAnnotation(int offset, String entityName, String tagName, String leftContext, String rightContext) {
-        super(offset, entityName, tagName);
+        this.offset = offset;
+        this.entity = entityName;
+        tags.set(tagName, 1);
         this.leftContext = leftContext;
         this.rightContext = rightContext;
     }
@@ -26,6 +40,66 @@ public class ContextAnnotation extends NerAnnotation {
     public ContextAnnotation(Annotated annotated) {
         this(annotated.getStartPosition(), annotated.getValue(), annotated.getTag(), StringUtils.EMPTY,
                 StringUtils.EMPTY);
+    }
+
+    public CategoryEntries getTags() {
+        return tags;
+    }
+
+    public void setEntity(String entity) {
+        this.entity = entity;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public void setTags(CategoryEntries tags) {
+        this.tags = new CategoryEntriesMap(tags);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Annotation [offset=");
+        builder.append(offset);
+        builder.append(", entity=");
+        builder.append(entity);
+        builder.append(", tag=");
+        builder.append(getTag());
+        builder.append("]");
+        return builder.toString();
+    }
+
+    @Override
+    public int getStartPosition() {
+        return offset;
+    }
+
+    @Override
+    public int getEndPosition() {
+        return offset + entity.length();
+    }
+
+    @Override
+    public String getValue() {
+        return entity;
+    }
+
+    @Override
+    public String getTag() {
+        return getTags().getMostLikelyCategory();
+    }
+
+    @Override
+    public int compareTo(Annotated other) {
+        return this.getStartPosition() - other.getStartPosition();
+    }
+
+    @Override
+    // FIXME this needs to go in parent
+    public boolean overlaps(Annotated annotated) {
+        return NerHelper.overlaps(this, annotated);
     }
 
     public String getLeftContext() {
@@ -134,6 +208,40 @@ public class ContextAnnotation extends NerAnnotation {
         }
 
         return contexts;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((entity == null) ? 0 : entity.hashCode());
+        result = prime * result + offset;
+        result = prime * result + ((tags == null) ? 0 : tags.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ContextAnnotation other = (ContextAnnotation)obj;
+        if (entity == null) {
+            if (other.entity != null)
+                return false;
+        } else if (!entity.equals(other.entity))
+            return false;
+        if (offset != other.offset)
+            return false;
+        if (tags == null) {
+            if (other.tags != null)
+                return false;
+        } else if (!tags.equals(other.tags))
+            return false;
+        return true;
     }
 
 }
