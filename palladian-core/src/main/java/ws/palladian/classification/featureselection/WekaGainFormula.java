bf7cf7b9828b01401a67b830f3ce7899f8f2952d
Klemens Muthmann <klemens_muthmann@gmx.de>
2013-08-19 23:22:31 +0200

    Continued implementation on InformationGainFeatureRanker according to sources from Crossvalidated and StackOverflow.

--- palladian-core/src/main/java/ws/palladian/classification/featureselection/WekaGainFormula.java
+++ palladian-core/src/main/java/ws/palladian/classification/featureselection/WekaGainFormula.java
@@ -10,28 +10,33 @@ import ws.palladian.processing.features.Feature;
 
 /**
  * <p>
- * Implements the Information Gain formula as proposed by the Weka Machine Learning Framework.
+ * Implements the Information Gain formula as proposed by the Weka Machine Learning Framework. A description can be
+ * found under <a href="http://arxiv.org/pdf/nlin/0307015v4.pdf">http://arxiv.org/pdf/nlin/0307015v4.pdf</a> on page 47.
  * </p>
- *
+ * 
  * @author Klemens Muthmann
  * @version 1.0
  * @since 2.1.0
  */
 public final class WekaGainFormula {
-    
+
     public double calculateGain(List<Classified> dataset, List<String> targetClasses, Feature<?> feature) {
         double ret = 0.0;
-        for(String targetClass:targetClasses) {
-            ret += entropy(dataset,targetClass) - entropy(dataset,targetClass,feature);
+        for (String targetClass : targetClasses) {
+            ret += entropy(dataset, targetClass) - conditionalEntropy(dataset, targetClass, feature);
         }
-        return ret/targetClasses.size();
+        return ret / targetClasses.size();
     }
-    
+
     private double entropy(List<Classified> dataset, String targetClass) {
         return 0.0d;
     }
-    
-    private double entropy(List<Classified> dataset, String targetClass, Feature<?> feature) {
+
+    private double jointEntropy(List<Classified> dataset, String targetClass, Feature<?> feature) {
         return 0.0d;
+    }
+
+    private double conditionalEntropy(List<Classified> dataset, String targetClass, Feature<?> feature) {
+        return jointEntropy(dataset, targetClass, feature) - entropy(dataset, feature.getName());
     }
 }
