e28b35e8bf1ded9290dbf8c87283c3710f81910c
Klemens Muthmann <klemens_muthmann@gmx.de>
2013-08-20 23:24:00 +0200

    Fixed InformationGain for dense features (sparse are not working with the current implementation).

--- palladian-core/src/main/java/ws/palladian/classification/featureselection/InformationGainFeatureRanker.java
+++ palladian-core/src/main/java/ws/palladian/classification/featureselection/InformationGainFeatureRanker.java
@@ -10,11 +10,10 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 import org.apache.commons.lang3.Validate;
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ws.palladian.classification.Instance;
 import ws.palladian.helper.collection.CollectionHelper;
 import ws.palladian.processing.Trainable;
 import ws.palladian.processing.features.Feature;
@@ -67,117 +66,123 @@ public final class InformationGainFeatureRanker extends AbstractFeatureRanker {
             return ret;
         }
 
-        Collection<Pair<Set<Feature<?>>, String>> preparedData = prepare(dataset);
-        Map<String, Double> classPriors = calculateTargetClassPriors(dataset);
-        // All occurrences of feature in the dataset
-        Map<Feature<?>, Integer> absoluteOccurences = CollectionHelper.newHashMap();
-        // Occurrences of feature together with a certain target class.
-        Map<Feature<?>, Map<String, Integer>> absoluteConditionalOccurences = CollectionHelper.newHashMap();
-        // the following two lines: vocabulary size required for smoothing
-        Map<String, Integer> featuresInClass = CollectionHelper.newHashMap();
-        Integer sumOfFeaturesInAllClasses = 0;
-
-        for (Pair<Set<Feature<?>>, String> instance : preparedData) {
-
-            for (Feature<?> feature : instance.getLeft()) {
-
-                // count absolute occurrences
-                Integer absoluteOccurrencesOfFeature = absoluteOccurences.get(feature);
-                if (absoluteOccurrencesOfFeature == null) {
-                    absoluteOccurrencesOfFeature = 0;
-                }
-                absoluteOccurrencesOfFeature++;
-                absoluteOccurences.put(feature, absoluteOccurrencesOfFeature);
-
-                // count conditional occurrences with all classes
-                Map<String, Integer> absoluteConditionalOccurence = absoluteConditionalOccurences.get(feature);
-                if (absoluteConditionalOccurence == null) {
-                    absoluteConditionalOccurence = CollectionHelper.newHashMap();
-                }
-                Integer occurrenceInTargetClass = absoluteConditionalOccurence.get(instance.getRight());
-                if (occurrenceInTargetClass == null) {
-                    occurrenceInTargetClass = 0;
-                }
-                occurrenceInTargetClass++;
-                absoluteConditionalOccurence.put(instance.getRight(), occurrenceInTargetClass);
-                absoluteConditionalOccurences.put(feature, absoluteConditionalOccurence);
-
-                // calculate the vocabulary size for smoothing.
-                Integer countOfFeaturesInClass = featuresInClass.get(instance.getRight());
-                if (countOfFeaturesInClass == null) {
-                    countOfFeaturesInClass = 0;
-                }
-                featuresInClass.put(instance.getRight(), ++countOfFeaturesInClass);
-                sumOfFeaturesInAllClasses++;
-            }
+        List<Trainable> preparedData = prepare(dataset);
+        InformationGainFormula formula = new InformationGainFormula();
+        for (Feature<?> preparedFeature : preparedData.get(0).getFeatureVector()) {
+            double gain = formula.calculateGain(preparedData, preparedFeature.getName());
+            ret.put(preparedFeature, gain);
         }
 
-        // calculate dataset constant class probability (first summand)
-        double classProb = 0.0d;
-        for (Entry<String, Double> classCount : classPriors.entrySet()) {
-            double Prci = classCount.getValue();
-            classProb += Prci * ld(Prci);
-        }
-
-        // calculate information gain.
-        for (Entry<Feature<?>, Integer> absoluteOccurrence : absoluteOccurences.entrySet()) {
-            ret.put(absoluteOccurrence.getKey(),
-                    gain(classProb, absoluteOccurrence.getValue(),
-                            absoluteConditionalOccurences.get(absoluteOccurrence.getKey()), dataset, featuresInClass,
-                            sumOfFeaturesInAllClasses));
-        }
+        // Map<String, Double> classPriors = calculateTargetClassPriors(dataset);
+        // // All occurrences of feature in the dataset
+        // Map<Feature<?>, Integer> absoluteOccurences = CollectionHelper.newHashMap();
+        // // Occurrences of feature together with a certain target class.
+        // Map<Feature<?>, Map<String, Integer>> absoluteConditionalOccurences = CollectionHelper.newHashMap();
+        // // the following two lines: vocabulary size required for smoothing
+        // Map<String, Integer> featuresInClass = CollectionHelper.newHashMap();
+        // Integer sumOfFeaturesInAllClasses = 0;
+        //
+        // for (Pair<Set<Feature<?>>, String> instance : preparedData) {
+        //
+        // for (Feature<?> feature : instance.getLeft()) {
+        //
+        // // count absolute occurrences
+        // Integer absoluteOccurrencesOfFeature = absoluteOccurences.get(feature);
+        // if (absoluteOccurrencesOfFeature == null) {
+        // absoluteOccurrencesOfFeature = 0;
+        // }
+        // absoluteOccurrencesOfFeature++;
+        // absoluteOccurences.put(feature, absoluteOccurrencesOfFeature);
+        //
+        // // count conditional occurrences with all classes
+        // Map<String, Integer> absoluteConditionalOccurence = absoluteConditionalOccurences.get(feature);
+        // if (absoluteConditionalOccurence == null) {
+        // absoluteConditionalOccurence = CollectionHelper.newHashMap();
+        // }
+        // Integer occurrenceInTargetClass = absoluteConditionalOccurence.get(instance.getRight());
+        // if (occurrenceInTargetClass == null) {
+        // occurrenceInTargetClass = 0;
+        // }
+        // occurrenceInTargetClass++;
+        // absoluteConditionalOccurence.put(instance.getRight(), occurrenceInTargetClass);
+        // absoluteConditionalOccurences.put(feature, absoluteConditionalOccurence);
+        //
+        // // calculate the vocabulary size for smoothing.
+        // Integer countOfFeaturesInClass = featuresInClass.get(instance.getRight());
+        // if (countOfFeaturesInClass == null) {
+        // countOfFeaturesInClass = 0;
+        // }
+        // featuresInClass.put(instance.getRight(), ++countOfFeaturesInClass);
+        // sumOfFeaturesInAllClasses++;
+        // }
+        // }
+        //
+        // // calculate dataset constant class probability (first summand)
+        // double classProb = 0.0d;
+        // for (Entry<String, Double> classCount : classPriors.entrySet()) {
+        // double Prci = classCount.getValue();
+        // classProb += Prci * ld(Prci);
+        // }
+        //
+        // // calculate information gain.
+        // for (Entry<Feature<?>, Integer> absoluteOccurrence : absoluteOccurences.entrySet()) {
+        // ret.put(absoluteOccurrence.getKey(),
+        // gain(classProb, absoluteOccurrence.getValue(),
+        // absoluteConditionalOccurences.get(absoluteOccurrence.getKey()), dataset, featuresInClass,
+        // sumOfFeaturesInAllClasses));
+        // }
         return ret;
     }
 
-    /**
-     * <p>
-     * 
-     * </p>
-     * 
-     * @param value
-     * @param map
-     * @return
-     */
-    private double gain(Double classProbability, Integer value, Map<String, Integer> map,
-            Collection<? extends Trainable> dataset, Map<String, Integer> featuresInClass, int sumOfFeaturesInAllClasses) {
-        double termClassCoocurrence = 0.0d;
-        double termClassNonCoocurrence = 0.0d;
-        for (Entry<String, Integer> absoluteConditionalOccurence : map.entrySet()) {
-            // Probability for class ci containing term t
-            double Prcit = laplaceSmooth(map.keySet().size(),
-                    featuresInClass.get(absoluteConditionalOccurence.getKey()), absoluteConditionalOccurence.getValue());
-            termClassCoocurrence += Prcit * ld(Prcit);
+    // /**
+    // * <p>
+    // *
+    // * </p>
+    // *
+    // * @param value
+    // * @param map
+    // * @return
+    // */
+    // private double gain(Double classProbability, Integer value, Map<String, Integer> map,
+    // Collection<? extends Trainable> dataset, Map<String, Integer> featuresInClass, int sumOfFeaturesInAllClasses) {
+    // double termClassCoocurrence = 0.0d;
+    // double termClassNonCoocurrence = 0.0d;
+    // for (Entry<String, Integer> absoluteConditionalOccurence : map.entrySet()) {
+    // // Probability for class ci containing term t
+    // double Prcit = laplaceSmooth(map.keySet().size(),
+    // featuresInClass.get(absoluteConditionalOccurence.getKey()), absoluteConditionalOccurence.getValue());
+    // termClassCoocurrence += Prcit * ld(Prcit);
+    //
+    // // Probability for class ci not containing term t
+    // double Prcint = laplaceSmooth(map.keySet().size(),
+    // sumOfFeaturesInAllClasses - featuresInClass.get(absoluteConditionalOccurence.getKey()),
+    // dataset.size() - absoluteConditionalOccurence.getValue());
+    // termClassNonCoocurrence += Prcint * ld(Prcint);
+    // }
+    // double termProb = value.doubleValue() / dataset.size() * termClassCoocurrence;
+    //
+    // double nonTermProb = (double)(dataset.size() - value) / dataset.size() * termClassNonCoocurrence;
+    //
+    // return -classProbability + termProb + nonTermProb;
+    // // double gain = .0;
+    // // for(Entry<String, Integer> entry:map.entrySet()) {
+    // // int probability = entry.getValue()/value;
+    // // gain = gain - probability * ld(probability);
+    // // }
+    // // return gain;
+    // }
 
-            // Probability for class ci not containing term t
-            double Prcint = laplaceSmooth(map.keySet().size(),
-                    sumOfFeaturesInAllClasses - featuresInClass.get(absoluteConditionalOccurence.getKey()),
-                    dataset.size() - absoluteConditionalOccurence.getValue());
-            termClassNonCoocurrence += Prcint * ld(Prcint);
-        }
-        double termProb = value.doubleValue() / dataset.size() * termClassCoocurrence;
-
-        double nonTermProb = (double)(dataset.size() - value) / dataset.size() * termClassNonCoocurrence;
-
-        return -classProbability + termProb + nonTermProb;
-        // double gain = .0;
-        // for(Entry<String, Integer> entry:map.entrySet()) {
-        // int probability = entry.getValue()/value;
-        // gain = gain - probability * ld(probability);
-        // }
-        // return gain;
-    }
-
-    /**
-     * <p>
-     * Calculates the base 2 logarithm for the provided argument.
-     * </p>
-     * 
-     * @param arg The argument to calculate the logarithm for.
-     * @return The base 2 logarithm for the provided argument.
-     */
-    private double ld(double arg) {
-        return Math.log(arg) / Math.log(2);
-    }
+    // /**
+    // * <p>
+    // * Calculates the base 2 logarithm for the provided argument.
+    // * </p>
+    // *
+    // * @param arg The argument to calculate the logarithm for.
+    // * @return The base 2 logarithm for the provided argument.
+    // */
+    // private double ld(double arg) {
+    // return Math.log(arg) / Math.log(2);
+    // }
 
     /**
      * <p>
@@ -188,52 +193,54 @@ public final class InformationGainFeatureRanker extends AbstractFeatureRanker {
      * @return The prepared dataset. Each entry corresponds to one instance from the dataset and contains the prepared
      *         {@link Feature}s from that instance's {@link FeatureVector} and the instance's target class.
      */
-    private Collection<Pair<Set<Feature<?>>, String>> prepare(Collection<? extends Trainable> dataset) {
-        Collection<Pair<Set<Feature<?>>, String>> ret = CollectionHelper.newHashSet();
+    private List<Trainable> prepare(Collection<? extends Trainable> dataset) {
+        List<Trainable> ret = CollectionHelper.newArrayList();
 
         for (Trainable instance : dataset) {
             // deduplicate // TODO is this necessary? Is it possible to include a duplicate feature in the feature
             // vector? is the same word at different positions the same feature?
             Set<Feature<?>> features = convertToSet(instance.getFeatureVector(), dataset);
 
-            ret.add(new ImmutablePair<Set<Feature<?>>, String>(features, instance.getTargetClass()));
+            Instance preparedInstance = new Instance(instance.getTargetClass());
+            preparedInstance.getFeatureVector().addAll(features);
+            ret.add(preparedInstance);
         }
 
         return ret;
     }
 
-    /**
-     * <p>
-     * Calculates the priors for all target classes occurring in the dataset.
-     * </p>
-     * 
-     * @param dataset The dataset to calculate the target class priors for.
-     * @return A mapping from target class to prior.
-     */
-    private Map<String, Double> calculateTargetClassPriors(final Collection<? extends Trainable> dataset) {
-        Map<String, Double> ret = CollectionHelper.newHashMap();
-        Map<String, Integer> absoluteOccurrences = CollectionHelper.newHashMap();
-
-        for (Trainable instance : dataset) {
-            Integer absoluteOccurrenceOfClass = absoluteOccurrences.get(instance.getTargetClass());
-            if (absoluteOccurrenceOfClass == null) {
-                absoluteOccurrenceOfClass = 0;
-            }
-            absoluteOccurrenceOfClass++;
-            absoluteOccurrences.put(instance.getTargetClass(), absoluteOccurrenceOfClass);
-        }
-
-        for (Entry<String, Integer> absoluteOccurrenceOfClass : absoluteOccurrences.entrySet()) {
-            ret.put(absoluteOccurrenceOfClass.getKey(),
-                    absoluteOccurrenceOfClass.getValue().doubleValue() / dataset.size());
-        }
-
-        return ret;
-    }
-
-    private static double laplaceSmooth(int vocabularySize, int countOfFeature, int countOfCoocurence) {
-        return (1.0d + countOfCoocurence) / (vocabularySize + countOfFeature);
-    }
+    // /**
+    // * <p>
+    // * Calculates the priors for all target classes occurring in the dataset.
+    // * </p>
+    // *
+    // * @param dataset The dataset to calculate the target class priors for.
+    // * @return A mapping from target class to prior.
+    // */
+    // private Map<String, Double> calculateTargetClassPriors(final Collection<? extends Trainable> dataset) {
+    // Map<String, Double> ret = CollectionHelper.newHashMap();
+    // Map<String, Integer> absoluteOccurrences = CollectionHelper.newHashMap();
+    //
+    // for (Trainable instance : dataset) {
+    // Integer absoluteOccurrenceOfClass = absoluteOccurrences.get(instance.getTargetClass());
+    // if (absoluteOccurrenceOfClass == null) {
+    // absoluteOccurrenceOfClass = 0;
+    // }
+    // absoluteOccurrenceOfClass++;
+    // absoluteOccurrences.put(instance.getTargetClass(), absoluteOccurrenceOfClass);
+    // }
+    //
+    // for (Entry<String, Integer> absoluteOccurrenceOfClass : absoluteOccurrences.entrySet()) {
+    // ret.put(absoluteOccurrenceOfClass.getKey(),
+    // absoluteOccurrenceOfClass.getValue().doubleValue() / dataset.size());
+    // }
+    //
+    // return ret;
+    // }
+    //
+    // private static double laplaceSmooth(int vocabularySize, int countOfFeature, int countOfCoocurence) {
+    // return (1.0d + countOfCoocurence) / (vocabularySize + countOfFeature);
+    // }
 
     @Override
     public FeatureRanking rankFeatures(Collection<? extends Trainable> dataset) {
