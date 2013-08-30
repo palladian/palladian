67888bfb226ebaa2c26fb57f9f68d3a0c6c20cf6
David <davidurbansky@gmail.com>
2013-08-30 17:27:07 +0200

    minor fix

---
+++ palladian-commons/src/main/java/ws/palladian/helper/collection/RoundRobinList.java
@@ -0,0 +1,19 @@
+package ws.palladian.helper.collection;
+
+import java.util.ArrayList;
+
+public class RoundRobinList<E> extends ArrayList<E> {
+
+    private static final long serialVersionUID = 8357139694464153452L;
+
+    public E getNextItem() {
+        if (iterator().hasNext()) {
+            return iterator().next();
+        }
+
+        // reset the iterator
+        listIterator();
+        return iterator().next();
+    }
+
+}
