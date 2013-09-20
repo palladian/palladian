cb438850de7fc9a38c54233d4d596ec2077856cf
Philipp Katz <philipp@philippkatz.de>
2013-09-20 22:28:24 +0200

    Further update deprectated parts, remove JPathHelper.

--- palladian-core/src/main/java/ws/palladian/extraction/location/sources/FreebaseLocationSource.java
+++ palladian-core/src/main/java/ws/palladian/extraction/location/sources/FreebaseLocationSource.java
@@ -1,13 +1,11 @@
 package ws.palladian.extraction.location.sources;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,8 +16,13 @@ import ws.palladian.extraction.location.LocationType;
 import ws.palladian.helper.UrlHelper;
 import ws.palladian.helper.collection.CollectionHelper;
 import ws.palladian.helper.constants.Language;
-import ws.palladian.retrieval.DocumentRetriever;
-import ws.palladian.retrieval.helper.JsonObjectWrapper;
+import ws.palladian.retrieval.HttpException;
+import ws.palladian.retrieval.HttpResult;
+import ws.palladian.retrieval.HttpRetriever;
+import ws.palladian.retrieval.HttpRetrieverFactory;
+import ws.palladian.retrieval.parser.json.JsonArray;
+import ws.palladian.retrieval.parser.json.JsonException;
+import ws.palladian.retrieval.parser.json.JsonObject;
 
 /**
  * <p>
@@ -27,7 +30,7 @@ import ws.palladian.retrieval.helper.JsonObjectWrapper;
  * </p>
  * 
  * @author David Urbansky
- * 
+ * @see <a href="http://wiki.freebase.com/wiki/How_to_obtain_an_API_key">How to obtain an API key</a>
  */
 public class FreebaseLocationSource extends SingleQueryLocationSource implements LocationSource {
 
@@ -55,14 +58,16 @@ public class FreebaseLocationSource extends SingleQueryLocationSource implements
         temp.put("Region", LocationType.REGION);
         temp.put("US County", LocationType.UNIT);
         temp.put("Membership organization", LocationType.UNIT);
-
         LOCATION_MAPPING = Collections.unmodifiableMap(temp);
     }
 
     private final String apiKey;
 
+    private final HttpRetriever httpRetriever;
+
     public FreebaseLocationSource(String apiKey) {
         this.apiKey = apiKey;
+        this.httpRetriever = HttpRetrieverFactory.getHttpRetriever();
     }
 
     @Override
@@ -74,40 +79,31 @@ public class FreebaseLocationSource extends SingleQueryLocationSource implements
     public List<Location> getLocations(String locationName, Set<Language> languages) {
         LOGGER.warn("getLocations(String,EnumSet<Language>) is not supported, ignoring language parameter");
         List<Location> locations = CollectionHelper.newArrayList();
-        DocumentRetriever documentRetriever = new DocumentRetriever();
 
-        String url = "https://www.googleapis.com/freebase/v1/search?query=" + UrlHelper.encodeParameter(locationName)
-                + "&filter=(any%20type:/location/)&key=" + apiKey;
+        String url = String.format(
+                "https://www.googleapis.com/freebase/v1/search?query=%s&filter=(any%%20type:/location/)&key=%s",
+                UrlHelper.encodeParameter(locationName), apiKey);
         LOGGER.debug("check {}", url);
-        JSONObject locationCandidatesObject = documentRetriever.getJSONObject(url);
-        if (locationCandidatesObject == null) {
-            throw new IllegalStateException("Null return from DocumentRetriever");
-        }
 
-        JSONArray locationCandidates = new JSONArray();
         try {
-            locationCandidates = locationCandidatesObject.getJSONArray("result");
-        } catch (JSONException e1) {
-            e1.printStackTrace();
-        }
+            HttpResult httpResult = httpRetriever.httpGet(url);
+            JsonObject locationCandidatesObject = new JsonObject(httpResult.getStringContent());
 
-        for (int i = 0; i < locationCandidates.length(); i++) {
-            try {
-                JsonObjectWrapper locationCandidate = new JsonObjectWrapper(locationCandidates.getJSONObject(i));
-                String concept = locationCandidate.get("notable/name", String.class);
-                // Double score = locationCandidate.getDouble("score");
+            JsonArray locationCandidates = locationCandidatesObject.getJsonArray("result");
+            for (int i = 0; i < locationCandidates.size(); i++) {
+                JsonObject locationCandidate = locationCandidates.getJsonObject(i);
+                JsonObject notableJson = locationCandidate.getJsonObject("notable");
+                if (notableJson == null) {
+                    continue;
+                }
+                String concept = notableJson.getString("name");
 
-                if (concept != null
-                        && (concept.equalsIgnoreCase("city/town/village")
-                                || concept.equalsIgnoreCase("country")
-                                || concept
-                                .equalsIgnoreCase("continent"))) {
+                if (Arrays.asList("city/town/village", "country", "continent").contains(concept.toLowerCase())) {
 
-                    JSONObject jsonObject = documentRetriever
-                            .getJSONObject("https://www.googleapis.com/freebase/v1/topic/"
-                                    + locationCandidate.getString("id") + "?key=" + apiKey);
-
-                    JsonObjectWrapper json = new JsonObjectWrapper(jsonObject);
+                    String topicUrl = String.format("https://www.googleapis.com/freebase/v1/topic/%s?key=%s",
+                            locationCandidate.getString("id"), apiKey);
+                    HttpResult topicResult = httpRetriever.httpGet(topicUrl);
+                    JsonObject jsonObject = new JsonObject(topicResult.getStringContent());
 
                     String primaryName = locationCandidate.getString("name");
                     LocationType locationType = LOCATION_MAPPING.get(concept);
@@ -115,45 +111,42 @@ public class FreebaseLocationSource extends SingleQueryLocationSource implements
                     Double longitude = null;
                     Long population = null;
 
-                    JsonObjectWrapper property = json.getJSONObject("property");
-                    try {
-                    } catch (Exception e) {
-                    }
+                    JsonObject property = jsonObject.getJsonObject("property");
 
                     try {
                         // population
-                        JsonObjectWrapper populationObject = property
-                                .getJSONObject("/location/statistical_region/population");
-                        JSONObject populationProperty = populationObject.get("values[0]/property", JSONObject.class);
-                        JSONArray valuesArray = populationProperty.getJSONObject(
-                                "/measurement_unit/dated_integer/number").getJSONArray("values");
-                        population = valuesArray.getJSONObject(0).getLong("value");
+                        JsonObject populationObject = property.getJsonObject("/location/statistical_region/population");
+                        JsonObject populationProperty = populationObject.queryJsonObject("values[0]/property");
+                        JsonArray valuesArray = populationProperty.getJsonObject(
+                                "/measurement_unit/dated_integer/number").getJsonArray("values");
+                        population = valuesArray.getJsonObject(0).getLong("value");
                     } catch (Exception e) {
                     }
 
                     try {
-                        JsonObjectWrapper locationObject = property.getJSONObject("/location/location/geolocation");
-                        JSONObject locationProperty = locationObject.get("values[0]/property", JSONObject.class);
+                        JsonObject locationObject = property.getJsonObject("/location/location/geolocation");
+                        JsonObject locationProperty = locationObject.queryJsonObject("values[0]/property");
 
                         // latitude
-                        JSONArray valuesArray = locationProperty.getJSONObject("/location/geocode/latitude")
-                                .getJSONArray("values");
-                        latitude = valuesArray.getJSONObject(0).getDouble("value");
+                        JsonArray valuesArray = locationProperty.getJsonObject("/location/geocode/latitude")
+                                .getJsonArray("values");
+                        latitude = valuesArray.getJsonObject(0).getDouble("value");
 
                         // longitude
-                        valuesArray = locationProperty.getJSONObject("/location/geocode/longitude").getJSONArray(
+                        valuesArray = locationProperty.getJsonObject("/location/geocode/longitude").getJsonArray(
                                 "values");
-                        longitude = valuesArray.getJSONObject(0).getDouble("value");
-
+                        longitude = valuesArray.getJsonObject(0).getDouble("value");
                     } catch (Exception e) {
                     }
 
                     locations
-                    .add(new ImmutableLocation(-1, primaryName, locationType, latitude, longitude, population));
+                            .add(new ImmutableLocation(-1, primaryName, locationType, latitude, longitude, population));
                 }
-            } catch (JSONException e) {
-                e.printStackTrace();
             }
+        } catch (JsonException e) {
+            throw new IllegalStateException(e);
+        } catch (HttpException e) {
+            throw new IllegalStateException(e);
         }
 
         return locations;
