f9592259968ec8e275580eb872d79c90199c73c3
Philipp Katz <philipp@philippkatz.de>
2013-10-09 14:07:29 +0200

    Update NewsSeecr searcher, remove MashapeUtil (closes #172).

--- palladian-retrieval/src/main/java/ws/palladian/retrieval/search/news/NewsSeecrSearcher.java
+++ palladian-retrieval/src/main/java/ws/palladian/retrieval/search/news/NewsSeecrSearcher.java
@@ -1,6 +1,5 @@
 package ws.palladian.retrieval.search.news;
 
-import java.security.GeneralSecurityException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -8,26 +7,26 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.configuration.Configuration;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ws.palladian.extraction.location.GeoCoordinate;
 import ws.palladian.helper.collection.CollectionHelper;
-import ws.palladian.helper.constants.Language;
 import ws.palladian.retrieval.HttpException;
 import ws.palladian.retrieval.HttpRequest;
 import ws.palladian.retrieval.HttpRequest.HttpMethod;
 import ws.palladian.retrieval.HttpResult;
 import ws.palladian.retrieval.HttpRetriever;
 import ws.palladian.retrieval.HttpRetrieverFactory;
-import ws.palladian.retrieval.helper.MashapeUtil;
 import ws.palladian.retrieval.parser.json.JsonArray;
 import ws.palladian.retrieval.parser.json.JsonException;
 import ws.palladian.retrieval.parser.json.JsonObject;
 import ws.palladian.retrieval.resources.BasicWebContent;
 import ws.palladian.retrieval.resources.WebContent;
-import ws.palladian.retrieval.search.AbstractSearcher;
+import ws.palladian.retrieval.search.AbstractMultifacetSearcher;
+import ws.palladian.retrieval.search.MultifacetQuery;
+import ws.palladian.retrieval.search.SearchResults;
 import ws.palladian.retrieval.search.SearcherException;
 
 /**
@@ -40,7 +39,7 @@ import ws.palladian.retrieval.search.SearcherException;
  *      authentication mechanism</a>
  * @author Philipp Katz
  */
-public final class NewsSeecrSearcher extends AbstractSearcher<WebContent> {
+public final class NewsSeecrSearcher extends AbstractMultifacetSearcher<WebContent> {
 
     /** The logger for this class. */
     private static final Logger LOGGER = LoggerFactory.getLogger(NewsSeecrSearcher.class);
@@ -48,47 +47,19 @@ public final class NewsSeecrSearcher extends AbstractSearcher<WebContent> {
     private static final String SEARCHER_NAME = "NewsSeecr";
 
     private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+    
+    private static final String DATE_PARAMETER_FORMAT = "yyyy-MM-dd";
 
-    private static final String BASE_URL = "https://qqilihq-newsseecr.p.mashape.com/news/search";
+    private static final String BASE_URL = "https://qqilihq-newsseecr.p.mashape.com/news";
 
     private static final int RESULTS_PER_REQUEST = 100;
 
-    private final String mashapePublicKey;
-
-    private final String mashapePrivateKey;
-
     private final String mashapeKey;
-    
+
     private final HttpRetriever retriever = HttpRetrieverFactory.getHttpRetriever();
 
-    /** Configuraiton key for the Mashape public key. */
-    @Deprecated
-    public static final String CONFIG_MASHAPE_PUBLIC_KEY = "api.newsseecr.mashapePublicKey";
-    /** Configuration key for the Mashape private key. */
-    @Deprecated
-    public static final String CONFIG_MASHAPE_PRIVATE_KEY = "api.newsseecr.mashapePrivateKey";
     /** Configuration key for the Mashape key. */
     public static final String CONFIG_MASHAPE_KEY = "api.newsseecr.mashapeKey";
-
-    /**
-     * <p>
-     * Create a new {@link NewsSeecrSearcher} with the provided credentials from Mashape.
-     * </p>
-     * 
-     * @param mashapePublicKey The Mashape public key, not empty or <code>null</code>.
-     * @param mashapePrivateKey The Mashape private key, not empty or <code>null</code>.
-     * @deprecated Prefer using the {@link #NewsSeecrSearcher(String)} and supply the new Mashape key. See <a
-     *             href="http://blog.mashape.com/important-changes-to-mashape-authorization-ke">here</a> for more
-     *             information.
-     */
-    @Deprecated
-    public NewsSeecrSearcher(String mashapePublicKey, String mashapePrivateKey) {
-        Validate.notEmpty(mashapePublicKey, "mashapePublicKey must not be empty");
-        Validate.notEmpty(mashapePrivateKey, "mashapePrivateKey must not be empty");
-        this.mashapePublicKey = mashapePublicKey;
-        this.mashapePrivateKey = mashapePrivateKey;
-        this.mashapeKey = null;
-    }
 
     /**
      * <p>
@@ -100,8 +71,6 @@ public final class NewsSeecrSearcher extends AbstractSearcher<WebContent> {
     public NewsSeecrSearcher(String mashapeKey) {
         Validate.notEmpty(mashapeKey, "mashapeKey must not be empty");
         this.mashapeKey = mashapeKey;
-        this.mashapePublicKey = null;
-        this.mashapePrivateKey = null;
     }
 
     /**
@@ -110,27 +79,12 @@ public final class NewsSeecrSearcher extends AbstractSearcher<WebContent> {
      * {@link Configuration}.
      * </p>
      * 
-     * @param configuration The configuration supplying the Mashape key as {@value #CONFIG_MASHAPE_KEY}. (Old,
-     *            deprecated authentication scheme is also accepted with public key as
-     *            {@value #CONFIG_MASHAPE_PUBLIC_KEY}, private key as {@value #CONFIG_MASHAPE_PRIVATE_KEY}). Not
+     * @param configuration The configuration supplying the Mashape key as {@value #CONFIG_MASHAPE_KEY}. Not
      *            <code>null</code>.
      */
     public NewsSeecrSearcher(Configuration configuration) {
         String mashapeKey = configuration.getString(CONFIG_MASHAPE_KEY);
-        String publicKey = configuration.getString(CONFIG_MASHAPE_PUBLIC_KEY);
-        String privateKey = configuration.getString(CONFIG_MASHAPE_PRIVATE_KEY);
-        if (StringUtils.isNotEmpty(mashapeKey)) {
-            this.mashapeKey = mashapeKey;
-            this.mashapePublicKey = null;
-            this.mashapePrivateKey = null;
-        } else if (StringUtils.isNotEmpty(publicKey) && StringUtils.isNotEmpty(privateKey)) {
-            this.mashapeKey = null;
-            this.mashapePublicKey = publicKey;
-            this.mashapePrivateKey = privateKey;
-        } else {
-            throw new IllegalArgumentException(
-                    "The authentication must either be supplied as one Mashape key, or as public/private key combination (old scheme).");
-        }
+        this.mashapeKey = mashapeKey;
     }
 
     @Override
@@ -138,26 +92,15 @@ public final class NewsSeecrSearcher extends AbstractSearcher<WebContent> {
         return SEARCHER_NAME;
     }
 
-    @SuppressWarnings("deprecation")
     @Override
-    public List<WebContent> search(String query, int resultCount, Language language) throws SearcherException {
+    public SearchResults<WebContent> search(MultifacetQuery query) throws SearcherException {
 
+        final int resultCount = query.getResultCount();
         List<WebContent> webResults = CollectionHelper.newArrayList();
 
         for (int offset = 0; offset < Math.ceil((double)resultCount / RESULTS_PER_REQUEST); offset++) {
 
-            HttpRequest request = new HttpRequest(HttpMethod.GET, BASE_URL);
-            request.addParameter("query", query);
-            request.addParameter("page", offset);
-            request.addParameter("numResults", Math.min(resultCount, RESULTS_PER_REQUEST));
-
-            if (mashapePrivateKey != null && mashapePublicKey != null) {
-                LOGGER.debug("Use old authentication scheme with private/public key");
-                MashapeUtil.signRequest(request, mashapePublicKey, mashapePrivateKey);
-            } else {
-                LOGGER.debug("Use now authentication scheme");
-                request.addHeader("X-Mashape-Authorization", mashapeKey);
-            }
+            HttpRequest request = makeRequest(query, resultCount, offset);
 
             LOGGER.debug("Performing request: " + request);
             HttpResult result;
@@ -168,11 +111,9 @@ public final class NewsSeecrSearcher extends AbstractSearcher<WebContent> {
                         + e.getMessage(), e);
             }
             if (result.getStatusCode() != 200) {
-				// TODO get message
-				throw new SearcherException("Encountered HTTP status "
-						+ result.getStatusCode()
-						+ " when executing the request: " + request
-						+ ", result: " + result.getStringContent());
+                // TODO get message
+                throw new SearcherException("Encountered HTTP status " + result.getStatusCode()
+                        + " when executing the request: " + request + ", result: " + result.getStringContent());
             }
 
             String jsonString = result.getStringContent();
@@ -199,7 +140,36 @@ public final class NewsSeecrSearcher extends AbstractSearcher<WebContent> {
             }
         }
 
-        return webResults;
+        return new SearchResults<WebContent>(webResults);
+    }
+
+    private HttpRequest makeRequest(MultifacetQuery query, int resultCount, int offset) {
+        HttpRequest request = new HttpRequest(HttpMethod.GET, BASE_URL);
+        request.addParameter("page", offset);
+        request.addParameter("numResults", Math.min(resultCount, RESULTS_PER_REQUEST));
+        if (query.getText() != null) {
+            request.addParameter("query", query.getText());
+        }
+        if (query.getStartDate() != null) {
+            request.addParameter("startDate", formatParameterDate(query.getStartDate()));
+        }
+        if (query.getEndDate() != null) {
+            request.addParameter("endDate", formatParameterDate(query.getEndDate()));
+        }
+        GeoCoordinate coordinate = query.getCoordinate();
+        if (coordinate != null) {
+            String coordinateParam = String.format("%s,%s", coordinate.getLatitude(), coordinate.getLongitude());
+            request.addParameter("coordinates", coordinateParam);
+        }
+        if (query.getRadius() != null) {
+            request.addParameter("distance", query.getRadius());
+        }
+        request.addHeader("X-Mashape-Authorization", mashapeKey);
+        return request;
+    }
+
+    private String formatParameterDate(Date date) {
+        return new SimpleDateFormat(DATE_PARAMETER_FORMAT).format(date);
     }
 
     public static Date parseDate(String dateString) {
@@ -212,16 +182,23 @@ public final class NewsSeecrSearcher extends AbstractSearcher<WebContent> {
         }
     }
 
-    public static void main(String[] args) throws SearcherException, GeneralSecurityException {
+    public static void main(String[] args) throws SearcherException {
         // old, deprecated:
         // String publicKey = "u3ewnlzvxvbg3gochzqcrulimgngsb";
         // String privateKey = "dxkyimj8rjoyti1mqx2lqragbbg71k";
         // NewsSeecrSearcher searcher = new NewsSeecrSearcher(publicKey, privateKey);
 
         // new:
-        String mashapeKey = "...";
+        String mashapeKey = "tr1dn3mc0bdhzzjngkvzahqloxph0e";
         NewsSeecrSearcher searcher = new NewsSeecrSearcher(mashapeKey);
-        List<WebContent> results = searcher.search("obama", 20);
+        // List<WebContent> results = searcher.search("obama", 20);
+        MultifacetQuery.Builder builder = new MultifacetQuery.Builder();
+        builder.setCoordinate(38.89, -77.023611);
+        builder.setRadius(25.);
+        // builder.setText("afghanistan");
+        // builder.setStartDate(DateParser.parseDate("2012-11-01").getNormalizedDate());
+        // builder.setEndDate(DateParser.parseDate("2012-11-15").getNormalizedDate());
+        SearchResults<WebContent> results = searcher.search(builder.create());
         CollectionHelper.print(results);
     }
 
