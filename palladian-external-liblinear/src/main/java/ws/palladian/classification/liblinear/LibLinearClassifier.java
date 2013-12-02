7d7906f8927b37a00df5eade0248307627fbd293
Philipp Katz <philipp@philippkatz.de>
2013-12-02 14:26:41 +0100

    Avoid exception when classifying instances with unknown features in LibLinear + test.

--- palladian-external-liblinear/src/main/java/ws/palladian/classification/liblinear/LibLinearClassifier.java
+++ palladian-external-liblinear/src/main/java/ws/palladian/classification/liblinear/LibLinearClassifier.java
@@ -6,13 +6,16 @@ import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.bwaldvogel.liblinear.Linear;
 import ws.palladian.classification.CategoryEntries;
 import ws.palladian.classification.CategoryEntriesMap;
 import ws.palladian.classification.Classifier;
+import ws.palladian.classification.utils.ClassificationUtils;
+import ws.palladian.helper.collection.EqualsFilter;
+import ws.palladian.helper.collection.Filter;
 import ws.palladian.helper.io.Slf4JOutputStream;
 import ws.palladian.helper.io.Slf4JOutputStream.Level;
 import ws.palladian.processing.Classifiable;
+import de.bwaldvogel.liblinear.Linear;
 
 /**
  * <p>
@@ -35,6 +38,7 @@ public final class LibLinearClassifier implements Classifier<LibLinearModel> {
     public CategoryEntries classify(Classifiable classifiable, LibLinearModel model) {
         Validate.notNull(classifiable, "classifiable must not be null");
         Validate.notNull(model, "model must not be null");
+        classifiable = removeUntrainedFeatures(classifiable, model);
         model.getNormalization().normalize(classifiable);
         de.bwaldvogel.liblinear.Feature[] instance = LibLinearLearner.makeInstance(model.getFeatureLabels(),
                 classifiable, model.getLLModel().getBias());
@@ -45,6 +49,20 @@ public final class LibLinearClassifier implements Classifier<LibLinearModel> {
             categoryEntries.add(model.getCategoryForIndex(i), probabilities[i]);
         }
         return categoryEntries;
+    }
+
+    /**
+     * Remove those features, which we have not trained.
+     */
+    private Classifiable removeUntrainedFeatures(Classifiable classifiable, LibLinearModel model) {
+        int oldSize = classifiable.getFeatureVector().size();
+        Filter<String> nameFilter = EqualsFilter.create(model.getFeatureLabels());
+        classifiable = ClassificationUtils.filterFeatures(classifiable, nameFilter);
+        int numIgnored = oldSize - classifiable.getFeatureVector().size();
+        if (numIgnored > 0 && LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Ignoring {} unknown features", numIgnored);
+        }
+        return classifiable;
     }
 
 }
--- palladian-external-liblinear/src/test/java/ws/palladian/classification/liblinear/LibLinearTest.java
+++ palladian-external-liblinear/src/test/java/ws/palladian/classification/liblinear/LibLinearTest.java
@@ -15,19 +15,14 @@ import ws.palladian.classification.InstanceBuilder;
 import ws.palladian.helper.collection.CollectionHelper;
 import ws.palladian.helper.math.ConfusionMatrix;
 import ws.palladian.processing.Trainable;
+import ws.palladian.processing.features.FeatureVector;
 
 public class LibLinearTest {
 
     @Test
     public void testLiblinear() {
         // sample data taken from de.bwaldvogel.liblinear.Problem
-        List<Trainable> data = CollectionHelper.newArrayList();
-        data.add(new InstanceBuilder().set("a", 0).set("b", 0.1).set("c", 0.2).set("d", 0).set("e", 0).create("1"));
-        data.add(new InstanceBuilder().set("a", 0).set("b", 0.1).set("c", 0.3).set("d", -1.2).set("e", 0).create("2"));
-        data.add(new InstanceBuilder().set("a", 0.4).set("b", 0).set("c", 0).set("d", 0).set("e", 0).create("1"));
-        data.add(new InstanceBuilder().set("a", 0).set("b", 0.1).set("c", 0).set("d", 1.4).set("e", 0.5).create("2"));
-        data.add(new InstanceBuilder().set("a", -0.1).set("b", -0.2).set("c", 0.1).set("d", 1.1).set("e", 0.1)
-                .create("3"));
+        List<Trainable> data = createSampleData();
         LibLinearModel model = new LibLinearLearner().train(data);
         // System.out.println(model);
 
@@ -37,6 +32,24 @@ public class LibLinearTest {
         assertEquals("1", classifier.classify(data.get(2), model).getMostLikelyCategory());
         // assertEquals("2", liblinear.classify(data.get(3), model).getMostLikelyCategory());
         assertEquals("3", classifier.classify(data.get(4), model).getMostLikelyCategory());
+    }
+
+    private List<Trainable> createSampleData() {
+        List<Trainable> data = CollectionHelper.newArrayList();
+        data.add(new InstanceBuilder().set("a", 0).set("b", 0.1).set("c", 0.2).set("d", 0).set("e", 0).create("1"));
+        data.add(new InstanceBuilder().set("a", 0).set("b", 0.1).set("c", 0.3).set("d", -1.2).set("e", 0).create("2"));
+        data.add(new InstanceBuilder().set("a", 0.4).set("b", 0).set("c", 0).set("d", 0).set("e", 0).create("1"));
+        data.add(new InstanceBuilder().set("a", 0).set("b", 0.1).set("c", 0).set("d", 1.4).set("e", 0.5).create("2"));
+        data.add(new InstanceBuilder().set("a", -0.1).set("b", -0.2).set("c", 0.1).set("d", 1.1).set("e", 0.1)
+                .create("3"));
+        return data;
+    }
+    
+    @Test
+    public void testUntrainedFeature() {
+        LibLinearModel model = new LibLinearLearner().train(createSampleData());
+        FeatureVector featureVector = new InstanceBuilder().set("a", 0.4).set("b", 0).set("c", 0).set("e", 0).set("f", 0).create();
+        assertEquals("1", new LibLinearClassifier().classify(featureVector, model).getMostLikelyCategory());
     }
 
     @Test
