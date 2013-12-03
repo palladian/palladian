20ced8cbf2e0278568468a89e3f224feeb5f3a9e
Philipp Katz <philipp@philippkatz.de>
2013-12-03 21:11:29 +0100

    + ReflectionFunction + test, HttpRetrieverFactory uses Factory interface.
    Former-commit-id: 8e9dd8c8f184343c05d2af62494bb4dc51c66077

--- palladian-commons/src/test/java/ws/palladian/helper/collection/FilterChainTest.java
+++ palladian-commons/src/test/java/ws/palladian/helper/collection/FilterChainTest.java
@@ -11,6 +11,7 @@ public class FilterChainTest {
     public void testFilterChain() {
         RegexFilter filter1 = new RegexFilter("[a-z]+");
         EqualsFilter<String> filter2 = EqualsFilter.create("apple", "banana", "cranberry", "Durian");
+        @SuppressWarnings("unchecked")
         Filter<String> chain = new FilterChain<String>(filter1, filter2);
         assertFalse(chain.accept("kiwi"));
         assertTrue(chain.accept("apple"));
