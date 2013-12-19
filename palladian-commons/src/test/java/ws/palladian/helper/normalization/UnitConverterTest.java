667ebcfb10e1c2dced2741800c9f38ae1286af27
David Urbansky <davidurbansky@gmail.com>
2013-12-19 13:50:33 +0100

    added test case

--- palladian-commons/src/test/java/ws/palladian/helper/normalization/UnitConverterTest.java
+++ palladian-commons/src/test/java/ws/palladian/helper/normalization/UnitConverterTest.java
@@ -21,6 +21,8 @@ public class UnitConverterTest {
         assertEquals(273.15, UnitConverter.convert(32., "fahrenheit", "kelvin"), 0.01);
         assertEquals(0., UnitConverter.convert(273.15, "kelvin", "celsius"), 0.01);
         assertEquals(32., UnitConverter.convert(273.15, "kelvin", "fahrenheit"), 0.01);
+
+        assertEquals(3.53, UnitConverter.convert(100., "mL", "ounces"), 0.01);
     }
 
 }
