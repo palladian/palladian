91a1f0103d940a2dbfc26541efc1a1bb945a6a07
Philipp Katz <philipp@philippkatz.de>
2013-12-23 18:50:33 +0100

    Allow to handle rate limits in TwitterSearcher.

---
+++ palladian-retrieval/src/main/java/ws/palladian/retrieval/search/RateLimitedException.java
@@ -0,0 +1,49 @@
+package ws.palladian.retrieval.search;
+
+/**
+ * <p>
+ * Exception, when the usage quota for a Web-based {@link Searcher} is exhausted.
+ * </p>
+ * 
+ * @author pk
+ * 
+ */
+public class RateLimitedException extends SearcherException {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer timeUntilReset;
+
+    /**
+     * <p>
+     * Create a new {@link RateLimitedException}.
+     * 
+     * @param message The message, not <code>null</code> or empty.
+     * @param timeUntilReset The time in seconds, until the block expires. May be <code>null</code> in case this
+     *            information is not available.
+     */
+    public RateLimitedException(String message, Integer timeUntilReset) {
+        super(message);
+        this.timeUntilReset = timeUntilReset;
+    }
+
+    /**
+     * @return The time in seconds, until the block expires. <code>null</code> in case this information is not provided.
+     */
+    public Integer getTimeUntilReset() {
+        return timeUntilReset;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("RateLimitedException [");
+        if (timeUntilReset != null) {
+            builder.append("timeUntilReset=");
+            builder.append(timeUntilReset);
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+
+}
