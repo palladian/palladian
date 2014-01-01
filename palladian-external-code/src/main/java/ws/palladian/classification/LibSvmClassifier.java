1435997999a0d4e3d227edcdc87c37284f54a48d
Philipp Katz <philipp@philippkatz.de>
2014-01-01 22:12:15 +0100

    wip

--- palladian-external-code/src/main/java/ws/palladian/classification/LibSvmClassifier.java
+++ palladian-external-code/src/main/java/ws/palladian/classification/LibSvmClassifier.java
@@ -3,29 +3,12 @@
  */
 package ws.palladian.classification;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import libsvm.svm;
 import libsvm.svm_node;
 
 import org.apache.commons.lang.Validate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import ws.palladian.classification.utils.Normalization;
 import ws.palladian.processing.Classifiable;
-import ws.palladian.processing.Trainable;
-import ws.palladian.processing.features.Feature;
-import ws.palladian.processing.features.FeatureVector;
-import ws.palladian.processing.features.ListFeature;
-import ws.palladian.processing.features.NominalFeature;
-import ws.palladian.processing.features.NumericFeature;
-import ws.palladian.processing.features.utils.FeatureUtils;
 
 /**
  * <p>
@@ -33,14 +16,11 @@ import ws.palladian.processing.features.utils.FeatureUtils;
  * </p>
  * 
  * @author Klemens Muthmann
- * @version 1.0
+ * @author Philipp Katz
+ * @version 2.0
  * @since 2.0
  */
 public final class LibSvmClassifier implements Classifier<LibSvmModel> {
-
-    private final static Logger LOGGER = LoggerFactory.getLogger(LibSvmClassifier.class);
-
-    private Map<NominalFeature, List<String>> possibleNominalValues;
 
     @Override
     public CategoryEntries classify(Classifiable classifiable, LibSvmModel model) {
@@ -49,11 +29,8 @@ public final class LibSvmClassifier implements Classifier<LibSvmModel> {
         
         CategoryEntriesMap ret = new CategoryEntriesMap();
 
-//        svm_node[] libsvmFeatureVector = transformPalladianFeatureVectorToLibsvmFeatureVector(
-//                classifiable.getFeatureVector(), model.getSchema(), false, model.getNormalization());
-        
-        svm_node[] libsvmFeatureVector = LibSvmLearner.transformPalladianFeatureVectorToLibsvmFeatureVector(
-                classifiable.getFeatureVector(), model.getSchema(), false, model.getNormalization());
+        svm_node[] libsvmFeatureVector = LibSvmLearner.convertFeatureVector(classifiable, model.getSchema(),
+                model.getNormalization(), model.getDummyCoder());
 
         double classIndex = svm.svm_predict(model.getModel(), libsvmFeatureVector);
         String className = model.transformClassToString(Double.valueOf(classIndex).intValue());
