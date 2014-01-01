061ad8af7e34e6dbb2a43b6885560f4eace623f4
Philipp Katz <philipp@philippkatz.de>
2014-01-01 19:32:03 +0100

    Build timestamps; need to check whether to integrate this in every project or how ...

---
+++ palladian-commons/src/main/java-templates/ws/palladian/PalladianVersion.java
@@ -0,0 +1,24 @@
+package ws.palladian;
+
+/**
+ * <p>
+ * This class provides constants with version information and copyright about the Palladian library.
+ * </p>
+ * 
+ * @author Philipp Katz
+ */
+public interface PalladianVersion {
+
+    /** Palladian version number. */
+    final String VERSION = "${project.version}";
+
+    /** Palladian build. */
+    final String BUILD = "${timestamp}";
+
+    /** Palladian copyright. */
+    final String COPYRIGHT = "Copyright 2009-2014 by David Urbansky, Philipp Katz, Klemens Muthmann";
+
+    /** Palladian info. */
+    final String INFO = "Palladian version " + VERSION + " (build " + BUILD + ")\n" + COPYRIGHT;
+
+}
