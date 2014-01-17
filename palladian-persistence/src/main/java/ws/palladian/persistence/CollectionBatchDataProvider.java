c5cc316ee5c7fc79db97db2378ddd5a52b5c9ba8
Philipp Katz <philipp@philippkatz.de>
2014-01-17 16:26:28 +0100

    +CollectionBatchDataProvider
    Former-commit-id: 9cb3915c24257f4fdc17387ace2adb2851a4993b

---
+++ palladian-persistence/src/main/java/ws/palladian/persistence/CollectionBatchDataProvider.java
@@ -0,0 +1,55 @@
+package ws.palladian.persistence;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang3.Validate;
+
+/**
+ * <p>
+ * A {@link BatchDataProvider} for a {@link Collection}. Only the conversion method needs to be implemented, in case,
+ * one cares about the inserted IDs, optionally implement {@link #insertedItem(int, int)}.
+ * </p>
+ * 
+ * @author pk
+ * 
+ * @param <T>
+ */
+public abstract class CollectionBatchDataProvider<T> implements BatchDataProvider {
+
+    private final Iterator<T> iterator;
+
+    private final int count;
+
+    public CollectionBatchDataProvider(Collection<T> collection) {
+        Validate.notNull(collection, "collection must not be null");
+        this.iterator = collection.iterator();
+        this.count = collection.size();
+    }
+
+    @Override
+    public final List<? extends Object> getData(int number) {
+        return getData(iterator.next());
+    }
+
+    /**
+     * @param nextItem the item in the {@link Collection} which to convert.
+     * @return List with parameters for the SQL statement, not <code>null</code>.
+     */
+    public abstract List<? extends Object> getData(T nextItem);
+
+    /**
+     * Override in case, you need to retrieve the generated IDs.
+     */
+    @Override
+    public void insertedItem(int number, int generatedId) {
+        // no operation per default.
+    }
+
+    @Override
+    public final int getCount() {
+        return count;
+    }
+
+}
