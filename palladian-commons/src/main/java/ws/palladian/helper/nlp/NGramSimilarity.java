f8109470ce608f890ece8b2dbc8cfac93f242e7b
Philipp Katz <philipp@philippkatz.de>
2014-01-28 01:28:13 +0100

    Fix for NGramSimilarity

--- palladian-commons/src/main/java/ws/palladian/helper/nlp/NGramSimilarity.java
+++ palladian-commons/src/main/java/ws/palladian/helper/nlp/NGramSimilarity.java
@@ -1,7 +1,9 @@
 package ws.palladian.helper.nlp;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
@@ -17,7 +19,62 @@ import ws.palladian.helper.collection.CollectionHelper;
  */
 public class NGramSimilarity implements StringSimilarity {
 
+    /** A similarity measure between two sets. */
+    public static interface SetSimilarity {
+        <T> double calculate(Collection<T> c1, Collection<T> c2);
+    }
+
+    public static final SetSimilarity DICE = new SetSimilarity() {
+        public <T> double calculate(Collection<T> c1, Collection<T> c2) {
+            Set<T> nGramsCommon = new HashSet<T>(c1);
+            nGramsCommon.retainAll(c2);
+            return (double)(2 * nGramsCommon.size()) / (c1.size() + c2.size());
+        }
+    };
+
+    public static final SetSimilarity JACCARD = new SetSimilarity() {
+        @Override
+        public <T> double calculate(Collection<T> c1, Collection<T> c2) {
+            Set<T> intersection = new HashSet<T>(c1);
+            intersection.retainAll(c2);
+            if (intersection.size() == 0) {
+                return 0;
+            }
+            Set<T> union = new HashSet<T>(c1);
+            union.addAll(c2);
+            return (double)intersection.size() / union.size();
+        }
+    };
+
+    public static final SetSimilarity OVERLAP = new SetSimilarity() {
+        @Override
+        public <T> double calculate(Collection<T> c1, Collection<T> c2) {
+            if (c1.size() == 0 || c2.size() == 0) {
+                return 0;
+            }
+            Set<T> intersection = new HashSet<T>(c1);
+            intersection.retainAll(c2);
+            return (double)intersection.size() / Math.min(c1.size(), c2.size());
+        }
+    };
+
     private final int n;
+    private final SetSimilarity setSimilarity;
+
+    /**
+     * <p>
+     * Create a new n-gram based {@link StringSimilarity} with the specified length for the n-grams.
+     * </p>
+     * 
+     * @param n The length of the n-grams, must be greater or equal 2.
+     * @param setSimilarity The similarity measure used for the sets, not <code>null</code>.
+     */
+    public NGramSimilarity(int n, SetSimilarity setSimilarity) {
+        Validate.isTrue(n >= 2, "n must be greater or equal 2.");
+        Validate.notNull(setSimilarity, "setSimilarity must not be null");
+        this.n = n;
+        this.setSimilarity = setSimilarity;
+    }
 
     /**
      * <p>
@@ -27,19 +84,19 @@ public class NGramSimilarity implements StringSimilarity {
      * @param n The length of the n-grams, must be greater or equal 2.
      */
     public NGramSimilarity(int n) {
-        Validate.isTrue(n >= 2, "n must be greater or equal 2.");
-        this.n = n;
+        this(n, DICE);
     }
 
     @Override
     public double getSimilarity(String s1, String s2) {
         Validate.notNull(s1, "s1 must not be null");
         Validate.notNull(s2, "s2 must not be null");
+        if (s1.equals(s2)) {
+            return 1;
+        }
         List<String> nGrams1 = createNGrams(s1, n);
         List<String> nGrams2 = createNGrams(s2, n);
-        List<String> nGramsCommon = new ArrayList<String>(nGrams1);
-        nGramsCommon.retainAll(nGrams2);
-        return (double)(2 * nGramsCommon.size()) / (nGrams1.size() + nGrams2.size());
+        return setSimilarity.calculate(nGrams1, nGrams2);
     }
 
     /**
@@ -63,6 +120,17 @@ public class NGramSimilarity implements StringSimilarity {
             ret.add(s.substring(i, i + n));
         }
         return ret;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("NGramSimilarity [n=");
+        builder.append(n);
+        builder.append(", setSimilarity=");
+        builder.append(setSimilarity.getClass().getSimpleName());
+        builder.append("]");
+        return builder.toString();
     }
 
 }
--- palladian-commons/src/test/java/ws/palladian/helper/nlp/NGramSimilarityTest.java
+++ palladian-commons/src/test/java/ws/palladian/helper/nlp/NGramSimilarityTest.java
@@ -5,18 +5,49 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class NGramSimilarityTest {
-    
+
+    private static final double DELTA = 0.01;
+
     @Test
     public void testNGramSimilarity() {
-        NGramSimilarity similarity = new NGramSimilarity(3);
-        assertEquals(0.0, similarity.getSimilarity("", "string"), 0.01);
-        assertEquals(1.0, similarity.getSimilarity("a", "a"),0.01);
-        assertEquals(1.0, similarity.getSimilarity("cat", "cat"),0.01);
-        assertEquals(0.727, similarity.getSimilarity("cat", "caat"),0.01);
-        assertEquals(0.0, similarity.getSimilarity("cat", "dog"),0.01);
-        assertEquals(0.824, similarity.getSimilarity("philipp", "philip"),0.01);
-        assertEquals(0.333, similarity.getSimilarity("philipp", "p"),0.01);
-        assertEquals(0.5, similarity.getSimilarity("word", "wort"),0.01);
+        NGramSimilarity similarity = new NGramSimilarity(3, NGramSimilarity.DICE);
+        assertEquals(1.0, similarity.getSimilarity("", ""), DELTA);
+        assertEquals(0.0, similarity.getSimilarity("", "string"), DELTA);
+        assertEquals(1.0, similarity.getSimilarity("a", "a"), DELTA);
+        assertEquals(1.0, similarity.getSimilarity("cat", "cat"), DELTA);
+        assertEquals(0.727, similarity.getSimilarity("cat", "caat"), DELTA);
+        assertEquals(0.0, similarity.getSimilarity("cat", "dog"), DELTA);
+        assertEquals(0.824, similarity.getSimilarity("philipp", "philip"), DELTA);
+        assertEquals(0.333, similarity.getSimilarity("philipp", "p"), DELTA);
+        assertEquals(0.5, similarity.getSimilarity("word", "wort"), DELTA);
+    }
+
+    @Test
+    public void testNGramSimilarityJaccard() {
+        NGramSimilarity similarity = new NGramSimilarity(3, NGramSimilarity.JACCARD);
+        assertEquals(1.0, similarity.getSimilarity("", ""), DELTA);
+        assertEquals(0.0, similarity.getSimilarity("", "string"), DELTA);
+        assertEquals(1.0, similarity.getSimilarity("a", "a"), DELTA);
+        assertEquals(1.0, similarity.getSimilarity("cat", "cat"), DELTA);
+        assertEquals(0.571, similarity.getSimilarity("cat", "caat"), DELTA);
+        assertEquals(0.0, similarity.getSimilarity("cat", "dog"), DELTA);
+        assertEquals(0.7, similarity.getSimilarity("philipp", "philip"), DELTA);
+        assertEquals(0.2, similarity.getSimilarity("philipp", "p"), DELTA);
+        assertEquals(0.333, similarity.getSimilarity("word", "wort"), DELTA);
+    }
+
+    @Test
+    public void testNGramSimilarityOverlap() {
+        NGramSimilarity similarity = new NGramSimilarity(3, NGramSimilarity.OVERLAP);
+        assertEquals(1.0, similarity.getSimilarity("", ""), DELTA);
+        assertEquals(0.0, similarity.getSimilarity("", "string"), DELTA);
+        assertEquals(1.0, similarity.getSimilarity("a", "a"), DELTA);
+        assertEquals(1.0, similarity.getSimilarity("cat", "cat"), DELTA);
+        assertEquals(0.8, similarity.getSimilarity("cat", "caat"), DELTA);
+        assertEquals(0.0, similarity.getSimilarity("cat", "dog"), DELTA);
+        assertEquals(0.875, similarity.getSimilarity("philipp", "philip"), DELTA);
+        assertEquals(0.666, similarity.getSimilarity("philipp", "p"), DELTA);
+        assertEquals(0.5, similarity.getSimilarity("word", "wort"), DELTA);
     }
 
 }
