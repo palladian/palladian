a95bcd3aa79aa1d12639ed66887aeb6ee18bb93c
Philipp Katz <philipp@philippkatz.de>
2014-02-03 20:13:34 +0100

    Possibility for Function chaining.
    Former-commit-id: 3966b11693dd56fc54985a07ac7420f2382cc8bd

---
+++ palladian-commons/src/main/java/ws/palladian/helper/collection/LinkedFunctionBuilder.java
@@ -0,0 +1,108 @@
+package ws.palladian.helper.collection;
+
+import org.apache.commons.lang3.Validate;
+
+/**
+ * <p>
+ * This class allows sequentially chaining of multiple functions. It provides syntactic sugar by using the builder
+ * pattern (start by invoking static {@link #with(Function)} method), which ensures type safety, and does not allow
+ * chaining of incompatible functions, and finally produces a typed {@link Function} representing the actual chain.
+ * Example usage:
+ * 
+ * <pre>
+ * Function&lt;String, Long&gt; function = LinkedFunctionBuilder // use the #with method
+ *         .with(new StringToIntegerFunction()) // transforming String to Integer
+ *         .add(new NumberSquareRootFunction()) // transforming Number to Double
+ *         .add(new RoundFunction()) // transforming Number to Long
+ *         .create(); // result is transforming String to Long
+ * 
+ * // notice the type safety, we know that the input is String,
+ * // and the output is Long
+ * Long result = function.compute(&quot;two&quot;);
+ * </pre>
+ * 
+ * @author pk
+ * 
+ * @param <I> Input type into first function.
+ * @param <E> Exchange type between first and second function.
+ * @param <O> Output type of second function.
+ */
+public final class LinkedFunctionBuilder {
+
+    /**
+     * <p>
+     * Create a new linked function with the given {@link Function} as first function.
+     * 
+     * @param function The first function in the chain, not <code>null</code>.
+     * @return A builder, which allows adding more functions.
+     */
+    public static <I, O> Builder<I, O> with(Function<I, O> function) {
+        Validate.notNull(function, "function must not be null");
+        return new Builder<I, O>(function);
+    }
+
+    public static class Builder<I, E> {
+
+        private final Function<I, E> firstFunction;
+
+        /** Instantiation only from enclosing class. */
+        private Builder(Function<I, E> function) {
+            this.firstFunction = function;
+        }
+
+        /**
+         * <p>
+         * Connect the given function with the existing predecessor.
+         * 
+         * @param function The function to connect, not <code>null</code>.
+         * @return A builder, which allows adding more function.
+         */
+        public <O> Builder<I, O> add(Function<? super E, O> function) {
+            Validate.notNull(function, "function must not be null");
+            return new Builder<I, O>(new LinkedFunction<I, E, O>(firstFunction, function));
+        }
+
+        /**
+         * <p>
+         * Create the final {@link Function}.
+         * 
+         * @return The function which consists of all specified functions.
+         */
+        public Function<I, E> create() {
+            return firstFunction;
+        }
+
+    }
+
+    /**
+     * <p>
+     * A function connecting two functions.
+     * 
+     * @author pk
+     * 
+     * @param <I> Input type into first function.
+     * @param <E> Exchange type between first and second function.
+     * @param <O> Output type of second function.
+     */
+    private static final class LinkedFunction<I, E, O> implements Function<I, O> {
+        private final Function<I, E> f1;
+        private final Function<? super E, O> f2;
+
+        public LinkedFunction(Function<I, E> f1, Function<? super E, O> f2) {
+            this.f1 = f1;
+            this.f2 = f2;
+        }
+
+        @Override
+        public O compute(I input) {
+            return f2.compute(f1.compute(input));
+        }
+
+        @Override
+        public String toString() {
+            return f1.toString() + " > " + f2.toString();
+        }
+
+    }
+
+}
---
+++ palladian-commons/src/test/java/ws/palladian/helper/collection/LinkedFunctionBuilderTest.java
@@ -0,0 +1,46 @@
+package ws.palladian.helper.collection;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class LinkedFunctionBuilderTest {
+
+    private class StringToIntegerFunction implements Function<String, Integer> {
+        @Override
+        public Integer compute(String input) {
+            return Arrays.asList("zero", "one", "two", "three", "four").indexOf(input.toLowerCase());
+        }
+    }
+
+    private class NumberSquareRootFunction implements Function<Number, Double> {
+        @Override
+        public Double compute(Number input) {
+            return Math.sqrt(input.doubleValue());
+        }
+    }
+
+    private class RoundFunction implements Function<Number, Long> {
+        @Override
+        public Long compute(Number input) {
+            return Math.round(input.doubleValue());
+        }
+    }
+
+    @Test
+    public void testLinkedFunction() {
+        Function<String, Long> function = LinkedFunctionBuilder //
+                .with(new StringToIntegerFunction()) //
+                .add(new NumberSquareRootFunction()) //
+                .add(new RoundFunction()) //
+                .create(); //
+
+        List<String> values = Arrays.asList("four", "two", "one");
+        List<Long> convertedValues = CollectionHelper.convertList(values, function);
+        assertEquals(Arrays.asList(2l, 1l, 1l), convertedValues);
+    }
+
+}
