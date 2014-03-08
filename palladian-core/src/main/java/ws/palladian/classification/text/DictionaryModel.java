7ae117ae959d2ea8219e5598d365f973cfe80d19
Philipp Katz <philipp@philippkatz.de>
2014-03-08 12:27:52 +0100

    Using linked list instead of array.
    Former-commit-id: f6436abc5658cd71fe9201de31e49a5b8308d3a9

--- palladian-core/src/main/java/ws/palladian/classification/text/DictionaryModel.java
+++ palladian-core/src/main/java/ws/palladian/classification/text/DictionaryModel.java
@@ -284,7 +284,9 @@ public final class DictionaryModel implements Model, Iterable<TermCategoryEntrie
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + CollectionHelper.newHashSet(entryArray).hashCode();
+        for (TermCategoryEntries entries : this) {
+            result += entries.hashCode();
+        }
         result = prime * result + (featureSetting == null ? 0 : featureSetting.hashCode());
         result = prime * result + numTerms;
         result = prime * result + priors.hashCode();
