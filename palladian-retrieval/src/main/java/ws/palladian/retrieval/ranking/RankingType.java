f755927ae8ceec14a410e7dc99eb253e6be5c07c
Philipp Katz <philipp@philippkatz.de>
2014-04-20 01:20:59 +0200

    SharedCount working (closes #287).

--- palladian-retrieval/src/main/java/ws/palladian/retrieval/ranking/RankingType.java
+++ palladian-retrieval/src/main/java/ws/palladian/retrieval/ranking/RankingType.java
@@ -1,5 +1,8 @@
 package ws.palladian.retrieval.ranking;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
+
 /**
  * <p>
  * A ranking value type for a RankingService, since every service can have more than one distinct ranking values. A
@@ -7,7 +10,7 @@ package ws.palladian.retrieval.ranking;
  * </p>
  * 
  * @author Julien Schmehl
- * 
+ * @author Philipp Katz
  */
 public class RankingType {
 
@@ -25,9 +28,23 @@ public class RankingType {
      * @param description A short description of this type
      */
     public RankingType(String id, String name, String description) {
+        Validate.notEmpty(id, "id must not be empty");
+        Validate.notEmpty(name, "name must not be empty");
         this.id = id.replace(" ", "");
         this.name = name;
         this.description = description;
+    }
+
+    /**
+     * <p>
+     * Initialize a new RankingType with the specified parameters.
+     * </p>
+     * 
+     * @param id A unique id for this type, max. 31 chars, without whitespaces, e.g. 'bitly_clicks'
+     * @param name A human readable name for this type
+     */
+    public RankingType(String id, String name) {
+        this(id, name, StringUtils.EMPTY);
     }
 
     public String getId() {
@@ -45,4 +62,32 @@ public class RankingType {
     public String toString() {
         return this.name;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id.hashCode();
+        result = prime * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        RankingType other = (RankingType)obj;
+        if (!id.equals(other.id)) {
+            return false;
+        }
+        if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+
 }
