42927a73f17a66ce7452ed2b0635e7d3b00cb773
Philipp Katz <philipp@philippkatz.de>
2014-06-02 10:47:02 +0200

    WIP integration testing.

--- palladian-core/src/integration-test/java/ws/palladian/extraction/location/NewsSeecrLocationExtractorIT.java
+++ palladian-core/src/integration-test/java/ws/palladian/extraction/location/NewsSeecrLocationExtractorIT.java
@@ -3,18 +3,34 @@ package ws.palladian.extraction.location;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.junit.Test;
+
+import ws.palladian.helper.io.ResourceHelper;
 
 public class NewsSeecrLocationExtractorIT {
 
-    // XXX move to properties
-    private static final String MASHAPE_TEST_KEY = "tr1dn3mc0bdhzzjngkvzahqloxph0e";
+    private static String mashapeTestKey = getMashapeTestKey();
+
+    private static String getMashapeTestKey() {
+        try {
+            PropertiesConfiguration config = new PropertiesConfiguration(
+                    ResourceHelper.getResourceFile("/palladian-test.properties"));
+            return config.getString("api.newsseecr.mashapeKey");
+        } catch (ConfigurationException e) {
+            throw new IllegalStateException("Could not read configuration");
+        } catch (FileNotFoundException e) {
+            throw new IllegalStateException("Configuration file not found");
+        }
+    }
 
     @Test
     public void testNewsSeecrLocationExtractor() {
-        LocationExtractor extractor = new NewsSeecrLocationExtractor(MASHAPE_TEST_KEY);
+        LocationExtractor extractor = new NewsSeecrLocationExtractor(mashapeTestKey);
         String text = "It's an odd thing, but anyone who disappears is said to be seen in San Francisco. (Oscar Wilde)";
         List<LocationAnnotation> annotations = extractor.getAnnotations(text);
         assertEquals(1, annotations.size());
--- palladian-core/src/integration-test/java/ws/palladian/extraction/location/PalladianLocationExtractorIT.java
+++ palladian-core/src/integration-test/java/ws/palladian/extraction/location/PalladianLocationExtractorIT.java
@@ -1,5 +1,6 @@
 package ws.palladian.extraction.location;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeTrue;
 
@@ -11,35 +12,61 @@ import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import ws.palladian.classification.dt.QuickDtModel;
+import ws.palladian.extraction.location.disambiguation.FeatureBasedDisambiguation;
 import ws.palladian.extraction.location.disambiguation.FeatureBasedDisambiguationLearner;
 import ws.palladian.extraction.location.disambiguation.HeuristicDisambiguation;
 import ws.palladian.extraction.location.disambiguation.LocationDisambiguation;
 import ws.palladian.extraction.location.evaluation.LocationExtractionEvaluator;
 import ws.palladian.extraction.location.evaluation.LocationExtractionEvaluator.LocationEvaluationResult;
+import ws.palladian.extraction.location.persistence.LocationDatabase;
 import ws.palladian.extraction.location.sources.NewsSeecrLocationSource;
 import ws.palladian.helper.io.ResourceHelper;
+import ws.palladian.persistence.DatabaseManagerFactory;
 
 public class PalladianLocationExtractorIT {
-    /** Mashape API key for accessing the NewsSeecr location source. */
-    private static String mashapeTestKey;
-    /** Path to the training data. */
-    private static String trainDataSet;
-    /** Path to the validation data. */
-    private static String validationDataSet;
 
+    /** The logger for this class. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(PalladianLocationExtractorIT.class);
+
+    /** The gazetteer. */
+    private static LocationSource locationSource;
+    /** Path to the training data. */
+    private static File trainDataSet;
+    /** Path to the validation data. */
+    private static File validationDataSet;
+
+    @SuppressWarnings("deprecation")
     @BeforeClass
     public static void readConfiguration() throws ConfigurationException {
         try {
             PropertiesConfiguration config = new PropertiesConfiguration(
                     ResourceHelper.getResourceFile("/palladian-test.properties"));
-            trainDataSet = config.getString("dataset.tudloc2013.train");
-            validationDataSet = config.getString("dataset.tudloc2013.test");
-            assumeDirectory(trainDataSet, validationDataSet);
-            mashapeTestKey = config.getString("api.newsseecr.mashapeKey");
-            assumeTrue("api.newsseecr.mashapeKey configuration is missing", StringUtils.isNotBlank(mashapeTestKey));
+            String trainDataPath = config.getString("dataset.tudloc2013.train");
+            String validationDataPath = config.getString("dataset.tudloc2013.test");
+            assumeDirectory(trainDataPath, validationDataPath);
+            trainDataSet = new File(trainDataPath);
+            validationDataSet = new File(validationDataPath);
+
+            String dbUrl = config.getString("db.jdbcUrl");
+            String dbUsername = config.getString("db.username");
+            String dbPassword = config.getString("db.password");
+            String mashapeTestKey = config.getString("api.newsseecr.mashapeKey");
+            if (StringUtils.isNotBlank(dbUrl) && StringUtils.isNotBlank(dbPassword)) {
+                locationSource = DatabaseManagerFactory.create(LocationDatabase.class, dbUrl, dbUsername, dbPassword);
+                LOGGER.info("Using local DB ({}) for testing", dbUrl);
+            } else if (StringUtils.isNotBlank(mashapeTestKey)) {
+                locationSource = new NewsSeecrLocationSource(mashapeTestKey);
+                LOGGER.info("Using NewsSeecr DB for testing");
+            } else {
+                assumeTrue(
+                        "palladian-test.properties must either provide a database configuration, or a Mashape API key",
+                        false);
+            }
         } catch (FileNotFoundException e) {
             fail("palladian-test.properties not found; test is skipped!");
         }
@@ -57,22 +84,38 @@ public class PalladianLocationExtractorIT {
     }
 
     @Test
-//    @Ignore
     public void testPalladianLocationExtractor_Heuristic() {
-        LocationSource locationSource = new NewsSeecrLocationSource(mashapeTestKey);
         LocationDisambiguation disambiguation = new HeuristicDisambiguation();
         LocationExtractor extractor = new PalladianLocationExtractor(locationSource, disambiguation);
-//        LocationExtractor extractor = new NewsSeecrLocationExtractor(mashapeTestKey);
-        LocationEvaluationResult result = LocationExtractionEvaluator.run(extractor, new File(validationDataSet), true);
-        System.out.println(result);
+        LocationEvaluationResult result = LocationExtractionEvaluator.run(extractor, validationDataSet, true);
+        // System.out.println(result);
+        assertGreater("MUC-Pr", result.mucPr, 0.8163);
+        assertGreater("MUC-Rc", result.mucRc, 0.7287);
+        assertGreater("MUC-F1", result.mucF1, 0.7700);
+        assertGreater("Geo-Pr", result.geoPr, 0.9214);
+        assertGreater("Geo-Rc", result.geoRc, 0.7825);
+        assertGreater("Geo-F1", result.geoF1, 0.8463);
     }
-    
+
     @Test
-    @Ignore
-    public void testPalladianLocationExtractor_FeatureBased() throws IOException {
-        LocationSource locationSource = new NewsSeecrLocationSource(mashapeTestKey);
-        FeatureBasedDisambiguationLearner learner = new FeatureBasedDisambiguationLearner(locationSource);
-        learner.learn(new File(trainDataSet));
+    public void testPalladianLocationExtractor_MachineLearning() throws IOException {
+        FeatureBasedDisambiguationLearner learner = new FeatureBasedDisambiguationLearner(locationSource, 100);
+        QuickDtModel model = learner.learn(trainDataSet);
+        LocationDisambiguation disambiguation = new FeatureBasedDisambiguation(model);
+        LocationExtractor extractor = new PalladianLocationExtractor(locationSource, disambiguation);
+        LocationEvaluationResult result = LocationExtractionEvaluator.run(extractor, validationDataSet, true);
+        System.out.println(result);
+        // assertGreater("MUC-Pr", result.mucPr, 0.81);
+        // assertGreater("MUC-Rc", result.mucRc, 0.75);
+        assertGreater("MUC-F1", result.mucF1, 0.78);
+        // assertGreater("Geo-Pr", result.geoPr, 0.92);
+        // assertGreater("Geo-Rc", result.geoRc, 0.79);
+        assertGreater("Geo-F1", result.geoF1, 0.84);
     }
-    
+
+    private static void assertGreater(String valueName, double value, double minimumExpected) {
+        String msg = valueName + " must be greater/equal " + minimumExpected + ", but was " + value;
+        assertTrue(msg, value >= minimumExpected);
+    }
+
 }
