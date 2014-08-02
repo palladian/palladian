a4455645e906c23ebe76dbec80d758a24e3adc71
Philipp Katz <philipp@philippkatz.de>
2014-08-02 16:24:13 +0200

    wip

--- palladian-core/src/main/java/ws/palladian/classification/text/LinkedCategoryEntries.java
+++ palladian-core/src/main/java/ws/palladian/classification/text/LinkedCategoryEntries.java
@@ -3,14 +3,72 @@ package ws.palladian.classification.text;
 import java.util.Iterator;
 
 import ws.palladian.classification.AbstractCategoryEntries;
+import ws.palladian.classification.ImmutableCategory;
 import ws.palladian.core.Category;
+import ws.palladian.helper.collection.AbstractIterator;
 
 public class LinkedCategoryEntries extends AbstractCategoryEntries {
-
+    
+//    public static final LinkedCategoryEntries EMPTY = new LinkedCategoryEntries();
+    
+    private LinkedCategoryCount firstCategory;
+    
+    private int totalCount;
+    
     @Override
     public Iterator<Category> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+        return new AbstractIterator<Category>() {
+            LinkedCategoryCount next = firstCategory;
+
+            @Override
+            protected Category getNext() throws Finished {
+                if (next == null) {
+                    throw FINISHED;
+                }
+                String categoryName = next.categoryName;
+                double probability = (double)next.count / totalCount;
+                int count = next.count;
+                next = next.nextCategory;
+                return new ImmutableCategory(categoryName, probability, count);
+            }
+
+        };
+    }
+    
+    @Override
+    public int getTotalCount() {
+        return totalCount;
+    }
+    
+    /**
+     * Increments a category count by the given value.
+     * 
+     * @param category the category to increment, not <code>null</code>.
+     * @param count the number by which to increment, greater/equal zero.
+     */
+    public void increment(String category, int count) {
+        for (LinkedCategoryCount current = firstCategory; current != null; current = current.nextCategory) {
+            if (category.equals(current.categoryName)) {
+                current.count += count;
+                totalCount += count;
+                return;
+            }
+        }
+        append(category, count);
+    }
+    
+    /**
+     * Add a category with a given count (no duplicate checking takes place: only to be used, when one can make sure
+     * that it does not already exist).
+     * 
+     * @param category the category to add, not <code>null</code>.
+     * @param count the count to set for the category.
+     */
+    public void append(String category, int count) {
+        LinkedCategoryCount tmp = firstCategory;
+        firstCategory = new LinkedCategoryCount(category, count);
+        firstCategory.nextCategory = tmp;
+        totalCount += count;
     }
     
     private static final class LinkedCategoryCount {
