ab2b346189982a70dbc9502c3c31ce7c82346a1d
Philipp Katz <philipp@philippkatz.de>
2014-08-27 09:52:43 +0200

    Rename Filters#chain -> Filters#and and fix generics bounds.
    Former-commit-id: feb39641458f6915e9da586ea63ef98c7cd9e2ce

--- palladian-commons/src/test/java/ws/palladian/helper/functional/FiltersTest.java
+++ palladian-commons/src/test/java/ws/palladian/helper/functional/FiltersTest.java
@@ -5,14 +5,14 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class FilterChainTest {
+public class FiltersTest {
 
     @Test
     public void testFilterChain() {
         Filter<String> filter1 = Filters.regex("[a-z]+");
         Filter<String> filter2 = Filters.equal("apple", "banana", "cranberry", "Durian");
         @SuppressWarnings("unchecked")
-        Filter<String> chain = Filters.chain(filter1, filter2);
+        Filter<String> chain = Filters.and(filter1, filter2);
         assertFalse(chain.accept("kiwi"));
         assertTrue(chain.accept("apple"));
         assertFalse(chain.accept("Durian"));
