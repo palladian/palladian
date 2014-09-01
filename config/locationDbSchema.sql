b9a63b011a72a2e66e5f00e7a2d038e9fd039d51
Philipp Katz <philipp@philippkatz.de>
2014-09-01 23:47:11 +0200

    Location DB schema changes (closes #254): shorten some columns (alternativeName, name, ancestorIds), remove unneded indices on locations and location_alternative_names, change type to enum, change double to float for latitude/longitude.

--- config/locationDbSchema.sql
+++ config/locationDbSchema.sql
@@ -5,9 +5,9 @@
 # http://www.sequelpro.com/
 # http://code.google.com/p/sequel-pro/
 #
-# Host: 127.0.0.1 (MySQL 5.5.33)
+# Host: 127.0.0.1 (MySQL 5.5.34)
 # Datenbank: locations
-# Erstellungsdauer: 2014-02-04 18:25:39 +0000
+# Erstellungsdauer: 2014-09-01 21:41:01 +0000
 # ************************************************************
 
 
@@ -27,12 +27,10 @@ DROP TABLE IF EXISTS `location_alternative_names`;
 
 CREATE TABLE `location_alternative_names` (
   `locationId` int(11) unsigned NOT NULL COMMENT 'The id of the location.',
-  `alternativeName` varchar(200) DEFAULT NULL COMMENT 'An alternative name used for the location.',
+  `alternativeName` varchar(200) NOT NULL DEFAULT '' COMMENT 'An alternative name used for the location.',
   `language` char(2) DEFAULT NULL COMMENT 'The language for this alternative name, in ISO 639-1 format. NULL means no specified language.',
   UNIQUE KEY `idNameLangUnique` (`locationId`,`alternativeName`,`language`),
-  KEY `locationId` (`locationId`),
-  KEY `alternativeName` (`alternativeName`),
-  KEY `language` (`language`)
+  KEY `alternativeName` (`alternativeName`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
@@ -44,15 +42,14 @@ DROP TABLE IF EXISTS `locations`;
 
 CREATE TABLE `locations` (
   `id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'The id of the location.',
-  `type` varchar(20) NOT NULL DEFAULT '' COMMENT 'The type of the location.',
-  `name` varchar(255) NOT NULL COMMENT 'The primary name of the location.',
-  `latitude` double(8,5) DEFAULT NULL COMMENT 'The latitude of the location.',
-  `longitude` double(8,5) DEFAULT NULL COMMENT 'The longitude of the location.',
+  `type` enum('CITY','CONTINENT','COUNTRY','LANDMARK','POI','REGION','STREET','UNDETERMINED','UNIT') NOT NULL DEFAULT 'UNDETERMINED',
+  `name` varchar(200) NOT NULL DEFAULT '' COMMENT 'The primary name of the location.',
+  `latitude` float(8,5) DEFAULT NULL,
+  `longitude` float(8,5) DEFAULT NULL,
   `population` bigint(15) unsigned DEFAULT NULL COMMENT 'If applicable, the population of the location.',
-  `ancestorIds` varchar(255) DEFAULT NULL COMMENT 'All ancestor IDs in the hierarchical relation, separated by slashes, starting with the root ancestor. String must start and end with slash character.',
+  `ancestorIds` varchar(100) DEFAULT NULL COMMENT 'All ancestor IDs in the hierarchical relation, separated by slashes, starting with the root ancestor. String must start and end with slash character.',
   PRIMARY KEY (`id`),
   KEY `name` (`name`),
-  KEY `type` (`type`),
   KEY `ancestorIds` (`ancestorIds`),
   KEY `latitudeLongitude` (`latitude`,`longitude`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
