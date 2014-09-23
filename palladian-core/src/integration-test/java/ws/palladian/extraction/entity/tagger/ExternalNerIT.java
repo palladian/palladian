986ae6dc8107be43565c05eabceb31d9e7c181fe
Philipp Katz <philipp@philippkatz.de>
2014-09-23 14:24:46 +0200

    Move LingPipe to palladian-external (ref #34).

--- palladian-core/src/integration-test/java/ws/palladian/extraction/entity/tagger/ExternalNerIT.java
+++ palladian-core/src/integration-test/java/ws/palladian/extraction/entity/tagger/ExternalNerIT.java
@@ -1,7 +1,6 @@
 package ws.palladian.extraction.entity.tagger;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
@@ -62,40 +61,6 @@ public class ExternalNerIT {
         trainPath = null;
         testPath = null;
         tempDirectory = null;
-    }
-
-    @Test
-    public void test_LingPipeNer_CoNLL() {
-        LingPipeNer tagger = new LingPipeNer();
-        String lingpipeNerModelFile = new File(tempDirectory, "lingpipe.model").getPath();
-        tagger.train(trainPath, lingpipeNerModelFile);
-        tagger.loadModel(lingpipeNerModelFile);
-
-        // precision MUC: 81.93%, recall MUC: 74.04%, F1 MUC: 77.79%
-        // precision exact: 72.96%, recall exact: 65.93%, F1 exact: 69.27%
-        EvaluationResult er = tagger.evaluate(testPath, TaggingFormat.COLUMN);
-        // System.out.println(er.getMUCResultsReadable());
-        // System.out.println(er.getExactMatchResultsReadable());
-        assertTrue(er.getF1(EvaluationMode.MUC) > 0.77);
-        assertTrue(er.getF1(EvaluationMode.EXACT_MATCH) > 0.69);
-
-        tagger.loadModel(lingpipeNerModelFile);
-        List<Annotation> annotations = tagger.getAnnotations(FileFormatParser.getText(testPath, TaggingFormat.COLUMN));
-
-        // System.out.println(annotations.size());
-        // System.out.println(annotations.get(0));
-        // System.out.println(annotations.get(500));
-        // System.out.println(annotations.get(annotations.size() - 1));
-
-        assertEquals(1903, annotations.size());
-        assertEquals(34, annotations.get(0).getStartPosition());
-        assertEquals(2, annotations.get(0).getValue().length());
-
-        assertEquals(17251, annotations.get(500).getStartPosition());
-        assertEquals(10, annotations.get(500).getValue().length());
-
-        assertEquals(104255, annotations.get(annotations.size() - 1).getStartPosition());
-        assertEquals(6, annotations.get(annotations.size() - 1).getValue().length());
     }
 
     @Test
---
+++ palladian-external/src/test/java/ws/palladian/extraction/sentence/LingPipeSentenceDetectorTest.java
@@ -0,0 +1,31 @@
+package ws.palladian.extraction.sentence;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Iterator;
+
+import org.junit.Test;
+
+import ws.palladian.core.Token;
+
+public class LingPipeSentenceDetectorTest {
+
+    private static final String TEXT = "I require some help from a SAP SD & FI consultant.\nThere is a main plant (godown) which services all the offices in the state Maharashtra. There are 2 offices in Maharashtra, one in Mumbai & other in Pune.\nThese two offices have got plants of their own as a business area is required for the sales office for accounting purpose. The sales employee is mapped to the sales office.\nWe want to sell from these offices, but dispatch should go from the godown (main plant), however; the accounting of COGS & Sales should happen in the Sales office so that profitability for the sales office is known.\nPresently, in order to cater to this, we have created a plant for the sales office . In order to fit the above requirement, we do an additional movement of stocks from main godown to Sales office plant & then do the sales. The godown keeper also does the delivery / PGI from the sales office plant.\nHow can we eliminate this sales office plant & additional stock movement process and also achieve our requirement of capturing profitability for the sales office.";
+
+    @Test
+    public void testLingPipeSentenceChunker() throws Exception {
+        SentenceDetector sentenceDetector = new LingPipeSentenceDetector();
+        Iterator<Token> sentences = sentenceDetector.iterateTokens(TEXT);
+        assertEquals(0, sentences.next().getStartPosition());
+        assertEquals(51, sentences.next().getStartPosition());
+        assertEquals(139, sentences.next().getStartPosition());
+        assertEquals(206, sentences.next().getStartPosition());
+        assertEquals(329, sentences.next().getStartPosition());
+        assertEquals(379, sentences.next().getStartPosition());
+        assertEquals(595, sentences.next().getStartPosition());
+        assertEquals(680, sentences.next().getStartPosition());
+        assertEquals(818, sentences.next().getStartPosition());
+        assertEquals(894, sentences.next().getStartPosition());
+    }
+
+}
