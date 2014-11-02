f2a904984913e59836cd832976bd298dd222c762
Philipp Katz <philipp@philippkatz.de>
2014-11-02 12:39:21 +0100

    Move more code from newsseecr, add integration test for dictionary-based scope detection.

---
+++ palladian-core/src/integration-test/java/ws/palladian/extraction/location/scope/DictionaryScopeDetectorIT.java
@@ -0,0 +1,94 @@
+package ws.palladian.extraction.location.scope;
+
+import java.io.File;
+
+import org.apache.commons.configuration.Configuration;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ws.palladian.classification.text.FeatureSetting;
+import ws.palladian.classification.text.FeatureSettingBuilder;
+import ws.palladian.classification.text.PalladianTextClassifier;
+import ws.palladian.classification.text.PalladianTextClassifier.Scorer;
+import ws.palladian.extraction.location.evaluation.LocationDocument;
+import ws.palladian.extraction.location.scope.DictionaryScopeDetector.DictionaryScopeDetectorLearner;
+import ws.palladian.extraction.location.scope.DictionaryScopeDetector.DictionaryScopeModel;
+import ws.palladian.extraction.location.scope.evaluation.ScopeDetectorEvaluator;
+import ws.palladian.extraction.location.scope.evaluation.WikipediaLocationScopeIterator;
+import ws.palladian.helper.constants.SizeUnit;
+import ws.palladian.helper.math.Stats;
+import ws.palladian.integrationtests.ITHelper;
+
+public class DictionaryScopeDetectorIT {
+
+    private static final Scorer scorer = new PalladianTextClassifier.DefaultScorer();
+
+    private static final double fineGridSize = 0.7;
+
+    private static final double coarseGridSize = 5.63;
+
+    private static Iterable<LocationDocument> trainingSet;
+
+    private static Iterable<LocationDocument> validationSet;
+
+    @BeforeClass
+    public static void readConfiguration() {
+        Configuration config = ITHelper.getTestConfig();
+        String split1Path = config.getString("dataset.wikipediaScope40k.split1");
+        String split2Path = config.getString("dataset.wikipediaScope40k.split2");
+        ITHelper.assertDirectory(split1Path, split2Path);
+        trainingSet = new WikipediaLocationScopeIterator(new File(split1Path));
+        validationSet = new WikipediaLocationScopeIterator(new File(split2Path));
+        ITHelper.assertMemory(750, SizeUnit.MEGABYTES);
+    }
+
+    @After
+    public void cleanup() {
+        ITHelper.forceGc();
+    }
+
+    @Test
+    public void testDictionaryScopeDetector_wikipedia40k() {
+        FeatureSetting setting = FeatureSettingBuilder.chars(6, 9).create();
+        DictionaryScopeDetectorLearner learner = new DictionaryScopeDetectorLearner(setting, coarseGridSize);
+        DictionaryScopeModel model = learner.train(trainingSet);
+        ScopeDetector scopeDetector = new DictionaryScopeDetector(model, scorer);
+        Stats stats = ScopeDetectorEvaluator.evaluateScopeDetection(scopeDetector, validationSet, false);
+        // printStats(stats);
+        ITHelper.assertMax("meanError", 1027, stats.getMean()); // 1026.048326852913
+        ITHelper.assertMax("medianError", 199, stats.getMedian()); // 198.47593580609546
+        ITHelper.assertMin("probability1km", 0, stats.getCumulativeProbability(1)); // 7.394626571358146E-4
+        ITHelper.assertMin("probability10km", 0.02, stats.getCumulativeProbability(10)); // 0.021074685728370717
+        ITHelper.assertMin("probability100km", 0.23, stats.getCumulativeProbability(100)); // 0.232684249445403
+        ITHelper.assertMin("probability1000km", 0.86, stats.getCumulativeProbability(1000)); // 0.8682524032536357
+    }
+
+    @Test
+    public void testTwoStepScopeDetector_wikipedia40k() {
+        FeatureSetting setting = FeatureSettingBuilder.chars(6, 9).create();
+        DictionaryScopeDetectorLearner learner = new DictionaryScopeDetectorLearner(setting, fineGridSize);
+        DictionaryScopeModel model = learner.train(trainingSet);
+        ScopeDetector scopeDetector = new MultiStepDictionaryScopeDetector(model, scorer, coarseGridSize);
+        Stats stats = ScopeDetectorEvaluator.evaluateScopeDetection(scopeDetector, validationSet, false);
+        // printStats(stats);
+        ITHelper.assertMax("meanError", 930, stats.getMean()); // 929.5262702219871
+        ITHelper.assertMax("medianError", 57, stats.getMedian()); // 56.23283449739908
+        ITHelper.assertMin("probability1km", 0, stats.getCumulativeProbability(1)); // 0.009120039438008381
+        ITHelper.assertMin("probability10km", 0.13, stats.getCumulativeProbability(10)); // 0.13273354695587872
+        ITHelper.assertMin("probability100km", 0.60, stats.getCumulativeProbability(100)); // 0.6006901651466601
+        ITHelper.assertMin("probability1000km", 0.87, stats.getCumulativeProbability(1000)); // 0.8715799852107469
+
+    }
+
+    @SuppressWarnings("unused")
+    private static void printStats(Stats stats) {
+        System.out.println(stats.getMean());
+        System.out.println(stats.getMedian());
+        System.out.println(stats.getCumulativeProbability(1));
+        System.out.println(stats.getCumulativeProbability(10));
+        System.out.println(stats.getCumulativeProbability(100));
+        System.out.println(stats.getCumulativeProbability(1000));
+    }
+
+}
---
+++ palladian-core/src/main/java/ws/palladian/extraction/location/scope/FocusDictionaryDecorator.java
@@ -0,0 +1,185 @@
+package ws.palladian.extraction.location.scope;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.commons.lang3.Validate;
+
+import ws.palladian.classification.text.AbstractDictionaryModel;
+import ws.palladian.classification.text.DictionaryModel;
+import ws.palladian.classification.text.FeatureSetting;
+import ws.palladian.classification.text.ImmutableDictionaryEntry;
+import ws.palladian.core.AbstractCategoryEntries;
+import ws.palladian.core.Category;
+import ws.palladian.core.CategoryEntries;
+import ws.palladian.helper.collection.CollectionHelper;
+import ws.palladian.helper.functional.Filter;
+import ws.palladian.helper.functional.Filters;
+import ws.palladian.helper.functional.Function;
+
+/**
+ * This decorator for a {@link DictionaryModel} only classifies into a given (sub)set of categories. This way, a little
+ * speedup during classification can be achieved.
+ * 
+ * @author pk
+ * 
+ */
+final class FocusDictionaryDecorator extends AbstractDictionaryModel implements DictionaryModel {
+
+    private static final class FocusedCategoryEntries extends AbstractCategoryEntries {
+
+        private final CategoryEntries wrapped;
+        private final Filter<String> categoryFilter;
+
+        FocusedCategoryEntries(CategoryEntries wrapped, Filter<String> categoryFilter) {
+            this.wrapped = wrapped;
+            this.categoryFilter = categoryFilter;
+        }
+
+        @Override
+        public Iterator<Category> iterator() {
+            return CollectionHelper.filter(wrapped.iterator(), new Filter<Category>() {
+                @Override
+                public boolean accept(Category item) {
+                    return categoryFilter.accept(item.getName());
+                }
+            });
+        }
+
+        @Override
+        public int getTotalCount() {
+            // this is not correct; but I guess actually calculating this here would take too much time
+            return wrapped.getTotalCount();
+        }
+
+        @Override
+        public Category getCategory(String categoryName) {
+            return categoryFilter.accept(categoryName) ? wrapped.getCategory(categoryName) : null;
+        }
+
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    private final DictionaryModel decorated;
+
+    private final Filter<String> categoryFilter;
+    
+    public FocusDictionaryDecorator(DictionaryModel decorated, Set<String> categories) {
+        this(decorated, Filters.equal(categories));
+    }
+
+    public FocusDictionaryDecorator(DictionaryModel decorated, Filter<String> categoryFilter) {
+        Validate.notNull(decorated, "decorated must not be null");
+        Validate.notNull(categoryFilter, "categoryFilter must not be null");
+        this.decorated = decorated;
+        this.categoryFilter = categoryFilter;
+    }
+
+    @Override
+    public Set<String> getCategories() {
+        return CollectionHelper.convertSet(getDocumentCounts(), new Function<Category, String>() {
+            @Override
+            public String compute(Category input) {
+                return input.getName();
+            }
+        });
+    }
+
+//    @Override
+//    public Iterator<TermCategoryEntries> iterator() {
+//        throw new UnsupportedOperationException();
+//    }
+
+    @Override
+    public String getName() {
+        return decorated.getName();
+    }
+
+//    @Override
+//    public void setName(String name) {
+//        throw new UnsupportedOperationException();
+//    }
+
+    @Override
+    public FeatureSetting getFeatureSetting() {
+        return decorated.getFeatureSetting();
+    }
+
+//    @Override
+//    public void addDocument(Collection<String> terms, String category) {
+//        throw new UnsupportedOperationException();
+//    }
+
+    @Override
+    public CategoryEntries getCategoryEntries(String term) {
+        CategoryEntries categoryEntries = decorated.getCategoryEntries(term);
+        return new FocusedCategoryEntries(categoryEntries, categoryFilter);
+    }
+
+    @Override
+    public int getNumTerms() {
+        return decorated.getNumTerms();
+    }
+
+    @Override
+    public int getNumCategories() {
+        return getCategories().size();
+    }
+
+    @Override
+    public int getNumEntries() {
+        return decorated.getNumEntries();
+    }
+
+    @Override
+    public CategoryEntries getDocumentCounts() {
+        return new FocusedCategoryEntries(decorated.getDocumentCounts(), categoryFilter);
+    }
+
+    @Override
+    public CategoryEntries getTermCounts() {
+        return new FocusedCategoryEntries(decorated.getTermCounts(), categoryFilter);
+    }
+
+//    @Override
+//    public void toCsv(PrintStream printStream) {
+//        throw new UnsupportedOperationException();
+//    }
+
+    @Override
+    public int getNumUniqTerms() {
+        return decorated.getNumUniqTerms();
+    }
+
+    @Override
+    public int getNumDocuments() {
+        return decorated.getNumDocuments();
+    }
+
+    @Override
+    public Iterator<DictionaryEntry> iterator() {
+        return new Iterator<DictionaryEntry>() {
+            final Iterator<DictionaryEntry> decoratedIterator = decorated.iterator();
+
+            @Override
+            public boolean hasNext() {
+                return decoratedIterator.hasNext();
+            }
+
+            @Override
+            public DictionaryEntry next() {
+                // XXX could be made more efficient I guess, this way we have two lookups...
+                String term = decoratedIterator.next().getTerm();
+                CategoryEntries categoryEntries = getCategoryEntries(term);
+                return new ImmutableDictionaryEntry(term, categoryEntries);
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+}
