b5ce8e5b0f43c530a2ce9dbfb9da98b74a7ab782
David <davidurbansky@gmail.com>
2015-01-02 11:44:26 +0100

    send body in put requests fix sitemap retriever

--- palladian-retrieval/src/main/java/ws/palladian/retrieval/analysis/SitemapRetriever.java
+++ palladian-retrieval/src/main/java/ws/palladian/retrieval/analysis/SitemapRetriever.java
@@ -1,5 +1,6 @@
 package ws.palladian.retrieval.analysis;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -60,6 +61,37 @@ public class SitemapRetriever {
 
             i++;
         }
+
+        return pageUrls;
+    }
+
+    protected List<String> readSitemap(String sitemapUrl, String goalNodeRegexp) {
+        Pattern goalRegexp = Pattern.compile(goalNodeRegexp, Pattern.CASE_INSENSITIVE);
+        List<String> urls = getUrlsFromSitemap(sitemapUrl);
+        List<String> goalUrls = new ArrayList<>();
+        for (String url : urls) {
+            if (goalRegexp.matcher(url).find()) {
+                goalUrls.add(url);
+            }
+        }
+
+        return goalUrls;
+    }
+
+    private List<String> getUrlsFromSitemap(String sitemapUrl) {
+        List<String> pageUrls = new ArrayList<String>();
+
+        // read
+        String sitemapText = new DocumentRetriever().getText(sitemapUrl);
+        List<String> sitemapUrls = StringHelper.getRegexpMatches(LOC_PATTERN, sitemapText);
+
+        // clean
+        List<String> cleanSitemapUrls = CollectionHelper.newArrayList();
+        for (String url : sitemapUrls) {
+            cleanSitemapUrls.add(normalizeUrl(url));
+        }
+
+        pageUrls.addAll(cleanSitemapUrls);
 
         return pageUrls;
     }
