3ebc07e52a5f5c63823cb81ee9e44e6485104207
David <davidurbansky@gmail.com>
2015-01-26 14:44:29 +0100

    fix $ problem with unit normalization
    Former-commit-id: 79ba9d1ddfdfd26bf54b482bb1a8aaa58483d7a8

---
+++ palladian-commons/src/test/java/ws/palladian/helper/normalization/UnitNormalizerTest.java
@@ -0,0 +1,26 @@
+package ws.palladian.helper.normalization;
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+public class UnitNormalizerTest {
+
+    @Rule
+    public ErrorCollector collector = new ErrorCollector();
+
+    @Test
+    public void testDetectUnit() throws Exception {
+
+        String input;
+
+        input = "39 hours";
+        collector.checkThat(UnitNormalizer.detectUnit(input), Matchers.is("hours"));
+
+        input = "filter_groess # filter_groess # gross # 39";
+        collector.checkThat(UnitNormalizer.detectUnit(input), Matchers.nullValue());
+
+
+    }
+}
