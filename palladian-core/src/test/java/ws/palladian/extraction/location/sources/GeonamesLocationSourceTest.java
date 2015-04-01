75429ac4a3ffed53d91aa876c249c431a72a672b
Philipp Katz <philipp@philippkatz.de>
2015-04-01 17:53:45 +0530

    Also retrieve alternative name information from Geonames location source.

--- palladian-core/src/test/java/ws/palladian/extraction/location/sources/GeonamesLocationSourceTest.java
+++ palladian-core/src/test/java/ws/palladian/extraction/location/sources/GeonamesLocationSourceTest.java
@@ -1,14 +1,19 @@
 package ws.palladian.extraction.location.sources;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
 import org.junit.Test;
 import org.w3c.dom.Document;
 
+import ws.palladian.extraction.location.AlternativeName;
 import ws.palladian.extraction.location.Location;
 import ws.palladian.helper.collection.CollectionHelper;
+import ws.palladian.helper.constants.Language;
+import ws.palladian.helper.geo.GeoCoordinate;
 import ws.palladian.helper.io.ResourceHelper;
 import ws.palladian.retrieval.parser.DocumentParser;
 import ws.palladian.retrieval.parser.ParserFactory;
@@ -22,14 +27,18 @@ public class GeonamesLocationSourceTest {
         Document document = xmlParser.parse(ResourceHelper.getResourceFile("/apiResponse/geonamesResult.xml"));
         List<Location> result = GeonamesLocationSource.parseLocations(document);
 
-        assertEquals(7, result.size());
+        assertEquals(8, result.size());
         Location location = CollectionHelper.getLast(result);
         assertEquals("Stuttgart", location.getPrimaryName());
-        assertEquals((Double)48.78232, location.getLatitude());
-        assertEquals((Double)9.17702, location.getLongitude());
+        GeoCoordinate coordinate = location.getCoordinate();
+        assertNotNull(coordinate);
+        assertEquals(48.78232, coordinate.getLatitude(), 0);
+        assertEquals(9.17702, coordinate.getLongitude(), 0);
         assertEquals(2825297, location.getId());
+        assertEquals(58, location.getAlternativeNames().size());
+        assertTrue(location.getAlternativeNames().contains(new AlternativeName("Stoccarda", Language.ITALIAN)));
     }
-    
+
     @Test(expected = IllegalStateException.class)
     public void testParseLimitExceeded() throws Exception {
         Document document = xmlParser.parse(ResourceHelper.getResourceFile("/apiResponse/geonamesResultExceeded.xml"));
