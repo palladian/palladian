c9dd9f45710e63bf0bb8259708f9222c5a0542a4
Philipp Katz <philipp@philippkatz.de>
2015-11-15 23:38:42 +0100

    Remove Sandro's old diss classes from palladian-experimental
    Former-commit-id: 65e6264203883007649a20955d13f3dcdd25d386

--- palladian-experimental/src/main/java/ws/palladian/retrieval/feeds/evaluation/disssandro_temp/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This is a temporary package for evaluation of feed update strategies on the TUDCS6 dataset. It will be refactored in
- * 10/2011. Run {@link ws.palladian.retrieval.feeds.evaluation.disssandro_temp.DatasetEvaluator} to evaluate a single
- * strategy.
- */
-package ws.palladian.retrieval.feeds.evaluation.disssandro_temp;
