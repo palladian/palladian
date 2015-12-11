efe58ab635a41770ea2b789e40621dab165f0b54
Philipp Katz <philipp@philippkatz.de>
2015-12-11 21:16:03 +0100

    Some generalization for OAuthUtil

---
+++ palladian-commons/src/main/java/ws/palladian/helper/collection/EntryKeyComparator.java
@@ -0,0 +1,22 @@
+package ws.palladian.helper.collection;
+
+import java.util.Comparator;
+import java.util.Map.Entry;
+
+/**
+ * Compares two {@link Entry}s by their keys (keys may also be null).
+ * 
+ * @author pk
+ */
+public final class EntryKeyComparator<K extends Comparable<K>> implements Comparator<Entry<K, ?>> {
+    @Override
+    public int compare(Entry<K, ?> e1, Entry<K, ?> e2) {
+        if (e1 == null && e2 == null) {
+            return 0;
+        }
+        if (e1 == null || e2 == null) {
+            return e1 == null ? -1 : 1;
+        }
+        return e1.getKey().compareTo(e2.getKey());
+    }
+}
