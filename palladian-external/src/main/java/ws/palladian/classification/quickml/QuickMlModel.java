f804134709d2ce3623f337b46e062dca09ef9fbd
Philipp Katz <philipp@philippkatz.de>
2016-02-07 01:22:22 +0100

    Current Quick ML
    Former-commit-id: d3979c11c34a39f15e13e5008fcf439c591aaf78

--- palladian-external/src/main/java/ws/palladian/classification/quickml/QuickMlModel.java
+++ palladian-external/src/main/java/ws/palladian/classification/quickml/QuickMlModel.java
@@ -1,14 +1,8 @@
 package ws.palladian.classification.quickml;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.Set;
 
-import org.apache.commons.lang3.StringUtils;
-
 import quickml.supervised.classifier.Classifier;
-import quickml.supervised.classifier.decisionTree.Tree;
-import quickml.supervised.classifier.randomForest.RandomForest;
 import ws.palladian.core.Model;
 
 /**
@@ -43,23 +37,7 @@ public class QuickMlModel implements Model {
 
     @Override
     public String toString() {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(out);
-        // XXX should go into the predictive model
-        if (classifier instanceof Tree) {
-            Tree tree = (Tree)classifier;
-            tree.node.dump(printStream);
-        } else if (classifier instanceof RandomForest) {
-            RandomForest forest = (RandomForest)classifier;
-            int i = 1;
-            for (Tree tree : forest.trees) {
-                printStream.println("Tree #" + i++);
-                printStream.println();
-                tree.node.dump(printStream);
-                printStream.println(StringUtils.repeat('-', 100));
-            }
-        }
-        return out.toString();
+    	return classifier.toString();
     }
 
 }
