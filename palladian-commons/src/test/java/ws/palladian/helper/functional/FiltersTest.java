8a1b88aa116829ed06f94fa075bbed2fc91aed6f
Philipp Katz <philipp@philippkatz.de>
2016-02-11 00:47:24 +0100

    Add Filters#or + test
    Former-commit-id: 139c2307c2d47ccdd5f7f5d9ba19475f2b728865

--- palladian-commons/src/test/java/ws/palladian/helper/functional/FiltersTest.java
+++ palladian-commons/src/test/java/ws/palladian/helper/functional/FiltersTest.java
@@ -8,13 +8,22 @@ import org.junit.Test;
 public class FiltersTest {
 
     @Test
-    public void testFilterChain() {
-        Filter<String> filter1 = Filters.regex("[a-z]+");
-        Filter<String> filter2 = Filters.equal("apple", "banana", "cranberry", "Durian");
-        Filter<String> chain = Filters.and(filter1, filter2);
-        assertFalse(chain.accept("kiwi"));
-        assertTrue(chain.accept("apple"));
-        assertFalse(chain.accept("Durian"));
+    public void testAndFilter() {
+    	Filter<String> filter1 = Filters.regex("[a-z]+");
+    	Filter<String> filter2 = Filters.equal("apple", "banana", "cranberry", "Durian");
+    	Filter<String> andFilter = Filters.and(filter1, filter2);
+        assertFalse(andFilter.accept("kiwi"));
+        assertTrue(andFilter.accept("apple"));
+        assertFalse(andFilter.accept("Durian"));
+    }
+    @Test
+    public void testOrFilter() {
+    	Filter<String> filter1 = Filters.equal("apple");
+    	Filter<String> filter2 = Filters.equal("banana");
+    	Filter<String> orFilter = Filters.or(filter1, filter2);
+    	assertTrue(orFilter.accept("apple"));
+    	assertTrue(orFilter.accept("banana"));
+    	assertFalse(orFilter.accept("kiwi"));
     }
 
 }
