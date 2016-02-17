e5df469fa69c211628c21df9c279f07e1397ef5b
Philipp Katz <philipp@philippkatz.de>
2016-02-17 18:56:06 +0100

    Additional statistic

---
+++ palladian-commons/src/main/java/ws/palladian/helper/math/AbstractStats.java
@@ -0,0 +1,50 @@
+package ws.palladian.helper.math;
+
+import org.apache.commons.lang.Validate;
+
+public abstract class AbstractStats implements Stats {
+
+	@Override
+	public Stats add(Number... values) {
+		Validate.notNull(values, "values must not be null");
+		for (Number value : values) {
+			add(value);
+		}
+		return this;
+	}
+
+	@Override
+	public double getMedian() {
+		return getPercentile(50);
+	}
+
+	@Override
+	public double getRange() {
+		if (getCount() == 0) {
+			return Double.NaN;
+		}
+		return getMax() - getMin();
+	}
+
+	@Override
+	public double getRmse() {
+		return Math.sqrt(getMse());
+	}
+
+	@Override
+	public double getRelativeStandardDeviation() {
+		if (getCount() == 0) {
+			return Double.NaN;
+		}
+		return getStandardDeviation() / getMean();
+	}
+	
+	@Override
+	public double getVariance() {
+		if (getCount() == 0) {
+			return Double.NaN;
+		}
+		return Math.sqrt(getStandardDeviation());
+	}
+
+}
