ab99f339657bd191e01d2f6a17c21936e4674d99
Philipp Katz <philipp@philippkatz.de>
2016-03-09 18:29:42 +0100

    Greater flexibility for CSV value parsing (semi-breaking change; will use doubles now per default, instead of guessing between long and double; use the configuration to alter this behavior; ref #326); move ImmutableLongValue class to appropriate package

--- palladian-core/src/main/java/ws/palladian/classification/utils/CsvValueParser.java
+++
@@ -1,9 +0,0 @@
-package ws.palladian.classification.utils;
-
-import ws.palladian.core.value.Value;
-
-public interface CsvValueParser {
-
-	Value parse(String name, String input);
-
-}
--- palladian-core/src/main/java/ws/palladian/core/value/ImmutableLongValue.java
+++ palladian-core/src/main/java/ws/palladian/core/value/ImmutableLongValue.java
@@ -1,10 +1,25 @@
-package ws.palladian.core;
+package ws.palladian.core.value;
 
-import ws.palladian.core.value.AbstractValue;
-import ws.palladian.core.value.NumericValue;
-import ws.palladian.core.value.Value;
+import ws.palladian.core.value.io.ValueParser;
 
 public final class ImmutableLongValue extends AbstractValue implements NumericValue {
+	
+	public static final ValueParser PARSER = new ValueParser() {
+		@Override
+		public Value parse(String input) {
+			return new ImmutableLongValue(Long.parseLong(input));
+		}
+
+		@Override
+		public boolean canParse(String input) {
+			try {
+				Long.parseLong(input);
+				return true;
+			} catch (NumberFormatException e) {
+				return false;
+			}
+		}
+	};
 
 	private final long longValue;
 
