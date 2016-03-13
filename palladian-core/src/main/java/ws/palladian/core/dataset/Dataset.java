613c848e5618fd57e739d84548a21383354126ce
Philipp Katz <philipp@philippkatz.de>
2016-03-13 12:06:01 +0100

    Add interface for future implementation
    Former-commit-id: ca52a355926fb96fe3eba3d3f74824f1744e3ffd

---
+++ palladian-core/src/main/java/ws/palladian/core/dataset/Dataset.java
@@ -0,0 +1,15 @@
+package ws.palladian.core.dataset;
+
+import ws.palladian.core.Instance;
+
+public interface Dataset extends Iterable<Instance> {
+	
+	// Set<String> getFeatureNames();
+
+	// long size();
+
+	// Dataset filterFeatures(Filter<? super String> nameFilter);
+	
+	// Dataset subset(Filter<? super Instance> instanceFilter);
+	
+}
