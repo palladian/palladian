a4dac8ad03b89b31a41c7bb33a699616905424ed
David Urbansky <davidurbansky@gmail.com>
2016-03-28 02:33:54 +0200

    moved pdc
    Former-commit-id: 11332c6563f4f3d25c9c726b44048f6618d55ba6

---
+++ palladian-image-processing/src/main/java/ws/palladian/classifiers/PalladianDictionaryClassifier.java
@@ -0,0 +1,78 @@
+package ws.palladian.classifiers;
+
+import ws.palladian.classification.text.DictionaryModel;
+import ws.palladian.classification.text.FeatureSetting;
+import ws.palladian.classification.text.FeatureSettingBuilder;
+import ws.palladian.classification.text.PalladianTextClassifier;
+import ws.palladian.classification.text.PalladianTextClassifier.Scorer;
+import ws.palladian.core.*;
+import ws.palladian.core.value.NumericValue;
+import ws.palladian.core.value.Value;
+import ws.palladian.helper.collection.CollectionHelper;
+import ws.palladian.helper.collection.Vector.VectorEntry;
+
+/**
+ * Wrapper for the {@link PalladianTextClassifier} for classifying image BOWs.
+ * 
+ * @author pk
+ */
+public class PalladianDictionaryClassifier implements Learner<DictionaryModel>, Classifier<DictionaryModel> {
+
+	/**
+	 * It makes no sense to do any customizations here, we simply concatenate
+	 * all SIFT features, separated by spaces.
+	 */
+	private static final FeatureSetting FEATURE_SETTING = FeatureSettingBuilder.words().create();
+
+	private final PalladianTextClassifier textClassifier;
+
+	private final Scorer scorer;
+
+	public PalladianDictionaryClassifier() {
+		this(new PalladianTextClassifier.DefaultScorer());
+	}
+
+	public PalladianDictionaryClassifier(Scorer scorer) {
+		this.textClassifier = new PalladianTextClassifier(FEATURE_SETTING, scorer);
+		this.scorer = scorer;
+	}
+
+	@Override
+	public CategoryEntries classify(FeatureVector featureVector, DictionaryModel model) {
+		String text = convertToTextFeatureVector(featureVector);
+		return textClassifier.classify(text, model);
+	}
+
+	@Override
+	public DictionaryModel train(Iterable<? extends Instance> instances) {
+		return textClassifier.train(convertToTextInstances(instances));
+	}
+
+	private Iterable<Instance> convertToTextInstances(Iterable<? extends Instance> instances) {
+		return CollectionHelper.convert(instances, input -> {
+			String text = convertToTextFeatureVector(input.getVector());
+			return new InstanceBuilder().setText(text).create(input.getCategory());
+		});
+	}
+
+	private static String convertToTextFeatureVector(FeatureVector vector) {
+		StringBuilder dummyText = new StringBuilder();
+		for (VectorEntry<String, Value> entry : vector) {
+			if (entry.value() instanceof NumericValue) {
+				// XXX introduce "BagOfWordsValue"?
+				NumericValue count = (NumericValue) entry.value();
+				for (long i = 0; i < count.getLong(); i++) {
+					dummyText.append(entry.key());
+					dummyText.append(" ");
+				}
+			}
+		}
+		return dummyText.toString();
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName() + " (" + scorer + ")";
+	}
+
+}
