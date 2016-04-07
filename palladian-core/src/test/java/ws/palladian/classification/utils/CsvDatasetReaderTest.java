palladian/palladian

commit 556146cbeed74613946e83f1e6fceaf8abaf72aa

Ability to skip parsing certain column when reading CSV data
Former-commit-id: ab9553d0216ba7282bb487794708a84ffd2df056

palladian-core/src/main/java/ws/palladian/classification/utils/CsvDatasetReader.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.Validate;
@@ -130,6 +130,9 @@ public class CsvDatasetReader extends AbstractDataset {
             FlyweightVectorBuilder builder = vectorSchema.builder();
 			for (int f = 0; f < splitLine.length - (config.readClassFromLastColumn() ? 1 : 0); f++) {
 				String name = headNames[f];
+				if (name == null) {
+					continue;
+				}
 				String value = splitLine[f];
 				Value parsedValue;
 				if (value.equals(config.nullValue())) {
@@ -172,6 +175,7 @@ public class CsvDatasetReader extends AbstractDataset {
 
 	private final CsvDatasetReaderConfig config;
 	
+	/** Names of the headers, in case a value is <code>null</code> it will be skipped during parsing. */
     private final String[] headNames;
     
     private final int expectedColumns;
@@ -255,14 +259,23 @@ public class CsvDatasetReader extends AbstractDataset {
 				String[] splitLine = line.split(config.fieldSeparator(), -1);
 				expectedColumns = splitLine.length;
 				int numValues = config.readClassFromLastColumn() ? splitLine.length - 1 : splitLine.length;
+				headNames = new String[numValues];
 				if (config.readHeader()) {
-					headNames = splitLine;
-					vectorSchema = new FlyweightVectorSchema(Arrays.copyOf(headNames, numValues));
+					List<String> usedHeadNames = new ArrayList<>();
+					for (int c = 0; c < numValues; c++) {
+						String columnName = splitLine[c];
+						if (config.isSkippedColumn(columnName)) {
+							LOGGER.debug("Skipping column {}", columnName);
+							continue;
+						}
+						headNames[c] = columnName;
+						usedHeadNames.add(columnName);
+					}
+					vectorSchema = new FlyweightVectorSchema(usedHeadNames.toArray(new String[0]));
 					// XXX consider case, that multiple blank lines might follow
 					line = reader.readLine();
 					splitLine = line.split(config.fieldSeparator(), -1);
 				} else { // generate default header names
-					headNames = new String[numValues];
 					for (int c = 0; c < numValues; c++) {
 						headNames[c] = String.valueOf(c);
 					}
@@ -311,7 +324,11 @@ public class CsvDatasetReader extends AbstractDataset {
 		ValueParser[] parsers = new ValueParser[numValues];
 		for (int i = 0; i < numValues; i++) {
 			// (1) try to use parser defined via configuration
-			ValueParser parser = config.getParser(headNames[i]);
+			String columnName = headNames[i];
+			if (columnName == null) {
+				continue;
+			}
+			ValueParser parser = config.getParser(columnName);
 			// (2) if not, auto-detect applicable parser
 			if (parser == null) {
 				String input = parts[i];
@@ -340,7 +357,11 @@ public class CsvDatasetReader extends AbstractDataset {
 		FeatureInformationBuilder builder = new FeatureInformationBuilder();
 		int numValues = config.readClassFromLastColumn() ? headNames.length-1 : headNames.length;
 		for (int i = 0; i < numValues; i++) {
-			builder.set(headNames[i], parsers[i].getType());
+			String columnName = headNames[i];
+			if (columnName == null) {
+				continue;
+			}
+			builder.set(columnName, parsers[i].getType());
 		}
 		return builder.create();
 	}

palladian-core/src/test/java/ws/palladian/classification/utils/CsvDatasetReaderTest.java
@@ -2,6 +2,7 @@ package ws.palladian.classification.utils;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static ws.palladian.helper.functional.Filters.regex;
 import static ws.palladian.helper.io.ResourceHelper.getResourceFile;
 
 import java.io.IOException;
@@ -104,6 +105,25 @@ public class CsvDatasetReaderTest {
 			assertEquals(new ImmutableStringValue("NaN"), instance.getVector().get("NaN"));
 			assertEquals(new ImmutableStringValue("Infinity"), instance.getVector().get("positiveInfinity"));
 			assertEquals(new ImmutableStringValue("-Infinity"), instance.getVector().get("negativeInfinity"));
+			assertEquals(NullValue.NULL, instance.getVector().get("null"));
+		}
+	}
+	
+	@Test
+	public void testCsvReading_skipColumns() throws IOException {
+		Builder config = CsvDatasetReaderConfig.filePath(getResourceFile("/csvDatasetSpecialValues.csv"));
+		config.readHeader(true);
+		config.readClassFromLastColumn(false);
+		config.fieldSeparator(";");
+		config.skipColumns(regex("NaN|positiveInfinity|negativeInfinity"));
+		CsvDatasetReader reader = config.create();
+		try (CloseableIterator<Instance> iterator = reader.iterator()) {
+			Instance instance = iterator.next();
+			assertEquals(4, instance.getVector().size());
+			assertEquals(4, reader.getFeatureInformation().count());
+			assertEquals(new ImmutableDoubleValue(1.23), instance.getVector().get("double"));
+			assertEquals(new ImmutableDoubleValue(123), instance.getVector().get("long"));
+			assertEquals(new ImmutableStringValue("test"), instance.getVector().get("string"));
 			assertEquals(NullValue.NULL, instance.getVector().get("null"));
 		}
 	}

palladian-core/src/main/java/ws/palladian/classification/utils/CsvDatasetReaderConfig.java
@@ -28,6 +28,7 @@ public class CsvDatasetReaderConfig {
 		private List<TargetValueParser> parsers = new ArrayList<>();
 		private String nullValue = DEFAULT_NULL_VALUE;
 		private boolean gzip = false;
+		private List<Filter<? super String>> skipColumns = new ArrayList<>();
 
 		private Builder(File filePath) {
 			Validate.notNull(filePath, "filePath must not be null");
@@ -129,6 +130,22 @@ public class CsvDatasetReaderConfig {
 			this.gzip = gzip;
 			return this;
 		}
+		
+		/**
+		 * Specify, that the columns <i>matching</i> the given filter will be
+		 * skipped during parsing. This can yield in a performance gain in
+		 * contrast to a filtering applied later, when parsing bug datasets and
+		 * not all columns are required.
+		 * 
+		 * @param name
+		 *            Filter for the column names to skip.
+		 * @return The builder.
+		 */
+		public Builder skipColumns(Filter<? super String> name) {
+			Validate.notNull(name, "name must not be null");
+			this.skipColumns.add(name);
+			return this;
+		}
 
 		@Override
 		public CsvDatasetReader create() {
@@ -176,6 +193,7 @@ public class CsvDatasetReaderConfig {
 	private final List<TargetValueParser> parsers;
 	private final String nullValue;
 	private final boolean gzip;
+	private final List<Filter<? super String>> skipColumns;
 
 	private CsvDatasetReaderConfig(Builder builder) {
 		this.filePath = builder.filePath;
@@ -185,6 +203,7 @@ public class CsvDatasetReaderConfig {
 		this.parsers = new ArrayList<>(builder.parsers);
 		this.nullValue = builder.nullValue;
 		this.gzip = builder.gzip;
+		this.skipColumns = new ArrayList<>(builder.skipColumns);
 	}
 
 	File filePath() {
@@ -231,5 +250,14 @@ public class CsvDatasetReaderConfig {
         	inputStream = new GZIPInputStream(inputStream);
         }
         return inputStream;
+	}
+	
+	public boolean isSkippedColumn(String name) {
+		for (Filter<? super String> columnFilter : skipColumns) {
+			if (columnFilter.accept(name)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }

commit a1828a604da6c56f7ac1543af31b4eb3a2b101c6

Fix invalid FeatureInformation from CSV reader (regression from 556146cbeed74613946e83f1e6fceaf8abaf72aa [formerly ab9553d0216ba7282bb487794708a84ffd2df056])
Former-commit-id: 313e3823c71edb5be691931b45da415686eae1f5

palladian-core/src/main/java/ws/palladian/classification/utils/CsvDatasetReader.java
@@ -356,8 +356,7 @@ public class CsvDatasetReader extends AbstractDataset {
 	@Override
 	public FeatureInformation getFeatureInformation() {
 		FeatureInformationBuilder builder = new FeatureInformationBuilder();
-		int numValues = config.readClassFromLastColumn() ? headNames.length-1 : headNames.length;
-		for (int i = 0; i < numValues; i++) {
+		for (int i = 0; i < headNames.length; i++) {
 			String columnName = headNames[i];
 			if (columnName == null) {
 				continue;

palladian-core/src/test/java/ws/palladian/classification/utils/CsvDatasetReaderTest.java
@@ -31,6 +31,7 @@ public class CsvDatasetReaderTest {
 			assertTrue(iterator.hasNext());
 			Instance instance = iterator.next();
 			assertEquals(14, instance.getVector().size());
+			assertEquals(14, reader.getFeatureInformation().count());
 			assertEquals(new ImmutableDoubleValue(25), instance.getVector().get("0"));
 			assertEquals(new ImmutableStringValue("Private"), instance.getVector().get("1"));
 			assertEquals("<=50K", instance.getCategory());
@@ -48,6 +49,7 @@ public class CsvDatasetReaderTest {
 		try (CloseableIterator<Instance> iterator = reader.iterator()) {
 			Instance instance = iterator.next();
 			assertEquals(15, instance.getVector().size());
+			assertEquals(15, reader.getFeatureInformation().count());
 			assertEquals(new ImmutableStringValue("<=50K"), instance.getVector().get("14"));
 			assertEquals(1000, CollectionHelper.count(reader.iterator()));
 		}
@@ -63,6 +65,7 @@ public class CsvDatasetReaderTest {
 		try (CloseableIterator<Instance> iterator = reader.iterator()) {
 			Instance instance = iterator.next();
 			assertEquals(8, instance.getVector().size());
+			assertEquals(8, reader.getFeatureInformation().count());
 			assertTrue(instance.getVector().keys().contains("numPregnant"));
 			assertEquals(768, CollectionHelper.count(reader.iterator()));
 		}
@@ -78,6 +81,7 @@ public class CsvDatasetReaderTest {
 		try (CloseableIterator<Instance> iterator = reader.iterator()) {
 			Instance instance = iterator.next();
 			assertEquals(7, instance.getVector().size());
+			assertEquals(7, reader.getFeatureInformation().count());
 			assertEquals(new ImmutableDoubleValue(1.23), instance.getVector().get("double"));
 			assertEquals(new ImmutableDoubleValue(123), instance.getVector().get("long"));
 			assertEquals(new ImmutableStringValue("test"), instance.getVector().get("string"));
@@ -99,6 +103,7 @@ public class CsvDatasetReaderTest {
 		try (CloseableIterator<Instance> iterator = reader.iterator()) {
 			Instance instance = iterator.next();
 			assertEquals(7, instance.getVector().size());
+			assertEquals(7, reader.getFeatureInformation().count());
 			assertEquals(new ImmutableStringValue("1.23"), instance.getVector().get("double"));
 			assertEquals(new ImmutableStringValue("123"), instance.getVector().get("long"));
 			assertEquals(new ImmutableStringValue("test"), instance.getVector().get("string"));