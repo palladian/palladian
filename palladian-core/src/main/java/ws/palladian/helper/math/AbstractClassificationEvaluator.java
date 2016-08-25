34aa5bee722f88a44d51fe2c4305825d8541f83a
Philipp Katz <philipp@philippkatz.de>
2016-08-25 09:15:55 +0200

    Methods with Dataset as parameter in ClassificationEvaluator
    Former-commit-id: 4865a02e7c6be7526adb8d986d01f1cfb1ae5183

--- palladian-core/src/main/java/ws/palladian/helper/math/AbstractClassificationEvaluator.java
+++ palladian-core/src/main/java/ws/palladian/helper/math/AbstractClassificationEvaluator.java
@@ -4,6 +4,8 @@ import ws.palladian.core.Classifier;
 import ws.palladian.core.Instance;
 import ws.palladian.core.Learner;
 import ws.palladian.core.Model;
+import ws.palladian.core.dataset.Dataset;
+import ws.palladian.core.dataset.DefaultDataset;
 
 public abstract class AbstractClassificationEvaluator<R> implements ClassificationEvaluator<R> {
 	@Override
@@ -11,5 +13,17 @@ public abstract class AbstractClassificationEvaluator<R> implements Classificati
 			Iterable<? extends Instance> trainData, Iterable<? extends Instance> testData) {
 		M model = learner.train(trainData);
 		return evaluate(classifier, model, testData);
+	}
+
+	@Override
+	public <M extends Model> R evaluate(Learner<M> learner, Classifier<M> classifier, Dataset trainData,
+			Dataset testData) {
+		M model = learner.train(trainData);
+		return evaluate(classifier, model, testData);
+	}
+
+	@Override
+	public <M extends Model> R evaluate(Classifier<M> classifier, M model, Iterable<? extends Instance> data) {
+		return evaluate(classifier, model, new DefaultDataset(data));
 	}
 }
--- palladian-core/src/main/java/ws/palladian/helper/math/ConfusionMatrixEvaluator.java
+++ palladian-core/src/main/java/ws/palladian/helper/math/ConfusionMatrixEvaluator.java
@@ -2,14 +2,13 @@ package ws.palladian.helper.math;
 
 import ws.palladian.classification.utils.ClassifierEvaluation;
 import ws.palladian.core.Classifier;
-import ws.palladian.core.Instance;
 import ws.palladian.core.Model;
+import ws.palladian.core.dataset.Dataset;
 
 public class ConfusionMatrixEvaluator extends AbstractClassificationEvaluator<ConfusionMatrix> {
 
 	@Override
-	public <M extends Model> ConfusionMatrix evaluate(Classifier<M> classifier, M model,
-			Iterable<? extends Instance> data) {
+	public <M extends Model> ConfusionMatrix evaluate(Classifier<M> classifier, M model, Dataset data) {
 		return ClassifierEvaluation.evaluate(classifier, data, model);
 	}
 	
