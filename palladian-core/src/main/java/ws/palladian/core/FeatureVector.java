d6fb4e9d1e677fb683965dd93890a5bb766ae140
Philipp Katz <philipp@philippkatz.de>
2016-09-16 08:37:24 +0200

    Shortcut getters for NominalValue and NumericValue in FeatureVector
    Former-commit-id: 1b7845bcb12e5c42354c406cdf634a9cd1fd8d2e

--- palladian-core/src/main/java/ws/palladian/core/FeatureVector.java
+++ palladian-core/src/main/java/ws/palladian/core/FeatureVector.java
@@ -1,8 +1,31 @@
 package ws.palladian.core;
 
+import ws.palladian.core.value.NominalValue;
+import ws.palladian.core.value.NumericValue;
 import ws.palladian.core.value.Value;
 import ws.palladian.helper.collection.Vector;
 
 public interface FeatureVector extends Vector<String, Value> {
+
+	/**
+	 * Get a nominal value.
+	 * 
+	 * @param key
+	 *            The key of the value to get.
+	 * @return The nominal value, never <code>null</code>.
+	 * @throws ClassCastException
+	 *             in case the value is not nominal
+	 */
+	NominalValue getNominal(String key);
+
+	/**
+	 * Get a numeric value.
+	 * @param key
+	 *            The key of the value to get.
+	 * @return The numeric value, never <code>null</code>.
+	 * @throws ClassCastException
+	 *             in case the value is not nominal
+	 */
+	NumericValue getNumeric(String key);
 
 }
