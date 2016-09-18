0693751b9f1205c66830b2d6101afc2bfed35661
Philipp Katz <philipp@philippkatz.de>
2016-09-18 09:22:45 +0200

    Code formatting
    Former-commit-id: 9732b4e9983ef309e72c1b291d29bff4d1e80c64

--- palladian-core/src/main/java/ws/palladian/classification/numeric/KnnClassifier.java
+++ palladian-core/src/main/java/ws/palladian/classification/numeric/KnnClassifier.java
@@ -13,11 +13,10 @@ import ws.palladian.helper.collection.EntryValueComparator;
 import ws.palladian.helper.collection.FixedSizePriorityQueue;
 
 /**
- * <p>
- * A KNN (k-nearest neighbor) classifier. It classifies {@link FeatureVector}s based on the k nearest {@link Instance}s
- * from a {@link KnnModel} created by a {@link KnnLearner}. Since this is an instance based classifier, it is fast
- * during the learning phase but has a more complicated prediction phase.
- * </p>
+ * A KNN (k-nearest neighbor) classifier. It classifies {@link FeatureVector}s
+ * based on the k nearest {@link Instance}s from a {@link KnnModel} created by a
+ * {@link KnnLearner}. Since this is an instance based classifier, it is fast
+ * during the learning phase but has a more expensive prediction phase.
  * 
  * @author David Urbansky
  * @author Klemens Muthmann
@@ -25,23 +24,15 @@ import ws.palladian.helper.collection.FixedSizePriorityQueue;
  */
 public final class KnnClassifier implements Classifier<KnnModel> {
 
-    /**
-     * <p>
-     * Number of nearest neighbors that are allowed to vote. If neighbors have the same distance they will all be
-     * considered for voting, k might increase in these cases.
-     * </p>
-     */
-    private final int k;
+	/** Number of nearest neighbors that are allowed to vote. */
+	private final int k;
 
 	/**
-	 * <p>
-	 * Creates a new KNN classifier with specified k. A typical value for k is
-	 * 3.
-	 * </p>
+	 * Create a KNN classifier with specified k.
 	 * 
 	 * @param k
-	 *            The parameter k specifying the k nearest neighbors to use for
-	 *            classification. Must be greater zero.
+	 *            The number of nearest neighbors used for voting. Greater zero,
+	 *            typical value is 3.
 	 */
 	public KnnClassifier(int k) {
 		Validate.isTrue(k > 0, "k must be greater zero");
@@ -49,43 +40,42 @@ public final class KnnClassifier implements Classifier<KnnModel> {
 	}
 
 	/**
-	 * <p>
-	 * Creates a new KNN classifier with a k of 3.
-	 * </p>
+	 * Create a KNN classifier with a k of 3.
 	 */
 	public KnnClassifier() {
 		this(3);
 	}
 
-    @Override
-    public CategoryEntries classify(FeatureVector featureVector, KnnModel model) {
+	@Override
+	public CategoryEntries classify(FeatureVector featureVector, KnnModel model) {
 
-        // initialize with all category names and a score of zero
-        CategoryEntriesBuilder builder = new CategoryEntriesBuilder().set(model.getCategories(), 0);
-        
-        double[] numericVector = model.getNormalizedVectorForClassification(featureVector);
-        
-        // find k nearest neighbors, compare instance to every known instance
-        FixedSizePriorityQueue<Pair<String, Double>> neighbors = new FixedSizePriorityQueue<>(k, new EntryValueComparator<Double>(Order.DESCENDING));
+		// initialize with all category names and a score of zero
+		CategoryEntriesBuilder builder = new CategoryEntriesBuilder().set(model.getCategories(), 0);
 
-        for (TrainingExample example : model.getTrainingExamples()) {
-            double distance = example.distance(numericVector);
-            neighbors.add(Pair.of(example.category, distance));
-        }
+		double[] numericVector = model.getNormalizedVectorForClassification(featureVector);
 
-        for (Pair<String, Double> neighbor : neighbors.asList()) {
-            double distance = neighbor.getValue();
-            double weight = 1.0 / (distance + 0.000000001);
-            String targetClass = neighbor.getKey();
-            builder.add(targetClass, weight);
-        }
+		// find k nearest neighbors, compare instance to every known instance
+		FixedSizePriorityQueue<Pair<String, Double>> neighbors = new FixedSizePriorityQueue<>(k,
+				new EntryValueComparator<Double>(Order.DESCENDING));
 
-        return builder.create();
-    }
-    
-    @Override
-    public String toString() {
-    	return getClass().getSimpleName() + " (k=" + k + ")"; 
-    }
+		for (TrainingExample example : model.getTrainingExamples()) {
+			double distance = example.distance(numericVector);
+			neighbors.add(Pair.of(example.category, distance));
+		}
+
+		for (Pair<String, Double> neighbor : neighbors.asList()) {
+			double distance = neighbor.getValue();
+			double weight = 1.0 / (distance + 0.000000001);
+			String targetClass = neighbor.getKey();
+			builder.add(targetClass, weight);
+		}
+
+		return builder.create();
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " (k=" + k + ")";
+	}
 
 }
