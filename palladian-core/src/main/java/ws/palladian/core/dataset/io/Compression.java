8804e55111f7a5f4b136b413d2dd227b19cc7eb8
Philipp Katz <philipp@philippkatz.de>
2016-09-23 23:48:55 +0200

    ZIP input support for CsvReader and refactoring
    Former-commit-id: d62d8a87a5163c1ba2693667c6c2f29a343d90cc

---
+++ palladian-core/src/main/java/ws/palladian/core/dataset/io/Compression.java
@@ -0,0 +1,36 @@
+package ws.palladian.core.dataset.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Defines a compression format which can be used as input for the CSV reader.
+ * 
+ * @author pk
+ * @see Compressions
+ */
+public interface Compression {
+
+	/**
+	 * Get an input stream for the specified file.
+	 * 
+	 * @param file
+	 *            The file.
+	 * @return The input stream with the data.
+	 * @throws IOException
+	 *             In case anything with I/O goes berserk.
+	 */
+	InputStream getInputStream(File file) throws IOException;
+
+	/**
+	 * Determine by file extension, whether the given file is supported by this
+	 * compression strategy.
+	 * 
+	 * @param file
+	 *            The file.
+	 * @return <code>true</code> in case the implementation can handle the file.
+	 */
+	boolean fileExtensionSupported(File file);
+
+}
