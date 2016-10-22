13f3beb39f88cf6ac0df9adf1384d38ead634125
David Urbansky <davidurbansky@gmail.com>
2016-10-23 01:15:53 +0200

    noop disambiguation
    Former-commit-id: 9d269ce59c165b28298ec558bdf3fcf937cd5d76

---
+++ palladian-core/src/main/java/ws/palladian/extraction/location/disambiguation/NoDisambiguation.java
@@ -0,0 +1,36 @@
+package ws.palladian.extraction.location.disambiguation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import ws.palladian.extraction.location.ClassifiedAnnotation;
+import ws.palladian.extraction.location.Location;
+import ws.palladian.extraction.location.LocationAnnotation;
+import ws.palladian.helper.collection.MultiMap;
+
+/**
+ * <p>
+ * Don't disambiguate.
+ * </p>
+ * 
+ * @author David Urbansky
+ */
+public class NoDisambiguation implements LocationDisambiguation {
+
+
+    @Override
+    public List<LocationAnnotation> disambiguate(String text, MultiMap<ClassifiedAnnotation, Location> locations) {
+
+        List<LocationAnnotation> result = new ArrayList<>();
+
+        for (Map.Entry<ClassifiedAnnotation, Collection<Location>> entry : locations.entrySet()) {
+            result.addAll(entry.getValue().stream().map(location -> new LocationAnnotation(entry.getKey(), location))
+                    .collect(Collectors.toList()));
+        }
+        return result;
+    }
+
+}
