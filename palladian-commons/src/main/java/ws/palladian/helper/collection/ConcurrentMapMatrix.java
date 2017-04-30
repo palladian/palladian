4c8b42e957038be145418baa7048d54c7eee8b40
David Urbansky <davidurbansky@gmail.com>
2017-05-01 01:00:00 +0200

    return null in matrix to make it compatible with map matrix
    Former-commit-id: 1724fd62c4c7b1ca4c30c18a427f4f56243de138

--- palladian-commons/src/main/java/ws/palladian/helper/collection/ConcurrentMapMatrix.java
+++ palladian-commons/src/main/java/ws/palladian/helper/collection/ConcurrentMapMatrix.java
@@ -29,8 +29,7 @@ public class ConcurrentMapMatrix<K, V> implements Serializable {
 
      public Map<K, V> getRow(K y) {
          // return Optional.ofNullable(matrix.get(y)).orElse(new HashMap<K, V>());
-    	 Map<K, V> row = matrix.get(y);
-    	 return row != null ? row : new HashMap<>();
+    	 return matrix.get(y);
     }
 
     public void set(K x, K y, V value) {
@@ -60,6 +59,7 @@ public class ConcurrentMapMatrix<K, V> implements Serializable {
     }
 
     public V get(K x, K y) {
-        return getRow(y).get(x);
+        Map<K, V> row = getRow(y);
+        return row != null ? row.get(x) : null;
     }
 }
