6bb52a10952dc1e4a67293b613454b36c497e8d2
David Urbansky <davidurbansky@gmail.com>
2017-05-27 15:24:47 +0200

    two cloud image classifiers
    Former-commit-id: 31bc30c8d44e606534ec0520266d51152c9e4c80

---
+++ palladian-image-processing/src/main/java/ws/palladian/classifiers/cloudservices/Clarifai.java
@@ -0,0 +1,77 @@
+package ws.palladian.classifiers.cloudservices;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.api.ClarifaiResponse;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.input.image.ClarifaiImage;
+import clarifai2.dto.model.ConceptModel;
+import clarifai2.dto.model.DefaultModels;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.commons.io.IOUtils;
+
+import ws.palladian.helper.collection.CollectionHelper;
+import ws.palladian.retrieval.parser.json.JsonArray;
+import ws.palladian.retrieval.parser.json.JsonException;
+import ws.palladian.retrieval.parser.json.JsonObject;
+
+/**
+ * Clarifai image classifier (with option to use different models).
+ * 
+ * @see https://developer.clarifai.com/guide/#getting-started
+ * @author David Urbansky
+ */
+public class Clarifai {
+
+    private final ClarifaiClient client;
+    private static final String appIdKey = "api.clarifai.appId";
+    private static final String appSecretKey = "api.clarifai.appSecret";
+
+    public Clarifai(Configuration configuration) {
+        this(configuration.getString(appIdKey), configuration.getString(appSecretKey));
+    }
+
+    public Clarifai(String appId, String appSecret) {
+        this.client = new ClarifaiBuilder(appId, appSecret).buildSync();
+    }
+
+    public ClarifaiClient getClient() {
+        return client;
+    }
+
+    public List<String> classify(File image) throws IOException {
+        return classify(image, client.getDefaultModels().generalModel());
+    }
+
+    public List<String> classify(File image, ConceptModel model) throws IOException {
+
+        List<String> labels = new ArrayList<>();
+
+        ClarifaiResponse<List<ClarifaiOutput<Concept>>> listClarifaiResponse = model.predict().withInputs(ClarifaiInput.forImage(ClarifaiImage.of(image))).executeSync();
+        for (ClarifaiOutput<Concept> conceptClarifaiOutput : listClarifaiResponse.get()) {
+            for (Concept concept : conceptClarifaiOutput.data()) {
+                labels.add(concept.name());
+            }
+        }
+
+        return labels;
+    }
+
+    public static void main(String... args) throws Exception {
+        Clarifai clarifaiClient = new Clarifai("appId", "appSecret");
+        List<String> labels = clarifaiClient.classify(new File("test.jpg"), clarifaiClient.getClient().getDefaultModels().generalModel());
+        CollectionHelper.print(labels);
+    }
+}
