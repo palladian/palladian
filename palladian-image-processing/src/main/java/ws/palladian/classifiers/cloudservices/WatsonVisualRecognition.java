19fd5a0cd52307cd8a718fd3d347e789895a8ede
David Urbansky <davidurbansky@gmail.com>
2017-06-23 01:14:17 +0200

    watson recognizer
    Former-commit-id: 74c45aed3801faf27c0d06a6a1aa4f0741d41db8

--- palladian-image-processing/src/main/java/ws/palladian/classifiers/cloudservices/WatsonVisualRecognition.java
+++ palladian-image-processing/src/main/java/ws/palladian/classifiers/cloudservices/WatsonVisualRecognition.java
@@ -6,10 +6,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ImageClassification;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassifier;
 import org.apache.commons.configuration.Configuration;
 
+import ws.palladian.core.Category;
+import ws.palladian.core.CategoryEntries;
+import ws.palladian.core.ImmutableCategory;
+import ws.palladian.core.ImmutableCategoryEntries;
 import ws.palladian.helper.collection.CollectionHelper;
 
 import com.amazonaws.AmazonClientException;
@@ -20,73 +31,75 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
 import com.amazonaws.services.rekognition.model.*;
 import com.amazonaws.util.IOUtils;
+import ws.palladian.retrieval.DocumentRetriever;
+import ws.palladian.retrieval.HttpRetrieverFactory;
 
 /**
- * Amazon Rekognition image classifier.
+ * Watson Visual Recognition
  * 
- * @see http://docs.aws.amazon.com/rekognition/latest/dg/get-started-exercise-detect-labels.html
+ * @see https://www.ibm.com/watson/developercloud/visual-recognition/api/v3/
  * @author David Urbansky
  */
 public class WatsonVisualRecognition {
 
-    private AWSCredentials credentials = null;
-    private static final String apiKeyKey = "api.amazon.cloud.key";
-    private static final String apiSecretKey = "api.amazon.cloud.secret";
+    private static final String apiKeyKey = "api.watson.key";
+
+    private VisualRecognition service;
 
     public WatsonVisualRecognition(Configuration configuration) {
-        this(configuration.getString(apiKeyKey), configuration.getString(apiSecretKey));
+        this(configuration.getString(apiKeyKey));
     }
 
-    public WatsonVisualRecognition(String apiKey, String apiSecret) {
-        try {
-            credentials = new BasicAWSCredentials(apiKey, apiSecret);
-        } catch (Exception e) {
-            throw new AmazonClientException("Incorrect apiKey and/or apiSecret.", e);
-        }
+    public WatsonVisualRecognition(String apiKey) {
+        service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
+        service.setApiKey(apiKey);
     }
 
-    public List<String> classify(File image) throws IOException {
-        return classify(image, 10);
+    public CategoryEntries classify(File image) throws IOException {
+        return classify(image, "default");
     }
 
-    public List<String> classify(File image, int maxLabels) throws IOException {
+    /**
+     * Classify an image.
+     * 
+     * @param image The image to classify.
+     * @param classifierIds Comma-separated list of classifier ids, possible ids are "default" and "food".
+     * @return
+     * @throws IOException
+     */
+    public CategoryEntries classify(File image, String classifierIds) throws IOException {
 
-        List<String> labels = new ArrayList<>();
+        Map<String, Category> entryMap = new LinkedHashMap<>();
+        Category mostLikely = new ImmutableCategory("unknown", 0.);
 
-        if (maxLabels < 1) {
-            maxLabels = 1;
-        }
+        ClassifyImagesOptions options = new ClassifyImagesOptions.Builder().images(image).classifierIds(classifierIds).build();
+        VisualClassification result = service.classify(options).execute();
 
-        com.amazonaws.services.rekognition.AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.standard().withRegion(Regions.DEFAULT_REGION)
-                .withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+        List<ImageClassification> images = result.getImages();
+        for (ImageClassification imageClassification : images) {
 
-        ByteBuffer imageBytes;
-        try (InputStream inputStream = new FileInputStream(image)) {
-            imageBytes = ByteBuffer.wrap(IOUtils.toByteArray(inputStream));
-        }
+            for (VisualClassifier visualClassifier : imageClassification.getClassifiers()) {
+                for (VisualClassifier.VisualClass visualClass : visualClassifier.getClasses()) {
 
-        Image amazonImage = new Image();
-        amazonImage.setBytes(imageBytes);
+                    ImmutableCategory category = new ImmutableCategory(visualClass.getName(), visualClass.getScore());
+                    entryMap.put(visualClass.getName(), category);
 
-        DetectLabelsRequest request = new DetectLabelsRequest().withImage(amazonImage).withMaxLabels(maxLabels).withMinConfidence(0F);
+                    if (visualClass.getScore() > mostLikely.getProbability()) {
+                        mostLikely = category;
+                    }
 
-        try {
-            DetectLabelsResult result = rekognitionClient.detectLabels(request);
-            List<Label> apiLabels = result.getLabels();
-
-            for (Label label : apiLabels) {
-                labels.add(label.getName());
+                }
+                break;
             }
-        } catch (AmazonRekognitionException e) {
-            e.printStackTrace();
+
         }
 
-        return labels;
+        return new ImmutableCategoryEntries(entryMap, mostLikely);
     }
 
     public static void main(String... args) throws Exception {
-        WatsonVisualRecognition amazonRekognition = new WatsonVisualRecognition("appId", "appSecret");
-        List<String> labels = amazonRekognition.classify(new File("dog.jpg"), 10);
+        WatsonVisualRecognition amazonRekognition = new WatsonVisualRecognition("TODO");
+        CategoryEntries labels = amazonRekognition.classify(new File("burger.jpg"), "food");
         CollectionHelper.print(labels);
     }
 }
