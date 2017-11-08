e7a167d14f46940bf7b83678e9e0aea436d084f0
David <davidurbansky@gmail.com>
2017-11-08 19:01:30 +0100

    dlls are binary
    Former-commit-id: 8b2e70243cac6fdd895e87f5a50a22544a26e096

--- palladian-commons/src/main/java/ws/palladian/helper/io/FileHelper.java
+++ palladian-commons/src/main/java/ws/palladian/helper/io/FileHelper.java
@@ -112,6 +112,7 @@ public final class FileHelper {
         binaryFileExtensions.add("exe");
         binaryFileExtensions.add("msi");
         binaryFileExtensions.add("swf");
+        binaryFileExtensions.add("dll");
         binaryFileExtensions.addAll(VIDEO_FILE_EXTENSIONS);
         binaryFileExtensions.addAll(AUDIO_FILE_EXTENSIONS);
         binaryFileExtensions.addAll(IMAGE_FILE_EXTENSIONS);
