f5b98bccc98ab113e420fa2a9321706f129bf278
David Urbansky <davidurbansky@gmail.com>
2017-12-08 01:04:21 +0100

    modernize
    Former-commit-id: 8b2518a29531af97168f01363111c77ca784d09a

--- palladian-core/src/main/java/ws/palladian/extraction/location/LocationExtractorUtils.java
+++ palladian-core/src/main/java/ws/palladian/extraction/location/LocationExtractorUtils.java
@@ -16,20 +16,10 @@ import ws.palladian.helper.geo.GeoCoordinate;
 public final class LocationExtractorUtils {
 
     /** {@link Function} to unwrap a {@link Location} from a {@link LocationAnnotation}. */
-    public static final Function<LocationAnnotation, Location> ANNOTATION_LOCATION_FUNCTION = new Function<LocationAnnotation, Location>() {
-        @Override
-        public Location compute(LocationAnnotation annotation) {
-            return annotation.getLocation();
-        }
-    };
+    public static final Function<LocationAnnotation, Location> ANNOTATION_LOCATION_FUNCTION = LocationAnnotation::getLocation;
 
     /** {@link Function} for unwrapping a {@link GeoCoordinate} from a {@link Location}. */
-    public static final Function<Location, GeoCoordinate> LOCATION_COORDINATE_FUNCTION = new Function<Location, GeoCoordinate>() {
-        @Override
-        public GeoCoordinate compute(Location location) {
-            return location.getCoordinate();
-        }
-    };
+    public static final Function<Location, GeoCoordinate> LOCATION_COORDINATE_FUNCTION = Location::getCoordinate;
 
     public static String normalizeName(String value) {
         if (value.matches("([A-Z]\\.)+")) {
@@ -58,7 +48,7 @@ public final class LocationExtractorUtils {
         for (Location location : locations) {
             Set<String> currentNames = location.collectAlternativeNames();
             if (allNames.size() > 0) {
-                Set<String> tempIntersection = new HashSet<String>(allNames);
+                Set<String> tempIntersection = new HashSet<>(allNames);
                 tempIntersection.retainAll(currentNames);
                 if (tempIntersection.isEmpty()) {
                     return true;
@@ -75,13 +65,10 @@ public final class LocationExtractorUtils {
     
     public static Comparator<Location> distanceComparator(final GeoCoordinate coordinate) {
         Validate.notNull(coordinate, "coordinate must not be null");
-        return new Comparator<Location>() {
-            @Override
-            public int compare(Location o1, Location o2) {
-                double d1 = o1.getCoordinate().distance(coordinate);
-                double d2 = o2.getCoordinate().distance(coordinate);
-                return Double.compare(d1, d2);
-            }
+        return (o1, o2) -> {
+            double d1 = o1.getCoordinate().distance(coordinate);
+            double d2 = o2.getCoordinate().distance(coordinate);
+            return Double.compare(d1, d2);
         };
     }
 
