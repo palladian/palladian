db609a1d0437d8650f39475d3b04bd4fbd22bee6
Philipp Katz <philipp@philippkatz.de>
2017-12-27 22:01:20 +0100

    TextVectorClassifier: Add new class
    Former-commit-id: 673de111e272a73f2d382fdd1a6b2c393159da30

---
+++ palladian-external/src/main/java/ws/palladian/classification/text/vector/TextVectorClassifier.java
@@ -0,0 +1,69 @@
+package ws.palladian.classification.text.vector;
+
+import java.util.Set;
+
+import de.bwaldvogel.liblinear.Parameter;
+import ws.palladian.classification.liblinear.LibLinearClassifier;
+import ws.palladian.classification.liblinear.LibLinearLearner;
+import ws.palladian.classification.liblinear.LibLinearModel;
+import ws.palladian.classification.utils.NoNormalizer;
+import ws.palladian.core.AbstractLearner;
+import ws.palladian.core.CategoryEntries;
+import ws.palladian.core.Classifier;
+import ws.palladian.core.FeatureVector;
+import ws.palladian.core.Learner;
+import ws.palladian.core.Model;
+import ws.palladian.core.dataset.Dataset;
+import ws.palladian.extraction.text.vector.TextVectorizer;
+import ws.palladian.helper.functional.Filters;
+
+public class TextVectorClassifier<M extends Model> extends AbstractLearner<TextVectorClassifier.TextVectorModel<M>> implements Classifier<TextVectorClassifier.TextVectorModel<M>> {
+	
+	public static class TextVectorModel<M extends Model> implements Model {
+		private static final long serialVersionUID = 1L;
+		private final M model;
+
+		public TextVectorModel(M model) {
+			this.model = model;
+		}
+
+		@Override
+		public Set<String> getCategories() {
+			return model.getCategories();
+		}
+		
+	}
+
+	private final TextVectorizer vectorizer;
+	private final Learner<M> learner;
+	private final Classifier<M> classifier;
+
+	public static TextVectorClassifier<LibLinearModel> libLinear(TextVectorizer vectorizer) {
+		return new TextVectorClassifier<>(vectorizer, new LibLinearLearner(new NoNormalizer()),
+				new LibLinearClassifier());
+	}
+
+	public static TextVectorClassifier<LibLinearModel> libLinear(TextVectorizer vectorizer, Parameter parameter) {
+		return new TextVectorClassifier<>(vectorizer, new LibLinearLearner(parameter, 1, new NoNormalizer()),
+				new LibLinearClassifier());
+	}
+
+	public TextVectorClassifier(TextVectorizer vectorizer, Learner<M> learner, Classifier<M> classifier) {
+		this.vectorizer = vectorizer;
+		this.learner = learner;
+		this.classifier = classifier;
+	}
+
+	@Override
+	public TextVectorModel<M> train(Dataset dataset) {
+		Dataset transformedDataset = dataset.transform(vectorizer).filterFeatures(Filters.not(Filters.equal("text")));
+		return new TextVectorModel<>(learner.train(transformedDataset));
+	}
+
+	@Override
+	public CategoryEntries classify(FeatureVector featureVector, TextVectorModel<M> model) {
+		FeatureVector vectorizedVector = vectorizer.compute(featureVector);
+		return classifier.classify(vectorizedVector, model.model);
+	}
+
+}
---
+++ palladian-external/src/test/java/ws/palladian/classification/text/vector/TextVectorClassifierIT.java
@@ -0,0 +1,64 @@
+package ws.palladian.classification.text.vector;
+
+import static org.junit.Assert.assertTrue;
+import static ws.palladian.extraction.text.vector.TextVectorizer.VectorizationStrategy.BINARY;
+import static ws.palladian.helper.constants.Language.ENGLISH;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ws.palladian.classification.evaluation.ConfusionMatrixEvaluator;
+import ws.palladian.classification.evaluation.roc.RocCurves;
+import ws.palladian.classification.liblinear.LibLinearModel;
+import ws.palladian.classification.text.FeatureSetting;
+import ws.palladian.classification.text.FeatureSettingBuilder;
+import ws.palladian.classification.text.evaluation.TextDatasetIterator;
+import ws.palladian.classification.text.vector.TextVectorClassifier.TextVectorModel;
+import ws.palladian.extraction.text.vector.TextVectorizer;
+import ws.palladian.helper.constants.SizeUnit;
+import ws.palladian.helper.math.ConfusionMatrix;
+import ws.palladian.integrationtests.ITHelper;
+
+public class TextVectorClassifierIT {
+	/** The configuration with the paths to the datasets. */
+	private static Configuration config;
+
+	@BeforeClass
+	public static void ignition() throws ConfigurationException {
+		config = ITHelper.getTestConfig();
+		ITHelper.assertMemory(750, SizeUnit.MEGABYTES);
+	}
+
+	@After
+	public void cleanup() {
+		ITHelper.forceGc();
+	}
+
+	@Test
+	public void testImdbWord() {
+		String trainFile = config.getString("dataset.imdb.train");
+		String testFile = config.getString("dataset.imdb.test");
+		ITHelper.assumeFile("IMDB", trainFile, testFile);
+		FeatureSetting featureSetting = FeatureSettingBuilder.words(1).maxTerms(1000).language(ENGLISH).stem()
+				.removeStopwords().create();
+
+		TextDatasetIterator trainIterator = new TextDatasetIterator(trainFile, " ", true);
+		TextVectorizer vectorizer = new TextVectorizer("text", featureSetting, trainIterator, BINARY, 10000);
+		TextVectorClassifier<LibLinearModel> classifier = TextVectorClassifier.libLinear(vectorizer);
+		TextVectorModel<LibLinearModel> model = classifier.train(trainIterator);
+		// System.out.println(model);
+
+		TextDatasetIterator testIterator = new TextDatasetIterator(testFile, " ", true);
+		ConfusionMatrix evaluation = new ConfusionMatrixEvaluator().evaluate(classifier, model, testIterator);
+		// System.out.println(evaluation.getAccuracy());
+		assertTrue(evaluation.getAccuracy() > 0.82);
+
+		RocCurves rocCurves = new RocCurves.RocCurvesEvaluator("pos").evaluate(classifier, model, testIterator);
+		// System.out.println(rocCurves.getAreaUnderCurve());
+		assertTrue(rocCurves.getAreaUnderCurve() > 0.90);
+	}
+
+}
