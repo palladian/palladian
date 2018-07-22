81ee50da23c3de2f39f7d3d4d63fa6d630a0385b
Philipp Katz <philipp@philippkatz.de>
2018-07-22 16:51:54 +0200

    Remove what 'muss so reingerutscht gewesen sein'
    Former-commit-id: 42fcfd6778c18517b3734f841b00d2bf23b912e2

--- palladian-commons/src/main/java/ws/palladian/helper/html/XPathHelper.java
+++ palladian-commons/src/main/java/ws/palladian/helper/html/XPathHelper.java
@@ -17,7 +17,6 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
-import com.sun.webkit.dom.NodeListImpl;
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
