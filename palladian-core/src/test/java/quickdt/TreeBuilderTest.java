d6d9cc35335f42f413f21a90e0aeb691340e89bd
Philipp Katz <philipp@philippkatz.de>
2021-04-23 15:20:50 +0200

    Make it compile

--- palladian-core/src/test/java/quickdt/TreeBuilderTest.java
+++ palladian-core/src/test/java/quickdt/TreeBuilderTest.java
@@ -1,20 +1,27 @@
 package quickdt;
 
-import com.beust.jcommander.internal.Lists;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import org.testng.internal.annotations.Sets;
+//import com.beust.jcommander.internal.Lists;
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//import org.testng.internal.annotations.Sets;
 import quickdt.scorers.Scorer1;
 
+import static org.junit.Assert.*;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import org.junit.Ignore;
+import org.junit.Test;
 
 
 public class TreeBuilderTest {
 	@Test
 	public void simpleBmiTest() throws Exception {
-		final List<Instance> instances = Lists.newArrayList();
+		final List<Instance> instances = new ArrayList<>();
         for (int x = 0; x < 10000; x++) {
 			final double height = (4 * 12) + Misc.random.nextInt(3 * 12);
 			final double weight = 120 + Misc.random.nextInt(110);
@@ -26,11 +33,12 @@ public class TreeBuilderTest {
 
         serializeDeserialize(node);
 
-		Assert.assertTrue(node.fullRecall(), "Confirm that the node achieves full recall on the training set");
-		Assert.assertTrue(node.size() < 400, "Tree size should be less than 350 nodes");
-		Assert.assertTrue(node.meanDepth() < 6, "Mean depth should be less than 6");
-		Assert.assertTrue((System.currentTimeMillis() - startTime) < 20000,
-				"Building this node should take far less than 20 seconds");
+        assertTrue("Confirm that the node achieves full recall on the training set", node.fullRecall());
+        assertTrue("Tree size should be less than 350 nodes", node.size() < 400);
+		assertTrue("Mean depth should be less than 6", node.meanDepth() < 6);
+		assertTrue(
+				"Building this node should take far less than 20 seconds",
+				(System.currentTimeMillis() - startTime) < 20000);
 	}
 
     private static void serializeDeserialize(final Serializable object) throws IOException, ClassNotFoundException {
@@ -45,9 +53,10 @@ public class TreeBuilderTest {
         objectInputStream.close();
     }
 
-    @Test(enabled = false)
+    @Test
+    @Ignore
 	public void multiScorerBmiTest() {
-		final Set<Instance> instances = Sets.newHashSet();
+		final Set<Instance> instances = new HashSet<>();
 
 		for (int x = 0; x < 10000; x++) {
 			final double height = (4 * 12) + Misc.random.nextInt(3 * 12);
