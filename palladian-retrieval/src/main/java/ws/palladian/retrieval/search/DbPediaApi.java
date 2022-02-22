429e2b89d5e608ce179f7f813aefbacdad4e64f8
David Urbansky <davidurbansky@gmail.com>
2022-02-22 20:01:58 +0100

    search wikipedia via wikidata and dbpedia

---
+++ palladian-retrieval/src/main/java/ws/palladian/retrieval/search/DbPediaApi.java
@@ -0,0 +1,29 @@
+package ws.palladian.retrieval.search;
+
+import ws.palladian.helper.UrlHelper;
+import ws.palladian.retrieval.DocumentRetriever;
+import ws.palladian.retrieval.parser.json.JsonObject;
+
+/**
+ * Query the SPARQL DBpedia API.
+ *
+ * @author David Urbansky
+ * @since 19-Feb-22 at 22:28
+ **/
+public class DbPediaApi {
+    public static JsonObject query(String query) {
+        String url = "https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=" + UrlHelper.encodeParameter(query)
+                + "&format=application%2Fsparql-results%2Bjson&timeout=90000&signal_void=on&signal_unconnected=on";
+        JsonObject jso = new DocumentRetriever().tryGetJsonObject(url);
+        return jso;
+    }
+
+    public static void main(String[] args) {
+        String query =
+                "PREFIX  rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" + "PREFIX  dbo: <http://dbpedia.org/ontology/>\n" + "PREFIX  dbp: <http://dbpedia.org/property/>\n"
+                        + "\n" + "SELECT ?resource ?name ?sa\n" + "WHERE {\n" + "  ?resource  rdf:type  dbo:Person;\n" + "             dbp:name ?name; \n" + "owl:sameAs ?sa.\n"
+                        + "  FILTER (lang(?name) = 'en')\n" + "}\n" + "ORDER BY ASC(?name)\n" + "LIMIT 10000 OFFSET 0";
+        JsonObject response = DbPediaApi.query(query);
+        System.out.println(response.toString(2));
+    }
+}
---
+++ palladian-retrieval/src/main/java/ws/palladian/retrieval/search/WikimediaApi.java
@@ -0,0 +1,58 @@
+package ws.palladian.retrieval.search;
+
+import ws.palladian.helper.collection.CollectionHelper;
+import ws.palladian.retrieval.DocumentRetriever;
+import ws.palladian.retrieval.parser.json.JsonObject;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Access the wikimedia API.
+ * See https://www.wikidata.org/wiki/Wikidata:Data_access
+ *
+ * @author David Urbansky
+ * @since 19-Feb-22 at 21:00
+ **/
+public class WikimediaApi {
+    /**
+     * Get the object by id, e.g. Q42
+     *
+     * @param id Must start with Q
+     * @return A json object with all information about the entity.
+     */
+    public static JsonObject getEntityById(String id) {
+        JsonObject responseJson = new DocumentRetriever().tryGetJsonObject("https://www.wikidata.org/wiki/Special:EntityData/" + id + ".json");
+        if (responseJson == null) {
+            return null;
+        }
+        JsonObject matchingEntity = responseJson.tryQueryJsonObject("entities/" + id);
+        if (matchingEntity != null) {
+            return matchingEntity;
+        }
+
+        JsonObject entities = responseJson.tryGetJsonObject("entities");
+        if (entities != null) {
+            String firstKey = CollectionHelper.getFirst(entities.keySet());
+            return entities.tryGetJsonObject(firstKey);
+        }
+
+        return null;
+    }
+
+    public static Set<String> getLabelsId(String id) {
+        JsonObject jsonObject = new DocumentRetriever().tryGetJsonObject("https://www.wikidata.org/w/api.php?action=wbgetentities&ids=" + id + "&props=labels&format=json");
+        JsonObject labelsJso = jsonObject.tryQueryJsonObject("entities/" + id + "/labels");
+        Set<String> labels = new HashSet<>();
+        for (Object value : labelsJso.values()) {
+            JsonObject labelJso = ((JsonObject) value);
+            labels.add(labelJso.tryGetString("value"));
+        }
+        return labels;
+    }
+
+    public static void main(String[] args) {
+        //        System.out.println(WikimediaApi.getEntityById("Q1731").toString(2));
+        CollectionHelper.print(WikimediaApi.getLabelsId("Q1731"));
+    }
+}
