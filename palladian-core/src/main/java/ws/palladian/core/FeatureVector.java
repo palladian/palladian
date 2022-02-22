061c04368d6085c77b1dc87b3eefeaf980018465
David Urbansky <davidurbansky@gmail.com>
2022-02-22 20:04:09 +0100

    formatting

--- palladian-core/src/main/java/ws/palladian/core/FeatureVector.java
+++ palladian-core/src/main/java/ws/palladian/core/FeatureVector.java
@@ -5,54 +5,46 @@ import ws.palladian.core.value.NominalValue;
 import ws.palladian.core.value.NumericValue;
 import ws.palladian.core.value.Value;
 import ws.palladian.helper.collection.Vector;
+
 import java.util.function.Predicate;
 
 public interface FeatureVector extends Vector<String, Value> {
+    /**
+     * Get a nominal value.
+     *
+     * @param key The key of the value to get.
+     * @return The nominal value, never <code>null</code>.
+     * @throws ClassCastException in case the value is not nominal
+     */
+    NominalValue getNominal(String key);
 
-	/**
-	 * Get a nominal value.
-	 * 
-	 * @param key
-	 *            The key of the value to get.
-	 * @return The nominal value, never <code>null</code>.
-	 * @throws ClassCastException
-	 *             in case the value is not nominal
-	 */
-	NominalValue getNominal(String key);
+    /**
+     * Get a numeric value.
+     *
+     * @param key The key of the value to get.
+     * @return The numeric value, never <code>null</code>.
+     * @throws ClassCastException in case the value is not nominal
+     */
+    NumericValue getNumeric(String key);
 
-	/**
-	 * Get a numeric value.
-	 * 
-	 * @param key
-	 *            The key of the value to get.
-	 * @return The numeric value, never <code>null</code>.
-	 * @throws ClassCastException
-	 *             in case the value is not nominal
-	 */
-	NumericValue getNumeric(String key);
+    /**
+     * Get a boolean value.
+     *
+     * @param key The key of the value to get.
+     * @return The numeric value, never <code>null</code>.
+     * @throws ClassCastException in case the value is not boolean
+     */
+    BooleanValue getBoolean(String key);
 
-	/**
-	 * Get a boolean value.
-	 * 
-	 * @param key
-	 *            The key of the value to get.
-	 * @return The numeric value, never <code>null</code>.
-	 * @throws ClassCastException
-	 *             in case the value is not boolean
-	 */
-	BooleanValue getBoolean(String key);
-	
-	// TODO add this in the future?
-	// Iterator<VectorEntry<String, Value>> iteratorNonNull();
-	
-	/**
-	 * Get a filtered vector which only contains the values accepted by the
-	 * filter.
-	 * 
-	 * @param nameFilter
-	 *            The filter, not <code>null</code>.
-	 * @return A filtered version of this vector.
-	 */
-	FeatureVector filter(Predicate<? super String> nameFilter);
+    // TODO add this in the future?
+    // Iterator<VectorEntry<String, Value>> iteratorNonNull();
 
+    /**
+     * Get a filtered vector which only contains the values accepted by the
+     * filter.
+     *
+     * @param nameFilter The filter, not <code>null</code>.
+     * @return A filtered version of this vector.
+     */
+    FeatureVector filter(Predicate<? super String> nameFilter);
 }
