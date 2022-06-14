ef800fa89d30312fa2797dca8db63248a7746067
David Urbansky <davidurbansky@gmail.com>
2022-06-14 11:17:35 +0200

    Create WebScrapingApiDocumentRetriever.java

---
+++ palladian-retrieval/src/main/java/ws/palladian/retrieval/WebScrapingApiDocumentRetriever.java
@@ -0,0 +1,56 @@
+package ws.palladian.retrieval;
+
+import org.apache.commons.configuration.Configuration;
+import org.w3c.dom.Document;
+import ws.palladian.helper.UrlHelper;
+import ws.palladian.retrieval.helper.RequestThrottle;
+import ws.palladian.retrieval.helper.TimeWindowRequestThrottle;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <p>
+ * Download content from a different IP via a cloud.
+ * </p>
+ *
+ * @author David Urbansky
+ * @see <a href="https://app.webscrapingapi.com/documentation/getting-started">Web Scraping API Docs</a>
+ * 14.06.2022
+ */
+public class WebScrapingApiDocumentRetriever extends WebDocumentRetriever {
+    private final String apiKey;
+    private boolean useJsRendering = false;
+
+    /**
+     * Identifier for the API key when supplied via {@link Configuration}.
+     */
+    public static final String CONFIG_API_KEY = "api.webscrapingapi.key";
+
+    private static final RequestThrottle THROTTLE = new TimeWindowRequestThrottle(1, TimeUnit.SECONDS, 5);
+
+    private final DocumentRetriever documentRetriever = new DocumentRetriever();
+
+    public WebScrapingApiDocumentRetriever(Configuration configuration) {
+        apiKey = configuration.getString(CONFIG_API_KEY);
+    }
+
+    public boolean isUseJsRendering() {
+        return useJsRendering;
+    }
+
+    public void setUseJsRendering(boolean useJsRendering) {
+        this.useJsRendering = useJsRendering;
+    }
+
+    @Override
+    public Document getWebDocument(String url) {
+        THROTTLE.hold();
+        String requestUrl = "https://api.webscrapingapi.com/v1?api_key=" + apiKey + "&render_js=" + (useJsRendering ? "1" : "0") + "&url=" + UrlHelper.encodeParameter(url);
+        Document webDocument = documentRetriever.getWebDocument(requestUrl);
+        if (webDocument != null) {
+            webDocument.setDocumentURI(url);
+            callRetrieverCallback(webDocument);
+        }
+        return webDocument;
+    }
+}
