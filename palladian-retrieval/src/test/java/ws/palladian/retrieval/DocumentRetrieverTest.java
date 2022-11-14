3490ebbf7c220279fd7a521086c944e58c7803be
David Urbansky <davidurbansky@gmail.com>
2022-11-14 14:50:18 +0100

    ignore test

--- palladian-retrieval/src/test/java/ws/palladian/retrieval/DocumentRetrieverTest.java
+++ palladian-retrieval/src/test/java/ws/palladian/retrieval/DocumentRetrieverTest.java
@@ -1,7 +1,6 @@
 package ws.palladian.retrieval;
 
-import static org.junit.Assert.assertEquals;
-
+import org.junit.Ignore;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import ws.palladian.helper.html.HtmlHelper;
@@ -10,26 +9,29 @@ import ws.palladian.retrieval.parser.DocumentParser;
 import ws.palladian.retrieval.parser.ParserException;
 import ws.palladian.retrieval.parser.ParserFactory;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-public class DocumentRetrieverTest {
-	@Test
-	public void testDocumentUriInCaseOfRedirects() {
-		// will redirect to http://example.com
-		String redirectingUrl = "https://httpbingo.org/redirect-to?url=http%3A%2F%2Fexample.com%2F";
-		Document document = new DocumentRetriever().getWebDocument(redirectingUrl);
-		assertEquals("http://example.com/", document.getDocumentURI());
-	}
+import static org.junit.Assert.assertEquals;
 
-	@Test
-	public void testGetLinks() throws ParserException {
-		DocumentParser htmlParser = ParserFactory.createHtmlParser();
-		Document document = htmlParser.parse(new StringInputStream("<html><body><a href=\"test.pdf\">Test 1</a><a href=\"test2.pdf\" data-pdf-title=\"A title bla bla\">Test 2</a></body></html>"));
-		Set<String> links = HtmlHelper.getLinks(document, "localhost", true, true, "", true, true, new HashSet<>(Collections.singletonList("data-pdf-title")));
-		assertEquals(2,  links.size());
-		assertEquals(true, links.contains("test2.pdf?data-pdf-title=A+title+bla+bla"));
-	}
+public class DocumentRetrieverTest {
+    @Ignore
+    @Test
+    public void testDocumentUriInCaseOfRedirects() {
+        // will redirect to http://example.com
+        String redirectingUrl = "https://httpbingo.org/redirect-to?url=http%3A%2F%2Fexample.com%2F"; // FIXME returns 403 now, forbidden link redirect
+        Document document = new DocumentRetriever().getWebDocument(redirectingUrl);
+        assertEquals("http://example.com/", document.getDocumentURI());
+    }
+
+    @Test
+    public void testGetLinks() throws ParserException {
+        DocumentParser htmlParser = ParserFactory.createHtmlParser();
+        Document document = htmlParser.parse(
+                new StringInputStream("<html><body><a href=\"test.pdf\">Test 1</a><a href=\"test2.pdf\" data-pdf-title=\"A title bla bla\">Test 2</a></body></html>"));
+        Set<String> links = HtmlHelper.getLinks(document, "localhost", true, true, "", true, true, new HashSet<>(Collections.singletonList("data-pdf-title")));
+        assertEquals(2, links.size());
+        assertEquals(true, links.contains("test2.pdf?data-pdf-title=A+title+bla+bla"));
+    }
 }
