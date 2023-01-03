f0bbe92a708a41b7aaa2c181e7a680d4bcfd55e8
David Urbansky <davidurbansky@gmail.com>
2023-01-03 11:12:45 +0100

    clean up test

--- palladian-core/src/test/java/ws/palladian/extraction/sentence/SentenceDetectorTest.java
+++ palladian-core/src/test/java/ws/palladian/extraction/sentence/SentenceDetectorTest.java
@@ -9,7 +9,6 @@ import ws.palladian.helper.constants.Language;
 import ws.palladian.helper.io.FileHelper;
 import ws.palladian.helper.io.ResourceHelper;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
@@ -19,42 +18,22 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class SentenceDetectorTest {
-    private static final String fixture = "I require some help from a SAP SD & FI consultant.\nThere is a main plant (godown) which services all the offices in the state Maharashtra. There are 2 offices in Maharashtra, one in Mumbai & other in Pune.\nThese two offices have got plants of their own as a business area is required for the sales office for accounting purpose. The sales employee is mapped to the sales office.\nWe want to sell from these offices, but dispatch should go from the godown (main plant), however; the accounting of COGS & Sales should happen in the Sales office so that profitability for the sales office is known.\nPresently, in order to cater to this, we have created a plant for the sales office . In order to fit the above requirement, we do an additional movement of stocks from main godown to Sales office plant & then do the sales. The godown keeper also does the delivery / PGI from the sales office plant.\nHow can we eliminate this sales office plant & additional stock movement process and also achieve our requirement of capturing profitability for the sales office.";
-    private static String fixture2;
-    private static Set<String> expectedResult;
-
-    static {
-        expectedResult = new HashSet<>();
-        expectedResult.add("I require some help from a SAP SD & FI consultant.");
-        expectedResult.add("There is a main plant (godown) which services all the offices in the state Maharashtra.");
-        expectedResult.add("There are 2 offices in Maharashtra, one in Mumbai & other in Pune.");
-        expectedResult
-                .add("These two offices have got plants of their own as a business area is required for the sales office for accounting purpose.");
-        expectedResult.add("The sales employee is mapped to the sales office.");
-        expectedResult
-                .add("We want to sell from these offices, but dispatch should go from the godown (main plant), however; the accounting of COGS & Sales should happen in the Sales office so that profitability for the sales office is known.");
-        expectedResult.add("Presently, in order to cater to this, we have created a plant for the sales office .");
-        expectedResult
-                .add("In order to fit the above requirement, we do an additional movement of stocks from main godown to Sales office plant & then do the sales.");
-        expectedResult.add("The godown keeper also does the delivery / PGI from the sales office plant.");
-        expectedResult
-                .add("How can we eliminate this sales office plant & additional stock movement process and also achieve our requirement of capturing profitability for the sales office.");
-    }
+    private static String fixture;
 
     @BeforeClass
     public static void setUp() throws Exception {
-        fixture2 = FileHelper.readFileToString(ResourceHelper.getResourceFile("/texts/contribution02.txt"));
+        fixture = FileHelper.readFileToString(ResourceHelper.getResourceFile("/texts/contribution02.txt"));
     }
 
     @AfterClass
-    public static void tearDown() throws Exception {
-        fixture2 = null;
+    public static void tearDown() {
+        fixture = null;
     }
 
     @Test
-    public void testPalladianSentenceChunker() throws FileNotFoundException {
+    public void testPalladianSentenceChunker() {
         SentenceDetector sentenceDetector = new PalladianSentenceDetector(Language.ENGLISH);
-        List<Token> sentences = CollectionHelper.newArrayList(sentenceDetector.iterateTokens(fixture2));
+        List<Token> sentences = CollectionHelper.newArrayList(sentenceDetector.iterateTokens(fixture));
 //        assertThat(sentences.size(), Matchers.is(269)); <- .java recognized as domain
         assertThat(sentences.get(sentences.size() - 1).getValue(), is("DBConnection disconnect\nINFO: disconnected"));
     }
