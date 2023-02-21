37b0b8f30d2c5118f3d7201aa9b88f4cda38a708
David Urbansky <davidurbansky@gmail.com>
2023-02-21 18:46:12 +0100

    add getEmbeddings

--- palladian-retrieval/src/main/java/ws/palladian/retrieval/search/web/OpenAiApi.java
+++ palladian-retrieval/src/main/java/ws/palladian/retrieval/search/web/OpenAiApi.java
@@ -4,6 +4,7 @@ import org.apache.commons.configuration.Configuration;
 import ws.palladian.helper.collection.MapBuilder;
 import ws.palladian.helper.nlp.StringHelper;
 import ws.palladian.retrieval.DocumentRetriever;
+import ws.palladian.retrieval.parser.json.JsonArray;
 import ws.palladian.retrieval.parser.json.JsonObject;
 
 /**
@@ -23,6 +24,30 @@ public class OpenAiApi {
 
     public OpenAiApi(Configuration configuration) {
         this(configuration.getString(CONFIG_API_KEY));
+    }
+
+    public float[] getEmbedding(String text) throws Exception {
+        DocumentRetriever documentRetriever = new DocumentRetriever();
+        documentRetriever.setGlobalHeaders(MapBuilder.createPut("Content-Type", "application/json").put("Authorization", "Bearer " + apiKey).create());
+        JsonObject requestJson = new JsonObject();
+        requestJson.put("input", text);
+        requestJson.put("model", "text-embedding-ada-002");
+        String postResponseText = documentRetriever.tryPostJsonObject("https://api.openai.com/v1/embeddings", requestJson, false);
+        JsonObject responseJson = JsonObject.tryParse(postResponseText);
+        if (responseJson == null) {
+            throw new Exception("Could not parse json " + postResponseText);
+        }
+        if (responseJson.tryQueryString("error/message") != null) {
+            throw new Exception(responseJson.tryQueryString("error/message"));
+        }
+
+        JsonArray embeddingArray = responseJson.tryQueryJsonArray("data[0]/embedding");
+        float[] embedding = new float[embeddingArray.size()];
+        for (Object o : embeddingArray) {
+            embedding[embeddingArray.indexOf(o)] = Float.parseFloat(o.toString());
+        }
+
+        return embedding;
     }
 
     public String ask(String text) throws Exception {
