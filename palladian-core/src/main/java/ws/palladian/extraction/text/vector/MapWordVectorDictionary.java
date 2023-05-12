6b94bc4ea89c30efef593d076ae5dd01a9e89cf7
David Urbansky <davidurbansky@gmail.com>
2023-05-12 18:03:56 +0200

    minor optimizations

--- palladian-core/src/main/java/ws/palladian/extraction/text/vector/MapWordVectorDictionary.java
+++ palladian-core/src/main/java/ws/palladian/extraction/text/vector/MapWordVectorDictionary.java
@@ -2,6 +2,7 @@ package ws.palladian.extraction.text.vector;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ws.palladian.helper.ProgressMonitor;
 import ws.palladian.helper.io.FileHelper;
 import ws.palladian.helper.io.LineAction;
 
@@ -9,6 +10,10 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+/***
+ * Keep the word vectors in a map.
+ * @author Philipp Katz, David Urbansky
+ */
 public class MapWordVectorDictionary implements WordVectorDictionary {
 
     /** The logger for this class. */
@@ -19,40 +24,32 @@ public class MapWordVectorDictionary implements WordVectorDictionary {
     }
 
     public static MapWordVectorDictionary readFromVecFile(File vecFile, final int lineLimit) {
+        ProgressMonitor progressMonitor = new ProgressMonitor(FileHelper.getNumberOfLines(vecFile), 0.1, "Reading vectors from " + vecFile);
         final Map<String, float[]> entries = new HashMap<>();
         final int[] vectorSize = {-1};
         final boolean[] caseSensitive = {false};
         FileHelper.performActionOnEveryLine(vecFile, new LineAction() {
             @Override
             public void performAction(String line, int lineNumber) {
-                //				if (lineNumber == 0) {
-                //					return;
-                //				}
-                if (lineNumber % 10000 == 0 && lineNumber > 0) {
-                    System.out.print('.');
-                    LOGGER.debug("Read {} lines", lineNumber);
-                }
-                int firstSpace = line.indexOf(' ');
-                String token = line.substring(0, firstSpace);
+                progressMonitor.incrementAndPrintProgress();
+                String[] values = line.split(" ");
+                String token = values[0];
 
                 // automatically detect if the dictionary is case-sensitive
                 if (!token.equals(token.toLowerCase())) {
                     caseSensitive[0] = true;
                 }
 
-                String vectorEntries = line.substring(firstSpace + 1);
-                String[] vectorSplit = vectorEntries.split(" ");
                 if (vectorSize[0] == -1) {
-                    vectorSize[0] = vectorSplit.length;
+                    vectorSize[0] = values.length - 1;
                 }
-                float[] vector = new float[vectorSplit.length];
-                for (int i = 0; i < vectorSplit.length; i++) {
-                    vector[i] = Float.parseFloat(vectorSplit[i]);
+                float[] vector = new float[values.length - 1];
+                for (int i = 1; i < values.length; i++) {
+                    vector[i - 1] = Float.parseFloat(values[i]);
                 }
                 entries.put(token, vector);
                 if (lineNumber >= lineLimit) {
                     breakLineLoop();
-                    return;
                 }
             }
         });
@@ -96,5 +93,4 @@ public class MapWordVectorDictionary implements WordVectorDictionary {
     public String toString() {
         return "MapWordVectorDictionary [" + vecFile.getName() + "]";
     }
-
 }
