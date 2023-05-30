e3fd10ea7d85cbe80213939ae2a87914149d1d5b
David Urbansky <davidurbansky@gmail.com>
2023-05-30 16:41:30 +0200

    fix test

--- palladian-retrieval/src/test/java/ws/palladian/retrieval/analysis/EmailAnalyzerTest.java
+++ palladian-retrieval/src/test/java/ws/palladian/retrieval/analysis/EmailAnalyzerTest.java
@@ -17,7 +17,7 @@ public class EmailAnalyzerTest {
         collector.checkThat(profile.getLastName(), is("Urbansky"));
         collector.checkThat(profile.getEmail(), is("david.urbansky@semknox.com"));
         collector.checkThat(profile.getUsername(), is("ddsky"));
-        collector.checkThat(profile.getImageUrl(), is("https://0.gravatar.com/avatar/e61fff419c2ddf685b1520e768d33e40"));
+        collector.checkThat(profile.getImageUrl(), is("https://0.gravatar.com/avatar/d6735b4b9a366d3842539d40a67504f63deb8c9a4cdbecdd947c770e92c0ad41"));
 
         profile = new EmailAnalyzer().getProfile("david.urbansky@anotherwebsite.com");
         //collector.checkThat(profile.getFirstName(), is("David"));
