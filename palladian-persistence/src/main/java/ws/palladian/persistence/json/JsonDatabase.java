88006fa89db4a89d05050f2f782348da7cf449bd
David Urbansky <davidurbansky@gmail.com>
2023-05-31 17:57:10 +0200

    get or create

--- palladian-persistence/src/main/java/ws/palladian/persistence/json/JsonDatabase.java
+++ palladian-persistence/src/main/java/ws/palladian/persistence/json/JsonDatabase.java
@@ -302,6 +302,14 @@ public class JsonDatabase {
         return JsonObject.tryParse(FileHelper.tryReadFileToStringNoReplacement(new File(rootPath + collection + "/" + getFolderedPath(id + ".json"))));
     }
 
+    public JsonObject getOrCreateById(String collection, String id) {
+        JsonObject jsonObject = getById(collection, id);
+        if (jsonObject == null) {
+            jsonObject = new JsonObject();
+        }
+        return jsonObject;
+    }
+
     public boolean exists(String collection, String id) {
         return new File(rootPath + collection + "/" + getFolderedPath(id + ".json")).exists();
     }
