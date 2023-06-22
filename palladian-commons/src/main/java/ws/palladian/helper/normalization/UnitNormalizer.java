20741ae59fa9ac058236033a1b278bba1393b4b2
David Urbansky <davidurbansky@gmail.com>
2023-06-22 11:07:09 +0200

    added °

--- palladian-commons/src/main/java/ws/palladian/helper/normalization/UnitNormalizer.java
+++ palladian-commons/src/main/java/ws/palladian/helper/normalization/UnitNormalizer.java
@@ -662,7 +662,7 @@ public class UnitNormalizer {
 
     public static double getNormalizedNumber(String unitText) throws NumberFormatException, NullPointerException {
         // add space in case it's missing "2.4Ghz" => "2.4 Ghz"
-        unitText = PatternHelper.compileOrGet("(\\d)([A-Za-z\"])").matcher(unitText).replaceAll("$1 $2").trim();
+        unitText = PatternHelper.compileOrGet("(\\d)([A-Za-z\"°])").matcher(unitText).replaceAll("$1 $2").trim();
         String[] words = unitText.split(" ");
 
         if (words.length == 0) {
