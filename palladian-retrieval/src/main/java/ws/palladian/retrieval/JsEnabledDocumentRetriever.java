9b2b446375a4434faffcd1c639e163ffd2f76e9b
JaroslavVankat <jaroslav.vankat@semknox.com>
2023-07-10 17:41:46 +0200

    allow setting multiple wait conditions for one url pattern

--- palladian-retrieval/src/main/java/ws/palladian/retrieval/JsEnabledDocumentRetriever.java
+++ palladian-retrieval/src/main/java/ws/palladian/retrieval/JsEnabledDocumentRetriever.java
@@ -1,7 +1,11 @@
 package ws.palladian.retrieval;
 
+import ws.palladian.helper.collection.CollectionHelper;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.regex.Pattern;
 
@@ -14,7 +18,7 @@ public abstract class JsEnabledDocumentRetriever extends WebDocumentRetriever {
     /**
      * We can configure the retriever to wait for certain elements on certain URLs that match the given pattern.
      */
-    protected Map<Pattern, String> waitForElementMap = new HashMap<>();
+    protected Map<Pattern, Set<String>> waitForElementsMap = new HashMap<>();
 
     public int getTimeoutSeconds() {
         return timeoutSeconds;
@@ -24,12 +28,29 @@ public abstract class JsEnabledDocumentRetriever extends WebDocumentRetriever {
         this.timeoutSeconds = timeoutSeconds;
     }
 
+    @Deprecated
     public Map<Pattern, String> getWaitForElementMap() {
+        Map<Pattern, String> waitForElementMap = new HashMap<>();
+        for (Map.Entry<Pattern, Set<String>> entry : waitForElementsMap.entrySet()) {
+            waitForElementMap.put(entry.getKey(), CollectionHelper.getFirst(entry.getValue()));
+        }
         return waitForElementMap;
     }
 
+    @Deprecated
     public void setWaitForElementMap(Map<Pattern, String> waitForElementMap) {
-        this.waitForElementMap = waitForElementMap;
+        this.waitForElementsMap = new HashMap<>();
+        for (Map.Entry<Pattern, String> entry : waitForElementMap.entrySet()) {
+            this.waitForElementsMap.put(entry.getKey(), Collections.singleton(entry.getValue()));
+        }
+    }
+
+    public Map<Pattern, Set<String>> getWaitForElementsMap() {
+        return waitForElementsMap;
+    }
+
+    public void setWaitForElementsMap(Map<Pattern, Set<String>> waitForElementsMap) {
+        this.waitForElementsMap = waitForElementsMap;
     }
 
     public Consumer<WaitException> getWaitExceptionCallback() {
