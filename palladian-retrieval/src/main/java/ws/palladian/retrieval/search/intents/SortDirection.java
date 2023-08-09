7e07b1a7f54e17b3a1ad0538a587b806dffd0d43
David Urbansky <davidurbansky@gmail.com>
2023-08-09 21:13:56 +0200

    get by short or long version

--- palladian-retrieval/src/main/java/ws/palladian/retrieval/search/intents/SortDirection.java
+++ palladian-retrieval/src/main/java/ws/palladian/retrieval/search/intents/SortDirection.java
@@ -1,5 +1,24 @@
 package ws.palladian.retrieval.search.intents;
 
 public enum SortDirection {
-    ASC, DESC
+    ASC("ASCENDING"), DESC("DESCENDING");
+
+    private final String longVersion;
+
+    SortDirection(String longVersion) {
+        this.longVersion = longVersion;
+    }
+
+    public String getLongVersion() {
+        return longVersion;
+    }
+
+    public static SortDirection getByName(String key) {
+        for (SortDirection direction : values()) {
+            if (direction.getLongVersion().equals(key)) {
+                return direction;
+            }
+        }
+        return valueOf(key);
+    }
 }
