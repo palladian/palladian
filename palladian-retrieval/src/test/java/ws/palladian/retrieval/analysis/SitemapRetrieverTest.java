89b3f3b82771ea43e4fc175d7cb928c18fe336a9
David Urbansky <davidurbansky@gmail.com>
2024-05-13 19:03:28 +0200

    fix test

--- palladian-retrieval/src/test/java/ws/palladian/retrieval/analysis/SitemapRetrieverTest.java
+++ palladian-retrieval/src/test/java/ws/palladian/retrieval/analysis/SitemapRetrieverTest.java
@@ -1,26 +1,25 @@
 package ws.palladian.retrieval.analysis;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class SitemapRetrieverTest {
 
     @Test
     public void testFindSitemap() {
         SitemapRetriever sitemapRetriever = new SitemapRetriever();
-        assertEquals(Arrays.asList("http://www.atlasobscura.com/sitemaps/sitemap_index.xml.gz"),
-                sitemapRetriever.findSitemaps("https://www.atlasobscura.com"));
+        assertEquals(Arrays.asList("http://www.atlasobscura.com/sitemaps/sitemap_index.xml.gz"), sitemapRetriever.findSitemaps("https://www.atlasobscura.com"));
 
         assertEquals(Arrays.asList( //
                 "https://www.apple.com/shop/sitemap.xml", //
                 "https://www.apple.com/autopush/sitemap/sitemap-index.xml", //
                 "https://www.apple.com/newsroom/sitemap.xml", //
                 "https://www.apple.com/retail/sitemap/sitemap.xml", //
-                "https://www.apple.com/today/sitemap.xml" //
+                "https://www.apple.com/today/sitemap-index.xml" //
         ), sitemapRetriever.findSitemaps("https://apple.com"));
 
         assertEquals(Collections.emptyList(), sitemapRetriever.findSitemaps("http://wikipedia.de"));
