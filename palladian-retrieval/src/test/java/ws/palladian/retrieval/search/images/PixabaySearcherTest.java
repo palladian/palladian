49b7f0dfe28feb64f423065b579320843d3c9072
David Urbansky <davidurbansky@gmail.com>
2025-08-06 14:46:03 +0200

    Update PixabaySearcherTest.java

--- palladian-retrieval/src/test/java/ws/palladian/retrieval/search/images/PixabaySearcherTest.java
+++ palladian-retrieval/src/test/java/ws/palladian/retrieval/search/images/PixabaySearcherTest.java
@@ -1,21 +1,20 @@
 package ws.palladian.retrieval.search.images;
 
+import org.junit.Test;
+import ws.palladian.retrieval.search.SearcherException;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import org.junit.Test;
-
-import ws.palladian.retrieval.search.SearcherException;
-
 public class PixabaySearcherTest {
     @Test
-    public void testInvalidApiKey() throws SearcherException {
+    public void testInvalidApiKey() {
         try {
             var searcher = new PixabaySearcher("invalid");
             searcher.search("kitten", 10);
             fail();
         } catch (SearcherException e) {
-            assertEquals(e.getMessage(), "Encountered HTTP status 400");
+            assertEquals("Encountered HTTP status 400, query kitten", e.getMessage());
         }
     }
 }
